f7b5492bd5333ef090b33896a9c79c3ce08c0c29
Petro Mandziuk <64119700+Petro-JS-Developer@users.noreply.github.com>
2020-09-14 15:48:05 +0300

    Solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -3,6 +3,54 @@
 // create SortField enum and export it
 // create SortOrder literal type
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+type student = {
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[],
+}
+export enum SortField {
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
+}
+
+type sortOrder = 'asc' | 'desc';
+
+export function sortStudents(
+  students: student[],
+  sortBy: SortField,
+  order: sortOrder
+) {
+  const copyStydent = [...students];
+
+  copyStydent.sort((a, b) => {
+    switch (sortBy) {
+      case SortField.Name:
+      case SortField.Surname:
+        return order === 'asc' ? a[sortBy].localeCompare(b[sortBy])
+          : b[sortBy].localeCompare(a[sortBy]);
+      case SortField.Age:
+        return order === 'asc' ? a[sortBy] - b[sortBy] : b[sortBy] - a[sortBy];
+      case SortField.Married:
+        if (a[sortBy] === b[sortBy]) {
+          return 0;
+        }
+
+        return a[sortBy] > b[sortBy] ? -1 : 1;
+      case SortField.AverageGrade:
+        const firstStudent = a[sortBy].reduce((sum = 0, item) => sum + item)
+        / a[sortBy].length;
+        const secondStudent = b[sortBy].reduce((sum = 0, item) => sum + item)
+        / b[sortBy].length;
+
+        return order === 'asc' ? firstStudent - secondStudent
+          : secondStudent - firstStudent;
+    }
+  });
+
+  return copyStydent;
 }
