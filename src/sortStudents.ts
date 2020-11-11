12b53d5163844dda15a8dbffbc043777d77efe38
Ivan Beletskiy <beletsky02@gmail.com>
2020-11-11 14:45:24 +0200

    solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -3,6 +3,55 @@
 // create SortField enum and export it
 // create SortOrder literal type
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+type Student = {
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
+}
+
+export enum SortField {
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
+}
+
+type SortOrder = 'asc' | 'desc';
+
+export function sortStudents(
+  students: Student[],
+  sortBy: SortField,
+  order: SortOrder
+) {
+  const sortType = typeof students[0][sortBy];
+
+  return [...students].sort((a, b) => {
+    switch (sortType) {
+      case 'string':
+        return order === 'asc'
+          ? a[sortBy].localeCompare(b[sortBy])
+          : b[sortBy].localeCompare(a[sortBy]);
+      case 'number':
+        return order === 'asc'
+          ? a[sortBy] - b[sortBy]
+          : b[sortBy] - a[sortBy];
+      case 'boolean':
+        return order === 'asc'
+          ? Number(a[sortBy]) - Number(b[sortBy])
+          : Number(b[sortBy]) - Number(a[sortBy]);
+      case 'object':
+        return order === 'asc'
+          ? calcAverageGrade(a[sortBy]) - calcAverageGrade(b[sortBy])
+          : calcAverageGrade(b[sortBy]) - calcAverageGrade(a[sortBy]);
+      default:
+        return 0;
+    };
+  });
+}
+
+function calcAverageGrade(arr: number[]) {
+  return arr.reduce((a, b) => a + b) / arr.length;
 }
