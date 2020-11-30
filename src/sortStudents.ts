788117da0018cb462f085064e839f7837886fb47
tarasprocishin <taras1procishin@gmail.com>
2020-12-01 00:04:20 +0200

    Add types

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -3,6 +3,24 @@
 // create SortField enum and export it
 // create SortOrder literal type
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+type Student = {
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[],
+}
+
+enum SortField {
+  Name,
+  Surname,
+  Age,
+  Married,
+  AverageGrade,
+}
+
+type SortOrder = 'asc' | 'desc';
+
+export function sortStudents(students: Student[], sortBy: SortField, order: SortOrder): Student[] {
+
 }
