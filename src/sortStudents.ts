40a0e0336fc15259475b1eb3a59781c54c529587
tarasprocishin <taras1procishin@gmail.com>
2020-12-06 19:17:40 +0200

    Solutions

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -12,15 +12,50 @@ type Student = {
 }
 
 enum SortField {
-  Name,
-  Surname,
-  Age,
-  Married,
-  AverageGrade,
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
 type SortOrder = 'asc' | 'desc';
 
-export function sortStudents(students: Student[], sortBy: SortField, order: SortOrder): Student[] {
+export function sortStudents(
+  students: Student[],
+  sortBy: SortField,
+  order: SortOrder): Student[] {
+  const studentsCopy = [...students];
+  const isAsc = order === 'asc';
 
+  studentsCopy.sort((student1, student2) => {
+    switch (sortBy) {
+      case SortField.Name:
+      case SortField.Surname:
+        return isAsc
+          ? student1[sortBy].localeCompare(student2[sortBy])
+          : student2[sortBy].localeCompare(student1[sortBy]);
+      case SortField.Age:
+        return isAsc
+          ? student1[sortBy] - student2[sortBy]
+          : student2[sortBy] - student1[sortBy];
+      case SortField.Married:
+        if (student1[sortBy] === student2[sortBy]) {
+          return 0;
+        }
+
+        return student1[sortBy] > student2[sortBy] ? -1 : 1;
+      case SortField.AverageGrade:
+        const firstStudent = student1[sortBy]
+          .reduce((sum = 0, item) => sum + item) / student1[sortBy].length;
+        const secondStudent = student2[sortBy]
+          .reduce((sum = 0, item) => sum + item) / student2[sortBy].length;
+
+        return isAsc
+          ? firstStudent - secondStudent
+          : secondStudent - firstStudent;
+    }
+  });
+
+  return studentsCopy;
 }
