7c9871ba5e063ad01a16e8e4d54e90536de92559
AndrewKreshchenko <mail2andyk@gmail.com>
2021-01-02 10:24:19 +0200

    Solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -3,17 +3,69 @@
 // create SortField enum and export it
 // create SortOrder literal type
 
+type AverageGrades = number;
+
 type Student = {
   name: string,
   surname: string,
   age: number,
   married: boolean,
-  grades: number[],
+  grades: AverageGrades[],
 }
 
-export function sortStudents(students: Array<Student>, sortBy: string, order: 'asc' | 'desc') {
-  if (students || sortBy || order) {
-    var st = students.map((student: Student) => student.name);
-    console.log(st);
+type SortOrder = 'asc' | 'desc';
+
+export enum SortField {
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades'
+};
+
+export function sortStudents(
+  students: Array<Student>,
+  sortBy: SortField, order: SortOrder
+) {
+  const studentsArr = [...students];
+
+  if (!sortBy || !order) {
+    return studentsArr;
   }
+
+  const getAverageGrade = (grades: number[]) => {
+    return grades.reduce((prev, next) => prev + next, 0) / grades.length;
+  };
+
+  if (sortBy === SortField.AverageGrade) {
+    if (order === 'asc') {
+      return studentsArr.sort((prev, next) =>
+        getAverageGrade(prev.grades)
+        - getAverageGrade(next.grades));
+    } else {
+      return studentsArr.sort((prev, next) =>
+        getAverageGrade(next.grades)
+        - getAverageGrade(prev.grades));
+    }
+  }
+
+  studentsArr.sort((prev, next) => {
+    switch (sortBy) {
+      case SortField.Name:
+      case SortField.Surname:
+        return order === 'asc'
+          ? prev[sortBy].localeCompare(next[sortBy])
+          : next[sortBy].localeCompare(prev[sortBy]);
+      case SortField.Age:
+        return order === 'asc'
+          ? prev[sortBy] - next[sortBy]
+          : next[sortBy] - prev[sortBy];
+      case SortField.Married:
+        return order === 'asc'
+          ? Number(prev[sortBy]) - Number(next[sortBy])
+          : Number(next[sortBy]) - Number(prev[sortBy]);
+    }
+  });
+
+  return studentsArr;
 }
