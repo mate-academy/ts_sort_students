75169fad5822809e4dfd1ebec5492904eba908ee
Andriy Ivanyk <76574404+AndriyPT82@users.noreply.github.com>
2021-04-01 18:15:24 +0100

    Solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -3,6 +3,63 @@
 // create SortField enum and export it
 // create SortOrder literal type
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+type Student = {
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
+
+export enum SortField {
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades'
+}
+
+type SortOrder = 'asc' | 'desc';
+
+export function sortStudents(
+  students: Student[], sortBy: SortField, order: SortOrder) {
+  const studentsList: Student[] = [...students];
+
+  switch (sortBy) {
+    case SortField.Name:
+      return studentsList.sort((a, b) => {
+        const [A, B] = order === 'desc' ? [b, a] : [a, b];
+
+        return A.name.localeCompare(B.name);
+      });
+
+    case SortField.Surname:
+      return studentsList.sort((a, b) => {
+        const [A, B] = order === 'desc' ? [b, a] : [a, b];
+
+        return A.surname.localeCompare(B.surname);
+      });
+
+    case SortField.Age:
+      return studentsList.sort((a, b) => {
+        const [A, B] = order === 'desc' ? [b, a] : [a, b];
+
+        return A.age - B.age;
+      });
+
+    case SortField.AverageGrade:
+      return studentsList.sort((a, b) => {
+        const A = a.grades.reduce((acc, num) => acc + num) / a.grades.length;
+        const B = b.grades.reduce((acc, num) => acc + num) / b.grades.length;
+
+        return order === 'desc' ? B - A : A - B;
+      });
+
+    case SortField.Married:
+      return studentsList.sort((a, b) => {
+        const [A, B] = order === 'desc' ? [b, a] : [a, b];
+
+        return +A.married - +B.married;
+      });
+  };
+};
