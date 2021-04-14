ceb46719ac293e4383f0d9da50463dea366003c9
Ruslan Shmadchenko <ruslanwpua@gmail.com>
2021-04-14 13:55:28 +0300

    solution

--- src/sortStudent.test.ts
+++ src/sortStudent.test.ts
@@ -1,12 +1,12 @@
-'use strict';
+// 'use strict';
 /* eslint-disable */
 /* global require, describe, test, expect */
 
-const students = [
+export const students = [
   {
     name: 'Diana',
     surname: 'Dorsey',
-    age: 24,
+    age: 25,
     married: false,
     grades: [3, 3, 4, 5, 4, 3, 5, 5],
   },
@@ -20,44 +20,37 @@ const students = [
   {
     name: 'Willie',
     surname: 'Barrera',
-    age: 22,
+    age: 18,
     married: false,
     grades: [3, 5, 5, 3, 3, 5, 4, 4],
   },
   {
     name: 'Douglas',
     surname: 'Paez',
-    age: 23,
+    age: 22,
     married: true,
     grades: [5, 5, 5, 4, 5, 5, 5, 5],
   },
   {
     name: 'Richard',
     surname: 'Hall',
-    age: 23,
+    age: 21,
     married: false,
     grades: [3, 2, 4, 5, 4, 3, 3, 3],
   },
   {
     name: 'Dale',
     surname: 'Gandy',
-    age: 23,
+    age: 20,
     married: false,
     grades: [5, 3, 3, 3, 3, 5, 4, 3, 4],
   },
   {
     name: 'Lillian',
     surname: 'Quinn',
-    age: 23,
+    age: 19,
     married: false,
     grades: [3, 4, 3, 4, 4, 4, 5, 2, 3],
-  },
-  {
-    name: 'Jessica',
-    surname: 'Buxton',
-    age: 26,
-    married: true,
-    grades: [5, 5, 4, 5, 4, 4, 4, 4, 5, 4, 5, 4],
   },
   {
     name: 'Jessica',
@@ -76,7 +69,7 @@ const students = [
   {
     name: 'Glenn',
     surname: 'Thompson',
-    age: 22,
+    age: 17,
     married: false,
     grades: [5, 5, 5, 5, 5, 5, 5, 5, 4, 4, 3, 2],
   }
@@ -110,52 +103,48 @@ describe('', () => {
       .toEqual([{
         name: 'Christina', surname: 'Branscome', age: 23, married: true, grades: [4, 4, 4, 5, 5, 5, 5, 5],
       }, {
-        name: 'Dale', surname: 'Gandy', age: 23, married: false, grades: [5, 3, 3, 3, 3, 5, 4, 3, 4],
+        name: 'Dale', surname: 'Gandy', age: 20, married: false, grades: [5, 3, 3, 3, 3, 5, 4, 3, 4],
       }, {
-        name: 'Diana', surname: 'Dorsey', age: 24, married: false, grades: [3, 3, 4, 5, 4, 3, 5, 5],
+        name: 'Diana', surname: 'Dorsey', age: 25, married: false, grades: [3, 3, 4, 5, 4, 3, 5, 5],
       }, {
-        name: 'Douglas', surname: 'Paez', age: 23, married: true, grades: [5, 5, 5, 4, 5, 5, 5, 5],
+        name: 'Douglas', surname: 'Paez', age: 22, married: true, grades: [5, 5, 5, 4, 5, 5, 5, 5],
       }, {
-        name: 'Glenn', surname: 'Thompson', age: 22, married: false, grades: [5, 5, 5, 5, 5, 5, 5, 5, 4, 4, 3, 2],
-      }, {
-        name: 'Jessica', surname: 'Buxton', age: 26, married: true, grades: [5, 5, 4, 5, 4, 4, 4, 4, 5, 4, 5, 4],
+        name: 'Glenn', surname: 'Thompson', age: 17, married: false, grades: [5, 5, 5, 5, 5, 5, 5, 5, 4, 4, 3, 2],
       }, {
         name: 'Jessica', surname: 'Buxton', age: 26, married: true, grades: [5, 5, 4, 5, 4, 4, 4, 4, 5, 4, 5, 4],
       }, {
-        name: 'Lillian', surname: 'Quinn', age: 23, married: false, grades: [3, 4, 3, 4, 4, 4, 5, 2, 3],
+        name: 'Lillian', surname: 'Quinn', age: 19, married: false, grades: [3, 4, 3, 4, 4, 4, 5, 2, 3],
       }, {
         name: 'Pamela', surname: 'Casillas', age: 24, married: false, grades: [4, 5, 4, 5, 5, 4, 3, 2, 3, 3, 3, 2],
       }, {
-        name: 'Richard', surname: 'Hall', age: 23, married: false, grades: [3, 2, 4, 5, 4, 3, 3, 3],
+        name: 'Richard', surname: 'Hall', age: 21, married: false, grades: [3, 2, 4, 5, 4, 3, 3, 3],
       }, {
-        name: 'Willie', surname: 'Barrera', age: 22, married: false, grades: [3, 5, 5, 3, 3, 5, 4, 4],
+        name: 'Willie', surname: 'Barrera', age: 18, married: false, grades: [3, 5, 5, 3, 3, 5, 4, 4],
       }]);
   });
 
   test(`Function 'sortStudents' should correctly sort array by 'surname' in ascending order`, () => {
     expect(sortStudents(students, SortField.Surname, 'asc'))
       .toEqual([{
-        name: 'Willie', surname: 'Barrera', age: 22, married: false, grades: [3, 5, 5, 3, 3, 5, 4, 4],
+        name: 'Willie', surname: 'Barrera', age: 18, married: false, grades: [3, 5, 5, 3, 3, 5, 4, 4],
       }, {
         name: 'Christina', surname: 'Branscome', age: 23, married: true, grades: [4, 4, 4, 5, 5, 5, 5, 5],
       }, {
         name: 'Jessica', surname: 'Buxton', age: 26, married: true, grades: [5, 5, 4, 5, 4, 4, 4, 4, 5, 4, 5, 4],
       }, {
-        name: 'Jessica', surname: 'Buxton', age: 26, married: true, grades: [5, 5, 4, 5, 4, 4, 4, 4, 5, 4, 5, 4],
-      }, {
         name: 'Pamela', surname: 'Casillas', age: 24, married: false, grades: [4, 5, 4, 5, 5, 4, 3, 2, 3, 3, 3, 2],
       }, {
-        name: 'Diana', surname: 'Dorsey', age: 24, married: false, grades: [3, 3, 4, 5, 4, 3, 5, 5],
+        name: 'Diana', surname: 'Dorsey', age: 25, married: false, grades: [3, 3, 4, 5, 4, 3, 5, 5],
       }, {
-        name: 'Dale', surname: 'Gandy', age: 23, married: false, grades: [5, 3, 3, 3, 3, 5, 4, 3, 4],
+        name: 'Dale', surname: 'Gandy', age: 20, married: false, grades: [5, 3, 3, 3, 3, 5, 4, 3, 4],
       }, {
-        name: 'Richard', surname: 'Hall', age: 23, married: false, grades: [3, 2, 4, 5, 4, 3, 3, 3],
+        name: 'Richard', surname: 'Hall', age: 21, married: false, grades: [3, 2, 4, 5, 4, 3, 3, 3],
       }, {
-        name: 'Douglas', surname: 'Paez', age: 23, married: true, grades: [5, 5, 5, 4, 5, 5, 5, 5],
+        name: 'Douglas', surname: 'Paez', age: 22, married: true, grades: [5, 5, 5, 4, 5, 5, 5, 5],
       }, {
-        name: 'Lillian', surname: 'Quinn', age: 23, married: false, grades: [3, 4, 3, 4, 4, 4, 5, 2, 3],
+        name: 'Lillian', surname: 'Quinn', age: 19, married: false, grades: [3, 4, 3, 4, 4, 4, 5, 2, 3],
       }, {
-        name: 'Glenn', surname: 'Thompson', age: 22, married: false, grades: [5, 5, 5, 5, 5, 5, 5, 5, 4, 4, 3, 2],
+        name: 'Glenn', surname: 'Thompson', age: 17, married: false, grades: [5, 5, 5, 5, 5, 5, 5, 5, 4, 4, 3, 2],
       }]);
   });
 
@@ -164,25 +153,23 @@ describe('', () => {
       .toEqual([{
         name: 'Jessica', surname: 'Buxton', age: 26, married: true, grades: [5, 5, 4, 5, 4, 4, 4, 4, 5, 4, 5, 4],
       }, {
-        name: 'Jessica', surname: 'Buxton', age: 26, married: true, grades: [5, 5, 4, 5, 4, 4, 4, 4, 5, 4, 5, 4],
-      }, {
-        name: 'Diana', surname: 'Dorsey', age: 24, married: false, grades: [3, 3, 4, 5, 4, 3, 5, 5],
+        name: 'Diana', surname: 'Dorsey', age: 25, married: false, grades: [3, 3, 4, 5, 4, 3, 5, 5],
       }, {
         name: 'Pamela', surname: 'Casillas', age: 24, married: false, grades: [4, 5, 4, 5, 5, 4, 3, 2, 3, 3, 3, 2],
       }, {
         name: 'Christina', surname: 'Branscome', age: 23, married: true, grades: [4, 4, 4, 5, 5, 5, 5, 5],
       }, {
-        name: 'Douglas', surname: 'Paez', age: 23, married: true, grades: [5, 5, 5, 4, 5, 5, 5, 5],
+        name: 'Douglas', surname: 'Paez', age: 22, married: true, grades: [5, 5, 5, 4, 5, 5, 5, 5],
       }, {
-        name: 'Richard', surname: 'Hall', age: 23, married: false, grades: [3, 2, 4, 5, 4, 3, 3, 3],
+        name: 'Richard', surname: 'Hall', age: 21, married: false, grades: [3, 2, 4, 5, 4, 3, 3, 3],
       }, {
-        name: 'Dale', surname: 'Gandy', age: 23, married: false, grades: [5, 3, 3, 3, 3, 5, 4, 3, 4],
+        name: 'Dale', surname: 'Gandy', age: 20, married: false, grades: [5, 3, 3, 3, 3, 5, 4, 3, 4],
       }, {
-        name: 'Lillian', surname: 'Quinn', age: 23, married: false, grades: [3, 4, 3, 4, 4, 4, 5, 2, 3],
+        name: 'Lillian', surname: 'Quinn', age: 19, married: false, grades: [3, 4, 3, 4, 4, 4, 5, 2, 3],
       }, {
-        name: 'Willie', surname: 'Barrera', age: 22, married: false, grades: [3, 5, 5, 3, 3, 5, 4, 4],
+        name: 'Willie', surname: 'Barrera', age: 18, married: false, grades: [3, 5, 5, 3, 3, 5, 4, 4],
       }, {
-        name: 'Glenn', surname: 'Thompson', age: 22, married: false, grades: [5, 5, 5, 5, 5, 5, 5, 5, 4, 4, 3, 2],
+        name: 'Glenn', surname: 'Thompson', age: 17, married: false, grades: [5, 5, 5, 5, 5, 5, 5, 5, 4, 4, 3, 2],
       }]);
   });
 
@@ -191,79 +178,73 @@ describe('', () => {
       .toEqual([{
         name: 'Christina', surname: 'Branscome', age: 23, married: true, grades: [4, 4, 4, 5, 5, 5, 5, 5],
       }, {
-        name: 'Douglas', surname: 'Paez', age: 23, married: true, grades: [5, 5, 5, 4, 5, 5, 5, 5],
+        name: 'Douglas', surname: 'Paez', age: 22, married: true, grades: [5, 5, 5, 4, 5, 5, 5, 5],
       }, {
         name: 'Jessica', surname: 'Buxton', age: 26, married: true, grades: [5, 5, 4, 5, 4, 4, 4, 4, 5, 4, 5, 4],
       }, {
-        name: 'Jessica', surname: 'Buxton', age: 26, married: true, grades: [5, 5, 4, 5, 4, 4, 4, 4, 5, 4, 5, 4],
+        name: 'Diana', surname: 'Dorsey', age: 25, married: false, grades: [3, 3, 4, 5, 4, 3, 5, 5],
       }, {
-        name: 'Diana', surname: 'Dorsey', age: 24, married: false, grades: [3, 3, 4, 5, 4, 3, 5, 5],
+        name: 'Willie', surname: 'Barrera', age: 18, married: false, grades: [3, 5, 5, 3, 3, 5, 4, 4],
       }, {
-        name: 'Willie', surname: 'Barrera', age: 22, married: false, grades: [3, 5, 5, 3, 3, 5, 4, 4],
+        name: 'Richard', surname: 'Hall', age: 21, married: false, grades: [3, 2, 4, 5, 4, 3, 3, 3],
       }, {
-        name: 'Richard', surname: 'Hall', age: 23, married: false, grades: [3, 2, 4, 5, 4, 3, 3, 3],
+        name: 'Dale', surname: 'Gandy', age: 20, married: false, grades: [5, 3, 3, 3, 3, 5, 4, 3, 4],
       }, {
-        name: 'Dale', surname: 'Gandy', age: 23, married: false, grades: [5, 3, 3, 3, 3, 5, 4, 3, 4],
-      }, {
-        name: 'Lillian', surname: 'Quinn', age: 23, married: false, grades: [3, 4, 3, 4, 4, 4, 5, 2, 3],
+        name: 'Lillian', surname: 'Quinn', age: 19, married: false, grades: [3, 4, 3, 4, 4, 4, 5, 2, 3],
       }, {
         name: 'Pamela', surname: 'Casillas', age: 24, married: false, grades: [4, 5, 4, 5, 5, 4, 3, 2, 3, 3, 3, 2],
       }, {
-        name: 'Glenn', surname: 'Thompson', age: 22, married: false, grades: [5, 5, 5, 5, 5, 5, 5, 5, 4, 4, 3, 2],
+        name: 'Glenn', surname: 'Thompson', age: 17, married: false, grades: [5, 5, 5, 5, 5, 5, 5, 5, 4, 4, 3, 2],
       }]);
   });
 
   test(`Function 'sortStudents' should correctly sort array by 'average_grade' in descending order`, () => {
     expect(sortStudents(students, SortField.AverageGrade, 'desc'))
       .toEqual([{
-        name: 'Douglas', surname: 'Paez', age: 23, married: true, grades: [5, 5, 5, 4, 5, 5, 5, 5],
+        name: 'Douglas', surname: 'Paez', age: 22, married: true, grades: [5, 5, 5, 4, 5, 5, 5, 5],
       }, {
         name: 'Christina', surname: 'Branscome', age: 23, married: true, grades: [4, 4, 4, 5, 5, 5, 5, 5],
       }, {
-        name: 'Jessica', surname: 'Buxton', age: 26, married: true, grades: [5, 5, 4, 5, 4, 4, 4, 4, 5, 4, 5, 4],
+        name: 'Glenn', surname: 'Thompson', age: 17, married: false, grades: [5, 5, 5, 5, 5, 5, 5, 5, 4, 4, 3, 2],
       }, {
         name: 'Jessica', surname: 'Buxton', age: 26, married: true, grades: [5, 5, 4, 5, 4, 4, 4, 4, 5, 4, 5, 4],
       }, {
-        name: 'Glenn', surname: 'Thompson', age: 22, married: false, grades: [5, 5, 5, 5, 5, 5, 5, 5, 4, 4, 3, 2],
+        name: 'Willie', surname: 'Barrera', age: 18, married: false, grades: [3, 5, 5, 3, 3, 5, 4, 4],
       }, {
-        name: 'Diana', surname: 'Dorsey', age: 24, married: false, grades: [3, 3, 4, 5, 4, 3, 5, 5],
+        name: 'Diana', surname: 'Dorsey', age: 25, married: false, grades: [3, 3, 4, 5, 4, 3, 5, 5],
       }, {
-        name: 'Willie', surname: 'Barrera', age: 22, married: false, grades: [3, 5, 5, 3, 3, 5, 4, 4],
-      }, {
-        name: 'Dale', surname: 'Gandy', age: 23, married: false, grades: [5, 3, 3, 3, 3, 5, 4, 3, 4],
+        name: 'Dale', surname: 'Gandy', age: 20, married: false, grades: [5, 3, 3, 3, 3, 5, 4, 3, 4],
       }, {
         name: 'Pamela', surname: 'Casillas', age: 24, married: false, grades: [4, 5, 4, 5, 5, 4, 3, 2, 3, 3, 3, 2],
       }, {
-        name: 'Lillian', surname: 'Quinn', age: 23, married: false, grades: [3, 4, 3, 4, 4, 4, 5, 2, 3],
+        name: 'Lillian', surname: 'Quinn', age: 19, married: false, grades: [3, 4, 3, 4, 4, 4, 5, 2, 3],
       }, {
-        name: 'Richard', surname: 'Hall', age: 23, married: false, grades: [3, 2, 4, 5, 4, 3, 3, 3],
+        name: 'Richard', surname: 'Hall', age: 21, married: false, grades: [3, 2, 4, 5, 4, 3, 3, 3],
       }]);
   });
 
   test(`Function 'sortStudents' should correctly sort array by 'average_grade' in ascending order`, () => {
     expect(sortStudents(students, SortField.AverageGrade, 'asc'))
       .toEqual([{
-        name: 'Richard', surname: 'Hall', age: 23, married: false, grades: [3, 2, 4, 5, 4, 3, 3, 3],
+        name: 'Richard', surname: 'Hall', age: 21, married: false, grades: [3, 2, 4, 5, 4, 3, 3, 3],
       }, {
-        name: 'Lillian', surname: 'Quinn', age: 23, married: false, grades: [3, 4, 3, 4, 4, 4, 5, 2, 3],
+        name: 'Lillian', surname: 'Quinn', age: 19, married: false, grades: [3, 4, 3, 4, 4, 4, 5, 2, 3],
       }, {
         name: 'Pamela', surname: 'Casillas', age: 24, married: false, grades: [4, 5, 4, 5, 5, 4, 3, 2, 3, 3, 3, 2],
       }, {
-        name: 'Dale', surname: 'Gandy', age: 23, married: false, grades: [5, 3, 3, 3, 3, 5, 4, 3, 4],
+        name: 'Dale', surname: 'Gandy', age: 20, married: false, grades: [5, 3, 3, 3, 3, 5, 4, 3, 4],
       }, {
-        name: 'Diana', surname: 'Dorsey', age: 24, married: false, grades: [3, 3, 4, 5, 4, 3, 5, 5],
+        name: 'Diana', surname: 'Dorsey', age: 25, married: false, grades: [3, 3, 4, 5, 4, 3, 5, 5],
       }, {
-        name: 'Willie', surname: 'Barrera', age: 22, married: false, grades: [3, 5, 5, 3, 3, 5, 4, 4],
-      }, {
-        name: 'Jessica', surname: 'Buxton', age: 26, married: true, grades: [5, 5, 4, 5, 4, 4, 4, 4, 5, 4, 5, 4],
+        name: 'Willie', surname: 'Barrera', age: 18, married: false, grades: [3, 5, 5, 3, 3, 5, 4, 4],
       }, {
         name: 'Jessica', surname: 'Buxton', age: 26, married: true, grades: [5, 5, 4, 5, 4, 4, 4, 4, 5, 4, 5, 4],
       }, {
-        name: 'Glenn', surname: 'Thompson', age: 22, married: false, grades: [5, 5, 5, 5, 5, 5, 5, 5, 4, 4, 3, 2],
+        name: 'Glenn', surname: 'Thompson', age: 17, married: false, grades: [5, 5, 5, 5, 5, 5, 5, 5, 4, 4, 3, 2],
       }, {
         name: 'Christina', surname: 'Branscome', age: 23, married: true, grades: [4, 4, 4, 5, 5, 5, 5, 5],
       }, {
-        name: 'Douglas', surname: 'Paez', age: 23, married: true, grades: [5, 5, 5, 4, 5, 5, 5, 5],
+        name: 'Douglas', surname: 'Paez', age: 22, married: true, grades: [5, 5, 5, 4, 5, 5, 5, 5],
       }]);
   });
 });
--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,8 +1,80 @@
 'use strict';
-// describe Student type
-// create SortField enum and export it
-// create SortOrder literal type
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+interface Student {
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[],
+};
+
+type SortOrder = 'asc' | 'desc';
+
+export enum SortField {
+  Name,
+  Surname,
+  Age,
+  Married,
+  AverageGrade,
+};
+
+export function sortStudents(students: Student[], field: SortField, order: SortOrder) {
+  const studentsCopy = students.map(student => ({...student}));
+
+  let result;
+
+  switch (field) {
+    case SortField.Name: {
+      result = studentsCopy
+        .sort((studentPrev, studentNext) => studentPrev.name.localeCompare(studentNext.name));
+      break;
+    }
+
+    case SortField.Surname: {
+      result = studentsCopy
+        .sort((studentPrev, studentNext) => studentPrev.surname.localeCompare(studentNext.surname));
+      break;
+    }
+
+    case SortField.Age: {
+      result = studentsCopy
+        .sort((studentPrev, studentNext) => studentPrev.age - studentNext.age);
+      break;
+    }
+
+    case SortField.Married: {
+      result = studentsCopy
+        .sort((a, b) => {
+          if (a.married === b.married) {
+            return 0;
+          }
+
+          if (a.married && !b.married) {
+            return -1;
+          }
+
+          if (!a.married && b.married) {
+            return 1;
+          }
+        }).reverse();
+      break;
+    }
+
+    case SortField.AverageGrade: {
+      result = studentsCopy
+        .sort((studentPrev, studentNext) => {
+          const curentAverageGrade = studentPrev.grades.reduce((acc, curVal) => Math.round((acc + curVal))) / studentPrev.grades.length;
+          const nextAverageGrade = studentNext.grades.reduce((acc, curVal) => Math.round((acc + curVal))) / studentNext.grades.length;
+
+          return curentAverageGrade - nextAverageGrade;
+        });
+      break;
+    };
+  };
+
+  if (order === 'desc') {
+    return result.reverse();
+  };
+
+  return result;
 }
