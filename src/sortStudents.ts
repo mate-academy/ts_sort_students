7c744f19951f4ff056d598d4767f313b48ea8969
Olga Vasylevskaya <olgavasylevskaya@ukr.net>
2021-05-31 19:21:58 +0300

    Solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -3,6 +3,58 @@
 // create SortField enum and export it
 // create SortOrder literal type
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+type Student = {
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
+};
+
+enum SortField {
+  name,
+  surname,
+  age,
+  married,
+  avarageGrade
+};
+
+type Order = 'asc' | 'desc';
+
+export function sortStudents(students: Student[], sortBy: keyof Student, order: Order) {
+  const fields = [];
+
+  for (const student of students) {
+    let newField = student[sortBy];
+
+    if (newField instanceof Array) {
+      newField = newField.reduce((sum, a) => sum + a, 0) / newField.length;
+    }
+
+    fields.push(newField);
+  }
+
+  if (sortBy === 'married') {
+    return order === 'asc'
+      ? fields.sort((a, b) => {
+        return (a === b)? 0 : a? -1 : 1
+      })
+      : fields.sort((a, b) => {
+        return (a === b)? 0 : a? 1 : -1
+      })
+  }
+
+  if (sortBy === 'age' || sortBy === 'grades') {
+    return order === 'asc'
+      ? fields.sort((a, b) => Number(a) - Number(b))
+      : fields.sort((a, b) => Number(b) - Number(a))
+  }
+
+  return order === 'asc'
+    ? fields.sort((a, b) => {
+      return a > b ? 1 : -1;
+    })
+    : fields.sort((a, b) => {
+      return a > b ? -1 : 1;
+    })
 }
