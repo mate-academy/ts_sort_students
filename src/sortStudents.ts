568881599837292fa3c9a92832c28327b6756e84
Kostrica <romiorw@ukr.net>
2021-06-18 19:36:55 +0300

    Code reduction

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -27,35 +27,21 @@ export function sortStudents(students: Student[],
   const copyStudents = students.map(student => ({ ...student }));
 
   function getSortedStudents(key: string, sortOrder: string) {
-    if (sortOrder === 'asc') {
-      copyStudents.sort((studentA: any, studentB: any) =>
-        key === 'grades'
-          ? (studentA[key]
-            .reduce((sum: number, curent: number) => sum + curent, 0)
-              / studentA[key].length)
-          - (studentB[key]
-            .reduce((sum: number, curent: number) => sum + curent, 0)
-              / studentB[key].length)
-          : isNaN(studentA[key])
-            ? studentA[key]
-              .localeCompare(studentB[key])
-            : +studentA[key] - +studentB[key]
-      );
-    } else {
-      copyStudents.sort((studentA: any, studentB: any) =>
-        key === 'grades'
-          ? (studentB[key]
-            .reduce((sum: number, curent: number) => sum + curent, 0)
-              / studentB[key].length)
-          - (studentA[key]
-            .reduce((sum: number, curent: number) => sum + curent, 0)
-              / studentA[key].length)
-          : isNaN(studentA[key])
-            ? studentB[key]
-              .localeCompare(studentA[key])
-            : +studentB[key] - +studentA[key]
-      );
-    }
+    copyStudents.sort((studentA: any, studentB: any) => {
+      const positionOfStudents = sortOrder === 'asc'
+        ? [studentA[key], studentB[key]]
+        : [studentB[key], studentA[key]];
+      const [first, second] = positionOfStudents;
+
+      return key === 'grades'
+        ? (first.reduce((sum: number, curent: number) => sum + curent, 0)
+          / first.length)
+          - (second.reduce((sum: number, curent: number) => sum + curent, 0)
+          / second.length)
+        : isNaN(first)
+          ? first.localeCompare(second)
+          : first - second;
+    });
   }
 
   switch (sortBy) {
