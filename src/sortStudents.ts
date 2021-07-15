1d2fd5d3daa8006e074227f214c1c5e82a5de364
Artem Fedoseev <afedoseev359@gmail.com>
2021-07-15 16:49:26 +0300

    Solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -3,6 +3,63 @@
 // create SortField enum and export it
 // create SortOrder literal type
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+type Student = {
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[]
+}
+
+export enum SortField {
+  Name,
+  Surname,
+  Age,
+  Married,
+  AverageGrade
+}
+
+type SortOrder = 'asc' | 'desc';
+
+function getAverageGrade(grades: number[]): number {
+  return grades.reduce((gr1, gr2) => gr1 + gr2) / grades.length;
+}
+
+function compareStrings(stringA: string, stringB: string): number {
+  return stringA.localeCompare(stringB);
+}
+
+export function sortStudents(
+  students: Student[],
+  sortBy: SortField,
+  order: SortOrder): Student[] {
+  const output: Student[] = [...students];
+
+  output.sort((a: Student, b: Student) => {
+    let result: any;
+
+    switch (sortBy) {
+      case SortField.Name:
+        result = compareStrings(a.name, b.name);
+        break;
+      case SortField.Surname:
+        result = compareStrings(a.surname, b.surname);
+        break;
+      case SortField.Age:
+        result = a.age - b.age;
+        break;
+      case SortField.Married:
+        result = a.married && !b.married ? 1 : -1;
+        break;
+      case SortField.AverageGrade:
+        const avgGradeA = getAverageGrade(a.grades);
+        const avgGradeB = getAverageGrade(b.grades);
+
+        result = avgGradeA - avgGradeB;
+    }
+
+    return order === 'asc' ? result : result * (-1);
+  });
+
+  return output;
 }
