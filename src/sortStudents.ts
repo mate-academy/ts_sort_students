037f8ab9ddb1cee33cc853d92ceda676fe75b8f1
SashaRudenko <for_london@ukr.net>
2021-08-20 01:01:36 +0300

    solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,7 +1,83 @@
-// describe Student type
-// create and export SortType enum
-// create SortOrder type
+/* eslint-disable no-nested-ternary */
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+import { setupMaster } from "cluster";
+
+// describe Student type
+interface Student {
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: Array<number>,
+}
+
+// create and export SortType enum
+export enum SortType {
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades'
+}
+
+// create SortOrder type
+enum SortOrder {
+  asc = 'asc',
+  desc = 'desc'
+}
+
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] | string {
+  const copiedStudent = students.map((person) => ({ ...person }));
+
+  function calcAverage(grades: number[]): number {
+    return grades
+      .reduce((accumulator: number, sum: number) => accumulator + sum, 0);
+  }
+
+  switch (sortBy) {
+    case SortType.Name:
+    case SortType.Surname:
+      copiedStudent.sort((studentA: Student, studentB: Student) => {
+        return order === SortOrder.asc
+          ? studentA[sortBy].localeCompare(studentB[sortBy])
+          : studentB[sortBy].localeCompare(studentA[sortBy]);
+      });
+      break;
+
+    case SortType.Age:
+      copiedStudent.sort((studentA: Student, studentB: Student) => {
+        return order === SortOrder.asc
+          ? studentA[sortBy] - studentB[sortBy]
+          : studentB[sortBy] - studentA[sortBy];
+      });
+      break;
+
+    case SortType.Married:
+
+      copiedStudent.sort((studentA: Student, studentB: Student) => {
+        return studentA[sortBy] === studentB[sortBy]
+          ? 0
+          : order === SortOrder.asc
+            ? studentA[sortBy] ? 1 : -1
+            : studentA[sortBy] ? -1 : 1;
+      });
+      break;
+
+    case SortType.AverageGrade:
+      copiedStudent.sort((studentA: Student, studentB: Student) => {
+        return order === SortOrder.asc
+          ? calcAverage(studentA[sortBy]) - calcAverage(studentB[sortBy])
+          : calcAverage(studentB[sortBy]) - calcAverage(studentA[sortBy]);
+      });
+      break;
+
+    default:
+      break;
+  }
+
+  return copiedStudent;
 }
