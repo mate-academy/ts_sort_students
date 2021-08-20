9b887789a1ee428a5b85718174942cf4029e58ca
Sasha Lifashkin <alifashkin@outlook.com>
2021-08-20 14:29:16 +0300

    done

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -2,6 +2,94 @@
 // create and export SortType enum
 // create SortOrder type
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+function averageGrade(grades: number[]): number {
+  return grades.reduce((sum, a) => sum + a, 0);
+}
+
+interface Student {
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
+}
+
+export enum SortType {
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
+}
+
+type SortOrder = 'asc' | 'desc';
+
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const studentsSort: Student[] = [...students];
+
+  switch (sortBy) {
+    case SortType.Name:
+    case SortType.Surname:
+      studentsSort.sort(
+        (firstStudent: Student, secondStudent: Student) => (
+          order === 'asc'
+            ? firstStudent[sortBy].localeCompare(secondStudent[sortBy])
+            : secondStudent[sortBy].localeCompare(firstStudent[sortBy])
+        ),
+      );
+
+      break;
+
+    case SortType.Age:
+      studentsSort.sort(
+        (firstStudent: Student, secondStudent: Student) => (
+          order === 'asc'
+            ? firstStudent[sortBy] - secondStudent[sortBy]
+            : secondStudent[sortBy] - firstStudent[sortBy]
+        ),
+      );
+
+      break;
+
+    case SortType.Married:
+      studentsSort.sort(
+        (firstStudent: Student, secondStudent: Student) => {
+          if (firstStudent[sortBy] === secondStudent[sortBy]) {
+            return 0;
+          }
+
+          if (order === 'asc') {
+            return firstStudent[sortBy] ? 1 : -1;
+          }
+
+          return firstStudent[sortBy] ? -1 : 1;
+        },
+      );
+
+      break;
+
+    case SortType.AverageGrade:
+      studentsSort.sort(
+        (firstStudent: Student, secondStudent: Student) => (
+          order === 'asc'
+            ? averageGrade(
+              firstStudent[sortBy],
+            ) - averageGrade(secondStudent[sortBy])
+            : averageGrade(
+              secondStudent[sortBy],
+            ) - averageGrade(firstStudent[sortBy])
+        ),
+      );
+
+      break;
+
+    default:
+      throw new Error('Bad valid data');
+  }
+
+  return studentsSort;
 }
