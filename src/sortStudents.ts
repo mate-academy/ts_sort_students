976d4a7f9c3a215e5a3b98081516e98b564b0c16
Ula Dutchak <ulana.dutchak@gmail.com>
2021-08-24 14:23:56 +0300

    sort students

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,7 +1,79 @@
 // describe Student type
 // create and export SortType enum
 // create SortOrder type
+interface Student {
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[]
+}
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+export enum SortType {
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
+}
+
+type SortOrder = 'asc' | 'desc';
+
+function sumOfGraders(grades: number[]): number {
+  return grades.reduce((sum: number, value: number) => sum + value, 0)
+    / grades.length;
+}
+
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const copyOfStudent: Student[] = [...students];
+
+  switch (sortBy) {
+    case SortType.Name:
+    case SortType.Surname:
+      copyOfStudent.sort((a: Student, b: Student) => {
+        return order === 'asc'
+          ? a[sortBy].localeCompare(b[sortBy])
+          : b[sortBy].localeCompare(a[sortBy]);
+      });
+      break;
+
+    case SortType.Age:
+      copyOfStudent.sort((a: Student, b: Student) => {
+        return order === 'asc'
+          ? a[sortBy] - b[sortBy]
+          : b[sortBy] - a[sortBy];
+      });
+      break;
+
+    case SortType.Married:
+      copyOfStudent.sort((a: Student, b: Student) => {
+        if (a[sortBy] === b[sortBy]) {
+          return 0;
+        }
+
+        if (order === 'asc') {
+          return a[sortBy] ? 1 : -1;
+        }
+
+        return a[sortBy] ? -1 : 1;
+      });
+      break;
+
+    case SortType.AverageGrade:
+      copyOfStudent.sort((a: Student, b: Student) => {
+        return order === 'asc'
+          ? (sumOfGraders(a[sortBy]) - sumOfGraders(b[sortBy]))
+          : (sumOfGraders(b[sortBy]) - sumOfGraders(a[sortBy]));
+      });
+      break;
+
+    default:
+      break;
+  }
+
+  return copyOfStudent;
 }
