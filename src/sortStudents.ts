be4d455819d9672ce40683766a74bb9628c91d90
Dima Tkachenko <DimaTkachenko3@gmail.com>
2021-08-25 23:20:03 +0300

    solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,7 +1,79 @@
-// describe Student type
-// create and export SortType enum
-// create SortOrder type
+type Student = {
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
+};
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+export enum SortType {
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
+}
+
+enum SortOrder {
+  Ascending = 'asc',
+  Descending = 'desc',
+}
+
+function getAverageGrade(student: Student): number {
+  return student.grades
+    .reduce((sum: number, grade: number) => sum + grade)
+    / student.grades.length;
+}
+
+export function sortStudents(
+  students: Student[],
+  sortBy: string,
+  order: string,
+): Student[] {
+  const copyStudents = students.map((student: Student) => ({ ...student }));
+
+  switch (sortBy) {
+    case SortType.Age:
+      copyStudents
+        .sort((student1: Student, student2: Student) => (
+          order === SortOrder.Ascending
+            ? student1[sortBy] - student2[sortBy]
+            : student2[sortBy] - student1[sortBy]));
+      break;
+
+    case SortType.AverageGrade:
+      copyStudents.sort((student1: Student, student2: Student) => (
+        order === SortOrder.Ascending
+          ? getAverageGrade(student1) - getAverageGrade(student2)
+          : getAverageGrade(student2) - getAverageGrade(student1)
+      ));
+      break;
+
+    case SortType.Name:
+    case SortType.Surname:
+      copyStudents.sort((student1: Student, student2: Student) => (
+        order === SortOrder.Ascending
+          ? student1[sortBy].localeCompare(student2[sortBy])
+          : student2[sortBy].localeCompare(student1[sortBy])
+      ));
+      break;
+
+    case SortType.Married:
+      copyStudents.sort((student1: Student, student2: Student) => {
+        if (student1[sortBy] === student2[sortBy]) {
+          return 0;
+        }
+
+        if (order === SortOrder.Ascending) {
+          return student1[sortBy] ? 1 : -1;
+        }
+
+        return student1[sortBy] ? -1 : 1;
+      });
+      break;
+
+    default: break;
+  }
+
+  return copyStudents;
 }
