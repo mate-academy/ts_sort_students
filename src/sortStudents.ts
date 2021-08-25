4cac79c85c2d205a65fda9799c93739fefaaf6a8
NastyaVovk <winc4ester@gmail.com>
2021-08-25 12:44:09 +0300

    changes

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,7 +1,74 @@
-// describe Student type
-// create and export SortType enum
-// create SortOrder type
+interface Student {
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
+}
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+export enum SortType {
+  Name,
+  Surname,
+  Age,
+  Married,
+  AverageGrade,
+}
+
+type SortOrder = 'asc' | 'desc';
+
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const sorted = [...students];
+
+  function getAverageGrade(grades: number[]): number {
+    return grades.reduce((sum: number, a: number) => sum + a) / grades.length;
+  }
+
+  switch (sortBy) {
+    case SortType.Name:
+    case SortType.Surname:
+      return order === 'desc'
+        ? sorted.sort((a: Student, b: Student):number => {
+          return b.surname.localeCompare(a.surname);
+        })
+        : sorted.sort((a: Student, b: Student):number => {
+          return a.surname.localeCompare(b.surname);
+        });
+
+    case SortType.Age:
+      return order === 'desc'
+        ? sorted.sort((a: Student, b: Student):number => b.age - a.age)
+        : sorted.sort((a: Student, b: Student):number => a.age - b.age);
+
+    case SortType.Married:
+      sorted.sort((a: Student, b: Student): number => {
+        if (a.married === b.married) {
+          return 0;
+        }
+
+        if (order === 'desc') {
+          return a.married ? -1 : 1;
+        }
+
+        return a.married ? 1 : -1;
+      });
+      break;
+
+    case SortType.AverageGrade:
+      return order === 'desc'
+        ? sorted.sort((a: Student, b: Student):number => {
+          return getAverageGrade(b.grades) - getAverageGrade(a.grades);
+        })
+        : sorted.sort((a: Student, b: Student):number => {
+          return getAverageGrade(a.grades) - getAverageGrade(b.grades);
+        });
+
+    default:
+      break;
+  }
+
+  return sorted;
 }
