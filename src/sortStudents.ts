5ecd6a0b66801f86458f3eedf83ddcf1c97d762d
Tymofii Fesenko <tymofii.fesenko1999@gmail.com>
2021-08-27 10:02:06 +0200

    edited solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -8,11 +8,11 @@ type Student = {
 };
 
 export enum SortType {
-  'Name' = 1,
-  'Surname' = 2,
-  'Age' = 3,
-  'Married' = 4,
-  'AverageGrade' = 5,
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'averageGrade',
 }
 
 type SortOrder = 'asc' | 'desc';
@@ -24,54 +24,27 @@ export function sortStudents(students: Student[],
   switch (sortBy) {
     case (SortType.AverageGrade):
       resultArr.sort((a: Student, b: Student): number => {
-        if (order === 'desc') {
-          return (b.grades.reduce((p, c) => p + c, 0) / b.grades.length)
-          - (a.grades.reduce((p, c) => p + c, 0) / a.grades.length);
-        }
+        [a, b] = order === 'desc' ? [a, b] : [b, a];
 
-        return (a.grades.reduce((p, c) => p + c, 0) / a.grades.length)
-          - (b.grades.reduce((p, c) => p + c, 0) / b.grades.length);
+        return (b.grades.reduce((p, c) => p + c, 0) / b.grades.length)
+          - (a.grades.reduce((p, c) => p + c, 0) / a.grades.length);
       });
       break;
     case (SortType.Name):
-      resultArr.sort((a: Student, b: Student): number => {
-        if (order === 'desc') {
-          b.name.localeCompare(a.name);
-        }
-
-        return a.name.localeCompare(b.name);
-      });
-      break;
     case (SortType.Surname):
       resultArr.sort((a: Student, b: Student): number => {
-        if (order === 'desc') {
-          return b.surname.localeCompare(a.surname);
-        }
-
-        return a.surname.localeCompare(b.surname);
+        return order === 'desc' ? b[sortBy].localeCompare(a[sortBy])
+          : a[sortBy].localeCompare(b[sortBy]);
       });
       break;
     case (SortType.Age):
-      resultArr.sort((a: Student, b: Student): number => {
-        if (order === 'desc') {
-          return b.age - a.age;
-        }
-
-        return a.age - b.age;
-      });
-      break;
     case (SortType.Married):
       resultArr.sort((a: Student, b: Student): number => {
-        if (a.married === b.married) {
-          return 0;
-        }
-
-        if (order === 'desc') {
-          return b.married ? 1 : -1;
-        }
-
-        return a.married ? 1 : -1;
+        return order === 'desc' ? +b[sortBy] - +a[sortBy]
+          : +a[sortBy] - +b[sortBy];
       });
+      break;
+    default:
       break;
   }
 
