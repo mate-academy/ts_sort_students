0d2763199febe1ed0e567aff39a7330538a43eac
karolina-nadDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD <westlinka20@gmail.com>
2021-08-27 20:39:47 +0300

    task done2

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -20,7 +20,7 @@ export enum SortType {
 
 type SortOrder = 'asc' | 'desc';
 
-function gradeSum(student : Student): number {
+function getAverageGrade(student : Student): number {
   return student.grades.reduce(
     (a: number, b: number) => a + b, 0,
   ) / student.grades.length;
@@ -31,47 +31,49 @@ export function sortStudents(
   sortBy: SortType,
   order: SortOrder,
 ): Student[] {
-  const copy = [...students];
+  const studentsCopy = [...students];
 
   if (sortBy === SortType.Name) {
     if (order === 'asc') {
-      return copy.sort((a, b) => a.name.localeCompare(b.name));
+      return studentsCopy.sort((a, b) => a.name.localeCompare(b.name));
     }
 
-    return copy.sort((a, b) => b.name.localeCompare(a.name));
+    return studentsCopy.sort((a, b) => b.name.localeCompare(a.name));
   }
 
   if (sortBy === SortType.Surname) {
     if (order === 'asc') {
-      return copy.sort((a, b) => a.surname.localeCompare(b.surname));
+      return studentsCopy.sort((a, b) => a.surname.localeCompare(b.surname));
     }
 
-    return copy.sort((a, b) => b.surname.localeCompare(a.surname));
+    return studentsCopy.sort((a, b) => b.surname.localeCompare(a.surname));
   }
 
   if (sortBy === SortType.Age) {
     if (order === 'asc') {
-      return copy.sort((a, b) => a.age - b.age);
+      return studentsCopy.sort((a, b) => a.age - b.age);
     }
 
-    return copy.sort((a, b) => b.age - a.age);
+    return studentsCopy.sort((a, b) => b.age - a.age);
   }
 
   if (sortBy === SortType.Married) {
     if (order === 'asc') {
-      return copy.sort((a, b) => +a.married - +b.married);
+      return studentsCopy.sort((a, b) => +a.married - +b.married);
     }
 
-    return copy.sort((a, b) => +b.married - +a.married);
+    return studentsCopy.sort((a, b) => +b.married - +a.married);
   }
 
   if (sortBy === SortType.AverageGrade) {
     if (order === 'asc') {
-      return copy.sort((a, b) => gradeSum(a) - gradeSum(b));
+      return studentsCopy.sort(
+        (a, b) => getAverageGrade(a) - getAverageGrade(b),
+      );
     }
 
-    return copy.sort((a, b) => gradeSum(b) - gradeSum(a));
+    return studentsCopy.sort((a, b) => getAverageGrade(b) - getAverageGrade(a));
   }
 
-  return copy;
+  return studentsCopy;
 }
