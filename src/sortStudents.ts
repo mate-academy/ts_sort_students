80b907c4b18acf21f0a2ac3fe0fca16f748ac737
Andrii <badekha@gmail.com>
2021-09-23 15:32:11 +0300

    optimized code

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -24,62 +24,45 @@ export function sortStudents(
   sortBy: SortType,
   order: SortOrder,
 ): Student[] {
+  // helper functions
   const reducer = (a: number, b: number): number => (a + b);
+  const sortByString = (str: 'name' | 'surname'): Student[] => {
+    return [...students].sort((a, b) => (
+      order === 'desc'
+        ? b[str].localeCompare(a[str])
+        : a[str].localeCompare(b[str])
+    ));
+  };
 
   switch (sortBy) {
     case SortType.Name:
-      if (order === 'desc') {
-        return [...students].sort((a, b) => (
-          b.name.localeCompare(a.name)
-        ));
-      }
-
-      return [...students].sort((a, b) => (
-        a.name.localeCompare(b.name)
-      ));
+      return sortByString('name');
 
     case SortType.Surname:
-      if (order === 'desc') {
-        return [...students].sort((a, b) => (
-          b.surname.localeCompare(a.surname)
-        ));
-      }
-
-      return [...students].sort((a, b) => (
-        b.surname.localeCompare(a.surname)
-      ));
+      return sortByString('surname');
 
     case SortType.Age:
-      if (order === 'desc') {
-        return [...students].sort((a, b) => (
-          b.age - a.age
-        ));
-      }
 
       return [...students].sort((a, b) => (
-        a.age - b.age
+        order === 'desc'
+          ? b.age - a.age
+          : a.age - b.age
       ));
 
     case SortType.Married:
-      if (order === 'desc') {
-        return [...students].sort((a, b) => (
-          +b.married - +a.married
-        ));
-      }
 
       return [...students].sort((a, b) => (
-        +a.married - +b.married
+        order === 'desc'
+          ? +b.married - +a.married
+          : +a.married - +b.married
       ));
 
     case SortType.AverageGrade:
-      if (order === 'desc') {
-        return [...students].sort((a, b) => (
-          b.grades.reduce(reducer) - a.grades.reduce(reducer)
-        ));
-      }
 
       return [...students].sort((a, b) => (
-        a.grades.reduce(reducer) - b.grades.reduce(reducer)
+        order === 'desc'
+          ? b.grades.reduce(reducer) - a.grades.reduce(reducer)
+          : a.grades.reduce(reducer) - b.grades.reduce(reducer)
       ));
 
     default:
