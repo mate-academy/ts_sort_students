d9185f1820b94bc6c5d17bd28daeefe0eeef61f3
Danil Nikolaichuk <s.person2734@gmail.com>
2021-10-04 21:27:50 +0300

    working but need some refactoring

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,7 +1,67 @@
-// describe Student type
-// create and export SortType enum
-// create SortOrder type
+interface Student {
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
+}
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+export enum SortType {
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades'
+}
+
+type SortOrder = 'asc' | 'desc';
+
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const copyOfStudents: Student[] = [...students];
+
+  switch (sortBy) {
+    case SortType.Name:
+    case SortType.Surname:
+      return order === 'asc'
+        ? copyOfStudents
+          .sort((a: Student, b: Student) => a[sortBy].localeCompare(b[sortBy]))
+        : copyOfStudents
+          .sort((a: Student, b: Student) => b[sortBy].localeCompare(a[sortBy]));
+    case SortType.Age:
+      return order === 'asc'
+        ? copyOfStudents
+          .sort((a: Student, b: Student) => a[sortBy] - b[sortBy])
+        : copyOfStudents
+          .sort((a: Student, b: Student) => b[sortBy] - a[sortBy]);
+    case SortType.Married:
+      return order === 'asc'
+        ? copyOfStudents
+          .sort((a: Student, b: Student) => +a[sortBy] - +b[sortBy])
+        : copyOfStudents
+          .sort((a: Student, b: Student) => +b[sortBy] - +a[sortBy]);
+    case SortType.AverageGrade:
+      // I think, it is necessary to create a function for average grade
+      // I will implement it in the next commit
+      return order === 'asc'
+        ? copyOfStudents
+          .sort((a: Student, b: Student) => a[sortBy]
+            .reduce((acc: number, grade: number) => acc + grade)
+              / a[sortBy].length
+            - b[sortBy]
+              .reduce((acc: number, grade: number) => acc + grade)
+                / b[sortBy].length)
+        : copyOfStudents
+          .sort((a: Student, b: Student) => b[sortBy]
+            .reduce((acc: number, grade: number) => acc + grade)
+              / b[sortBy].length
+            - a[sortBy]
+              .reduce((acc: number, grade: number) => acc + grade)
+                / a[sortBy].length);
+    default:
+      return copyOfStudents;
+  }
 }
