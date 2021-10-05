6ccf76b7b5be229328f384b2dca808e598681ca6
Danil Nikolaichuk <s.person2734@gmail.com>
2021-10-05 17:51:17 +0300

    solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -21,47 +21,32 @@ export function sortStudents(
   sortBy: SortType,
   order: SortOrder,
 ): Student[] {
-  const copyOfStudents: Student[] = [...students];
-
-  switch (sortBy) {
-    case SortType.Name:
-    case SortType.Surname:
-      return order === 'asc'
-        ? copyOfStudents
-          .sort((a: Student, b: Student) => a[sortBy].localeCompare(b[sortBy]))
-        : copyOfStudents
-          .sort((a: Student, b: Student) => b[sortBy].localeCompare(a[sortBy]));
-    case SortType.Age:
-      return order === 'asc'
-        ? copyOfStudents
-          .sort((a: Student, b: Student) => a[sortBy] - b[sortBy])
-        : copyOfStudents
-          .sort((a: Student, b: Student) => b[sortBy] - a[sortBy]);
-    case SortType.Married:
-      return order === 'asc'
-        ? copyOfStudents
-          .sort((a: Student, b: Student) => +a[sortBy] - +b[sortBy])
-        : copyOfStudents
-          .sort((a: Student, b: Student) => +b[sortBy] - +a[sortBy]);
-    case SortType.AverageGrade:
-      // I think, it is necessary to create a function for average grade
-      // I will implement it in the next commit
-      return order === 'asc'
-        ? copyOfStudents
-          .sort((a: Student, b: Student) => a[sortBy]
-            .reduce((acc: number, grade: number) => acc + grade)
-              / a[sortBy].length
-            - b[sortBy]
-              .reduce((acc: number, grade: number) => acc + grade)
-                / b[sortBy].length)
-        : copyOfStudents
-          .sort((a: Student, b: Student) => b[sortBy]
-            .reduce((acc: number, grade: number) => acc + grade)
-              / b[sortBy].length
-            - a[sortBy]
-              .reduce((acc: number, grade: number) => acc + grade)
-                / a[sortBy].length);
-    default:
-      return copyOfStudents;
+  function calcAverageGrade(grades: number[]): number {
+    return grades.reduce((acc: number, grade: number) => acc + grade)
+      / grades.length;
   }
+
+  return [...students].sort((a: Student, b: Student) => {
+    switch (sortBy) {
+      case SortType.Name:
+      case SortType.Surname:
+        return order === 'asc'
+          ? a[sortBy].localeCompare(b[sortBy])
+          : b[sortBy].localeCompare(a[sortBy]);
+      case SortType.Age:
+        return order === 'asc'
+          ? a[sortBy] - b[sortBy]
+          : b[sortBy] - a[sortBy];
+      case SortType.Married:
+        return order === 'asc'
+          ? +a[sortBy] - +b[sortBy]
+          : +b[sortBy] - +a[sortBy];
+      case SortType.AverageGrade:
+        return order === 'asc'
+          ? calcAverageGrade(a[sortBy]) - calcAverageGrade(b[sortBy])
+          : calcAverageGrade(b[sortBy]) - calcAverageGrade(a[sortBy]);
+      default:
+        return 0;
+    }
+  });
 }
