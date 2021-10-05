230bc6c3671e9a6e4f5e0f813ce82d85f122e1aa
Kate Kasuba <otroshko12@gmail.com>
2021-10-05 14:55:07 +0300

    Solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,7 +1,65 @@
-// describe Student type
-// create and export SortType enum
-// create SortOrder type
+type Student = {
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[],
+};
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+export enum SortType {
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
+}
+
+type SortOrder = 'asc' | 'desc';
+
+function averageGrade(student: Student): number {
+  const sumOfGrades = student.grades.reduce((sum, grade) => sum + grade, 0);
+
+  return sumOfGrades / student.grades.length;
+}
+
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const studentsCopy = [...students];
+
+  if (sortBy === SortType.Name || sortBy === SortType.Surname) {
+    if (order === 'asc') {
+      studentsCopy.sort((a, b) => a[sortBy].localeCompare(b[sortBy]));
+    } else {
+      studentsCopy.sort((a, b) => b[sortBy].localeCompare(a[sortBy]));
+    }
+  }
+
+  if (sortBy === SortType.Age) {
+    if (order === 'asc') {
+      studentsCopy.sort((a, b) => a[sortBy] - (b[sortBy]));
+    } else {
+      studentsCopy.sort((a, b) => b[sortBy] - (a[sortBy]));
+    }
+  }
+
+  if (sortBy === SortType.Married) {
+    if (order === 'asc') {
+      studentsCopy.sort((a, b) => +a[sortBy] - +(b[sortBy]));
+    } else {
+      studentsCopy.sort((a, b) => +b[sortBy] - +(a[sortBy]));
+    }
+  }
+
+  if (sortBy === SortType.AverageGrade) {
+    if (order === 'asc') {
+      studentsCopy.sort((a, b) => averageGrade(a) - averageGrade(b));
+    } else {
+      studentsCopy.sort((a, b) => averageGrade(b) - averageGrade(a));
+    }
+  }
+
+  return studentsCopy;
 }
