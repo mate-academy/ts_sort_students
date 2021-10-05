fd1781ec56e38a8e99bc75814dd1d0ef3add84ef
Iurii Medvedchuk <io.medvedchuk@gmail.com>
2021-10-05 16:15:55 +0300

    solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,7 +1,91 @@
-// describe Student type
-// create and export SortType enum
-// create SortOrder type
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+interface Student {
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
+}
+
+export enum SortType {
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
+}
+
+type SortOrder = 'asc' | 'desc';
+
+function getAvarageGrade(student: Student): number {
+  const summOfGrades: number = student.grades.reduce(
+    (a, b) => a + b,
+    0,
+  );
+
+  return summOfGrades / student.grades.length;
+}
+
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  let callBack;
+  const studentsCopy = [...students];
+
+  switch (sortBy) {
+    case SortType.Name:
+    case SortType.Surname:
+      callBack
+        = (a: Student, b: Student): number => {
+          return (order === 'asc')
+            ? a[sortBy].localeCompare(b[sortBy])
+            : b[sortBy].localeCompare(a[sortBy]);
+        };
+      break;
+
+    case SortType.Age:
+      callBack = (a: Student, b: Student): number => {
+        return (order === 'asc')
+          ? a[sortBy] - b[sortBy]
+          : b[sortBy] - a[sortBy];
+      };
+      break;
+
+    case SortType.Married:
+      callBack = (a: Student, b: Student): number => {
+        if (a[sortBy] && !b[sortBy]) {
+          return (order === 'asc')
+            ? 1
+            : -1;
+        }
+
+        if (!a[sortBy] && b[sortBy]) {
+          return (order === 'asc')
+            ? -1
+            : 1;
+        }
+
+        return 0;
+      };
+      break;
+
+    case SortType.AverageGrade:
+      callBack = (a: Student, b: Student): number => {
+        return (order === 'asc')
+          ? getAvarageGrade(a) - getAvarageGrade(b)
+          : getAvarageGrade(b) - getAvarageGrade(a);
+      };
+      break;
+
+    default:
+      callBack = (): 0 => {
+        return 0;
+      };
+  }
+
+  const sorted: Student[] = studentsCopy.sort(callBack);
+
+  return sorted;
 }
