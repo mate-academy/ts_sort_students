01b7d8f6e7f8149e265d6d4010d1667b966e397b
Sviat Torba <torba.svyatoslav.fox@gmail.com>
2021-10-06 09:47:33 +0300

    solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -2,6 +2,54 @@
 // create and export SortType enum
 // create SortOrder type
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+export interface Student {
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
+}
+
+export enum SortType {
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
+}
+
+type SortOrder = 'asc' | 'desc';
+
+function AverageGradeCalc(gr: number[]): number {
+  return gr.reduce((sum: number, n: number) => sum + n, 0) / gr.length;
+}
+
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  return [...students].sort((a, b) => {
+    switch (sortBy) {
+      case SortType.Age:
+      case SortType.Married:
+        return order === 'asc'
+          ? Number(a[sortBy]) - Number(b[sortBy])
+          : Number(b[sortBy]) - Number(a[sortBy]);
+
+      case SortType.Name:
+      case SortType.Surname:
+        return order === 'asc'
+          ? a[sortBy].localeCompare(b[sortBy])
+          : b[sortBy].localeCompare(a[sortBy]);
+
+      case SortType.AverageGrade:
+        return order === 'asc'
+          ? AverageGradeCalc(a[sortBy]) - AverageGradeCalc(b[sortBy])
+          : AverageGradeCalc(b[sortBy]) - AverageGradeCalc(a[sortBy]);
+
+      default:
+        return 0;
+    }
+  });
 }
