fca114175cf791f33f1c4aad2d21fecae679a075
Kate Kuzmenko <rina.punina@gmail.com>
2021-10-06 09:31:10 +0300

    changed to switch case, fixed calculating function

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,4 +1,4 @@
-export interface Student{
+export interface Student {
   name: string,
   surname: string,
   age: number,
@@ -7,74 +7,62 @@ export interface Student{
 }
 
 export enum SortType {
-  Name='name',
-  Surname='surname',
-  Age='age',
-  Married='married',
-  AverageGrade='averagegrade',
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'averagegrade',
 }
 
 export type SortOrder = 'asc' | 'desc';
 
-export function calculateAverageGrade(studentsMarks: number[] = []):number {
-  return studentsMarks.reduce((sum, curr) => sum + curr, 0)
-    / studentsMarks.length;
+export function calculateAverageGrade(student: Student): number {
+  return student.grades.reduce((prev, current) => prev + current, 0)
+    / student.grades.length;
 }
 
 export function sortStudents(
   students:Student[],
   sortBy:SortType,
   order: SortOrder,
-) :Student[] {
+) : Student[] {
   const studentsCopy = students.map((student) => ({ ...student }));
-  const fieldForSorting = sortBy.toLowerCase();
 
-  if (order === 'asc') {
-    if (fieldForSorting === SortType.AverageGrade) {
-      studentsCopy
-        .sort((student1, student2) => calculateAverageGrade(student1.grades)
-          - calculateAverageGrade(student2.grades));
-    } else if (fieldForSorting === SortType.Name
-        || fieldForSorting === SortType.Surname) {
-      studentsCopy.sort((student1, student2) => student1[fieldForSorting]
-        .localeCompare(student2[fieldForSorting]));
-    } else if (fieldForSorting === SortType.Age) {
-      studentsCopy.sort((student1, student2) => student1.age - student2.age);
-    } else {
-      studentsCopy.sort((student1, student2) => {
-        if (student1.married === student2.married) {
-          return 0;
-        }
-
-        if (student1.married) {
-          return 1;
-        }
-
-        return -1;
+  switch (sortBy) {
+    case SortType.Name:
+    case SortType.Surname:
+      studentsCopy.sort((student1: Student, student2: Student) => {
+        return order === 'asc'
+          ? student1[sortBy].localeCompare(student2[sortBy])
+          : student2[sortBy].localeCompare(student1[sortBy]);
       });
-    }
-  } else if (fieldForSorting === SortType.AverageGrade) {
-    studentsCopy
-      .sort((student2, student1) => calculateAverageGrade(student1.grades)
-        - calculateAverageGrade(student2.grades));
-  } else if (fieldForSorting === SortType.Name
-      || fieldForSorting === SortType.Surname) {
-    studentsCopy.sort((student2, student1) => student1[fieldForSorting]
-      .localeCompare(student2[fieldForSorting]));
-  } else if (fieldForSorting === SortType.Age) {
-    studentsCopy.sort((student2, student1) => student1.age - student2.age);
-  } else {
-    studentsCopy.sort((student2, student1) => {
-      if (student1.married === student2.married) {
-        return 0;
-      }
+      break;
 
-      if (student1.married) {
-        return 1;
-      }
+    case SortType.Age:
+      studentsCopy.sort((student1: Student, student2: Student) => {
+        return order === 'asc'
+          ? +student1[sortBy] - +student2[sortBy]
+          : +student2[sortBy] - +student1[sortBy];
+      });
+      break;
 
-      return -1;
-    });
+    case SortType.Married:
+      studentsCopy.sort((student1: Student, student2: Student) => {
+        return order === 'asc'
+          ? +student1[sortBy] - +student2[sortBy]
+          : +student2[sortBy] - +student1[sortBy];
+      });
+      break;
+
+    case SortType.AverageGrade:
+      studentsCopy.sort((student1: Student, student2: Student) => {
+        return order === 'asc'
+          ? calculateAverageGrade(student1) - calculateAverageGrade(student2)
+          : calculateAverageGrade(student2) - calculateAverageGrade(student1);
+      });
+      break;
+
+    default: break;
   }
 
   return studentsCopy;
