12fb4e68ff2e6d2d1233a1a0a5304d10a6f2ac05
Anton Forzun <anton_forzun@ukr.net>
2021-10-07 14:15:46 +0300

    sort_students

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,54 @@
 
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean
+  grades: [number];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
 // create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+function calculateAverageGrades(array: number[]): number {
+  return array.reduce((acc, n) => acc + n, 0) / array.length;
+}
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  return [...students].sort((a, b) => {
+    if (sortBy === SortType.Name || sortBy === SortType.Surname) {
+      return order === 'asc'
+        ? a[sortBy].localeCompare(b[sortBy])
+        : b[sortBy].localeCompare(a[sortBy]);
+    }
+
+    if (sortBy === SortType.Age || sortBy === SortType.Married) {
+      return order === 'asc'
+        ? Number(a[sortBy]) - Number(b[sortBy])
+        : Number(b[sortBy]) - Number(a[sortBy]);
+    }
+
+    if (sortBy === SortType.AverageGrade) {
+      const aAverage = calculateAverageGrades(a[sortBy]);
+      const bAverage = calculateAverageGrades(b[sortBy]);
+
+      return order === 'asc'
+        ? aAverage - bAverage
+        : bAverage - aAverage;
+    }
+
+    return 0;
+  });
 }
