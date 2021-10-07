5ab8731a1085ad311b0e8d58f49ea115f71b708a
Alisa Ivashkevych <57598825+aliceblunt8@users.noreply.github.com>
2021-10-07 12:07:23 +0300

    Update sortStudents.ts

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,6 +1,15 @@
-// describe Student type
-// create and export SortType enum
+
+export interface Student {
+  // describe Student interface
+}
+
+export enum SortType {
+  // describe SortType enum
+}
+
 // create SortOrder type
+export type SortOrder;
+
 
 export function sortStudents(students, sortBy, order) {
   // write your function
