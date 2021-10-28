63d155eee0afb004d1a1d7073eac9af8423879c1
Max <gorodtira51@gmail.com>
2021-10-28 21:35:15 +0300

    done

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,73 @@
 
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name,
+  Surname,
+  Age,
+  Married,
+  AverageGrade
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder= 'asc' | 'desc';
 
+export function getAverageGrade(grades: number[]): number {
+  return (grades.reduce((sum, curr) => sum + curr, 0)) / grades.length;
+}
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const sortedStudents: Student[] = [...students];
+  const areAscending = order === 'asc';
+
+  switch (sortBy) {
+    case SortType.Name:
+      sortedStudents.sort((first, second) => (areAscending
+        ? first.name.localeCompare(second.name)
+        : second.name.localeCompare(first.name)
+      ));
+      break;
+
+    case SortType.Surname:
+      sortedStudents.sort((first, second) => (areAscending
+        ? first.surname.localeCompare(second.surname)
+        : second.surname.localeCompare(first.surname)
+      ));
+      break;
+
+    case SortType.Age:
+      sortedStudents.sort((first, second) => (areAscending
+        ? first.age - second.age
+        : second.age - first.age
+      ));
+      break;
+
+    case SortType.Married:
+      sortedStudents.sort((first, second) => (areAscending
+        ? Number(first.married) - Number(second.married)
+        : Number(second.married) - Number(first.married)
+      ));
+      break;
+
+    case SortType.AverageGrade:
+      sortedStudents.sort((first, second) => (areAscending
+        ? getAverageGrade(first.grades) - getAverageGrade(second.grades)
+        : getAverageGrade(second.grades) - getAverageGrade(first.grades)
+      ));
+      break;
+
+    default:
+      throw new Error('Oops... Something went wrong');
+  }
+
+  return sortedStudents;
 }
