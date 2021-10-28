f22b5154e358ce6fb42fc767f3dcfd60887ff93b
cyb1ove <cyb1ove.ua@gmail.com>
2021-10-28 19:53:11 +0300

    Task was solved

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,70 @@
-
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[],
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name,
+  Surname,
+  Age,
+  Married,
+  AverageGrade,
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+function getAvarage(grades: number[]): number {
+  return grades.reduce((sum, grade) => sum + grade, 0) / grades.length;
+}
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+function sortTwoStudent(
+  pastStudent: Student,
+  nextStudent: Student,
+  sortBy: SortType,
+  order: SortOrder,
+): number {
+  switch (sortBy) {
+    case SortType.Name:
+      return order === 'asc'
+        ? pastStudent.name.localeCompare(nextStudent.name)
+        : nextStudent.name.localeCompare(pastStudent.name);
+
+    case SortType.Surname:
+      return order === 'asc'
+        ? pastStudent.surname.localeCompare(nextStudent.surname)
+        : nextStudent.surname.localeCompare(pastStudent.surname);
+
+    case SortType.Age:
+      return order === 'asc'
+        ? pastStudent.age - nextStudent.age
+        : nextStudent.age - pastStudent.age;
+
+    case SortType.Married:
+      return order === 'asc'
+        ? Number(pastStudent.married) - Number(nextStudent.married)
+        : Number(nextStudent.married) - Number(pastStudent.married);
+
+    case SortType.AverageGrade:
+      return order === 'asc'
+        ? getAvarage(pastStudent.grades) - getAvarage(nextStudent.grades)
+        : getAvarage(nextStudent.grades) - getAvarage(pastStudent.grades);
+
+    default:
+      throw new Error('Wrong data');
+  }
+}
+
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const studentsCopy: Student[] = [...students];
+
+  return studentsCopy.sort((pastStudent: Student, nextStudent: Student) => {
+    return sortTwoStudent(pastStudent, nextStudent, sortBy, order);
+  });
 }
