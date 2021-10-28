47373e393512c3ce03ef68e2ffce95ea97aee5ef
Irka Shemchuk <ilysbbk89@yahoo.com>
2021-10-28 23:14:54 +0300

    solved

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,67 @@
 
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: 'string',
+  age: number,
+  married: boolean,
+  grades: number[],
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name,
+  Surname,
+  Age,
+  Married,
+  AverageGrade,
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const studentsCopy = [...students];
+  const ascOrder = (order === 'asc');
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  if (sortBy === SortType.Name) {
+    return ascOrder
+      ? [...students].sort((a, b) => a.name.localeCompare(b.name))
+      : [...students].sort((a, b) => a.name.localeCompare(b.name)).reverse();
+  }
+
+  if (sortBy === SortType.Surname) {
+    return ascOrder
+      ? [...students]
+        .sort((a, b) => a.surname.localeCompare(b.surname))
+      : [...students]
+        .sort((a, b) => a.surname.localeCompare(b.surname)).reverse();
+  }
+
+  if (sortBy === SortType.Age) {
+    return ascOrder
+      ? [...students].sort((a, b) => a.age - b.age)
+      : [...students].sort((a, b) => b.age - a.age);
+  }
+
+  if (sortBy === SortType.Married) {
+    return ascOrder
+      ? [...students].sort((a, b) => +a.married - +b.married)
+      : [...students].sort((a, b) => +b.married - +a.married);
+  }
+
+  if (sortBy === SortType.AverageGrade) {
+    return !ascOrder
+      ? [...students]
+        .sort((a, b) => b.grades
+          .reduce((sum, x) => sum + x) / b.grades.length - a.grades
+          .reduce((sum, x) => sum + x) / a.grades.length)
+      : [...students]
+        .sort((a, b) => a.grades
+          .reduce((sum, x) => sum + x) / a.grades.length - b.grades
+          .reduce((sum, x) => sum + x) / b.grades.length);
+  }
+
+  return studentsCopy;
 }
