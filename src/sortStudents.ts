d0a08240b3522e8b0143bacb7f82fc3fab2247ff
Taras Kozak <taraskozakmail@gmail.com>
2021-10-28 17:36:20 +0300

    solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,106 @@
+// import { type } from "os"
 
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades:number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name,
+  Surname,
+  Age,
+  Married,
+  AverageGrade,
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+function isAscending(order:SortOrder):boolean {
+  return order === 'asc';
+}
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+function getAverageGrade(numbers:number[]):number {
+  return numbers
+    .reduce((acc:number, curr:number) => acc + curr) / numbers.length;
+}
+
+type CallBack = (el1:Student, el2:Student,) => number;
+
+function sortByName(order:boolean):CallBack {
+  return function callback(student1:Student, student2:Student):number {
+    return order
+      ? student1.name.localeCompare(student2.name)
+      : student2.name.localeCompare(student1.name);
+  };
+}
+
+function sortBySurname(order:boolean):CallBack {
+  return function callback(student1:Student, student2:Student):number {
+    return order
+      ? student1.surname.localeCompare(student2.surname)
+      : student2.surname.localeCompare(student1.surname);
+  };
+}
+
+function sortByAge(order:boolean):CallBack {
+  return function callback(student1: Student, student2: Student):number {
+    return order
+      ? student1.age - student2.age
+      : student2.age - student1.age;
+  };
+}
+
+function sortByMarriage(order:boolean):CallBack {
+  return function callback(student1: Student, student2: Student):number {
+    return order
+      ? Number(student1.married) - Number(student2.married)
+      : Number(student2.married) - Number(student1.married);
+  };
+}
+
+function sortByAverageGrade(order: boolean):CallBack {
+  return function callback(student1: Student, student2: Student):number {
+    return order
+      ? getAverageGrade(student1.grades) - getAverageGrade(student2.grades)
+      : getAverageGrade(student2.grades) - getAverageGrade(student1.grades);
+  };
+}
+
+export function sortStudents(
+  students:Student[],
+  sortBy:SortType,
+  order:SortOrder,
+):Student[] {
+  const studentsCopy:Student[] = students
+    .map((student:Student) => ({ ...student }));
+
+  switch (sortBy) {
+    case SortType.Name:
+      studentsCopy.sort(sortByName(isAscending(order)));
+      break;
+
+    case SortType.Surname:
+      studentsCopy.sort(sortBySurname(isAscending(order)));
+      break;
+
+    case SortType.Age:
+      studentsCopy.sort(sortByAge(isAscending(order)));
+      break;
+
+    case SortType.Married:
+      studentsCopy.sort(sortByMarriage(isAscending(order)));
+      break;
+
+    case SortType.AverageGrade:
+      studentsCopy.sort(sortByAverageGrade(isAscending(order)));
+      break;
+
+    default:
+      break;
+  }
+
+  return studentsCopy;
 }
