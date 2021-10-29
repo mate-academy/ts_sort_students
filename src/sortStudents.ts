9057edff6f780811de29bb05c90311ebeedb81ed
Denys Anosov <anosovdenys@gmail.com>
2021-10-29 21:52:18 +0300

    solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -22,79 +22,54 @@ export function sortStudents(
   sortBy: SortType,
   order: SortOrder,
 ): object[] {
-  const output: Student[] = students
+  const studentsCopy: Student[] = students
     .map((student: Student) => ({ ...student }));
 
-  const averageGrade = function getAverageGrade(student: Student): number {
+  const getAverageGrade = (student: Student): number => {
     return student.grades
       .reduce((sum, grade) => sum + grade, 0) / student.grades.length;
   };
 
+  const isAscending = order === 'asc';
+
   switch (sortBy) {
     case SortType.Name:
-      if (order === 'asc') {
-        output.sort((firstStudent, secondStudent) => firstStudent.name
-          .localeCompare(secondStudent.name));
-      }
-
-      if (order === 'desc') {
-        output.sort((firstStudent, secondStudent) => secondStudent.name
-          .localeCompare(firstStudent.name));
-      }
+      studentsCopy.sort((firstStudent, secondStudent) => (isAscending
+        ? firstStudent.name.localeCompare(secondStudent.name)
+        : secondStudent.name.localeCompare(firstStudent.name)
+      ));
       break;
 
     case SortType.Surname:
-      if (order === 'asc') {
-        output.sort((firstStudent, secondStudent) => firstStudent.surname
-          .localeCompare(secondStudent.surname));
-      }
-
-      if (order === 'desc') {
-        output.sort((firstStudent, secondStudent) => secondStudent.surname
-          .localeCompare(firstStudent.surname));
-      }
+      studentsCopy.sort((firstStudent, secondStudent) => (isAscending
+        ? firstStudent.surname.localeCompare(secondStudent.surname)
+        : secondStudent.surname.localeCompare(firstStudent.surname)
+      ));
       break;
 
     case SortType.Age:
-      if (order === 'asc') {
-        output.sort((firstStudent, secondStudent) => firstStudent.age
-          - secondStudent.age);
-      }
-
-      if (order === 'desc') {
-        output.sort((firstStudent, secondStudent) => secondStudent.age
-          - firstStudent.age);
-      }
+      studentsCopy.sort((firstStudent, secondStudent) => (isAscending
+        ? firstStudent.age - secondStudent.age
+        : secondStudent.age - firstStudent.age
+      ));
       break;
 
     case SortType.Married:
-      if (order === 'asc') {
-        output
-          .sort((firstStudent, secondStudent) => Number(firstStudent.married)
-            - Number(secondStudent.married));
-      }
-
-      if (order === 'desc') {
-        output
-          .sort((firstStudent, secondStudent) => Number(secondStudent.married)
-            - Number(firstStudent.married));
-      }
+      studentsCopy.sort((firstStudent, secondStudent) => (isAscending
+        ? Number(firstStudent.married) - Number(secondStudent.married)
+        : Number(secondStudent.married) - Number(firstStudent.married)
+      ));
       break;
 
     case SortType.AverageGrade:
-      if (order === 'asc') {
-        output.sort((firstStudent, secondStudent) => averageGrade(firstStudent)
-          - averageGrade(secondStudent));
-      }
-
-      if (order === 'desc') {
-        output.sort((firstStudent, secondStudent) => averageGrade(secondStudent)
-          - averageGrade(firstStudent));
-      }
+      studentsCopy.sort((firstStudent, secondStudent) => (isAscending
+        ? getAverageGrade(firstStudent) - getAverageGrade(secondStudent)
+        : getAverageGrade(secondStudent) - getAverageGrade(firstStudent)
+      ));
       break;
 
     default:
   }
 
-  return output;
+  return studentsCopy;
 }
