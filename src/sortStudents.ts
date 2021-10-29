f64f3ac51e0bf1e67695b1696a5fa61b8d5c7f75
Irka Shemchuk <ilysbbk89@yahoo.com>
2021-10-29 13:30:15 +0300

    fixed

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -23,45 +23,42 @@ export function sortStudents(
   order: SortOrder,
 ): Student[] {
   const studentsCopy = [...students];
-  const ascOrder = (order === 'asc');
+  const ascOrder = order === 'asc';
 
-  if (sortBy === SortType.Name) {
-    return ascOrder
-      ? [...students].sort((a, b) => a.name.localeCompare(b.name))
-      : [...students].sort((a, b) => a.name.localeCompare(b.name)).reverse();
+  switch (sortBy) {
+    case SortType.Name:
+      return ascOrder
+        ? [...students].sort((a, b) => a.name.localeCompare(b.name))
+        : [...students].sort((a, b) => b.name.localeCompare(a.name));
+
+    case SortType.Surname:
+      return ascOrder
+        ? [...students]
+          .sort((a, b) => a.surname.localeCompare(b.surname))
+        : [...students]
+          .sort((a, b) => b.surname.localeCompare(a.surname));
+
+    case SortType.Age:
+      return ascOrder
+        ? [...students].sort((a, b) => a.age - b.age)
+        : [...students].sort((a, b) => b.age - a.age);
+
+    case SortType.Married:
+      return ascOrder
+        ? [...students].sort((a, b) => +a.married - +b.married)
+        : [...students].sort((a, b) => +b.married - +a.married);
+
+    case SortType.AverageGrade:
+      return !ascOrder
+        ? [...students]
+          .sort((a, b) => b.grades
+            .reduce((sum, x) => sum + x) / b.grades.length - a.grades
+            .reduce((sum, x) => sum + x) / a.grades.length)
+        : [...students]
+          .sort((a, b) => a.grades
+            .reduce((sum, x) => sum + x) / a.grades.length - b.grades
+            .reduce((sum, x) => sum + x) / b.grades.length);
+    default:
+      return studentsCopy;
   }
-
-  if (sortBy === SortType.Surname) {
-    return ascOrder
-      ? [...students]
-        .sort((a, b) => a.surname.localeCompare(b.surname))
-      : [...students]
-        .sort((a, b) => a.surname.localeCompare(b.surname)).reverse();
-  }
-
-  if (sortBy === SortType.Age) {
-    return ascOrder
-      ? [...students].sort((a, b) => a.age - b.age)
-      : [...students].sort((a, b) => b.age - a.age);
-  }
-
-  if (sortBy === SortType.Married) {
-    return ascOrder
-      ? [...students].sort((a, b) => +a.married - +b.married)
-      : [...students].sort((a, b) => +b.married - +a.married);
-  }
-
-  if (sortBy === SortType.AverageGrade) {
-    return !ascOrder
-      ? [...students]
-        .sort((a, b) => b.grades
-          .reduce((sum, x) => sum + x) / b.grades.length - a.grades
-          .reduce((sum, x) => sum + x) / a.grades.length)
-      : [...students]
-        .sort((a, b) => a.grades
-          .reduce((sum, x) => sum + x) / a.grades.length - b.grades
-          .reduce((sum, x) => sum + x) / b.grades.length);
-  }
-
-  return studentsCopy;
 }
