b486f5de48b3427985d6f7c116d220e21b5cad9b
Svyatoslav Victor <victorsvyatoslav@gmail.com>
2021-10-31 10:50:32 +0200

    Task Done

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,76 @@
-
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'averageGrade',
 }
 
-// create SortOrder type
-export type SortOrder;
+function average(args: number[]): number {
+  return args.reduce((previous, current) => previous + current) / args.length;
+}
 
+export type SortOrder = 'asc' | 'desc';
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const result = [...students];
+
+  switch (sortBy) {
+    case 'name':
+      if (order === 'asc') {
+        result.sort((a, b) => a.name.localeCompare(b.name));
+      } else {
+        result.sort((a, b) => b.name.localeCompare(a.name));
+      }
+      break;
+
+    case 'surname':
+      if (order === 'asc') {
+        result.sort((a, b) => a.surname.localeCompare(b.surname));
+      } else {
+        result.sort((a, b) => b.surname.localeCompare(a.surname));
+      }
+      break;
+
+    case 'age':
+      if (order === 'asc') {
+        result.sort((a, b) => a.age - b.age);
+      } else {
+        result.sort((a, b) => b.age - a.age);
+      }
+      break;
+
+    case 'married':
+      if (order === 'asc') {
+        result.sort((a, b) => Number(a.married) - Number(b.married));
+      } else {
+        result.sort((a, b) => Number(b.married) - Number(a.married));
+      }
+      break;
+
+    case 'averageGrade':
+      if (order === 'asc') {
+        result.sort((a, b) => average(a.grades) - average(b.grades));
+      } else {
+        result.sort((a, b) => average(b.grades) - average(a.grades));
+      }
+      break;
+
+    default:
+      return [];
+  }
+
+  return result;
 }
