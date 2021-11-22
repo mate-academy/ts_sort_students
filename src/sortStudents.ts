ca904b374a7f561482e5e0b9c9c765500908f82f
Serhii Dvorets <dvorets.sergey@gmail.com>
2021-11-22 13:16:27 +0200

    Solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -37,60 +37,37 @@ export function sortStudents(students: Student[],
   const sortedStudents: Student[] = [...students];
   let result: Student[] = [];
 
-  if (order === 'asc') {
-    switch (sortBy) {
-      case SortType.Name:
-        result = sortedStudents.sort((a: Student,
-          b: Student) => a.name.localeCompare(b.name));
-        break;
-      case SortType.Surname:
-        result = sortedStudents.sort((a: Student,
-          b: Student) => a.surname.localeCompare(b.surname));
-        break;
-      case SortType.Age:
-        result = sortedStudents.sort((a: Student,
-          b: Student) => a.age - b.age);
-        break;
-      case SortType.Married:
-        result = sortedStudents.sort((a: Student,
-          b: Student) => isMarried(a.married) - isMarried(b.married));
-        break;
-      case SortType.AverageGrade:
-        result = sortedStudents.sort((a: Student,
-          b: Student) => average(a.grades) - average(b.grades));
-        break;
-      default:
-        result = sortedStudents.sort((a: Student,
-          b: Student) => a.name.localeCompare(b.name));
-    }
-  }
-
-  if (order === 'desc') {
-    switch (sortBy) {
-      case SortType.Name:
-        result = sortedStudents.sort((a: Student,
-          b: Student) => b.name.localeCompare(a.name));
-        break;
-      case SortType.Surname:
-        result = sortedStudents.sort((a: Student,
-          b: Student) => b.surname.localeCompare(a.surname));
-        break;
-      case SortType.Age:
-        result = sortedStudents.sort((a: Student,
-          b: Student) => b.age - a.age);
-        break;
-      case SortType.Married:
-        result = sortedStudents.sort((a: Student,
-          b: Student) => isMarried(b.married) - isMarried(a.married));
-        break;
-      case SortType.AverageGrade:
-        result = sortedStudents.sort((a: Student,
-          b: Student) => average(b.grades) - average(a.grades));
-        break;
-      default:
-        result = sortedStudents.sort((a: Student,
-          b: Student) => a.name.localeCompare(b.name));
-    }
+  switch (sortBy) {
+    case SortType.Name:
+      result = sortedStudents.sort((a: Student,
+        b: Student) => (order === 'asc') ? a.name.localeCompare(b.name)
+        : b.name.localeCompare(a.name));
+      break;
+    case SortType.Surname:
+      result = sortedStudents.sort((a: Student,
+        b: Student) => (order === 'asc') ? a.surname.localeCompare(b.surname)
+        : b.surname.localeCompare(a.surname));
+      break;
+    case SortType.Age:
+      result = sortedStudents.sort((a: Student,
+        b: Student) => (order === 'asc') ? a.age - b.age
+        : b.age - a.age);
+      break;
+    case SortType.Married:
+      result = sortedStudents.sort((a: Student,
+        b: Student) => (order === 'asc')
+        ? isMarried(a.married) - isMarried(b.married)
+        : isMarried(b.married) - isMarried(a.married));
+      break;
+    case SortType.AverageGrade:
+      result = sortedStudents.sort((a: Student,
+        b: Student) => (order === 'asc') ? average(a.grades) - average(b.grades)
+        : average(b.grades) - average(a.grades));
+      break;
+    default:
+      result = sortedStudents.sort((a: Student,
+        b: Student) => (order === 'asc') ? a.name.localeCompare(b.name)
+        : b.name.localeCompare(a.name));
   }
 
   return result;
