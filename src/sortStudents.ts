063fe8278e58c31dfed6206dceafac552fabfd6c
Yevhen Khmelnytskyi <summerssscrystal@gmail.com>
2021-11-23 16:56:07 +0200

    solution was added

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,65 @@
-
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
-export enum SortType {
-  // describe SortType enum
+export enum SortType{
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades'
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+function getAverageGrade(students: Student): number {
+  const sumOfGrades = students.grades.reduce((sum, grade) => sum + grade);
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  return sumOfGrades / students.grades.length;
+}
+
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const studentsCopy: Student[] = [...students];
+
+  if ((sortBy === SortType.Name || sortBy === SortType.Surname)) {
+    if (order === 'asc') {
+      studentsCopy.sort((s1, s2) => s1[sortBy].localeCompare(s2[sortBy]));
+    } else {
+      studentsCopy.sort((s1, s2) => s2[sortBy].localeCompare(s1[sortBy]));
+    }
+  }
+
+  if (sortBy === SortType.Age) {
+    if (order === 'asc') {
+      studentsCopy.sort((s1, s2) => s1[sortBy] - s2[sortBy]);
+    } else {
+      studentsCopy.sort((s1, s2) => s2[sortBy] - s1[sortBy]);
+    }
+  }
+
+  if (sortBy === SortType.Married) {
+    if (order === 'asc') {
+      studentsCopy.sort((s1, s2) => +s1[sortBy] - +s2[sortBy]);
+    } else {
+      studentsCopy.sort((s1, s2) => +s2[sortBy] - +s1[sortBy]);
+    }
+  }
+
+  if (sortBy === SortType.AverageGrade) {
+    if (order === 'asc') {
+      studentsCopy.sort((s1, s2) => getAverageGrade(s1) - getAverageGrade(s2));
+    } else {
+      studentsCopy.sort((s1, s2) => getAverageGrade(s2) - getAverageGrade(s1));
+    }
+  }
+
+  return studentsCopy;
 }
