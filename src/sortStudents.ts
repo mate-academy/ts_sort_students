8ea481a4e32e2d7c3546a8ca54a10c4ba7145777
Vitalii Kononets <vitalii.kononets.85@gmail.com>
2021-11-25 18:50:43 +0200

    Solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,73 @@
 
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[],
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
 // create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const cloneStudents: Student[] = [...students];
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  switch (sortBy) {
+    case SortType.Name:
+    case SortType.Surname:
+      cloneStudents.sort((a: Student, b: Student) => {
+        if (order === 'asc') {
+          return a[sortBy].localeCompare(b[sortBy]);
+        }
+
+        return b[sortBy].localeCompare(a[sortBy]);
+      });
+      break;
+    case SortType.Age:
+    case SortType.Married:
+      cloneStudents.sort((a: Student, b: Student) => {
+        if (order === 'asc') {
+          return +a[sortBy] - +b[sortBy];
+        }
+
+        return +b[sortBy] - +a[sortBy];
+      });
+      break;
+    case SortType.AverageGrade:
+      cloneStudents.sort((a: Student, b: Student) => {
+        if (order === 'asc') {
+          return (
+            a[sortBy].reduce((sum, current) => sum + current, 0)
+            / a[sortBy].length
+            - b[sortBy].reduce((sum, current) => sum + current, 0)
+            / b[sortBy].length
+          );
+        }
+
+        return (
+          b[sortBy].reduce((sum, current) => sum + current, 0)
+          / b[sortBy].length
+          - a[sortBy].reduce((sum, current) => sum + current, 0)
+          / a[sortBy].length
+        );
+      });
+      break;
+    default:
+      break;
+  }
+
+  return cloneStudents;
 }
