24ed9e1e00ec82ffa7735ffdedb1ff6c21db7ae8
Alex Kostomarov <kostomarov.aleks@gmail.com>
2021-11-27 17:17:17 +0200

    Solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,72 @@
 
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
+}
+
+function getAverageGrade(student: Student): number {
+  return (student.grades.reduce((a, b) => a + b) / student.grades.length);
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const sortedStudents = [...students];
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  sortedStudents.sort((prevStudent: Student, currentStudent: Student) => {
+    switch (sortBy) {
+      case SortType.Name:
+      case SortType.Surname:
+        if (order === 'asc') {
+          return prevStudent[sortBy].localeCompare(currentStudent[sortBy]);
+        }
+
+        return currentStudent[sortBy].localeCompare(prevStudent[sortBy]);
+
+      case SortType.Age:
+        if (order === 'asc') {
+          return prevStudent[sortBy] - currentStudent[sortBy];
+        }
+
+        return currentStudent[sortBy] - prevStudent[sortBy];
+
+      case SortType.Married:
+        if (prevStudent[sortBy] && !currentStudent[sortBy]) {
+          return order === 'asc' ? 1 : -1;
+        }
+
+        if (!prevStudent[sortBy] && currentStudent[sortBy]) {
+          return order === 'asc' ? -1 : 1;
+        }
+
+        return 0;
+
+      case SortType.AverageGrade:
+        if (order === 'asc') {
+          return getAverageGrade(prevStudent) - getAverageGrade(currentStudent);
+        }
+
+        return getAverageGrade(currentStudent) - getAverageGrade(prevStudent);
+
+      default:
+        return 0;
+    }
+  });
+
+  return sortedStudents;
 }
