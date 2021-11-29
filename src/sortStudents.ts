20a955728d38472a5b4515e7504ad4c9241e55d7
Andriy Bulbuk <bulbuk.andriy@gmail.com>
2021-11-29 20:15:08 +0200

    Solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,70 @@
 
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[],
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name,
+  Surname,
+  Age,
+  Married,
+  AverageGrade,
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const sortedArray: Student[] = [...students];
+  const orderIsAsc: boolean = order === 'asc';
+  const reducer = (prev: number, cur: number): number => prev + cur;
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  switch (sortBy) {
+    case SortType.Name:
+      sortedArray.sort((prev, cur) => (orderIsAsc
+        ? (prev.name).localeCompare(cur.name)
+        : (cur.name).localeCompare(prev.name)));
+      break;
+
+    case SortType.Surname:
+      sortedArray.sort((prev, cur) => (orderIsAsc
+        ? (prev.surname).localeCompare(cur.surname)
+        : (cur.surname).localeCompare(prev.surname)));
+      break;
+
+    case SortType.Age:
+      sortedArray.sort((prev, cur) => (orderIsAsc
+        ? prev.age - cur.age
+        : cur.age - prev.age));
+      break;
+
+    case SortType.Married:
+      sortedArray.sort((prev, cur) => (orderIsAsc
+        ? (+prev.married - +cur.married)
+        : (+cur.married - +prev.married)));
+      break;
+
+    case SortType.AverageGrade:
+      sortedArray.sort((prev, cur) => {
+        const prevAverGr = prev.grades.reduce(reducer, 0) / prev.grades.length;
+        const curAverGr = cur.grades.reduce(reducer, 0) / cur.grades.length;
+
+        return orderIsAsc
+          ? prevAverGr - curAverGr
+          : curAverGr - prevAverGr;
+      });
+      break;
+
+    default:
+      throw new Error('I don\'t understand you!');
+  }
+
+  return sortedArray;
 }
