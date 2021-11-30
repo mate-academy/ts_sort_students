6103a265e5e9dbf8ed392da72a291d96d87f766a
Ihor Poliakov <48531156+junglq@users.noreply.github.com>
2021-11-30 11:46:39 +0200

    thx

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -14,10 +14,7 @@ export enum SortType {
   AverageGrade = 'averageGrade',
 }
 
-export enum SortOrder {
-  ascending = 'asc',
-  descending = 'desc',
-}
+export type SortOrder = 'asc' | 'desc';
 
 export function sortStudents(
   students: Student[],
@@ -33,47 +30,29 @@ export function sortStudents(
   switch (sortBy) {
     case SortType.Name:
     case SortType.Surname:
-      newArrOfStudents.sort((s1, s2) => {
-        if (order === SortOrder.ascending) {
-          return s1[sortBy].localeCompare(s2[sortBy]);
-        }
-
-        return s2[sortBy].localeCompare(s1[sortBy]);
-      });
+      newArrOfStudents.sort((prevStudent, nextStudent) => (order === 'asc'
+        ? prevStudent[sortBy].localeCompare(nextStudent[sortBy])
+        : nextStudent[sortBy].localeCompare(prevStudent[sortBy])
+      ));
       break;
 
     case SortType.Age:
-      newArrOfStudents.sort((s1, s2) => {
-        if (order === SortOrder.ascending) {
-          return s1[sortBy] - s2[sortBy];
-        }
-
-        return s2[sortBy] - s1[sortBy];
-      });
-      break;
-
     case SortType.Married:
-      newArrOfStudents.sort((s1, s2) => {
-        if (order === SortOrder.ascending) {
-          return +s1[sortBy] - +(s2[sortBy]);
-        }
-
-        return +s2[sortBy] - +(s1[sortBy]);
-      });
+      newArrOfStudents.sort((prevStudent, nextStudent) => (order === 'asc'
+        ? +prevStudent[sortBy] - +nextStudent[sortBy]
+        : +nextStudent[sortBy] - +prevStudent[sortBy]
+      ));
       break;
 
     case SortType.AverageGrade:
-      newArrOfStudents.sort((s1, s2) => {
-        if (order === SortOrder.ascending) {
-          return gradeAverage(s1.grades) - gradeAverage(s2.grades);
-        }
-
-        return gradeAverage(s2.grades) - gradeAverage(s1.grades);
-      });
+      newArrOfStudents.sort((prevStudent, nextStudent) => (order === 'asc'
+        ? gradeAverage(prevStudent.grades) - gradeAverage(nextStudent.grades)
+        : gradeAverage(nextStudent.grades) - gradeAverage(prevStudent.grades)
+      ));
       break;
 
     default:
-      break;
+      return newArrOfStudents;
   }
 
   return newArrOfStudents;
