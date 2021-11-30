4a60a91757d393d8dc9651a8d42f0c17f2dbf81e
TatianaMorozova04 <morozovatatiana04@gmail.com>
2021-11-30 14:45:18 +0200

    'Sort Students'

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,61 @@
-
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[],
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+const getAverageGrade = (arr: number[]): number => arr.reduce(
+  (a, b) => a + b,
+) / arr.length;
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+export function sortStudents(
+  students: Student[], sortBy: SortType, order: SortOrder,
+): Student[] {
+  const newStudentsArr = [...students];
+
+  switch (sortBy) {
+    case SortType.Name:
+    case SortType.Surname:
+      return newStudentsArr.sort((a, b) => (order === 'asc'
+        ? a[sortBy].localeCompare(b[sortBy])
+        : b[sortBy].localeCompare(a[sortBy])));
+
+    case SortType.Age:
+      return newStudentsArr.sort((a, b) => (order === 'asc'
+        ? a[sortBy] - b[sortBy]
+        : b[sortBy] - a[sortBy]));
+
+    case SortType.Married:
+      return newStudentsArr.sort((a, b) => {
+        const valueA = a[sortBy] ? 0 : 1;
+        const valueB = b[sortBy] ? 0 : 1;
+
+        return valueA - valueB;
+      });
+
+    case SortType.AverageGrade:
+      return newStudentsArr.sort((a, b) => {
+        const valueA = getAverageGrade(a[sortBy]);
+        const valueB = getAverageGrade(b[sortBy]);
+
+        return order === 'asc'
+          ? valueA - valueB
+          : valueB - valueA;
+      });
+
+    default:
+      return newStudentsArr;
+  }
 }
