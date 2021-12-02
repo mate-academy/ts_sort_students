12ba3f9e57c0f18d81095229a138ba0e5fcb4fc5
Vladyslav.Sak <vlsak@deloitte.com>
2021-12-02 18:33:06 +0200

    Created solution for sorting students

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,49 @@
-
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades'
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+function averageGrade(grades: number[]): number {
+  return grades.reduce((p, c) => p + c, 0) / grades.length;
+}
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const copyStudents: Student[] = [...students];
+
+  return copyStudents.sort((studentA: Student, studentB: Student) => {
+    switch (sortBy) {
+      case SortType.Name:
+      case SortType.Surname:
+        return order === 'asc'
+          ? studentA[sortBy].localeCompare(studentB[sortBy])
+          : studentB[sortBy].localeCompare(studentA[sortBy]);
+
+      case SortType.AverageGrade:
+        return order === 'asc'
+          ? averageGrade(studentA[sortBy]) - averageGrade(studentB[sortBy])
+          : averageGrade(studentB[sortBy]) - averageGrade(studentA[sortBy]);
+
+      default:
+        return order === 'asc'
+          ? +studentA[sortBy] - +studentB[sortBy]
+          : +studentB[sortBy] - +studentA[sortBy];
+    }
+  });
 }
