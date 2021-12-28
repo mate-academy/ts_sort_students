43506b4dc2a0a029fad73096236a62421e2dec34
KHTRE <KHTREE@gmail.com>
2021-12-28 18:32:19 +0200

    works, but tests are not passed

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,79 @@
 
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[],
 }
-
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(students: Student[],
+  sortBy: SortType, order: SortOrder): Student[] {
+  let studentsCopy: Student[] = [];
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  studentsCopy = students.map((a) => ({ ...a }));
+
+  switch (sortBy) {
+    case SortType.Name:
+    case SortType.Surname:
+      if (order === 'asc') {
+        studentsCopy.sort((a: Student,
+          b: Student) => (a[sortBy].localeCompare(b[sortBy])));
+      } else {
+        studentsCopy.sort((a: Student,
+          b: Student) => (b[sortBy].localeCompare(a[sortBy])));
+      }
+      break;
+
+    case SortType.Age:
+      if (order === 'asc') {
+        studentsCopy.sort((a: Student, b: Student) => (a[sortBy] - b[sortBy]));
+      } else {
+        studentsCopy.sort((a: Student, b: Student) => (b[sortBy] - a[sortBy]));
+      }
+      break;
+
+    case SortType.Married:
+      if (order === 'asc') {
+        studentsCopy.sort((a: Student,
+          b: Student) => (+a[sortBy] - +b[sortBy]));
+      } else {
+        studentsCopy.sort((a: Student,
+          b: Student) => (+b[sortBy] - +a[sortBy]));
+      }
+
+      break;
+
+    case SortType.AverageGrade:
+      studentsCopy.sort((first: Student, second: Student) => {
+        let gradesFirst = first[sortBy].reduce((prev, a) => (prev + a));
+
+        gradesFirst /= first[sortBy].length;
+
+        let gradesSecond = second[sortBy].reduce((prev, a) => (prev + a));
+
+        gradesSecond /= second[sortBy].length;
+
+        if (order === 'asc') {
+          return gradesFirst - gradesSecond;
+        }
+
+        return gradesSecond - gradesFirst;
+      });
+      break;
+
+    default:
+      throw new Error('error');
+  }
+
+  return studentsCopy;
 }
