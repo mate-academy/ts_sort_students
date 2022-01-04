6ac38b43cd717263434b0c8af90d224483cc4088
levix2l <alexleviz13@gmail.com>
2022-01-04 21:53:53 +0200

    sort students solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,82 @@
-
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'average'
 }
 
 // create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const studentsCopy: Student[] = [...students];
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  function average(grades: number[]) : number {
+    return grades.reduce((num1, num2) => num1 + num2, 0) / grades.length;
+  }
+
+  switch (sortBy) {
+    case (SortType.Name):
+      studentsCopy.sort((prev, curr) => {
+        return order === 'asc'
+          ? prev.name.localeCompare(curr.name)
+          : curr.name.localeCompare(prev.name);
+      });
+
+      break;
+
+    case (SortType.Surname):
+      studentsCopy.sort((prev, curr) => {
+        return order === 'asc'
+          ? prev.surname.localeCompare(curr.surname)
+          : curr.surname.localeCompare(prev.surname);
+      });
+
+      break;
+
+    case (SortType.Age):
+      studentsCopy.sort((prev, curr) => {
+        return order === 'asc'
+          ? prev.age - curr.age
+          : curr.age - prev.age;
+      });
+
+      break;
+
+    case (SortType.Married):
+      studentsCopy.sort((prev, curr) => {
+        return order === 'asc'
+          ? +prev.married - +curr.married
+          : +curr.married - +prev.married;
+      });
+
+      break;
+
+    case (SortType.AverageGrade):
+      studentsCopy.sort((prev, curr) => {
+        return order === 'asc'
+          ? average(prev.grades) - average(curr.grades)
+          : average(curr.grades) - average(prev.grades);
+      });
+
+      break;
+
+    default:
+      return studentsCopy;
+  }
+
+  return studentsCopy;
 }
