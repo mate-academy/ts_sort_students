17e4cf8ed84a96d67456867ed3da6d28e20260a0
Nataliia Hrynyk <nataliiahrynyk2001@gmail.com>
2022-01-05 00:08:42 +0200

    Solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,91 @@
 
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
 // create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+function sortStrings(
+  sortBy: 'name' | 'surname',
+  order: SortOrder,
+): (a: Student, b: Student) => number {
+  switch (order) {
+    case 'asc':
+      return (a: Student, b: Student): number => a[sortBy]
+        .localeCompare(b[sortBy]);
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+    case 'desc':
+      return (a: Student, b: Student): number => b[sortBy]
+        .localeCompare(a[sortBy]);
+
+    default:
+      throw new Error('Enter valid sort type');
+  }
+}
+
+function toNumber(value: number | boolean | number[]): number {
+  switch (typeof value) {
+    case 'number':
+      return value;
+
+    case 'boolean':
+      return Number(value);
+
+    default:
+      return value
+        .reduce((total, num) => total + num, 0) / value.length;
+  }
+}
+
+function sortNumbers(
+  sortBy: 'age' | 'married' | 'grades',
+  order: SortOrder,
+): (a: Student, b: Student) => number {
+  switch (order) {
+    case 'asc':
+      return (a: Student, b: Student): number => toNumber(a[sortBy])
+        - toNumber(b[sortBy]);
+
+    case 'desc':
+      return (a: Student, b: Student): number => toNumber(b[sortBy])
+        - toNumber(a[sortBy]);
+
+    default:
+      throw new Error('Enter valid sort type');
+  }
+}
+
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const studentsCopy: Student[] = JSON.parse(JSON.stringify(students));
+
+  switch (sortBy) {
+    case SortType.Name:
+    case SortType.Surname:
+      return studentsCopy.sort(sortStrings(sortBy, order));
+
+    case SortType.Age:
+    case SortType.Married:
+    case SortType.AverageGrade:
+      return studentsCopy.sort(sortNumbers(sortBy, order));
+
+    default:
+      throw new Error('Enter valid sort type');
+  }
 }
