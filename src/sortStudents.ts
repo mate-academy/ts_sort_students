e2eeecbae293f2c5e7d883db5686a3134758734c
Samson Sim <samson.sim@ukr.net>
2022-01-05 19:19:38 +0200

    solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,106 @@
 
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[],
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const sortedStudents: Student[] = [...students];
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  if (order === 'asc') {
+    switch (sortBy) {
+      case SortType.Name:
+      case SortType.Surname:
+        sortedStudents.sort((a: Student, b: Student) => (
+          a[sortBy].localeCompare(b[sortBy])));
+
+        break;
+
+      case SortType.Age:
+        sortedStudents.sort((a: Student, b: Student) => (
+          a[sortBy] - b[sortBy]));
+
+        break;
+
+      case SortType.Married:
+        sortedStudents.sort((a: Student, b: Student) => (
+          +a[sortBy] - +b[sortBy]));
+
+        break;
+
+      case SortType.AverageGrade:
+        sortedStudents.sort((a: Student, b: Student) => {
+          const averageGrade1 = a[sortBy].reduce((prev, accum) => prev + accum)
+          / a[sortBy].length;
+
+          const averageGrade2 = b[sortBy].reduce((prev, accum) => prev + accum)
+          / b[sortBy].length;
+
+          return averageGrade1 - averageGrade2;
+        });
+
+        break;
+
+      default:
+        throw new Error('error');
+    }
+  }
+
+  if (order === 'desc') {
+    switch (sortBy) {
+      case SortType.Name:
+      case SortType.Surname:
+        sortedStudents.sort((a: Student, b: Student) => (
+          b[sortBy].localeCompare(a[sortBy])));
+
+        break;
+
+      case SortType.Age:
+        sortedStudents.sort((a: Student, b: Student) => (
+          b[sortBy] - a[sortBy]));
+
+        break;
+
+      case SortType.Married:
+        sortedStudents.sort((a: Student, b: Student) => (
+          +b[sortBy] - +a[sortBy]));
+
+        break;
+
+      case SortType.AverageGrade:
+        sortedStudents.sort((a: Student, b: Student) => {
+          const averageGrade1 = a[sortBy].reduce((prev, accum) => prev + accum)
+          / a[sortBy].length;
+
+          const averageGrade2 = b[sortBy].reduce((prev, accum) => prev + accum)
+          / b[sortBy].length;
+
+          return averageGrade2 - averageGrade1;
+        });
+
+        break;
+
+      default:
+        throw new Error('error');
+    }
+  }
+
+  return sortedStudents;
 }
