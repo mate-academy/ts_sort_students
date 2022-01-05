d55c7f86ae3c9318371921c8a233992dd15d11d8
Vlad Harkusha <sivari888@gmail.com>
2022-01-05 20:19:23 +0200

    solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,57 @@
 
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[],
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+const getAverageMarks = (student: Student): number => student.grades
+  .reduce((sum, nextMark) => sum + nextMark)
+    / student.grades.length;
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const studentsCopy: Student[] = students.slice();
+
+  return studentsCopy.sort(
+    (studentA: Student, studentB: Student) => {
+      switch (sortBy) {
+        case SortType.Name:
+        case SortType.Surname:
+          return (order === 'asc')
+            ? studentA[sortBy].localeCompare(studentB[sortBy])
+            : studentB[sortBy].localeCompare(studentA[sortBy]);
+
+        case SortType.Age:
+        case SortType.Married:
+          return (order === 'asc')
+            ? +studentA[sortBy] - +studentB[sortBy]
+            : +studentB[sortBy] - +studentA[sortBy];
+
+        case SortType.AverageGrade: {
+          return (order === 'asc')
+            ? getAverageMarks(studentA) - getAverageMarks(studentB)
+            : getAverageMarks(studentB) - getAverageMarks(studentA);
+        }
+
+        default:
+          throw new Error('something going wrong');
+      }
+    },
+  );
 }
