97ccdb4e4cd01e4da816ada0974f468e408a2d0e
levix2l <alexleviz13@gmail.com>
2022-01-05 16:02:38 +0200

    fix

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -7,14 +7,13 @@ export interface Student {
 }
 
 export enum SortType {
-  Name = 'name',
-  Surname = 'surname',
-  Age = 'age',
-  Married = 'married',
-  AverageGrade = 'average'
+  Name,
+  Surname,
+  Age,
+  Married,
+  AverageGrade
 }
 
-// create SortOrder type
 export type SortOrder = 'asc' | 'desc';
 
 export function sortStudents(
