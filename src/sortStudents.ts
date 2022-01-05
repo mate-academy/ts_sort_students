be625267b29043f36cfd2ddb9b99acbb14045dd4
Yulia Koshkina <julyasav4uk@gmail.com>
2022-01-05 17:35:16 +0200

    first solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,67 @@
 
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[],
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
-// create SortOrder type
-export type SortOrder;
+export enum SortOrder {
+  Asc = 'asc',
+  Desc = 'desc'
+}
 
+function averageGrade(student: Student): number {
+  return student.grades.reduce((a, b) => a + b) / student.grades.length;
+}
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  let sorted: Student[] = [];
+
+  sorted = students.map((student) => ({ ...student }));
+
+  switch (sortBy) {
+    case SortType.Name:
+    case SortType.Surname:
+      return order === SortOrder.Asc
+        ? sorted.sort((a: Student, b: Student) => a[sortBy]
+          .localeCompare(b[sortBy]))
+        : sorted.sort((a: Student, b: Student) => b[sortBy]
+          .localeCompare(a[sortBy]));
+
+    case SortType.Age:
+      return order === SortOrder.Asc
+        ? sorted.sort((a: Student, b: Student) => a[sortBy] - b[sortBy])
+        : sorted.sort((a: Student, b: Student) => b[sortBy] - a[sortBy]);
+
+    case SortType.Married:
+      return order === SortOrder.Asc
+        ? sorted.sort((a: Student, b: Student) => (
+          +a[sortBy] - +b[sortBy]))
+        : sorted.sort((a: Student, b: Student) => (
+          +b[sortBy] - +a[sortBy]));
+
+    case SortType.AverageGrade:
+      return order === SortOrder.Asc
+        ? sorted.sort((a: Student, b: Student) => (
+          averageGrade(a) - averageGrade(b)))
+        : sorted.sort((a: Student, b: Student) => (
+          averageGrade(b) - averageGrade(a)));
+
+    default:
+      return sorted;
+  }
 }
