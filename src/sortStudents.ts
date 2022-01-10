d5f267a5d407e5d80a591278451b532d38d6a483
Oleksandr Ostrovskyi <o.ostrovskiy62@gmail.com>
2022-01-10 11:41:46 +0200

    students fixed

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -22,63 +22,50 @@ export function sortStudents(students: Student[],
   sortBy: SortType, order: SortOrder): Student[] {
   const copyStudents = students.map((student: Student) => ({ ...student }));
 
-  switch (true) {
-    default:
-      return copyStudents;
+  return copyStudents.sort((student1, student2): number => {
+    const firstStudentMarks = student1.grades.reduce((sum: number,
+      grade: number) => sum + grade, 0) / student1.grades.length;
+    const secondStudentMarks = student2.grades.reduce((sum: number,
+      grade: number) => sum + grade, 0) / student2.grades.length;
 
-    case sortBy === 'name':
-      return copyStudents.sort((student1: Student,
-        student2: Student): number => {
+    switch (sortBy) {
+      default:
+        return 0;
+
+      case SortType.Name:
         if (order === 'asc') {
           return student1.name.localeCompare(student2.name);
         }
 
         return student2.name.localeCompare(student1.name);
-      });
 
-    case sortBy === 'surname':
-      return copyStudents.sort((student1: Student,
-        student2: Student): number => {
+      case SortType.Surname:
         if (order === 'asc') {
           return student1.surname.localeCompare(student2.surname);
         }
 
         return student2.surname.localeCompare(student1.surname);
-      });
 
-    case sortBy === 'age' && order === 'desc':
-      return copyStudents.sort((student1: Student,
-        student2: Student): number => {
+      case SortType.Age:
         if (order === 'desc') {
           return student2.age - student1.age;
         }
 
         return student1.age - student2.age;
-      });
 
-    case sortBy === 'married':
-      return copyStudents.sort((student1: Student,
-        student2: Student): number => {
+      case SortType.Married:
         if (order === 'desc') {
           return student2.married - student1.married;
         }
 
         return student1.married - student2.married;
-      });
 
-    case sortBy === 'avarageGrade':
-      return copyStudents.sort((student1: Student,
-        student2: Student): number => {
-        const firstStudentMarks = student1.grades.reduce((sum: number,
-          grade: number) => sum + grade, 0) / student1.grades.length;
-        const secondStudentMarks = student2.grades.reduce((sum: number,
-          grade: number) => sum + grade, 0) / student2.grades.length;
-
+      case SortType.AverageGrade:
         if (order === 'asc') {
           return firstStudentMarks - secondStudentMarks;
         }
 
         return secondStudentMarks - firstStudentMarks;
-      });
-  }
+    }
+  });
 }
