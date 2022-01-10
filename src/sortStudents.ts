c1fc38f7c3348560a722c02925e054466b56c776
SavelovSerhii <savelov12111@gmail.com>
2022-01-10 12:21:03 +0200

    First version

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,92 @@
 
 export interface Student {
-  // describe Student interface
+  name: string
+  surname: string
+  age: number
+  married: boolean
+  grades: number[]
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name,
+  Surname,
+  Age,
+  Married,
+  AverageGrade,
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  return [...students].sort((studentOne, studentTwo) => {
+    let firstAverage: number;
+    let secondAverage: number;
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+    switch (order) {
+      case 'asc':
+        switch (sortBy) {
+          case SortType.Name:
+            return studentOne.name.localeCompare(studentTwo.name);
+
+          case SortType.Surname:
+            return studentOne.surname.localeCompare(studentTwo.surname);
+
+          case SortType.Age:
+            return studentOne.age - studentTwo.age;
+
+          case SortType.Married:
+            return +studentOne.married - +studentTwo.married;
+
+          case SortType.AverageGrade:
+            firstAverage = studentOne.grades.reduce((a, b) => {
+              return a + b;
+            }) / studentOne.grades.length;
+
+            secondAverage = studentTwo.grades.reduce((a, b) => {
+              return a + b;
+            }) / studentTwo.grades.length;
+
+            return firstAverage - secondAverage;
+
+          default:
+            return 0;
+        }
+
+      case 'desc':
+        switch (sortBy) {
+          case SortType.Name:
+            return studentTwo.name.localeCompare(studentOne.name);
+
+          case SortType.Surname:
+            return studentTwo.surname.localeCompare(studentOne.surname);
+
+          case SortType.Age:
+            return studentTwo.age - studentOne.age;
+
+          case SortType.Married:
+            return +studentTwo.married - +studentOne.married;
+
+          case SortType.AverageGrade:
+            firstAverage = studentOne.grades.reduce((a, b) => {
+              return a + b;
+            }) / studentOne.grades.length;
+
+            secondAverage = studentTwo.grades.reduce((a, b) => {
+              return a + b;
+            }) / studentTwo.grades.length;
+
+            return secondAverage - firstAverage;
+
+          default:
+            return 0;
+        }
+
+      default:
+        return 0;
+    }
+  });
 }
