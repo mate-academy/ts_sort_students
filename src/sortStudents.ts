9569cc05f51ef60ef447de6e96f07e6675b339d8
Yevhenii Rozdorozhnii <rozdorozhnii@ukr.net>
2022-01-10 18:08:59 +0200

    solution_v1.0

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,62 @@
 
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name,
+  Surname,
+  Age,
+  Married,
+  AverageGrade,
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder = 'asc',
+): Student[] {
+  const copyStudents = [...students];
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  return copyStudents.sort((a, b) => {
+    switch (sortBy) {
+      case SortType.Name:
+        return (order === 'asc')
+          ? a.name.localeCompare(b.name)
+          : b.name.localeCompare(a.name);
+
+      case SortType.Surname:
+        return (order === 'asc')
+          ? a.surname.localeCompare(b.surname)
+          : b.surname.localeCompare(a.surname);
+
+      case SortType.Age:
+        return (order === 'asc')
+          ? a.age - b.age
+          : b.age - a.age;
+
+      case SortType.Married:
+        return (order === 'asc')
+          ? +a.married - +b.married
+          : +b.married - +a.married;
+
+      case SortType.AverageGrade:
+        return (order === 'asc')
+          ? a.grades.reduce((acum, grade) => acum + grade, 0) / a.grades.length
+            - b.grades.reduce((acum, grade) => acum + grade, 0)
+              / b.grades.length
+          : b.grades.reduce((acum, grade) => acum + grade, 0) / b.grades.length
+            - a.grades.reduce((acum, grade) => acum + grade, 0)
+              / a.grades.length;
+
+      default:
+        return 0;
+    }
+  });
 }
