29acfbb22121e6a025db4e69bc94e80041e7759e
Artem <tema.maksimovich@gmail.com>
2022-01-10 13:16:24 +0200

    Task is done

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,80 @@
-
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[],
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name,
+  Surname,
+  Age,
+  Married,
+  AverageGrade,
 }
 
 // create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const copyStudents = [...students];
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  copyStudents.sort((a, b) => {
+    if (order === 'asc') {
+      switch (sortBy) {
+        case SortType.Name:
+          return a.name.localeCompare(b.name);
+        case SortType.Surname:
+          return a.surname.localeCompare(b.surname);
+        case SortType.Age:
+          return a.age - b.age;
+        case SortType.Married:
+          return +a.married - +b.married;
+
+        case SortType.AverageGrade: {
+          const firstNum = a.grades.reduce((prev, next) => {
+            return prev + next;
+          }, 0) / a.grades.length;
+          const secondNum = b.grades.reduce((prev, next) => {
+            return prev + next;
+          }, 0) / b.grades.length;
+
+          return firstNum - secondNum;
+        }
+        default:
+          return 0;
+      }
+    } else {
+      switch (sortBy) {
+        case SortType.Name:
+          return b.name.localeCompare(a.name);
+        case SortType.Surname:
+          return b.surname.localeCompare(a.surname);
+        case SortType.Age:
+          return b.age - a.age;
+        case SortType.Married:
+          return +b.married - +a.married;
+
+        case SortType.AverageGrade: {
+          const firstNum = a.grades.reduce((prev, next) => {
+            return prev + next;
+          }, 0) / a.grades.length;
+          const secondNum = b.grades.reduce((prev, next) => {
+            return prev + next;
+          }, 0) / b.grades.length;
+
+          return secondNum - firstNum;
+        }
+        default:
+          return 0;
+      }
+    }
+  });
+
+  return copyStudents;
 }
