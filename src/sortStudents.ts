7e7327665845666f03fb33527ac75c5adc51f3c4
Oleksandr Ostrovskyi <o.ostrovskiy62@gmail.com>
2022-01-10 14:08:11 +0200

    optimized

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -29,43 +29,33 @@ export function sortStudents(students: Student[],
       grade: number) => sum + grade, 0) / student2.grades.length;
 
     switch (sortBy) {
-      default:
-        return 0;
-
       case SortType.Name:
-        if (order === 'asc') {
-          return student1.name.localeCompare(student2.name);
-        }
-
-        return student2.name.localeCompare(student1.name);
+        return order === 'asc'
+          ? student1.name.localeCompare(student2.name)
+          : student2.name.localeCompare(student1.name);
 
       case SortType.Surname:
-        if (order === 'asc') {
-          return student1.surname.localeCompare(student2.surname);
-        }
-
-        return student2.surname.localeCompare(student1.surname);
+        return order === 'asc'
+          ? student1.surname.localeCompare(student2.surname)
+          : student2.surname.localeCompare(student1.surname);
 
       case SortType.Age:
-        if (order === 'desc') {
-          return student2.age - student1.age;
-        }
-
-        return student1.age - student2.age;
+        return order === 'desc'
+          ? student2.age - student1.age
+          : student1.age - student2.age;
 
       case SortType.Married:
-        if (order === 'desc') {
-          return student2.married - student1.married;
-        }
-
-        return student1.married - student2.married;
+        return order === 'desc'
+          ? student2.married - student1.married
+          : student1.married - student2.married;
 
       case SortType.AverageGrade:
-        if (order === 'asc') {
-          return firstStudentMarks - secondStudentMarks;
-        }
+        return order === 'asc'
+          ? firstStudentMarks - secondStudentMarks
+          : secondStudentMarks - firstStudentMarks;
 
-        return secondStudentMarks - firstStudentMarks;
+      default:
+        return 0;
     }
   });
 }
