627559efc19615af74794e8c6faeb562d46317f0
Mykola Koval <zavul0n.ua@gmail.com>
2022-01-10 13:40:04 +0200

    switch case rewrite and other improvements

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -13,7 +13,6 @@ export enum SortType {
   Age = 'age',
   Married = 'married',
   AverageGrade = 'averageGrade',
-  // (grades.reduce((a,b) => a + b, 0) / grades.length)
 }
 
 export type SortOrder = 'asc' | 'desc';
@@ -22,39 +21,33 @@ export function sortStudents(
   students: Student[],
   sortBy: SortType,
   order: SortOrder,
-): object {
+): Student[] {
   const studentsCopy = JSON.parse(JSON.stringify(students));
 
-  if (sortBy === 'averageGrade') {
-    return studentsCopy.sort((el1: Student, el2: Student) => {
-      const st1 = (el1.grades.reduce((a, b) => a + b, 0)) / el1.grades.length;
-      const st2 = (el2.grades.reduce((a, b) => a + b, 0)) / el2.grades.length;
+  switch (sortBy) {
+    case 'name':
+    case 'surname':
+      return studentsCopy.sort(
+        (el1, el2) => el1[sortBy].localeCompare(el2[sortBy]),
+      );
 
+    case 'married':
+    case 'age':
       if (order === 'desc') {
-        return st2 - st1;
+        return studentsCopy.sort((el1, el2) => el2[sortBy] - el1[sortBy]);
       }
 
-      return st1 - st2;
-    });
+      return studentsCopy.sort((el1, el2) => el1[sortBy] - el2[sortBy]);
+
+    case 'averageGrade':
+      return studentsCopy.sort((el1: Student, el2: Student) => {
+        const st1 = (el1.grades.reduce((a, b) => a + b, 0)) / el1.grades.length;
+        const st2 = (el2.grades.reduce((a, b) => a + b, 0)) / el2.grades.length;
+
+        return (order === 'desc' ? st2 - st1 : st1 - st2);
+      });
+
+    default:
+      return studentsCopy;
   }
-
-  if (sortBy === 'name' || sortBy === 'surname') {
-    return studentsCopy.sort((el1, el2) => {
-      if (el1[sortBy] < el2[sortBy]) {
-        return -1;
-      }
-
-      if (el1[sortBy] > el2[sortBy]) {
-        return 1;
-      }
-
-      return 0;
-    });
-  }
-
-  if (order === 'desc') {
-    return studentsCopy.sort((el1, el2) => el2[sortBy] - el1[sortBy]);
-  }
-
-  return studentsCopy.sort((el1, el2) => el1[sortBy] - el2[sortBy]);
 }
