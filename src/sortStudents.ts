421efa9d404a2ab7182e1e0ea7a4b29732cee0f7
Denys Sierhiievskyi <luckdenny@gmail.comecho>
2022-01-10 15:05:08 +0200

    solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,77 @@
 
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
 // create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(
+  students:Student[],
+  sortBy:SortType,
+  order:SortOrder,
+):Student[] {
+  const sortedStudents = [...students];
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  sortedStudents.sort((a, b) => {
+    if (order === 'asc') {
+      switch (sortBy) {
+        case SortType.Name:
+        case SortType.Surname:
+          return a[sortBy].localeCompare(b[sortBy]);
+        case SortType.Age:
+        case SortType.Married:
+          return +a[sortBy] - +b[sortBy];
+
+        case SortType.AverageGrade: {
+          const AverageGradeA = a.grades.reduce((prev, next) => {
+            return prev + next;
+          }, 0) / a.grades.length;
+          const AverageGradeB = b.grades.reduce((prev, next) => {
+            return prev + next;
+          }, 0) / b.grades.length;
+
+          return AverageGradeA - AverageGradeB;
+        }
+        default:
+          return 0;
+      }
+    } else {
+      switch (sortBy) {
+        case SortType.Name:
+        case SortType.Surname:
+          return b[sortBy].localeCompare(a[sortBy]);
+        case SortType.Age:
+        case SortType.Married:
+          return +b[sortBy] - +a[sortBy];
+
+        case SortType.AverageGrade: {
+          const AverageGradeA = a.grades.reduce((prev, next) => {
+            return prev + next;
+          }, 0) / a.grades.length;
+          const AverageGradeB = b.grades.reduce((prev, next) => {
+            return prev + next;
+          }, 0) / b.grades.length;
+
+          return AverageGradeB - AverageGradeA;
+        }
+        default:
+          return 0;
+      }
+    }
+  });
+
+  return sortedStudents;
 }
