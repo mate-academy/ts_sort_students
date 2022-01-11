75bf78438a8153acc3dee11c48bde92a5cf1c1a3
Ernesto Fernandez <ernesto.fernandez.1980@gmail.com>
2022-01-11 16:34:30 +0200

    solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,68 @@
-
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[],
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const sortedStudent: Student[] = [...students];
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  function averageGrade(studentGrades: number[]): number {
+    if (studentGrades.length === 0) {
+      return 0;
+    }
+
+    return studentGrades.reduce((sum, studentGrade) => sum + studentGrade, 0)
+      / studentGrades.length;
+  }
+
+  switch (sortBy) {
+    case SortType.Name:
+    case SortType.Surname:
+      return sortedStudent.sort((studentA: Student, studentB: Student) => {
+        return order === 'asc'
+          ? studentA[sortBy].localeCompare(studentB[sortBy])
+          : studentB[sortBy].localeCompare(studentA[sortBy]);
+      });
+
+    case SortType.Age:
+      return sortedStudent.sort((studentA: Student, studentB: Student) => {
+        return order === 'asc'
+          ? (studentA.age - studentB.age)
+          : (studentB.age - studentA.age);
+      });
+
+    case SortType.Married:
+      return sortedStudent.sort((studentA: Student, studentB: Student) => {
+        return order === 'asc'
+          ? Number(studentA.married) - Number(studentB.married)
+          : Number(studentB.married) - Number(studentA.married);
+      });
+
+    case SortType.AverageGrade:
+      return sortedStudent.sort((studentA: Student, studentB: Student) => {
+        return order === 'asc'
+          ? averageGrade(studentA.grades) - averageGrade(studentB.grades)
+          : averageGrade(studentB.grades) - averageGrade(studentA.grades);
+      });
+
+    default:
+      return sortedStudent;
+  }
 }
