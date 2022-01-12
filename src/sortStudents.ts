d2f58c5a41f544130e2b44e68ca8988cfe0aef4b
E-Sharpilo <evgeniy.sharpilo@gmail.com>
2022-01-12 10:54:41 +0200

    solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,53 @@
 
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[]
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+function avarage(arr: number[]): number {
+  const sum = arr.reduce((a, b) => a + b);
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  return sum / arr.length;
+}
+
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const resultArr = [...students];
+
+  switch (sortBy) {
+    case SortType.Name:
+    case SortType.Surname:
+      return (order === 'asc')
+        ? resultArr.sort((a, b) => a[sortBy].localeCompare(b[sortBy]))
+        : resultArr.sort((a, b) => b[sortBy].localeCompare(a[sortBy]));
+
+    case SortType.Age:
+    case SortType.Married:
+      return (order === 'asc')
+        ? resultArr.sort((a, b) => +a[sortBy] - +b[sortBy])
+        : resultArr.sort((a, b) => +b[sortBy] - +a[sortBy]);
+
+    case SortType.AverageGrade:
+      return (order === 'asc')
+        ? resultArr.sort((a, b) => avarage(a.grades) - avarage(b.grades))
+        : resultArr.sort((a, b) => avarage(b.grades) - avarage(a.grades));
+    default:
+      return resultArr;
+  }
 }
