103fae3e522697ada289b24e1c264d6b931303ae
Roman Shevchuk <14rom.shevchuk@gmail.com>
2022-01-12 11:45:23 +0200

    s_s fix

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -23,12 +23,12 @@ export function sortStudents(
   sortBy: SortType,
   order: SortOrder,
 ): Array<Student> {
-  const copyStudents = students.map((a: Student) => a);
+  const copyStudents = students.map((student: Student) => student);
 
   switch (sortBy) {
     case SortType.Name:
-      if (order === 'asc') {
-        return copyStudents.sort((a, b) => {
+      return (order === 'asc')
+        ? copyStudents.sort((a, b) => {
           const fa = a.name.toLowerCase();
           const fb = b.name.toLowerCase();
 
@@ -41,27 +41,25 @@ export function sortStudents(
           }
 
           return 0;
+        })
+        : copyStudents.sort((a, b) => {
+          const fa = a.name.toLowerCase();
+          const fb = b.name.toLowerCase();
+
+          if (fa > fb) {
+            return -1;
+          }
+
+          if (fa < fb) {
+            return 1;
+          }
+
+          return 0;
         });
-      }
-
-      return copyStudents.sort((a, b) => {
-        const fa = a.name.toLowerCase();
-        const fb = b.name.toLowerCase();
-
-        if (fa > fb) {
-          return -1;
-        }
-
-        if (fa < fb) {
-          return 1;
-        }
-
-        return 0;
-      });
 
     case SortType.Surname:
-      if (order === 'asc') {
-        return copyStudents.sort((a, b) => {
+      return (order === 'asc')
+        ? copyStudents.sort((a, b) => {
           const fa = a.surname.toLowerCase();
           const fb = b.surname.toLowerCase();
 
@@ -74,66 +72,58 @@ export function sortStudents(
           }
 
           return 0;
+        })
+        : copyStudents.sort((a, b) => {
+          const fa = a.surname.toLowerCase();
+          const fb = b.surname.toLowerCase();
+
+          if (fa > fb) {
+            return -1;
+          }
+
+          if (fa < fb) {
+            return 1;
+          }
+
+          return 0;
         });
-      }
-
-      return copyStudents.sort((a, b) => {
-        const fa = a.surname.toLowerCase();
-        const fb = b.surname.toLowerCase();
-
-        if (fa > fb) {
-          return -1;
-        }
-
-        if (fa < fb) {
-          return 1;
-        }
-
-        return 0;
-      });
 
     case SortType.Age:
-      if (order === 'asc') {
-        return copyStudents.sort((a, b) => {
+      return (order === 'asc')
+        ? copyStudents.sort((a, b) => {
           return a.age - b.age;
+        })
+        : copyStudents.sort((a, b) => {
+          return b.age - a.age;
         });
-      }
-
-      return copyStudents.sort((a, b) => {
-        return b.age - a.age;
-      });
 
     case SortType.Married:
-      if (order === 'asc') {
-        return copyStudents.sort((a, b) => {
+      return (order === 'asc')
+        ? copyStudents.sort((a, b) => {
           return Number(a.married) - Number(b.married);
+        })
+        : copyStudents.sort((a, b) => {
+          return Number(b.married) - Number(a.married);
         });
-      }
-
-      return copyStudents.sort((a, b) => {
-        return Number(b.married) - Number(a.married);
-      });
 
     case SortType.AverageGrade:
-      if (order === 'asc') {
-        return copyStudents.sort((a, b) => {
+      return (order === 'asc')
+        ? copyStudents.sort((a, b) => {
           const aAverage = a.grades.reduce((first, second) => first + second)
             / a.grades.length;
           const bAverage = b.grades.reduce((first, second) => first + second)
             / b.grades.length;
 
           return aAverage - bAverage;
+        })
+        : copyStudents.sort((a, b) => {
+          const aAverage = a.grades.reduce((first, second) => first + second)
+            / a.grades.length;
+          const bAverage = b.grades.reduce((first, second) => first + second)
+            / b.grades.length;
+
+          return bAverage - aAverage;
         });
-      }
-
-      return copyStudents.sort((a, b) => {
-        const aAverage = a.grades.reduce((first, second) => first + second)
-          / a.grades.length;
-        const bAverage = b.grades.reduce((first, second) => first + second)
-          / b.grades.length;
-
-        return bAverage - aAverage;
-      });
 
     default:
       return students;
