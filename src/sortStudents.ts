47bac74cc8ce8a7131cc47f7b8f54f0acfeda267
helmax-y <max_rad@urk.net>
2022-01-21 17:12:21 +0200

    Solution is provided

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,7 +1,54 @@
-// describe Student type
-// create and export SortType enum
-// create SortOrder type
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+export interface Student {
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
+}
+
+export enum SortType {
+  Name,
+  Surname,
+  Age,
+  Married,
+  AverageGrade
+}
+
+export type SortOrder = 'asc' | 'desc';
+
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const calculate = (marks: number[]): number => marks
+    .reduce((sum: number, el: number) => el + sum, 0) / marks.length;
+
+  return [...students].sort((a: Student, b: Student) => {
+    switch (sortBy) {
+      case SortType.Name:
+        return order === 'asc'
+          ? +a.name.localeCompare(b.name)
+          : +!a.name.localeCompare(b.name);
+      case SortType.Surname:
+        return order === 'asc'
+          ? +a.surname.localeCompare(b.surname)
+          : +!a.surname.localeCompare(b.surname);
+      case SortType.Age:
+        return order === 'asc'
+          ? a.age - b.age
+          : b.age - a.age;
+      case SortType.Married:
+        return order === 'asc'
+          ? +a.married - +b.married
+          : +b.married - +a.married;
+      case SortType.AverageGrade:
+        return order === 'asc'
+          ? calculate(a.grades) - calculate(b.grades)
+          : calculate(b.grades) - calculate(a.grades);
+      default:
+        return 0;
+    }
+  });
 }
