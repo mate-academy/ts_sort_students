2abac04fd274797edc34a0fd6c0e234085e43f37
Andriy Fesych <andriyhimself@gmail.com>
2022-01-23 18:52:31 +0200

    Solution_0.1

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,50 @@
 
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(students:Student[],
+  sortBy:SortType, order:SortOrder):Student[] {
+  let sortedArr:Student[] = [...students];
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  sortedArr = sortedArr
+    .sort((el1:Student, el2:Student) => {
+      if (sortBy === 'grades') {
+        return (el1[sortBy]
+          .reduce((a, b) => a + b, 0) / el1[sortBy].length
+        > el2[sortBy]
+          .reduce((a, b) => a + b, 0) / el2[sortBy].length) ? 1 : -1;
+      }
+
+      return (el1[sortBy] > el2[sortBy]) ? 1 : -1;
+    });
+
+  if (order === 'desc') {
+    return sortedArr.reverse();
+  }
+
+  if (sortBy === 'grades') {
+    return students.sort((el1:Student, el2:Student) => {
+      return (el1[sortBy]
+        .reduce((a, b) => a + b, 0) / el1[sortBy].length
+        >= el2[sortBy]
+          .reduce((a, b) => a + b, 0) / el2[sortBy].length) ? 1 : -1;
+    });
+  }
+
+  return sortedArr;
 }
