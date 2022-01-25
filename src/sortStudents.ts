21b856c61a950876f789a4737abaad16b080e894
Andriy Fesych <andriyhimself@gmail.com>
2022-01-25 18:20:05 +0200

    Version_0.2

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -19,32 +19,33 @@ export type SortOrder = 'asc' | 'desc';
 
 export function sortStudents(students:Student[],
   sortBy:SortType, order:SortOrder):Student[] {
-  let sortedArr:Student[] = [...students];
-
-  sortedArr = sortedArr
+  const sortedArr:Student[] = [...students]
     .sort((el1:Student, el2:Student) => {
       if (sortBy === 'grades') {
         return (el1[sortBy]
           .reduce((a, b) => a + b, 0) / el1[sortBy].length
-        > el2[sortBy]
+        >= el2[sortBy] // pay attention to comparison operator
           .reduce((a, b) => a + b, 0) / el2[sortBy].length) ? 1 : -1;
       }
 
       return (el1[sortBy] > el2[sortBy]) ? 1 : -1;
     });
 
-  if (order === 'desc') {
-    return sortedArr.reverse();
+  if (order === 'asc') {
+    return sortedArr;
   }
 
   if (sortBy === 'grades') {
     return students.sort((el1:Student, el2:Student) => {
       return (el1[sortBy]
         .reduce((a, b) => a + b, 0) / el1[sortBy].length
-        >= el2[sortBy]
+        <= el2[sortBy] // pay attention to comparison operator
           .reduce((a, b) => a + b, 0) / el2[sortBy].length) ? 1 : -1;
+    // this exception is required because for grades simple .reverse()
+    // does not work. For every other sorting method .reverse() works fine
+    // and code looks much cleaner
     });
   }
 
-  return sortedArr;
+  return sortedArr.reverse();
 }
