46fc1a379504d67cd214d10854abed56d41915ab
Andriy Fesych <andriyhimself@gmail.com>
2022-01-25 18:44:51 +0200

    Solution_0.3_complex_sorting_mechanism

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -19,33 +19,27 @@ export type SortOrder = 'asc' | 'desc';
 
 export function sortStudents(students:Student[],
   sortBy:SortType, order:SortOrder):Student[] {
-  const sortedArr:Student[] = [...students]
+  return [...students]
     .sort((el1:Student, el2:Student) => {
-      if (sortBy === 'grades') {
+      if (sortBy !== 'grades') {
+        if (order === 'asc') {
+          return (el1[sortBy] >= el2[sortBy]) ? 1 : -1;
+        }
+
+        return (el1[sortBy] <= el2[sortBy]) ? 1 : -1;
+      }
+
+      if (sortBy === 'grades' && order === 'asc') {
         return (el1[sortBy]
           .reduce((a, b) => a + b, 0) / el1[sortBy].length
         >= el2[sortBy] // pay attention to comparison operator
           .reduce((a, b) => a + b, 0) / el2[sortBy].length) ? 1 : -1;
       }
 
-      return (el1[sortBy] > el2[sortBy]) ? 1 : -1;
-    });
-
-  if (order === 'asc') {
-    return sortedArr;
-  }
-
-  if (sortBy === 'grades') {
-    return students.sort((el1:Student, el2:Student) => {
-      return (el1[sortBy]
+      return (el1[sortBy] // this code can be reached only
+        // if sortBy is grades and order is 'desc'
         .reduce((a, b) => a + b, 0) / el1[sortBy].length
-        <= el2[sortBy] // pay attention to comparison operator
-          .reduce((a, b) => a + b, 0) / el2[sortBy].length) ? 1 : -1;
-    // this exception is required because for grades simple .reverse()
-    // does not work. For every other sorting method .reverse() works fine
-    // and code looks much cleaner
+      <= el2[sortBy] // pay attention to comparison operator
+        .reduce((a, b) => a + b, 0) / el2[sortBy].length) ? 1 : -1;
     });
-  }
-
-  return sortedArr.reverse();
 }
