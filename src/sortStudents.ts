41c7d936e20622511ab62ed08669363c2ae1fe4f
loralevitska <larakad94@gmail.com>
2022-02-02 20:49:01 +0200

    fixes after review

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -16,54 +16,57 @@ export enum SortType {
 }
 
 export type SortOrder = 'asc' | 'desc';
+
 type SortStudentsFunction =
   (students: Student[], sortBy: SortType, order: SortOrder) => Student[];
-type CallBack = (grades1: number[], grades2: number[], order: string) => number;
 
-const SortCallbackByGrades:CallBack = (grades1, grades2, order) => {
-  const firstAvgGrade = grades1.reduce((p, c) => p + c, 0)
+const SortCallbackByGrades
+  = (grades1: number[], grades2: number[], order: string): number => {
+    const firstAvgGrade = grades1.reduce((sum, grade) => sum + grade, 0)
     / grades1.length;
-  const secondAvgGrade = grades2.reduce((p, c) => p + c, 0)
+    const secondAvgGrade = grades2.reduce((sum, grade) => sum + grade, 0)
     / grades2.length;
 
-  return order === 'asc'
-    ? firstAvgGrade - secondAvgGrade
-    : secondAvgGrade - firstAvgGrade;
-};
+    return order === 'asc'
+      ? firstAvgGrade - secondAvgGrade
+      : secondAvgGrade - firstAvgGrade;
+  };
+
+const SortByString
+  = (string1: string, string2: string, order: string): number => {
+    return order === 'asc'
+      ? string1.localeCompare(string2)
+      : string2.localeCompare(string1);
+  };
+
+const SortByNumber
+  = (number1: number, number2: number, order: string): number => {
+    return order === 'asc'
+      ? number1 - number2
+      : number2 - number1;
+  };
 
 export const sortStudents: SortStudentsFunction = (students, sortBy, order) => {
-  const arrayCopy = [...students];
+  const studentsCopy = [...students];
 
-  switch (sortBy) {
-    case SortType.Name:
-      return arrayCopy.sort((a: Student, b: Student) => (
-        order === 'asc'
-          ? a.name.localeCompare(b.name)
-          : b.name.localeCompare(a.name)
-      ));
+  return studentsCopy.sort((a, b) => {
+    switch (sortBy) {
+      case SortType.Name:
+        return SortByString(a.name, b.name, order);
 
-    case SortType.Surname:
-      return arrayCopy.sort((a: Student, b: Student) => (
-        order === 'asc'
-          ? a.surname.localeCompare(b.surname)
-          : b.surname.localeCompare(a.surname)
-      ));
+      case SortType.Surname:
+        return SortByString(a.surname, b.surname, order);
 
-    case SortType.Age:
-      return arrayCopy.sort((a :Student, b: Student) => (order === 'asc'
-        ? a.age - b.age
-        : b.age - a.age));
+      case SortType.Age:
+        return SortByNumber(a.age, b.age, order);
 
-    case SortType.Married:
-      return arrayCopy.sort((a :Student, b: Student) => (order === 'asc'
-        ? +a.married - (+b.married)
-        : +b.married - (+a.married)));
+      case SortType.Married:
+        return SortByNumber(Number(a.married), Number(b.married), order);
 
-    case SortType.AverageGrade:
-      return arrayCopy.sort((a: Student, b: Student) => {
+      case SortType.AverageGrade:
         return SortCallbackByGrades(a.grades, b.grades, order);
-      });
 
-    default: return arrayCopy;
-  }
+      default: return 0;
+    }
+  });
 };
