e4b6caa019db8e41718b2305653861847eb3e079
Yevhenii Kriuchek <evhenii.kriuchek@gmail.com>
2022-02-07 17:00:23 +0200

    tipo typed

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,89 @@
 
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[],
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'averageGrade'
 }
 
 // create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+function sortStrings(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  return order === 'asc'
+    ? students.sort((st1, st2) => st1[sortBy].localeCompare(st2[sortBy]))
+    : students.sort((st1, st2) => st2[sortBy].localeCompare(st1[sortBy]));
+}
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+function getAverageGrade(grades): number {
+  return grades.reduce((a: number, b: number) => a + b) / grades.length;
+}
+
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const studentsToSort = [...students];
+
+  switch (sortBy) {
+    case 'name':
+    case 'surname':
+      return sortStrings(studentsToSort, sortBy, order);
+
+    case 'age':
+      return order === 'asc'
+        ? studentsToSort.sort((st1, st2) => st1.age - st2.age)
+        : studentsToSort.sort((st1, st2) => st2.age - st1.age);
+
+    case 'married':
+      return order === 'asc'
+        ? studentsToSort.sort((st1, st2) => {
+          if (st1.married === st2.married) {
+            return 0;
+          }
+
+          if (st1.married) {
+            return 1;
+          }
+
+          return -1;
+        })
+        : studentsToSort.sort((st1, st2) => {
+          if (st1.married === st2.married) {
+            return 0;
+          }
+
+          if (st1.married) {
+            return -1;
+          }
+
+          return 1;
+        });
+
+    case 'averageGrade': {
+      return order === 'asc'
+        ? studentsToSort.sort((st1, st2) => (
+          getAverageGrade(st1.grades) - getAverageGrade(st2.grades)))
+        : studentsToSort.sort((st1, st2) => (
+          getAverageGrade(st2.grades) - getAverageGrade(st1.grades)));
+    }
+
+    default: {
+      return studentsToSort;
+    }
+  }
 }
