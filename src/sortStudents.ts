26746974f7a7ac05bd14eed6c9cf9bdd927c7c04
trtskvalerie <trtsk.tea.valerie@gmail.com>
2022-02-08 14:33:53 +0200

    what have i done...

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,88 @@
 
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const sorted = [...students];
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  switch (sortBy) {
+    case SortType.Name:
+    case SortType.Surname:
+      sorted.sort((s1, s2) => {
+        switch (order) {
+          case 'desc':
+            return s2[sortBy].localeCompare(s1[sortBy]);
+
+          default:
+            return s1[sortBy].localeCompare(s2[sortBy]);
+        }
+      });
+      break;
+
+    case SortType.Age:
+      sorted.sort((s1, s2) => {
+        switch (order) {
+          case 'desc':
+            return s2[sortBy] - s1[sortBy];
+
+          default:
+            return s1[sortBy] - s2[sortBy];
+        }
+      });
+      break;
+
+    case SortType.Married:
+      sorted.sort((s1, s2) => {
+        if (s1[sortBy] === s2[sortBy]) {
+          return 0;
+        }
+
+        switch (order) {
+          case 'desc':
+            return s1[sortBy] ? -1 : 1;
+
+          default:
+            return s1[sortBy] ? 1 : -1;
+        }
+      });
+      break;
+
+    case SortType.AverageGrade:
+      sorted.sort((s1, s2) => {
+        const avg1 = s1[sortBy].reduce((a, b) => a + b) / s1[sortBy].length;
+        const avg2 = s2[sortBy].reduce((a, b) => a + b) / s2[sortBy].length;
+
+        switch (order) {
+          case 'desc':
+            return avg2 - avg1;
+          default:
+            return avg1 - avg2;
+        }
+      });
+      break;
+
+    default:
+      break;
+  }
+
+  return sorted;
 }
