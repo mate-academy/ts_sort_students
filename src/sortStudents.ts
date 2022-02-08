94dbe6870e9aaeacb6ccdeb925027eacb36cb746
Heorhii Plugoyi <gladiator943work@gmail.com>
2022-02-08 18:01:45 +0200

    save 2/8/2022

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,71 @@
 
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
 // create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const studentsCopy: Student[] = [...students];
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  const midlNum = (array: number[]): number => {
+    return (array.reduce((a, b) => a + b) / array.length);
+  };
+
+  const result: Student[] = studentsCopy
+    .sort((studentA: Student, studentB: Student) => {
+      let sortNum = 0;
+
+      switch (sortBy) {
+        case SortType.Name:
+          sortNum = studentA[sortBy].localeCompare(studentB[sortBy]);
+          break;
+
+        case SortType.Surname:
+          sortNum = studentA[sortBy].localeCompare(studentB[sortBy]);
+          break;
+
+        case SortType.Age:
+          sortNum = studentA[sortBy] - studentB[sortBy];
+          break;
+
+        case SortType.Married:
+          sortNum = studentA[sortBy] && !studentB[sortBy]
+            ? 1
+            : -1;
+          break;
+
+        case SortType.AverageGrade:
+          sortNum = midlNum(studentA[sortBy]) - midlNum(studentB[sortBy]);
+          break;
+
+        default:
+          return 0;
+      }
+
+      if (order === 'desc') {
+        return sortNum * -1;
+      }
+
+      return sortNum;
+    });
+
+  return result;
 }
