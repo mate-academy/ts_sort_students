c56ac7e35292d633369fcaf83adb58462d96d08d
Олег Майборода <xa12850446@student.karazin.ua>
2022-02-08 15:47:13 +0200

    CRINGE

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,79 @@
 
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname:string;
+  age: number;
+  married: boolean;
+  grades:number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
 // create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc'|'desc';
 
+function avg(grades:number[]):number {
+  return grades.reduce((sum:number, grade:number) => sum + grade, 0)
+  / grades.length;
+}
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+export function sortStudents(
+
+  students :Student[],
+  sortBy : SortType,
+  order:SortOrder,
+)
+  :Student[] {
+  const sortedArray:Student[] = [...students].map((student: Student) => {
+    return { ...student };
+  });
+
+  switch (sortBy) {
+    case SortType.Name:
+      sortedArray.sort((studentA:Student, studentB:Student) => {
+        return order === 'asc' ? studentA.name.localeCompare(studentB.name)
+          : studentB.name.localeCompare(studentA.name);
+      });
+      break;
+
+    case 'surname':
+
+      sortedArray.sort((studentA:Student, studentB:Student) => {
+        return order === 'asc'
+          ? studentA.surname.localeCompare(studentB.surname)
+          : studentB.surname.localeCompare(studentA.name);
+      });
+      break;
+
+    case 'age':
+
+      sortedArray.sort((
+        studentA:Student, studentB:Student,
+      ) => studentB.age - studentA.age);
+      break;
+
+    case 'married':
+
+      sortedArray.sort((studentA:Student, studentB:Student) => {
+        return (studentA.married && !studentB.married) ? -1 : 1;
+      });
+      break;
+
+    default:
+
+      sortedArray.sort((studentA:Student, studentB:Student) => {
+        return order === 'asc'
+          ? avg(studentA.grades) - avg(studentB.grades)
+          : avg(studentB.grades) - avg(studentA.grades);
+      });
+      break;
+  }
+
+  return sortedArray;
 }
