90d354c294e65e26abca3e3b7b31660da217786f
Illia Shukh <ilyashuh0@gmail.com>
2022-02-08 20:49:56 +0200

    sol

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,55 @@
 
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
+  avarageGrade?: number;
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
 // create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  function studentAverageGrade(student: Student): number {
+    return student.grades
+      .reduce((a, b) => a + b, 0) / student.grades.length;
+  }
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  return [...students].sort((studentA, studentB) => {
+    let current: Student = studentA;
+    let next: Student = studentB;
+
+    if (order === 'desc') {
+      [current, next] = [next, current];
+    }
+
+    switch (sortBy) {
+      case SortType.Name:
+      case SortType.Surname:
+        return current[sortBy].localeCompare(next[sortBy]);
+
+      case SortType.Married:
+      case SortType.Age:
+        return Number(current[sortBy]) - Number(next[sortBy]);
+      case SortType.AverageGrade:
+        return studentAverageGrade(current) - studentAverageGrade(next);
+
+      default:
+        throw new Error('Unexpected value');
+    }
+  });
 }
