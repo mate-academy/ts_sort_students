0349d97c5560d6918da8810227c37eaf14bdfec3
DanyloLipar <danilo.lipar2016@gmail.com>
2022-02-09 14:05:04 +0200

    Slution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,65 @@
 
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[],
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+const avarageSum = (stud: number[]): number => {
+  return stud.reduce((a, b) => a + b) / stud.length;
+};
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+export function sortStudents(students: Student[],
+  sortBy: SortType,
+  order: SortOrder): Student[] {
+  const copy = [...students];
+
+  const final: Student[] = copy.sort((student1: Student, student2: Student) => {
+    let numer: number;
+
+    switch (sortBy) {
+      case 'name':
+        numer = student1[sortBy].localeCompare(student2[sortBy]);
+        break;
+
+      case 'surname':
+        numer = student1[sortBy].localeCompare(student2[sortBy]);
+        break;
+
+      case 'age':
+        numer = student1[sortBy] - student2[sortBy];
+        break;
+
+      case 'married':
+        numer = student1[sortBy] && !student2[sortBy] ? 1 : -1;
+        break;
+
+      case 'grades':
+        numer = avarageSum(student1[sortBy]) - avarageSum(student2[sortBy]);
+        break;
+
+      default:
+        return 0;
+    }
+
+    if (order !== 'asc') {
+      return numer * -1;
+    }
+
+    return numer;
+  });
+
+  return final;
 }
