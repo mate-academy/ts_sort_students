cf95a58533446041be2c4de10edf4ae91df9b257
AndryKK <korbutiaktso@gmail.com>
2022-02-10 12:44:28 +0200

    Solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -23,10 +23,10 @@ export function sortStudents(
 ): Student[] {
   const copy = [...students];
 
-  const average = (student: Student): number => student.grades
+  const getAverageGrade = (student: Student): number => student.grades
     .reduce((acc, grade) => acc + grade) / student.grades.length;
 
-  function rev(a: number, b: number): number {
+  function revers(a: number, b: number): number {
     if (order === 'desc') {
       return b - a;
     }
@@ -37,18 +37,18 @@ export function sortStudents(
   copy.sort((a: Student, b: Student) => {
     switch (sortBy) {
       case SortType.AverageGrade:
-        return rev(average(a), average(b));
+        return revers(getAverageGrade(a), getAverageGrade(b));
 
       case SortType.Age:
 
-        return rev(a.age, b.age);
+        return revers(a.age, b.age);
 
       case SortType.Surname:
       case SortType.Name:
         return a[sortBy].localeCompare(b[sortBy]);
 
       case SortType.Married:
-        return rev(+a.married, +b.married);
+        return revers(+a.married, +b.married);
 
       default:
         return 0;
