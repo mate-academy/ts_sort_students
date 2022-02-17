2897e618a7d155e483932fd1ca433170fa4aaf34
Anna Morozevych <annyshka2210@gmail.com>
2022-02-17 23:48:55 +0200

    Solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,61 @@
+// import { arrayExpression } from "@babel/types";
 
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'averageGrade',
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+function averageGrade(arr: number[]): number {
+  return arr.reduce((sum, n) => sum + n, 0) / arr.length;
+}
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+) : Student[] {
+  const newArr = [...students];
+
+  switch (sortBy) {
+    case SortType.Name:
+    case SortType.Surname:
+      return order === 'asc'
+        ? newArr.sort((el1, el2) => el1[sortBy].localeCompare(el2[sortBy]))
+        : newArr.sort((el1, el2) => el2[sortBy].localeCompare(el1[sortBy]));
+
+    case SortType.Age:
+      return order === 'asc'
+        ? newArr.sort((el1, el2) => el1[sortBy] - el2[sortBy])
+        : newArr.sort((el1, el2) => el2[sortBy] - el1[sortBy]);
+
+    case SortType.Married:
+      return order === 'asc'
+        ? newArr.sort((el1, el2) => +el1[sortBy] - +el2[sortBy])
+        : newArr.sort((el1, el2) => +el2[sortBy] - +el1[sortBy]);
+
+    case SortType.AverageGrade:
+      return order === 'asc'
+        ? newArr.sort((el1, el2) => averageGrade(el1.grades)
+          - averageGrade(el2.grades))
+        : newArr.sort((el1, el2) => averageGrade(el2.grades)
+        - averageGrade(el1.grades));
+
+    default:
+      break;
+  }
+
+  return newArr;
 }
