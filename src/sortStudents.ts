1b1f4bc0d2ecf332a6049d5b7cc8bc238e325ee6
Ivan Shulhan <92580512+IvanShulhan@users.noreply.github.com>
2022-02-17 10:17:24 +0200

    solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,50 @@
-
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+const average = (arr: number[]): number => {
+  return arr.reduce((acc, el) => acc + el, 0) / arr.length;
+};
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+export function sortStudents(students: Student[],
+  sortBy: SortType, order: SortOrder): Student[] {
+  const clone = [...students];
+
+  switch (sortBy) {
+    case 'name':
+    case 'surname':
+      return order === 'asc'
+        ? clone.sort((a, b) => a[sortBy].localeCompare(b[sortBy]))
+        : clone.sort((a, b) => b[sortBy].localeCompare(a[sortBy]));
+
+    case 'age':
+    case 'married':
+      return order === 'asc'
+        ? clone.sort((a, b) => +a[sortBy] - +b[sortBy])
+        : clone.sort((a, b) => +b[sortBy] - +a[sortBy]);
+
+    case 'grades':
+      return order === 'asc'
+        ? clone.sort((a, b) => average(a.grades) - average(b.grades))
+        : clone.sort((a, b) => average(b.grades) - average(a.grades));
+
+    default:
+      break;
+  }
+
+  return clone;
 }
