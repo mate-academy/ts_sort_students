7dd95d85469b02a2b0dab688fe33fb8070a9a00c
Ladyha Roman <ladrom.ua@gmail.com>
2022-02-18 15:45:24 +0200

    solution 2

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -9,11 +9,11 @@ export interface Student {
 }
 
 export enum SortType {
-  Name,
-  Surname,
-  Age,
-  Married,
-  AverageGrade,
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
 export type SortOrder = 'asc' | 'desc';
@@ -23,49 +23,21 @@ export function sortStudents(students: Student[], sortBy: SortType, order: SortO
 
   switch (sortBy) {
     case SortType.Name:
-      if (order === 'asc') {
-        return newArr.sort((a, b) => a.name.localeCompare(b.name));
-      }
-
-      return newArr.sort((a, b) => b.name.localeCompare(a.name));
-
     case SortType.Surname:
-      if (order === 'asc') {
-        return newArr.sort((a, b) => a.surname.localeCompare(b.surname));
-      }
-
-      return newArr.sort((a, b) => b.surname.localeCompare(a.surname));
+      return order === 'asc'
+        ? newArr.sort((a, b) => a[sortBy].localeCompare(b[sortBy]))
+        : newArr.sort((a, b) => b[sortBy].localeCompare(a[sortBy]));
 
     case SortType.Age:
-      if (order === 'asc') {
-        return newArr.sort((a, b) => a.age - b.age);
-      }
-
-      return newArr.sort((a, b) => b.age - a.age);
-
     case SortType.Married:
-      if (order === 'asc') {
-        return newArr.sort((a, b) => +a.married - +b.married);
-      }
-
-      return newArr.sort((a, b) => +b.married - +a.married);
+      return order === 'asc'
+        ? newArr.sort((a, b) => +a[sortBy] - +b[sortBy])
+        : newArr.sort((a, b) => +b[sortBy] - +a[sortBy]);
 
     case SortType.AverageGrade:
-      if (order === 'asc') {
-        return newArr.sort((a, b) => {
-          const firstItem = a.grades.reduce((sum, current) => sum + current) / a.grades.length;
-          const secondItem = b.grades.reduce((sum, current) => sum + current) / b.grades.length;
-
-          return firstItem - secondItem;
-        });
-      }
-
-      return newArr.sort((a, b) => {
-        const firstItem: number = a.grades.reduce((sum, current) => sum + current) / a.grades.length;
-        const secondItem: number = b.grades.reduce((sum, current) => sum + current) / b.grades.length;
-
-        return secondItem - firstItem;
-      });
+      return order === 'asc'
+        ? newArr.sort((a, b) => a[sortBy].reduce((sum, current) => sum + current) / a[sortBy].length - b[sortBy].reduce((sum, current) => sum + current) / b[sortBy].length)
+        : newArr.sort((a, b) => b[sortBy].reduce((sum, current) => sum + current) / b[sortBy].length - a[sortBy].reduce((sum, current) => sum + current) / a[sortBy].length);
     default:
       return students;
   }
