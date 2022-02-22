fe5ae69bccbb820ec7ca4db5b6d02bb5145c7e02
Yaroslav Rehulskyi <barsinvest84@gmail.com>
2022-02-22 21:12:11 +0200

    sorting students

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,69 @@
 
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[]
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'avg'
 }
 
 // create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const copy: Student[] = [...students];
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  return copy.sort((a: Student, b: Student) => {
+    const firstAverageGrade: number = a.grades.reduce(
+      (sum, value) => sum + value,
+    ) / a.grades.length;
+
+    const secondAverageGrade: number = b.grades.reduce(
+      (sum, value) => sum + value,
+    ) / b.grades.length;
+
+    if (order === 'asc') {
+      switch (sortBy) {
+        case SortType.Name:
+          return a.name.localeCompare(b.name);
+        case SortType.Surname:
+          return a.surname.localeCompare(b.surname);
+        case SortType.Age:
+          return a.age - b.age;
+        case SortType.Married:
+          return +a.married - +b.married;
+        case SortType.AverageGrade:
+          return firstAverageGrade - secondAverageGrade;
+        default:
+          return a.name.localeCompare(b.name);
+      }
+    }
+
+    switch (sortBy) {
+      case SortType.Name:
+        return b.name.localeCompare(a.name);
+      case SortType.Surname:
+        return b.surname.localeCompare(a.surname);
+      case SortType.Age:
+        return b.age - a.age;
+      case SortType.Married:
+        return +b.married - +a.married;
+      case SortType.AverageGrade:
+        return secondAverageGrade - firstAverageGrade;
+      default:
+        return b.name.localeCompare(a.name);
+    }
+  });
 }
