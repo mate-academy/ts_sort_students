0a1861eb7da4bdce1b4ff06e795b9d2241c78f25
Dmytro Pelovych <falcon.patron@gmail.com>
2022-02-23 21:16:21 +0200

    Solution for #Sort_students

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,52 @@
-
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: Array<number>;
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+function getAverageAgeOf(student: Student): number {
+  return student.grades.reduce(
+    (accumulator, nextGrade) => accumulator + nextGrade,
+  ) / student.grades.length;
+}
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+export function sortStudents(
+  students: Array<Student>, sortBy: SortType, order: SortOrder,
+): Array<Student> {
+  return [...students].sort((studentA, studentB) => {
+    let studentARef: Student = studentA;
+    let studentBRef: Student = studentB;
+
+    if (order === 'desc') {
+      [studentARef, studentBRef] = [studentBRef, studentARef];
+    }
+
+    switch (sortBy) {
+      case SortType.Name:
+      case SortType.Surname:
+        return studentARef[sortBy].localeCompare(studentBRef[sortBy]);
+
+      case SortType.Age:
+      case SortType.Married:
+        return <number>studentARef[sortBy] - <number>studentBRef[sortBy];
+
+      case SortType.AverageGrade:
+        return getAverageAgeOf(studentARef) - getAverageAgeOf(studentBRef);
+
+      default:
+        return 0;
+    }
+  });
 }
