8eaa4a42c3a774944c38c4aab412dc9e9aa4e741
Anton Chernokulskyi <ton7.ok@icloud.com>
2022-03-14 13:36:54 +0200

    solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,47 @@
 
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[],
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades'
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'decs';
 
+export function avg(grades: number[]): number {
+  return grades.reduce((acc, cur) => acc + cur) / grades.length;
+}
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+export function sortStudents(
+  students: Student[], sortBy: SortType, order: SortOrder,
+): Student[] {
+  return [...students].sort((a: Student, b: Student) => {
+    const first = order === 'asc' ? a : b;
+    const second = order === 'asc' ? b : a;
+
+    switch (sortBy) {
+      case SortType.Name:
+      case SortType.Surname:
+        return first[sortBy].localeCompare(second[sortBy]);
+
+      case SortType.Age:
+      case SortType.Married:
+        return <number>first[sortBy] - <number>second[sortBy];
+
+      case SortType.AverageGrade:
+        return avg(first.grades) - avg(second.grades);
+
+      default:
+        return 0;
+    }
+  });
 }
