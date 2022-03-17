a7a2bf9a72fc72d38a5c5d321afbfc45e22e853a
Max Shlikhta <maxok.vibe@gmail.com>
2022-03-17 16:47:45 +0200

    Sort students

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,58 @@
 
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[],
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades'
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+function getAverageGrade(grades: number[]): number {
+  let result: number = 0;
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  if (grades.length !== 0) {
+    result = grades.reduce((prev, curr) => prev + curr, 0) / grades.length;
+  }
+
+  return result;
+}
+
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const copyStudents: Student[] = [...students];
+
+  return copyStudents.sort((currStudent: Student, nextStudent: Student) => {
+    switch (sortBy) {
+      case SortType.Name:
+      case SortType.Surname:
+        return order === 'asc'
+          ? currStudent[sortBy].localeCompare(nextStudent[sortBy])
+          : nextStudent[sortBy].localeCompare(currStudent[sortBy]);
+
+      case SortType.AverageGrade:
+        return order === 'asc'
+          ? getAverageGrade(currStudent[sortBy])
+            - getAverageGrade(nextStudent[sortBy])
+          : getAverageGrade(nextStudent[sortBy])
+            - getAverageGrade(currStudent[sortBy]);
+
+      default:
+        return order === 'asc'
+          ? +currStudent[sortBy] - +nextStudent[sortBy]
+          : +nextStudent[sortBy] - +currStudent[sortBy];
+    }
+  });
 }
