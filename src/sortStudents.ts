63bddaa060bd4e04211e62e472bdd5ab0b7557f0
MaximTsyhanenko <maksym.tsyganenko85@gmail.com>
2022-04-17 18:31:55 +0300

    solution v.1

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,71 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint no-param-reassign: "error" */
 
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'averageGrade',
 }
 
-// create SortOrder type
-export type SortOrder;
+type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(
+  students: any[], sortBy: SortType, order: SortOrder,
+): object[] {
+  let resultArray: any[] = [];
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  if (sortBy === 'averageGrade') {
+    resultArray = [...students].map((item) => {
+      item.averageGrade = item.grades
+        .reduce(
+          (sum: number, current: number) => sum + current, 0,
+        )
+        / item.grades.length;
+
+      return item;
+    });
+  } else {
+    resultArray = [...students];
+  }
+
+  if (typeof resultArray[1][sortBy] === 'string') {
+    resultArray.sort((a, b) => {
+      const aLowercase = a[sortBy].toLowerCase();
+      const bLowercase = b[sortBy].toLowerCase();
+
+      if (aLowercase < bLowercase) {
+        return order === 'asc' ? -1 : 1;
+      }
+
+      if (aLowercase > bLowercase) {
+        return order === 'asc' ? 1 : -1;
+      }
+
+      return 0;
+    });
+  } else {
+    resultArray.sort((a, b) => {
+      if (a[sortBy] < b[sortBy]) {
+        return order === 'asc' ? -1 : 1;
+      }
+
+      if (a[sortBy] > b[sortBy]) {
+        return order === 'asc' ? 1 : -1;
+      }
+
+      return 0;
+    });
+  }
+
+  return resultArray;
 }
