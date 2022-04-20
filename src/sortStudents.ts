6941da07d71fdd5199abe1af01f3e81233bd8f4b
Vlad Skoromnyi <VectraSton@gmail.com>
2022-04-20 10:46:21 +0300

    task complete

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,79 @@
 
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[],
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'average',
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+function byName(a, b, order, sortBy): number {
+  return order === 'asc'
+    ? a[sortBy].localeCompare(b[sortBy])
+    : b[sortBy].localeCompare(a[sortBy]);
+}
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+function byNumbers(a, b, order, sortBy): number {
+  return order === 'asc'
+    ? +a[sortBy] - +b[sortBy]
+    : +b[sortBy] - +a[sortBy];
+}
+
+function averageGrade(array: number[]): number {
+  return array.reduce((a: number, b: number): number => {
+    return a + b;
+  }, 0) / array.length;
+}
+
+function byAverage(a, b, order): number {
+  const prev = averageGrade(a.grades);
+  const next = averageGrade(b.grades);
+
+  return order === 'asc'
+    ? prev - next
+    : next - prev;
+}
+
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const sorted: Student[] = [...students];
+
+  switch (sortBy) {
+    case SortType.Name:
+      sorted.sort((a, b): number => byName(a, b, order, sortBy));
+      break;
+
+    case SortType.Surname:
+      sorted.sort((a, b): number => byName(a, b, order, sortBy));
+      break;
+
+    case SortType.Age:
+      sorted.sort((a, b): number => byNumbers(a, b, order, sortBy));
+      break;
+
+    case SortType.Married:
+      sorted.sort((a, b): number => byNumbers(a, b, order, sortBy));
+      break;
+
+    case SortType.AverageGrade:
+      sorted.sort((a, b): number => byAverage(a, b, order));
+      break;
+
+    default: break;
+  }
+
+  return sorted;
 }
