ea910aed49fbdcb2c5ed171daf812338c31ce154
Oksana Tyshchenko <oksanasergeevna30@gmail.com>
2022-04-21 17:59:38 +0300

    Sort student was created

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,63 @@
-
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
 // create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const copy: Student[] = [...students];
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  switch (sortBy) {
+    case SortType.Name:
+    case SortType.Surname: {
+      return copy.sort((std1: Student, std2: Student) => (order === 'asc'
+        ? std1[sortBy].localeCompare(std2[sortBy])
+        : std2[sortBy].localeCompare(std1[sortBy])
+      ));
+    }
+
+    case SortType.Age: {
+      return copy.sort((std1: Student, std2: Student) => (order === 'asc'
+        ? std1.age - std2.age
+        : std2.age - std1.age
+      ));
+    }
+
+    case SortType.AverageGrade: {
+      return copy.sort((std1: Student, std2: Student) => (order === 'asc'
+        ? std1.grades.reduce((sum, b) => sum + b, 0) / std1.grades.length
+          - std2.grades.reduce((sum, b) => sum + b, 0) / std2.grades.length
+        : std2.grades.reduce((sum, b) => sum + b, 0) / std2.grades.length
+          - std1.grades.reduce((sum, b) => sum + b, 0) / std1.grades.length
+      ));
+    }
+
+    case SortType.Married: {
+      return copy.sort((std1: Student, std2: Student) => (order === 'asc'
+        ? +std1.married - +std2.married
+        : +std2.married - +std1.married
+      ));
+    }
+
+    default: {
+      return copy;
+    }
+  }
 }
