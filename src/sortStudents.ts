dfc7c4bcb06b0b0b819a5757c9ab6522548cb1ef
Helen Petrovska <paznachka@gmail.com>
2022-04-22 13:51:24 +0300

    sort was added

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,69 @@
 
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(students: Student[],
+  sortBy: SortType, order: SortOrder): Student[] {
+  let arrSort: Student[] = [];
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  function getAverageAge(array: number[]): number {
+    return array
+      .reduce((sum: number, el: number) => sum + el, 0) / array.length;
+  }
+
+  const copyStudents: Student[] = [...students];
+
+  switch (sortBy) {
+    case SortType.Age:
+      arrSort = copyStudents.sort((aObj: Student, bObj: Student) => (
+        (order === 'asc')
+          ? aObj.age - bObj.age
+          : bObj.age - aObj.age
+      ));
+      break;
+
+    case SortType.Name:
+    case SortType.Surname:
+      arrSort = copyStudents.sort((aObj: Student, bObj: Student) => (
+        (order === 'asc')
+          ? aObj[sortBy].localeCompare(bObj[sortBy])
+          : bObj[sortBy].localeCompare(aObj[sortBy])
+      ));
+      break;
+
+    case SortType.Married:
+      arrSort = copyStudents.sort((aObj: Student, bObj: Student) => (
+        (order === 'asc')
+          ? Number(aObj.married) - Number(bObj.married)
+          : Number(bObj.married) - Number(aObj.married)
+      ));
+      break;
+
+    case SortType.AverageGrade:
+      arrSort = copyStudents.sort((aObj: Student, bObj: Student) => (
+        (order === 'asc')
+          ? getAverageAge(aObj.grades) - getAverageAge(bObj.grades)
+          : getAverageAge(bObj.grades) - getAverageAge(aObj.grades)
+      ));
+      break;
+
+    default: throw new Error('Error');
+  }
+
+  return arrSort;
 }
