61079af8bc00a17ebd9bd4c7fc074b4fb1a4300a
yurii837 <gorogotskiy@gmail.com>
2022-04-26 16:17:10 +0300

    done

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,44 @@
 
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  return [...students].sort((stuA, stuB) => {
+    const studentA: Student = order === 'asc' ? stuA : stuB;
+    const studentB: Student = order === 'asc' ? stuB : stuA;
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+    if (sortBy === SortType.Name || sortBy === SortType.Surname) {
+      return studentA[sortBy].localeCompare(studentB[sortBy]);
+    }
+
+    if (sortBy === SortType.AverageGrade) {
+      const avarageGradeA = studentA[sortBy].reduce((a, b) => a + b)
+      / studentA[sortBy].length;
+      const avarageGradeB = studentB[sortBy].reduce((a, b) => a + b)
+      / studentB[sortBy].length;
+
+      return avarageGradeA - avarageGradeB;
+    }
+
+    return +studentA[sortBy] - +studentB[sortBy];
+  });
 }
