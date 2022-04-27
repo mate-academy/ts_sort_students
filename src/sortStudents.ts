3cf2e415da3a735395d696620cf8e2154e785e4c
Ivan Boiko <ivan.boiko.code@gmail.com>
2022-04-27 23:07:58 +0300

    ts-sort-students

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,54 @@
 
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[]
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+function averageGrade(grades: number[]): number {
+  return grades.reduce((first, last) => first + last, 0) / grades.length;
+}
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const pupils: Student[] = [...students];
+
+  switch (sortBy) {
+    case 'name':
+    case 'surname':
+      return pupils.sort((first, last) => {
+        return order === 'asc'
+          ? first[sortBy].localeCompare(last[sortBy])
+          : last[sortBy].localeCompare(first[sortBy]);
+      });
+
+    case 'grades':
+      return pupils.sort((first, last) => {
+        return order === 'asc'
+          ? averageGrade(first[sortBy]) - averageGrade(last[sortBy])
+          : averageGrade(last[sortBy]) - averageGrade(first[sortBy]);
+      });
+
+    default:
+      return pupils.sort((first, last) => {
+        return order === 'asc'
+          ? +first[sortBy] - +last[sortBy]
+          : +last[sortBy] - +first[sortBy];
+      });
+  }
 }
