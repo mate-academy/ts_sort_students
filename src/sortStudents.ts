2714834e365187a28296ad95e17384c370e9bcce
Maria Luschik <marialyschik@Marias-MacBook-Air.local>
2022-05-18 18:44:15 +0300

    done

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,80 @@
 
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[],
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name,
+  Surname,
+  Age,
+  Married,
+  AverageGrade,
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'dsc';
 
+export function sortStudents(students: Student[], sortBy: SortType, order: SortOrder): Student[] {
+  const arr: Student[] = [...students];
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  switch (sortBy) {
+    case SortType.Name:
+      if (order === 'asc') {
+        return arr.sort((a, b) =>
+          a.name.toLowerCase().localeCompare(b.surname.toLowerCase()));
+      };
+
+      return arr.sort((a, b) =>
+      b.name.toLowerCase().localeCompare(a.surname.toLowerCase()));
+
+    case SortType.Surname:
+      if (order = 'asc') {
+        return arr.sort((a, b) =>
+          a.surname.toLowerCase().localeCompare(b.surname.toLowerCase()));
+      };
+
+      return arr.sort((a, b) =>
+      b.surname.toLowerCase().localeCompare(a.surname.toLowerCase()));
+
+    case SortType.Age:
+      if (order === 'asc') {
+        return arr.sort((a, b) => a.age - b.age);
+      };
+
+      return arr.sort((a, b) => b.age - a.age);
+
+    case SortType.Married:
+      if (order === 'asc') {
+        return arr.sort((a, b) => +a.married - +b.married);
+      };
+
+      return arr.sort((a, b) => +b.married - +a.married);
+
+    case SortType.AverageGrade:
+      if (order === 'asc') {
+        return arr.sort((a, b) => {
+          const el1 = a.grades
+          .reduce((total, current) => total + current) / a.grades.length;
+          const el2 = b.grades
+          .reduce((total, current) => total + current) / b.grades.length;
+
+          return el1 - el2;
+        });
+      };
+
+      return arr.sort((a, b) => {
+        const el1 = a.grades
+        .reduce((total, current) => total + current) / a.grades.length;
+        const el2 = b.grades
+        .reduce((total, current) => total + current) / b.grades.length;
+
+        return el2 - el1;
+      });
+
+    default:
+      return students;
+  }
 }
