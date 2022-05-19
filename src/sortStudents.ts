bef54dfc3f4a6f360d6c53a3c2e5d2c0aab0616a
Anatolii Vlasiuk <vlasyuk_a@ukr.net>
2022-05-19 20:58:59 +0300

    SortStudent was done

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,78 @@
 
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[],
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'averageGrade',
 }
 
 // create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  function averGrade(arr: number[]): number {
+    const grade = arr.reduce((sum: number, a: number):
+    number => sum + a, 0) / arr.length;
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+    return grade;
+  }
+
+  const newStudents = [...students];
+
+  switch (sortBy) {
+    case SortType.Name:
+    case SortType.Surname:
+      return newStudents.sort((
+        a: Student,
+        b: Student,
+      ): number => {
+        return order === 'asc'
+          ? a[sortBy].localeCompare(b[sortBy])
+          : b[sortBy].localeCompare(a[sortBy]);
+      });
+
+    case SortType.Age:
+      return newStudents.sort((
+        a: Student,
+        b: Student,
+      ): number => {
+        return (order === 'asc')
+          ? a[sortBy] - b[sortBy]
+          : b[sortBy] - a[sortBy];
+      });
+
+    case SortType.Married:
+      return newStudents.sort((
+        a: Student,
+        b: Student,
+      ): number => {
+        return (order === 'asc')
+          ? +a[sortBy] - +b[sortBy]
+          : +b[sortBy] - +a[sortBy];
+      });
+    case SortType.AverageGrade:
+      return newStudents.sort((
+        a: Student,
+        b: Student,
+      ): number => {
+        return order === 'asc'
+          ? averGrade(a.grades) - averGrade(b.grades)
+          : averGrade(b.grades) - averGrade(a.grades);
+      });
+    default:
+      throw new Error('No correct data');
+  }
 }
