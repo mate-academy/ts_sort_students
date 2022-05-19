b5fb5a204f1446394f9c4f56e9b7f000f7c36053
Vlad Demchuk <vladyslavdemchuk1999@gmail.com>
2022-05-19 17:26:08 +0300

    solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,64 @@
 
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[],
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
 // create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const copy: Student[] = [...students];
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  copy.sort((person1, person2) => {
+    let personAAverage: number;
+    let personBAverage: number;
+
+    switch (sortBy) {
+      case 'name':
+      case 'surname':
+        return order === 'desc'
+          ? person2[sortBy].localeCompare(person1[sortBy])
+          : person1[sortBy].localeCompare(person2[sortBy]);
+
+      case 'age':
+      case 'married':
+        return order === 'desc'
+          ? +person2[sortBy] - +person1[sortBy]
+          : +person1[sortBy] - +person2[sortBy];
+
+      case 'grades':
+        personAAverage = person1[sortBy]
+          .reduce((prev: number, item: number) => (prev + item)
+            , 0) / person1[sortBy].length;
+
+        personBAverage = person2[sortBy]
+          .reduce((prev: number, item: number) => (prev + item)
+            , 0) / person2[sortBy].length;
+
+        return order === 'desc'
+          ? personBAverage - personAAverage
+          : personAAverage - personBAverage;
+
+      default:
+        return 0;
+    }
+  });
+
+  return copy;
 }
