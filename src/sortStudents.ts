a3f683115f1a7daae6c24681d59d1bb3ca3942ed
Andrii Rutkovskii <greateman94@gmail.com>
2022-05-20 13:46:32 +0300

    solution in one test run

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,67 @@
+// import { numberLiteralTypeAnnotation } from "@babel/types";
 
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: [],
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name,
+  Surname,
+  Age,
+  Married,
+  AverageGrade
 }
 
 // create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+function average(arr: number[]): number {
+  return arr.reduce((a, b) => a + b, 0) / arr.length;
+}
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const array = [...students];
+
+  switch (sortBy) {
+    case SortType.Name:
+      return array.sort((a: Student, b: Student) => (
+        (order === 'asc')
+          ? a.name.localeCompare(b.name)
+          : b.name.localeCompare(a.name)
+      ));
+    case SortType.Surname:
+      return array.sort((a: Student, b: Student) => (
+        (order === 'asc')
+          ? `${a.surname}`.localeCompare(`${b.surname}`)
+          : `${b.surname}`.localeCompare(`${a.surname}`)
+      ));
+    case SortType.Age:
+      return array.sort((a: Student, b: Student) => (
+        (order === 'asc')
+          ? a.age - b.age
+          : b.age - a.age
+      ));
+    case SortType.Married:
+      return array.sort((a: Student, b: Student) => (
+        (order === 'asc')
+          ? (`${a.married}`).localeCompare(`${b.married}`)
+          : (`${b.married}`).localeCompare(`${a.married}`)
+      ));
+    case SortType.AverageGrade:
+      return array.sort((a: Student, b: Student) => (
+        (order === 'asc')
+          ? average(a.grades) - average(b.grades)
+          : average(b.grades) - average(a.grades)
+      ));
+    default:
+      return array;
+  }
 }
