0ed659f8389ae8345646e5f24676c2158ba2c005
Swazi Kunene <kuneneswazi18@gmail.com>
2022-05-20 11:10:23 +0200

    Solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,64 @@
 
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[],
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
 // create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+function averageGrade(gradesArr: number[]): number {
+  return gradesArr.reduce((a, b) => a + b, 0) / gradesArr.length;
+}
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const sortedStudents: Student[] = [...students];
+
+  switch (sortBy) {
+    case SortType.Name:
+    case SortType.Surname:
+      return sortedStudents.sort((student1: Student, student2: Student) => {
+        return order === 'asc'
+          ? student1[sortBy].localeCompare(student2[sortBy])
+          : student2[sortBy].localeCompare(student1[sortBy]);
+      });
+
+    case SortType.Age:
+      return sortedStudents.sort((student1: Student, student2: Student) => {
+        return order === 'asc'
+          ? student1[sortBy] - student2[sortBy]
+          : student2[sortBy] - student1[sortBy];
+      });
+
+    case SortType.Married:
+      return sortedStudents.sort((student1: Student, student2: Student) => {
+        return order === 'asc'
+          ? +student1[sortBy] - +student2[sortBy]
+          : +student2[sortBy] - +student1[sortBy];
+      });
+
+    case SortType.AverageGrade:
+      return sortedStudents.sort((student1: Student, student2: Student) => {
+        return order === 'asc'
+          ? averageGrade(student1[sortBy]) - averageGrade(student2[sortBy])
+          : averageGrade(student2[sortBy]) - averageGrade(student1[sortBy]);
+      });
+    default:
+      return sortedStudents;
+  }
 }
