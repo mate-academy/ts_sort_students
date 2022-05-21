d793555059a7c73b4c9b964e28621e9c6e3b8f84
efdalyalcin <efdalyalcin@hotmail.com>
2022-05-21 12:54:26 +0300

    solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,69 @@
 
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function takeAverage(gradesAverage: number[]): number {
+  const average: number
+    = gradesAverage.reduce((sum: number, current: number) => (
+      sum + current
+    ), 0);
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  return average / gradesAverage.length;
+}
+
+export function sortStudents(students: Student[],
+  sortBy: SortType, order: SortOrder): Student[] {
+  const sortedStudents: Student[] = [...students];
+
+  switch (sortBy) {
+    case SortType.Name:
+    case SortType.Surname:
+      return sortedStudents.sort((firstSt: Student, secondSt: Student) => {
+        return order === 'asc'
+          ? firstSt[sortBy].localeCompare(secondSt[sortBy])
+          : secondSt[sortBy].localeCompare(firstSt[sortBy]);
+      });
+
+    case SortType.Age:
+      return sortedStudents.sort((firstSt: Student, secondSt: Student) => {
+        return order === 'asc'
+          ? firstSt[sortBy] - secondSt[sortBy]
+          : secondSt[sortBy] - firstSt[sortBy];
+      });
+
+    case SortType.Married:
+      return sortedStudents.sort((firstSt: Student, secondSt: Student) => {
+        return order === 'asc'
+          ? Number(firstSt[sortBy]) - Number(secondSt[sortBy])
+          : Number(secondSt[sortBy]) - Number(firstSt[sortBy]);
+      });
+
+    case SortType.AverageGrade:
+      return sortedStudents.sort((firstSt: Student, secondSt: Student) => {
+        const firstStAverage: number = takeAverage(firstSt[sortBy]);
+        const secondStAverage: number = takeAverage(secondSt[sortBy]);
+
+        return order === 'asc'
+          ? firstStAverage - secondStAverage
+          : secondStAverage - firstStAverage;
+      });
+
+    default:
+      return [];
+  }
 }
