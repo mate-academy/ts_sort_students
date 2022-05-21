312ba27d4eda656122719d55f92b89356c3f2e9d
Oleksandr Ivanchenko <aleksandroc92@gmail.com>
2022-05-21 22:46:23 +0300

    sorted students

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,68 @@
-
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
+}
+
+export function getAverageAge(student: Student): number {
+  return student.grades.reduce(
+    (firstStudent, secondStudent) => firstStudent + secondStudent, 0,
+  ) / student.grades.length;
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const copyStudents = [...students];
+  let callback: (firstStudent: Student, secondStudent: Student) => number;
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  switch (sortBy) {
+    case 'age':
+      callback = (firstStudent: Student, secondStudent: Student):number => (
+        order === 'asc'
+          ? firstStudent[sortBy] - secondStudent[sortBy]
+          : secondStudent[sortBy] - firstStudent[sortBy]
+      );
+      break;
+    case 'married':
+      callback = (firstStudent: Student, secondStudent: Student):number => (
+        order === 'asc'
+          ? +firstStudent[sortBy] - +secondStudent[sortBy]
+          : +secondStudent[sortBy] - +firstStudent[sortBy]
+      );
+      break;
+    case 'grades':
+      callback = (firstStudent: Student, secondStudent: Student):number => (
+        order === 'asc'
+          ? getAverageAge(firstStudent) - getAverageAge(secondStudent)
+          : getAverageAge(secondStudent) - getAverageAge(firstStudent)
+      );
+      break;
+    case 'name':
+    case 'surname':
+      callback = (firstStudent: Student, secondStudent: Student):number => (
+        order === 'asc'
+          ? firstStudent[sortBy].localeCompare(secondStudent[sortBy])
+          : secondStudent[sortBy].localeCompare(firstStudent[sortBy])
+      );
+      break;
+    default:
+      return [];
+  }
+
+  return copyStudents.sort(callback);
 }
