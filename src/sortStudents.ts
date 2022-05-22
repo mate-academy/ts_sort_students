c3526d7711d37700e7036d4ad91fbaf6a667fb58
Uliton <x3m@Oleksandrs-MacBook-Pro.local>
2022-05-22 17:41:00 +0200

    sorte students

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,71 @@
 
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[],
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name,
+  Surname,
+  Age,
+  Married,
+  AverageGrade
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const res = [...students];
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  function averageGrades(arr: number[]): number {
+    return arr.reduce((a: number, b: number) => a + b, 0) / arr.length;
+  }
+
+  switch (sortBy) {
+    case SortType.Name:
+      res.sort((st1, st2) => {
+        return order === 'desc'
+          ? st2.name.localeCompare(st1.name)
+          : st1.name.localeCompare(st2.name);
+      });
+      break;
+    case SortType.Surname:
+      res.sort((st1, st2) => {
+        return order === 'asc'
+          ? st1.surname.localeCompare(st2.surname)
+          : st2.surname.localeCompare(st1.surname);
+      });
+      break;
+    case SortType.Age:
+      res.sort((st1, st2) => {
+        return order === 'asc'
+          ? st1.age - st2.age
+          : st2.age - st1.age;
+      });
+      break;
+    case SortType.Married:
+      res.sort((st1, st2) => {
+        return order === 'desc'
+          ? +st2.married - +st1.married
+          : +st1.married - +st2.married;
+      });
+      break;
+    case SortType.AverageGrade:
+      res.sort((st1, st2) => {
+        return order === 'asc'
+          ? averageGrades(st1.grades) - averageGrades(st2.grades)
+          : averageGrades(st2.grades) - averageGrades(st1.grades);
+      });
+      break;
+    default: return [];
+  }
+
+  return res;
 }
