e5fe3cdc55b6f3891b61e3d0fcd1c2ec184620fa
Amit Patil <amitp0156@gmail.com>
2022-05-22 12:45:36 +0530

    solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,65 @@
 
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'averageGrade',
 }
 
 // create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const studentsCopy = [...students];
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  switch (sortBy) {
+    case SortType.Name:
+    case SortType.Surname:
+      return studentsCopy.sort((student1: Student, student2: Student) => {
+        return order === 'asc'
+          ? student1[sortBy].localeCompare(student2[sortBy])
+          : student2[sortBy].localeCompare(student1[sortBy]);
+      });
+
+    case SortType.Age:
+      return studentsCopy.sort((student1: Student, student2: Student) => {
+        return order === 'asc'
+          ? student1[sortBy] - student2[sortBy]
+          : student2[sortBy] - student1[sortBy];
+      });
+
+    case SortType.Married:
+      return studentsCopy.sort((student1: Student, student2: Student) => {
+        return order === 'asc'
+          ? +student1[sortBy] - +student2[sortBy]
+          : +student2[sortBy] - +student1[sortBy];
+      });
+
+    case SortType.AverageGrade:
+      return studentsCopy.sort((el1: Student, el2: Student) => {
+        const student1 = (el1.grades.reduce((sum, subject) => sum
+          + subject, 0)) / el1.grades.length;
+        const student2 = (el2.grades.reduce((sum, subject) => sum
+          + subject, 0)) / el2.grades.length;
+
+        return order === 'asc'
+          ? student1 - student2
+          : student2 - student1;
+      });
+    default:
+      return studentsCopy;
+  }
 }
