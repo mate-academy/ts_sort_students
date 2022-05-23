f62bfb47dadd82035847b2dd5a2affe1c9023da4
rostyslav-pyliak <r.pyliak@gmail.com>
2022-05-23 17:46:57 +0300

    Solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,71 @@
 
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+function calcAvgGrades(arrayGrades: number[]): number {
+  return arrayGrades
+    .reduce((acc, num) => acc + num, 0) / arrayGrades.length;
+}
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const copyStudents: Student[] = [...students];
+
+  switch (sortBy) {
+    case SortType.Name:
+    case SortType.Surname:
+      copyStudents.sort((student1: Student, student2: Student) => {
+        return order === 'asc'
+          ? student1[sortBy].localeCompare(student2[sortBy])
+          : student2[sortBy].localeCompare(student1[sortBy]);
+      });
+      break;
+
+    case SortType.Age:
+      copyStudents.sort((student1: Student, student2: Student) => {
+        return order === 'asc'
+          ? student1[sortBy] - student2[sortBy]
+          : student2[sortBy] - student1[sortBy];
+      });
+      break;
+
+    case SortType.Married:
+      copyStudents.sort((student1: Student, student2: Student) => {
+        return order === 'asc'
+          ? (+student1[sortBy]) - (+student2[sortBy])
+          : (+student2[sortBy]) - (+student1[sortBy]);
+      });
+      break;
+
+    case SortType.AverageGrade:
+      copyStudents.sort((student1: Student, student2: Student) => {
+        return order === 'asc'
+          ? calcAvgGrades(student1.grades) - calcAvgGrades(student2.grades)
+          : calcAvgGrades(student2.grades) - calcAvgGrades(student1.grades);
+      });
+      break;
+
+    default:
+      return [];
+  }
+
+  return copyStudents;
 }
