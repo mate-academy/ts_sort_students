645a7805331054ba62ceda896739add5dec4f1e8
Uliton <x3m@Oleksandrs-MacBook-Pro.local>
2022-05-23 10:57:27 +0200

    rename variables and move function averageGrades outside the function sortStudents

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -17,55 +17,55 @@ export enum SortType {
 
 export type SortOrder = 'asc' | 'desc';
 
+function averageGrades(arr: number[]): number {
+  return arr.reduce((a: number, b: number) => a + b, 0) / arr.length;
+}
+
 export function sortStudents(
   students: Student[],
   sortBy: SortType,
   order: SortOrder,
 ): Student[] {
-  const res = [...students];
-
-  function averageGrades(arr: number[]): number {
-    return arr.reduce((a: number, b: number) => a + b, 0) / arr.length;
-  }
+  const resultArray: Student[] = [...students];
 
   switch (sortBy) {
     case SortType.Name:
-      res.sort((st1, st2) => {
+      resultArray.sort((student1, student2) => {
         return order === 'desc'
-          ? st2.name.localeCompare(st1.name)
-          : st1.name.localeCompare(st2.name);
+          ? student2.name.localeCompare(student1.name)
+          : student1.name.localeCompare(student2.name);
       });
       break;
     case SortType.Surname:
-      res.sort((st1, st2) => {
+      resultArray.sort((student1, student2) => {
         return order === 'asc'
-          ? st1.surname.localeCompare(st2.surname)
-          : st2.surname.localeCompare(st1.surname);
+          ? student1.surname.localeCompare(student2.surname)
+          : student2.surname.localeCompare(student1.surname);
       });
       break;
     case SortType.Age:
-      res.sort((st1, st2) => {
+      resultArray.sort((student1, student2) => {
         return order === 'asc'
-          ? st1.age - st2.age
-          : st2.age - st1.age;
+          ? student1.age - student2.age
+          : student2.age - student1.age;
       });
       break;
     case SortType.Married:
-      res.sort((st1, st2) => {
+      resultArray.sort((student1, student2) => {
         return order === 'desc'
-          ? +st2.married - +st1.married
-          : +st1.married - +st2.married;
+          ? +student2.married - +student1.married
+          : +student1.married - +student2.married;
       });
       break;
     case SortType.AverageGrade:
-      res.sort((st1, st2) => {
+      resultArray.sort((student1, student2) => {
         return order === 'asc'
-          ? averageGrades(st1.grades) - averageGrades(st2.grades)
-          : averageGrades(st2.grades) - averageGrades(st1.grades);
+          ? averageGrades(student1.grades) - averageGrades(student2.grades)
+          : averageGrades(student2.grades) - averageGrades(student1.grades);
       });
       break;
     default: return [];
   }
 
-  return res;
+  return resultArray;
 }
