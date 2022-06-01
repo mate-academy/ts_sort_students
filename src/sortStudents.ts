02ef408b76a55d5668cc944c6b81c7ceb75247ca
Nazar Muliar <nazarmuliar26@gmail.com>
2022-06-01 21:35:12 +0300

    Solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,49 @@
-
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades'
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(
+  students: Student[], sortBy: SortType, order: SortOrder,
+): Student[] {
+  const getAvg = (grades: number[]): number => grades
+    .reduce((a: number, b: number) => a + b) / grades.length;
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  switch (sortBy) {
+    case 'name':
+    case 'surname':
+      return [...students].sort((current, next) => {
+        return order === 'asc'
+          ? current[sortBy].localeCompare(next[sortBy])
+          : next[sortBy].localeCompare(current[sortBy]);
+      });
+    case 'married':
+    case 'age':
+      return [...students].sort((current, next) => {
+        return order === 'asc'
+          ? +current[sortBy] - +next[sortBy]
+          : +next[sortBy] - +current[sortBy];
+      });
+    case 'grades':
+      return [...students].sort((current, next) => {
+        return order === 'asc'
+          ? getAvg(current[sortBy]) - getAvg(next[sortBy])
+          : getAvg(next[sortBy]) - getAvg(current[sortBy]);
+      });
+    default:
+      return students;
+  }
 }
