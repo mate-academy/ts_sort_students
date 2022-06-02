b1839e8504a2f40b100392ebf5da7b05b5803fa5
Ilona Shulha <ilon.sh.ilo@gmail.com>
2022-06-02 19:32:06 +0300

    Solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,60 @@
+// /* eslint-disable max-len */
 
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[],
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades'
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+):Student[] {
+  let resultArr: Student[] = [...students];
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  if (sortBy === 'name' || sortBy === 'surname') {
+    resultArr = (order === 'asc')
+      ? resultArr.sort((a, b) => a[sortBy].localeCompare(b[sortBy]))
+      : resultArr.sort((a, b) => b[sortBy].localeCompare(a[sortBy]));
+  }
+
+  if (sortBy === 'grades') {
+    resultArr.sort((a, b) => {
+      const firstLenght: number = a.grades.length;
+      const secondLenght: number = b.grades.length;
+      const first: number = a[sortBy].reduce((prev, el) => {
+        return prev + el;
+      }, 0) / firstLenght;
+      const second: number = b[sortBy].reduce((prev, el) => {
+        return prev + el;
+      }, 0) / secondLenght;
+
+      const result = (order === 'asc')
+        ? (first - second)
+        : (second - first);
+
+      return result;
+    });
+  }
+
+  if (sortBy === 'age' || sortBy === 'married') {
+    resultArr = (order === 'asc')
+      ? resultArr.sort((a, b) => +a[sortBy] - +b[sortBy])
+      : resultArr.sort((a, b) => +b[sortBy] - +a[sortBy]);
+  }
+
+  return resultArr;
 }
