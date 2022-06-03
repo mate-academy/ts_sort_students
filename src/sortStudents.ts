8b02dc02068da5f0d2169794e13a2cc1d217f2d6
Anastasiia Sydorchuk <nastia.sydorchuk@gmail.com>
2022-06-03 19:39:16 +0200

    solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -9,97 +9,50 @@ export interface Student {
 }
 
 export enum SortType {
-  Name,
-  Surname,
-  Age,
-  Married,
-  AverageGrade,
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
 export type SortOrder = 'asc' | 'desc';
+
+export function getAverageGrade(grades: number[]): number {
+  const sum: number = grades.reduce((s: number, n: number) => s + n, 0);
+
+  return sum / grades.length;
+}
 
 export function sortStudents(
   students: Student[],
   sortBy: SortType,
   order: SortOrder,
 ): Student[] {
-  if (order === 'asc') {
-    switch (sortBy) {
-      case SortType.Name:
-        return [...students].sort((a: Student, b: Student) => {
-          return a.name.localeCompare(b.name);
-        });
-      case SortType.Surname:
-        return [...students].sort((a: Student, b: Student) => {
-          return a.surname.localeCompare(b.surname);
-        });
-      case SortType.Age:
-        return [...students].sort((a: Student, b: Student) => {
-          return a.age - b.age;
-        });
-      case SortType.Married:
-        return [...students].sort((a: Student, b: Student) => {
-          switch (true) {
-            case a.married && b.married:
-              return 0;
-            case a.married:
-              return 1;
-            default:
-              return -1;
-          }
-        });
-      case SortType.AverageGrade:
-        return [...students].sort((a: Student, b: Student) => {
-          const sumA: number
-          = a.grades.reduce((s: number, n: number) => s + n, 0);
-          const averageA = sumA / a.grades.length;
-          const sumB: number
-          = b.grades.reduce((s: number, n: number) => s + n, 0);
-          const averageB = sumB / b.grades.length;
+  const arrayStudents = [...students];
 
-          return averageA - averageB;
-        });
-      default:
-        return students;
-    }
-  } else {
-    switch (sortBy) {
-      case SortType.Name:
-        return [...students].sort((a: Student, b: Student) => {
-          return b.name.localeCompare(a.name);
-        });
-      case SortType.Surname:
-        return [...students].sort((a: Student, b: Student) => {
-          return b.surname.localeCompare(a.surname);
-        });
-      case SortType.Age:
-        return [...students].sort((a: Student, b: Student) => {
-          return b.age - a.age;
-        });
-      case SortType.Married:
-        return [...students].sort((a: Student, b: Student) => {
-          switch (true) {
-            case a.married && b.married:
-              return 0;
-            case a.married:
-              return -1;
-            default:
-              return 1;
-          }
-        });
-      case SortType.AverageGrade:
-        return [...students].sort((a: Student, b: Student) => {
-          const sumA: number
-          = a.grades.reduce((s: number, n: number) => s + n, 0);
-          const averageA = sumA / a.grades.length;
-          const sumB: number
-          = b.grades.reduce((s: number, n: number) => s + n, 0);
-          const averageB = sumB / b.grades.length;
-
-          return averageB - averageA;
-        });
-      default:
-        return students;
-    }
+  switch (sortBy) {
+    case SortType.Name:
+    case SortType.Surname:
+      return arrayStudents.sort((a: Student, b: Student) => {
+        return order === 'asc'
+          ? a[sortBy].localeCompare(b[sortBy])
+          : b[sortBy].localeCompare(a[sortBy]);
+      });
+    case SortType.Age:
+    case SortType.Married:
+      return arrayStudents.sort((a: Student, b: Student) => {
+        return order === 'asc'
+          ? +a[sortBy] - +b[sortBy]
+          : +b[sortBy] - +a[sortBy];
+      });
+    case SortType.AverageGrade:
+      return arrayStudents.sort((a: Student, b: Student) => {
+        return order === 'asc'
+          ? getAverageGrade(a.grades) - getAverageGrade(b.grades)
+          : getAverageGrade(b.grades) - getAverageGrade(a.grades);
+      });
+    default:
+      return students;
   }
 }
