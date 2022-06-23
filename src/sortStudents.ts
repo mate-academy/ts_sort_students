5a2b75c778be7c43a2467e11a80b5528b809f178
mira1zu <mira1zu733@gmail.com>
2022-06-23 21:03:06 +0200

    Solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,88 @@
-
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
 // create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const sortedArray: Student[] = [...students];
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  switch (sortBy) {
+    case SortType.Name:
+    // fallsthrough
+
+    case SortType.Surname: {
+      if (order === 'asc') {
+        sortedArray.sort((a: Student, b: Student) => (
+          a[sortBy].localeCompare(b[sortBy])
+        ));
+
+        break;
+      }
+
+      sortedArray.sort((a: Student, b: Student) => (
+        b.name.localeCompare(a.name)
+      ));
+      break;
+    }
+
+    case SortType.Married:
+    // fallsthrough
+
+    case SortType.Age: {
+      if (order === 'asc') {
+        sortedArray.sort((a: Student, b: Student) => (
+          +a[sortBy] - +b[sortBy]
+        ));
+
+        break;
+      }
+
+      sortedArray.sort((a: Student, b: Student) => (
+        +b[sortBy] - +a[sortBy]
+      ));
+      break;
+    }
+
+    case SortType.AverageGrade: {
+      const reduceFn = (sum: number, accum: number): number => sum + accum;
+
+      if (order === 'asc') {
+        sortedArray.sort((a: Student, b: Student) => (
+          a[sortBy].reduce(reduceFn, 0) / a[sortBy].length
+          - b[sortBy].reduce(reduceFn, 0) / b[sortBy].length
+        ));
+
+        break;
+      }
+
+      sortedArray.sort((a: Student, b: Student) => (
+        b[sortBy].reduce(reduceFn, 0) / b[sortBy].length
+        - a[sortBy].reduce(reduceFn, 0) / a[sortBy].length
+      ));
+      break;
+    }
+
+    default:
+      break;
+  }
+
+  return sortedArray;
 }
