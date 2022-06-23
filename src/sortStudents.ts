dc5de3d6d9f9937fef492574990be0ee5a5642d1
mira1zu <mira1zu733@gmail.com>
2022-06-23 21:11:42 +0200

    More creative solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -24,6 +24,8 @@ export function sortStudents(
 ): Student[] {
   const sortedArray: Student[] = [...students];
 
+  const sortOrder: 1 | -1 = order === 'asc' ? 1 : -1;
+
   switch (sortBy) {
     case SortType.Name:
     // fallsthrough
@@ -47,36 +49,22 @@ export function sortStudents(
     // fallsthrough
 
     case SortType.Age: {
-      if (order === 'asc') {
-        sortedArray.sort((a: Student, b: Student) => (
-          +a[sortBy] - +b[sortBy]
-        ));
-
-        break;
-      }
-
       sortedArray.sort((a: Student, b: Student) => (
-        +b[sortBy] - +a[sortBy]
+        (+a[sortBy] - +b[sortBy]) * sortOrder
       ));
+
       break;
     }
 
     case SortType.AverageGrade: {
       const reduceFn = (sum: number, accum: number): number => sum + accum;
 
-      if (order === 'asc') {
-        sortedArray.sort((a: Student, b: Student) => (
-          a[sortBy].reduce(reduceFn, 0) / a[sortBy].length
-          - b[sortBy].reduce(reduceFn, 0) / b[sortBy].length
-        ));
-
-        break;
-      }
-
       sortedArray.sort((a: Student, b: Student) => (
-        b[sortBy].reduce(reduceFn, 0) / b[sortBy].length
-        - a[sortBy].reduce(reduceFn, 0) / a[sortBy].length
+        (a[sortBy].reduce(reduceFn, 0) / a[sortBy].length
+        - b[sortBy].reduce(reduceFn, 0) / b[sortBy].length)
+        * sortOrder
       ));
+
       break;
     }
 
