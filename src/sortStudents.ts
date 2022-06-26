03219781d52b41909afb8cd84b5a1985922f030d
Andrii Nychaiuk <anichajuk@gmail.com>
2022-06-26 20:29:44 +0300

    some name changes + better formating

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -15,7 +15,6 @@ export enum SortType {
   AverageGrade = 'grades'
 }
 
-// create SortOrder type
 export type SortOrder = 'asc' | 'desc';
 
 export function sortStudents(
@@ -28,36 +27,33 @@ export function sortStudents(
   switch (sortBy) {
     case SortType.Name:
     case SortType.Surname:
-      sortedList = sortedList
-        .sort((a: Student, b: Student) => {
-          return order === 'asc'
-            ? a[sortBy].localeCompare(b[sortBy])
-            : b[sortBy].localeCompare(a[sortBy]);
-        });
+      sortedList = sortedList.sort((previous: Student, next: Student) => {
+        return order === 'asc'
+          ? previous[sortBy].localeCompare(next[sortBy])
+          : next[sortBy].localeCompare(previous[sortBy]);
+      });
       break;
 
     case SortType.Married:
     case SortType.Age:
-      sortedList = sortedList
-        .sort((a: Student, b: Student) => {
-          return order === 'asc'
-            ? +a[sortBy] - +b[sortBy]
-            : +b[sortBy] - +a[sortBy];
-        });
+      sortedList = sortedList.sort((previous: Student, next: Student) => {
+        return order === 'asc'
+          ? +previous[sortBy] - +next[sortBy]
+          : +next[sortBy] - +previous[sortBy];
+      });
       break;
 
     case SortType.AverageGrade:
-      sortedList = sortedList
-        .sort((a: Student, b: Student) => {
-          const AverageA = a.grades
-            .reduce((sum, prev) => sum + prev, 0) / a.grades.length;
-          const AverageB = b.grades
-            .reduce((sum, prev) => sum + prev, 0) / b.grades.length;
+      sortedList = sortedList.sort((previous: Student, next: Student) => {
+        const AverageGradePrev = previous.grades
+          .reduce((sum, prev) => sum + prev, 0) / previous.grades.length;
+        const AverageGradeNext = next.grades
+          .reduce((sum, prev) => sum + prev, 0) / next.grades.length;
 
-          return order === 'asc'
-            ? AverageA - AverageB
-            : AverageB - AverageA;
-        });
+        return order === 'asc'
+          ? AverageGradePrev - AverageGradeNext
+          : AverageGradeNext - AverageGradePrev;
+      });
       break;
 
     default:
