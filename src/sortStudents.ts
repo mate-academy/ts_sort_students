1a68343cb99c9c29c9019dd942c08ad8ba5e3925
Yura Gavrilyuk <gavrilyuk220@gmail.com>
2022-06-27 20:26:40 +0300

    Solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,84 @@
 
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name,
+  Surname,
+  Age,
+  Married,
+  AverageGrade,
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+function getAverageGrade(grades: number[]): number {
+  return grades.reduce((prev, current) => prev + current, 0) / grades.length;
+}
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const copy = [...students];
+
+  switch (sortBy) {
+    case SortType.Name:
+      if (order === 'asc') {
+        copy.sort((std1, std2) => std1.name.localeCompare(std2.name));
+      } else {
+        copy.sort((std1, std2) => std2.name.localeCompare(std1.name));
+      }
+
+      break;
+    case SortType.Surname:
+      if (order === 'asc') {
+        copy.sort((std1, std2) => std1.surname.localeCompare(std2.surname));
+      } else {
+        copy.sort((std1, std2) => std2.surname.localeCompare(std1.surname));
+      }
+
+      break;
+
+    case SortType.Age:
+      if (order === 'asc') {
+        copy.sort((std1, std2) => std1.age - std2.age);
+      } else {
+        copy.sort((std1, std2) => std2.age - std1.age);
+      }
+
+      break;
+
+    case SortType.Married:
+      if (order === 'asc') {
+        copy.sort((std1, std2) => Number(std1.married) - Number(std2.married));
+      } else {
+        copy.sort((std1, std2) => Number(std2.married) - Number(std1.married));
+      }
+
+      break;
+
+    case SortType.AverageGrade:
+      if (order === 'asc') {
+        copy.sort((std1, std2) => getAverageGrade(std1.grades)
+          - getAverageGrade(std2.grades));
+      } else {
+        copy.sort((std1, std2) => getAverageGrade(std2.grades)
+        - getAverageGrade(std1.grades));
+      }
+
+      break;
+
+    default:
+
+      break;
+  }
+
+  return copy;
 }
