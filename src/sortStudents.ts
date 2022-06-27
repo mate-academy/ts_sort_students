0637e9756377d52ccde3ff81c00c41515886e18c
Pavlo <Pmoroz2004@gmail.com>
2022-06-27 15:46:09 +0300

    my solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,115 @@
 
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[],
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+function sortedByName(objectOfStudents : Student[], order: string): Student[] {
+  if (order === 'asc') {
+    return objectOfStudents
+      .sort((first, second) => first.name.localeCompare(second.name));
+  }
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  return objectOfStudents
+    .sort((first, second) => second.name.localeCompare(first.name));
+}
+
+function sortedBySurname(
+  objectOfStudents : Student[],
+  order: string,
+): Student[] {
+  if (order === 'asc') {
+    return objectOfStudents
+      .sort((first, second) => first.surname.localeCompare(second.surname));
+  }
+
+  return objectOfStudents
+    .sort((first, second) => second.surname.localeCompare(first.surname));
+}
+
+function sortedByAge(objectOfStudents : Student[], order: string): Student[] {
+  if (order === 'asc') {
+    return objectOfStudents
+      .sort((first, second) => first.age - second.age);
+  }
+
+  return objectOfStudents
+    .sort((first, second) => second.age - first.age);
+}
+
+function sortedByMarried(
+  objectOfStudents : Student[],
+  order: string,
+): Student[] {
+  if (order === 'asc') {
+    return objectOfStudents
+      .sort((first, second) => Number(first.married) - Number(second.married));
+  }
+
+  return objectOfStudents
+    .sort((first, second) => Number(second.married) - Number(first.married));
+}
+
+function getAverageAge(grades:number[]): number {
+  return grades.reduce((prev, current) => prev + current, 0) / grades.length;
+}
+
+function sortedByAverageGrade(
+  objectOfStudents : Student[],
+  order: string,
+): Student[] {
+  if (order === 'asc') {
+    return objectOfStudents
+      .sort((first, second) => getAverageAge(first.grades)
+      - getAverageAge(second.grades));
+  }
+
+  return objectOfStudents
+    .sort((first, second) => getAverageAge(second.grades)
+      - getAverageAge(first.grades));
+}
+
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const array: Student[] = [...students];
+
+  switch (sortBy) {
+    case (SortType.Name): {
+      return sortedByName(array, order);
+    }
+
+    case (SortType.Surname): {
+      return sortedBySurname(array, order);
+    }
+
+    case (SortType.Age): {
+      return sortedByAge(array, order);
+    }
+
+    case (SortType.AverageGrade): {
+      return sortedByAverageGrade(array, order);
+    }
+
+    default: {
+      const newArray = sortedByMarried(array, order);
+
+      return newArray;
+    }
+  }
 }
