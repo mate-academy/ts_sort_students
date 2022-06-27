5405e374b2f187bda03f93d0cb9a1d4b57cc588e
Stanislav Topikha <stanislav.topikha@gmail.com>
2022-06-27 15:33:46 +0300

    Simplified

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -24,46 +24,46 @@ export function sortStudents(
   order: SortOrder,
 ): Student[] {
   const sortedArr: Student[] = [...students];
+  const compareMod: 1 | -1 = (order === 'desc') ? -1 : 1;
 
   function avgGrade(grades: number[]) :number {
     return grades.reduce((sum:number, grade:number): number => (sum + grade),
       0) / grades.length;
   }
 
-  sortedArr.sort((studentA: Student, studentB:Student): number => {
-    let compare = 0;
+  switch (sortBy) {
+    case SortType.Name:
+    case SortType.Surname:
+      sortedArr.sort((studentA, studentB) => (
+        studentA[sortBy].localeCompare(studentB[sortBy]) * compareMod
+      ));
+      break;
 
-    switch (sortBy) {
-      case SortType.Name:
-      case SortType.Surname:
-        compare = studentA[sortBy].localeCompare(studentB[sortBy]);
-        break;
+    case SortType.Age:
+      sortedArr.sort((studentA, studentB) => (
+        (studentA.age - studentB.age) * compareMod
+      ));
+      break;
 
-      case SortType.Age:
-        compare = studentA.age - studentB.age;
-        break;
-
-      case SortType.Married:
+    case SortType.Married:
+      sortedArr.sort((studentA, studentB) => {
         if (studentA.married !== studentB.married) {
-          compare = studentA.married ? 1 : -1;
+          return (studentA.married ? 1 : -1) * compareMod;
         }
 
-        break;
+        return 0;
+      });
+      break;
 
-      case SortType.AverageGrade:
-        compare = avgGrade(studentA.grades) - avgGrade(studentB.grades);
-        break;
+    case SortType.AverageGrade:
+      sortedArr.sort((studentA, studentB) => (
+        (avgGrade(studentA.grades) - avgGrade(studentB.grades)) * compareMod
+      ));
+      break;
 
-      default:
-        break;
-    }
-
-    if (order === 'desc') {
-      compare *= (-1);
-    }
-
-    return compare;
-  });
+    default:
+      break;
+  }
 
   return sortedArr;
 }
