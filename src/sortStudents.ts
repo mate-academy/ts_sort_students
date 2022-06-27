f46664eb3e9c11e767301a5f9e90aeededb42062
Illia Vodonis <illavodonis@gmail.com>
2022-06-27 17:13:15 +0300

    solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,63 @@
 
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
 // create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function averageNumber(grades: number[]) :number {
+  return grades.reduce((sum:number, grade:number): number => {
+    return sum + grade;
+  }, 0) / grades.length;
+}
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+):Student[] {
+  const sortedStudent = [...students];
+  const sortRule: number = order === 'asc' ? 1 : -1;
+
+  switch (sortBy) {
+    case 'name':
+    case 'surname':
+      sortedStudent.sort((student1, student2) => {
+        return student1[sortBy].localeCompare(student2[sortBy]);
+      });
+      break;
+    case 'age':
+    case 'married':
+      sortedStudent.sort((student1, student2) => {
+        return (Number(student1[sortBy]) - Number(student2[sortBy])) * sortRule;
+      });
+      break;
+
+    case 'grades':
+      sortedStudent.sort((student1, student2) => {
+        const avgGradesStudent1 = averageNumber(student1.grades);
+        const avgGradesStudent2 = averageNumber(student2.grades);
+
+        return (avgGradesStudent1 - avgGradesStudent2) * sortRule;
+      });
+      break;
+
+    default:
+      break;
+  }
+
+  return sortedStudent;
 }
