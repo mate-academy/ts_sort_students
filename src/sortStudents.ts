4b0596bee74f464a87ddcdb2f3837df57e7d0aeb
Dima Belotskyi <dimonbelotskiy@gmail.com>
2022-06-28 17:36:30 +0300

    Solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,67 @@
-
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades'
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const sortedStudents: Student[] = [...students];
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  type Callback = (x: number, y: number) => number;
+
+  const sum: Callback = (x, y) => x + y;
+
+  switch (sortBy) {
+    case SortType.Name:
+    case SortType.Surname:
+      if (order === 'asc') {
+        sortedStudents.sort((a: Student, b: Student) => a[sortBy]
+          .localeCompare(b[sortBy]));
+      } else {
+        sortedStudents.sort((a: Student, b: Student) => b[sortBy]
+          .localeCompare(a[sortBy]));
+      }
+      break;
+
+    case SortType.Age:
+    case SortType.Married:
+      if (order === 'asc') {
+        sortedStudents
+          .sort((a: Student, b: Student) => +a[sortBy] - +b[sortBy]);
+      } else {
+        sortedStudents
+          .sort((a: Student, b: Student) => +b[sortBy] - +a[sortBy]);
+      }
+      break;
+
+    default:
+      if (order === 'asc') {
+        sortedStudents.sort((a: Student, b: Student) => a.grades
+          .reduce(sum, 0) / a.grades.length
+            - b.grades.reduce(sum, 0) / b.grades.length);
+      } else {
+        sortedStudents.sort((a: Student, b: Student) => b.grades
+          .reduce(sum, 0) / b.grades.length
+            - a.grades.reduce(sum, 0) / a.grades.length);
+      }
+      break;
+  }
+
+  return sortedStudents;
 }
