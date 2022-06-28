2d38a964dde9ac1b31960992d8d2deab8b785e5b
Alina Komashchuk <akomashchuk24@gmail.com>
2022-06-29 00:05:02 +0300

    solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,74 @@
-
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[],
+}
+
+interface ExtendedStudent extends Student {
+  averageGrade: number,
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'averageGrade',
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
+type SortValue = string & number & boolean;
+type SortCallback = (a: SortValue, b: SortValue) => number;
 
+const sortByOrder = (
+  a: SortValue,
+  b: SortValue,
+  order: SortOrder,
+  callback: SortCallback,
+): number => {
+  return order === 'asc' ? callback(a, b) : callback(b, a);
+};
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+const sortNumbers = (a: number, b: number): number => a - b;
+
+const sortStrings = (a: string, b: string): number => a.localeCompare(b);
+
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const copyData: ExtendedStudent[] = students.map((student) => {
+    const averageGrade = student.grades.reduce((acc, item) => (
+      acc + item
+    ), 0) / student.grades.length;
+
+    return {
+      ...student,
+      averageGrade,
+    };
+  });
+
+  const result = copyData.sort((a, b) => {
+    const first = a[sortBy] as SortValue;
+    const second = b[sortBy] as SortValue;
+
+    switch (sortBy) {
+      case SortType.Name:
+        return sortByOrder(first, second, order, sortStrings);
+
+      case SortType.Surname:
+        return sortByOrder(first, second, order, sortStrings);
+
+      case SortType.Married:
+        return sortByOrder(first, second, 'desc', sortNumbers);
+
+      default:
+        return sortByOrder(first, second, order, sortNumbers);
+    }
+  });
+
+  return result;
 }
