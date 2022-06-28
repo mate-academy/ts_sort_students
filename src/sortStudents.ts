164818638506ff1691f31c53e22c2fee27b720b6
Sasha Skrinnik <sasha.skrinnik@gmail.com>
2022-06-28 18:02:54 +0300

    Solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,59 @@
 
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'averGrade'
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'decs';
 
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const sortedStd = [...students];
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  switch (sortBy) {
+    case SortType.Name:
+    case SortType.Surname:
+      return order === 'asc'
+        ? sortedStd.sort((a: Student, b: Student) => a[sortBy]
+          .localeCompare(b[sortBy]))
+        : sortedStd.sort((a: Student, b: Student) => b[sortBy]
+          .localeCompare(a[sortBy]));
+    case SortType.Age:
+    case SortType.Married:
+      return order === 'asc'
+        ? sortedStd.sort((a: Student, b: Student) => +a[sortBy] - +b[sortBy])
+        : sortedStd.sort((a: Student, b: Student) => +b[sortBy] - +a[sortBy]);
+    case SortType.AverageGrade:
+      return order === 'asc'
+        ? sortedStd.sort((a: Student, b: Student) => (
+          a.grades.reduce(
+            (prV: number, nxtV: number) => prV + nxtV, 0,
+          ) / a.grades.length)
+            - (b.grades.reduce(
+              (prV: number, nxtV: number) => prV + nxtV, 0,
+            ) / b.grades.length))
+        : sortedStd.sort((a: Student, b: Student) => (
+          b.grades.reduce(
+            (prV: number, nxtV: number) => prV + nxtV, 0,
+          ) / b.grades.length)
+            - (a.grades.reduce(
+              (prV: number, nxtV: number) => prV + nxtV, 0,
+            ) / a.grades.length));
+    default:
+      return sortedStd;
+  }
 }
