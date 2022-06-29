334710c4a4bd74c9198afb1debffec2fbc71d5b3
Maksym Stupin <masik1850@gmail.com>
2022-06-29 20:32:31 +0300

    done

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,69 @@
 
 export interface Student {
-  // describe Student interface
+  name:string,
+  surname:string,
+  age:number,
+  married:boolean,
+  grades:number[],
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name,
+  Surname,
+  Age,
+  Married,
+  AverageGrade,
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+):Student[] {
+  const copyOfStudetns = [...students];
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  copyOfStudetns.sort((firstStudent:Student, secondStudent:Student) => {
+    switch (sortBy) {
+      case SortType.Name:
+        return firstStudent.name.localeCompare(secondStudent.name);
+
+      case SortType.Surname:
+        return firstStudent.surname.localeCompare(secondStudent.surname);
+
+      case SortType.Age:
+        if (order === 'desc') {
+          return secondStudent.age - firstStudent.age;
+        }
+
+        return firstStudent.age - secondStudent.age;
+
+      case SortType.Married:
+        if (order === 'desc') {
+          return Number(secondStudent.married) - Number(firstStudent.married);
+        }
+
+        return Number(firstStudent.married) - Number(secondStudent.married);
+
+      case SortType.AverageGrade:
+        if (order === 'desc') {
+          return secondStudent.grades
+            .reduce((grade1, grade2) => grade1
+            + grade2, 0) / secondStudent.grades.length
+            - firstStudent.grades.reduce((grade1, grade2) => grade1
+            + grade2, 0) / firstStudent.grades.length;
+        }
+
+        return firstStudent.grades.reduce((grade1, grade2) => grade1
+          + grade2, 0) / firstStudent.grades.length
+          - secondStudent.grades.reduce((grade1, grade2) => grade1
+          + grade2, 0) / secondStudent.grades.length;
+
+      default:
+        return 0;
+    }
+  });
+
+  return copyOfStudetns;
 }
