cb9ab42978ce572220ffa98623a553d523e46901
Mikhail Shchuka <mikhail.shchuka@gmail.com>
2022-06-30 02:46:05 +0300

    Solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,53 @@
 
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(students: Student[],
+  sortBy: SortType, order: SortOrder): Student[] {
+  const signum: number = (order === 'asc')
+    ? 1
+    : -1;
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  const sort = (student1: Student, student2: Student): number => {
+    const reduce = (prev: number, item: number): number => prev + item;
+
+    switch (sortBy) {
+      case SortType.Name:
+        return signum * (
+          student1[SortType.Name].localeCompare(student2[SortType.Name]));
+      case SortType.Surname:
+        return signum * (
+          student1[SortType.Surname]
+            .localeCompare(student2[SortType.Surname]));
+      case SortType.Age:
+        return signum * (student1[SortType.Age] - (student2[SortType.Age]));
+      case SortType.Married:
+        return signum * (Number(student1[SortType.Married])
+            - Number(student2[SortType.Married]));
+      case SortType.AverageGrade:
+        return signum * (
+          student1[SortType.AverageGrade].reduce(reduce)
+              / student1[SortType.AverageGrade].length
+                - student2[SortType.AverageGrade].reduce(reduce)
+                    / student2[SortType.AverageGrade].length);
+      default: return 0;
+    }
+  };
+
+  return [...students].sort(sort);
 }
