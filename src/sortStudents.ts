614326cdaf464292c11fa41c51d0e2348ae6aff8
Sasha Havryliuk <princesasha9178@icloud.com>
2022-06-29 17:56:59 +0300

    Change "if" to "swich"

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -21,35 +21,38 @@ export function sortStudents(
   students: Student[],
   sortBy: SortType,
   order: SortOrder,
-): Student[] | number {
+): Student[] {
   const copyList: Student[] = [...students];
-
-  if (sortBy === SortType.Name && order === 'asc') {
-    return copyList.sort((st1, st2) => st1.name.localeCompare(st2.name));
-  }
-
-  if (sortBy === SortType.Surname && order === 'asc') {
-    return copyList.sort((st1, st2) => st1.surname.localeCompare(st2.surname));
-  }
-
-  if (sortBy === SortType.Age && order === 'desc') {
-    return copyList.sort((st1, st2) => st2.age - st1.age);
-  }
-
-  if (sortBy === SortType.Married && order === 'desc') {
-    return copyList
-      .sort((st1, st2) => Number(st2.married) - Number(st1.married));
-  }
 
   function evaluation(list: number[]): number {
     return list.reduce((sum, n) => sum + n, 0) / list.length;
   }
 
-  if (sortBy === SortType.AverageGrade && order === 'asc') {
-    return copyList
-      .sort((st1, st2) => evaluation(st1.grades) - evaluation(st2.grades));
-  }
+  switch (sortBy || order) {
+    case SortType.Name || 'asc':
+      return copyList
+        .sort((st1, st2) => st1.name.localeCompare(st2.name));
 
-  return copyList
-    .sort((st1, st2) => evaluation(st2.grades) - evaluation(st1.grades));
+    case SortType.Surname || 'asc':
+      return copyList
+        .sort((st1, st2) => st1.surname.localeCompare(st2.surname));
+
+    case SortType.Age || 'desc':
+      return copyList
+        .sort((st1, st2) => st2.age - st1.age);
+
+    case SortType.Married || 'desc':
+      return copyList
+        .sort((st1, st2) => Number(st2.married) - Number(st1.married));
+
+    case SortType.AverageGrade:
+      return order === 'desc'
+        ? copyList
+          .sort((st1, st2) => evaluation(st2.grades) - evaluation(st1.grades))
+        : copyList
+          .sort((st1, st2) => evaluation(st1.grades) - evaluation(st2.grades));
+
+    default:
+      return copyList;
+  }
 }
