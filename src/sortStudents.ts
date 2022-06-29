222f621b42c963ce81007a2a8ebd431cc093eff2
Olya Yevsieienko <olya.evsnko@gmail.com>
2022-06-29 10:47:29 +0300

    Solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -11,7 +11,7 @@ export enum SortType {
   Surname = 'surname',
   Age = 'age',
   Married = 'married',
-  AverageGrade = 'average grades',
+  AverageGrade = 'grades',
 }
 
 export type SortOrder = 'asc' | 'desc';
