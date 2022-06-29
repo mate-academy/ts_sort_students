9585da17a45df83aa35f0be048963f5a68a252a9
Alex Burkivskyi <bur4ik003@gmail.com>
2022-06-29 22:30:15 +0300

    Solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,75 @@
 
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[],
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades'
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+function getMiddleGrade(grades: number[]): number {
+  return grades.reduce((prev, current) => prev + current, 0) / grades.length;
+}
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const copyStudents: Student[] = [...students];
+
+  switch (sortBy) {
+    case SortType.Name:
+    case SortType.Surname:
+      if (order === 'asc') {
+        copyStudents.sort((a: Student, b: Student) => a[sortBy]
+          .localeCompare(b[sortBy]));
+      } else {
+        copyStudents.sort((a: Student, b: Student) => b[sortBy]
+          .localeCompare(a[sortBy]));
+      }
+      break;
+    case SortType.Age:
+      if (order === 'asc') {
+        copyStudents.sort((a, b) => a.age - b.age);
+      } else {
+        copyStudents.sort((a, b) => b.age - a.age);
+      }
+      break;
+    case SortType.Married:
+      if (order === 'asc') {
+        copyStudents.sort(
+          (a, b) => Number(a.married) - Number(b.married),
+        );
+      } else {
+        copyStudents.sort(
+          (a, b) => Number(b.married) - Number(a.married),
+        );
+      }
+
+      break;
+
+    default:
+      if (order === 'asc') {
+        copyStudents.sort((a, b) => getMiddleGrade(a.grades)
+          - getMiddleGrade(b.grades));
+      } else {
+        copyStudents.sort((a, b) => getMiddleGrade(b.grades)
+        - getMiddleGrade(a.grades));
+      }
+
+      break;
+  }
+
+  return copyStudents;
 }
