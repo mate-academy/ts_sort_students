2bb47d32d47e6b2b103e5feeb4c9a2737bb293c2
Illia Vodonis <illavodonis@gmail.com>
2022-06-29 12:30:18 +0300

    Fixes

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -29,35 +29,36 @@ export function sortStudents(
   sortBy: SortType,
   order: SortOrder,
 ):Student[] {
-  const sortedStudent = [...students];
-  const sortRule: number = order === 'asc' ? 1 : -1;
+  let sortedStudents:Student[];
+  const sortOrder: number = order === 'asc' ? 1 : -1;
 
   switch (sortBy) {
-    case 'name':
-    case 'surname':
-      sortedStudent.sort((student1, student2) => {
+    case SortType.Surname:
+    case SortType.Name:
+      sortedStudents = [...students].sort((student1, student2) => {
         return student1[sortBy].localeCompare(student2[sortBy]);
       });
       break;
-    case 'age':
-    case 'married':
-      sortedStudent.sort((student1, student2) => {
-        return (Number(student1[sortBy]) - Number(student2[sortBy])) * sortRule;
+    case SortType.Age:
+    case SortType.Married:
+      sortedStudents = [...students].sort((student1, student2) => {
+        return ((+student1[sortBy]) - (+student2[sortBy])) * sortOrder;
       });
       break;
 
-    case 'grades':
-      sortedStudent.sort((student1, student2) => {
+    case SortType.AverageGrade:
+      sortedStudents = [...students].sort((student1, student2) => {
         const avgGradesStudent1 = averageNumber(student1.grades);
         const avgGradesStudent2 = averageNumber(student2.grades);
 
-        return (avgGradesStudent1 - avgGradesStudent2) * sortRule;
+        return (avgGradesStudent1 - avgGradesStudent2) * sortOrder;
       });
       break;
 
     default:
+      sortedStudents = [...students];
       break;
   }
 
-  return sortedStudent;
+  return sortedStudents;
 }
