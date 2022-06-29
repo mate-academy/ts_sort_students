43d021e71bb0c02899d802a4f0ca811556191457
Luidmyla Omelchenko <lui.omelika@gmail.com>
2022-06-29 17:24:33 +0300

    finished with sortStudents

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,75 @@
+// import { setOriginalNode } from "typescript";
 
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[],
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grade'
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+function getAverageGrade(studentGrades: number[]): number {
+  return studentGrades.reduce((previousGrade, currentGrade) => {
+    return (previousGrade + currentGrade);
+  }, 0) / studentGrades.length;
+}
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const copy = [...students].sort((a: Student, b: Student) => {
+    switch (sortBy) {
+      case SortType.Age:
+        if (order === 'desc') {
+          return b.age - a.age;
+        }
+
+        return a.age - b.age;
+
+      case SortType.Married:
+        if (order === 'desc') {
+          return Number(b.married) - Number(a.married);
+        }
+
+        return Number(a.married) - Number(b.married);
+
+      case SortType.Name:
+        if (order === 'desc') {
+          return b.name.localeCompare(a.name);
+        }
+
+        return a.name.localeCompare(b.name);
+
+      case SortType.Surname:
+        if (order === 'desc') {
+          return b.surname.localeCompare(a.surname);
+        }
+
+        return a.surname.localeCompare(b.surname);
+
+      case SortType.AverageGrade:
+        if (order === 'desc') {
+          return getAverageGrade(b.grades) - getAverageGrade(a.grades);
+        }
+
+        return getAverageGrade(a.grades) - getAverageGrade(b.grades);
+
+      default:
+        return 0;
+    }
+  });
+
+  return copy;
 }
