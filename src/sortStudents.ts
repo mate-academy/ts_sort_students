994cfca0ed85fef61d5b2700a587a49e1679b3b6
Dmitrii Pirohov <dmitriy_pirogov@live.com>
2022-06-30 16:43:44 +0300

    second version

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -7,81 +7,67 @@ export interface Student {
 }
 
 export enum SortType {
-  Name,
-  Surname,
-  Age,
-  Married,
-  AverageGrade,
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
 export type SortOrder = 'asc' | 'desc';
 
 function countAverageGrade(array: number[]): number {
   const arrLength = array.length;
-  const result = array.reduce((a, b) => a + b) / arrLength;
+  const AverageGradeByStudent = array.reduce((a, b) => a + b) / arrLength;
 
-  return result;
+  return AverageGradeByStudent;
 }
 
 export function sortStudents(students: Student[],
   sortBy: SortType, order: SortOrder): Student[] {
-  const result = [...students];
+  const copyOfStudents = [...students];
 
-  if (order === 'asc') {
-    switch (sortBy) {
-      case SortType.Name:
-        result.sort((a, b) => a.name.localeCompare(b.name));
-        break;
+  switch (sortBy) {
+    case SortType.Name:
+      return copyOfStudents.sort((a, b) => {
+        return a.name.localeCompare(b.name);
+      });
 
-      case SortType.Surname:
-        result.sort((a, b) => a.surname.localeCompare(b.surname));
-        break;
+    case SortType.Surname:
+      return copyOfStudents.sort((a, b) => {
+        return a.surname.localeCompare(b.surname);
+      });
 
-      case SortType.Age:
-        result.sort((a, b) => a.age - b.age);
-        break;
+    case SortType.Age:
+      return copyOfStudents.sort((a, b) => {
+        if (order === 'desc') {
+          return b.age - a.age;
+        }
 
-      case SortType.Married:
-        result.sort((a, b) => a.married - b.married);
-        break;
+        return a.age - b.age;
+      });
 
-      case SortType.AverageGrade:
-        result
-          .sort((a, b) => countAverageGrade(a.grades)
-            - countAverageGrade(b.grades));
-        break;
+    case SortType.Married:
+      if (order === 'desc') {
+        return copyOfStudents.sort((a, b) => b.married - a.married);
+      }
 
-      default:
-        break;
-    }
-  } else {
-    switch (sortBy) {
-      case SortType.Name:
-        result.sort((a, b) => b.name.localeCompare(a.name));
-        break;
+      return copyOfStudents.sort((a, b) => a.married - b.married);
 
-      case SortType.Surname:
-        result.sort((a, b) => b.surname.localeCompare(a.surname));
-        break;
-
-      case SortType.Age:
-        result.sort((a, b) => b.age - a.age);
-        break;
-
-      case SortType.Married:
-        result.sort((a, b) => b.married - a.married);
-        break;
-
-      case SortType.AverageGrade:
-        result
+    case SortType.AverageGrade:
+      if (order === 'desc') {
+        return copyOfStudents
           .sort((a, b) => countAverageGrade(b.grades)
           - countAverageGrade(a.grades));
-        break;
+      }
 
-      default:
-        break;
-    }
+      return copyOfStudents
+        .sort((a, b) => countAverageGrade(a.grades)
+        - countAverageGrade(b.grades));
+
+    default:
+      break;
   }
 
-  return result;
+  return copyOfStudents;
 }
