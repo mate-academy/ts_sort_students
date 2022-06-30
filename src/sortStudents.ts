45201c5a13e5afa734716a0ae6d0a4649faf3e88
Roman Nikitenko <Lomkaaa@ukr.net>
2022-06-30 06:36:12 +0300

    Task Complete

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,50 @@
 
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'averageGrade'
 }
 
 // create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
+// eslint-disable-next-line
+export function sortStudents(students: Student[], sortBy: SortType, order: SortOrder): Student[] {
+  const copyStudents: Student[] = JSON.parse(JSON.stringify(students));
 
+  copyStudents.sort((a, b) => {
+    const sortA = order === 'asc' ? a : b;
+    const sortB = order === 'asc' ? b : a;
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+    if (sortBy === SortType.Age) {
+      return sortA.age - sortB.age;
+    }
+
+    if (sortBy === SortType.AverageGrade) {
+      // eslint-disable-next-line
+      const averageA = sortA.grades.reduce((sum, x) => sum + x, 0) / sortA.grades.length;
+      // eslint-disable-next-line
+      const averageB = sortB.grades.reduce((sum, x) => sum + x, 0) / sortB.grades.length;
+
+      return averageA - averageB;
+    }
+
+    if (sortBy === SortType.Married) {
+      // eslint-disable-next-line
+      return (sortA.married === sortB.married) ? 0 : sortB.married ? -1 : 1;
+    }
+
+    return sortA[sortBy].localeCompare(sortB[sortBy]);
+  });
+
+  return copyStudents;
 }
