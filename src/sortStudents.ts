781a7fc7430c546da887d4c9876d4e2443a901d9
Yaroslav Kolbaiev <103733788+YaroslavKolbaiev@users.noreply.github.com>
2022-07-18 12:12:32 +0300

    firstCommit

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,74 @@
 
 export interface Student {
   // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
   // describe SortType enum
+  Name,
+  Surname,
+  Age,
+  Married,
+  AverageGrade
 }
 
 // create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
-
-export function sortStudents(students, sortBy, order) {
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
   // write your function
+  const newStudents = [...students];
+
+  function reduce(param: number[]): number {
+    return param.reduce((a, b) => a + b) / param.length;
+  }
+
+  if (order === 'asc') {
+    switch (sortBy) {
+      default:
+        return [];
+
+      case SortType.Name:
+        return newStudents.sort((a, b) => a.name.localeCompare(b.name));
+
+      case SortType.Surname:
+        return newStudents.sort((a, b) => a.surname.localeCompare(b.surname));
+
+      case SortType.AverageGrade:
+        return newStudents.sort((a, b) => reduce(a.grades) - reduce(b.grades));
+    }
+  }
+
+  if (order === 'desc') {
+    switch (sortBy) {
+      default:
+        return [];
+
+      case SortType.Name:
+        return newStudents.sort((a, b) => b.name.localeCompare(a.name));
+
+      case SortType.Surname:
+        return newStudents.sort((a, b) => b.surname.localeCompare(a.surname));
+
+      case SortType.Age:
+        return newStudents.sort((a, b) => b.age - a.age);
+
+      case SortType.Married:
+        return newStudents.sort((a, b) => +b.married - +a.married);
+
+      case SortType.AverageGrade:
+        return newStudents.sort((a, b) => reduce(b.grades) - reduce(a.grades));
+    }
+  }
+
+  return students;
 }
