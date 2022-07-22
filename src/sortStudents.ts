344c7b8c8f3404ff4df6f2c017dcc0da7fb858cc
fledith <barateon023@gmail.com>
2022-07-22 17:02:35 +0300

    Solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,76 @@
 
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name,
+  Surname,
+  Age,
+  Married,
+  AverageGrade,
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const copy = [...students];
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  const averageGrades = (grades: number[]): number => {
+    return grades
+      .reduce((acc: number, cur: number) => acc + cur, 0) / grades.length;
+  };
+
+  switch (sortBy) {
+    case SortType.Name:
+      copy.sort((a:Student, b: Student) => {
+        return order === 'asc'
+          ? a.name.localeCompare(b.name)
+          : b.name.localeCompare(a.name);
+      });
+
+      break;
+    case SortType.Surname:
+      copy.sort((a:Student, b: Student) => {
+        return order === 'asc'
+          ? a.surname.localeCompare(b.surname)
+          : b.surname.localeCompare(a.surname);
+      });
+
+      break;
+    case SortType.Age:
+      copy.sort((a:Student, b: Student) => {
+        return order === 'asc'
+          ? a.age - b.age
+          : b.age - a.age;
+      });
+
+      break;
+    case SortType.Married:
+      copy.sort((a:Student, b: Student) => {
+        return order === 'asc'
+          ? +a.married - +b.married
+          : +b.married - +a.married;
+      });
+
+      break;
+    default:
+      copy.sort((a:Student, b: Student) => {
+        return order === 'asc'
+          ? averageGrades(a.grades) - averageGrades(b.grades)
+          : averageGrades(b.grades) - averageGrades(a.grades);
+      });
+
+      break;
+  }
+
+  return copy;
 }
