edd77fccf66eaa2429eac835e4b92a4e0b054079
Oleg Vetrov <oleg94038@gmail.com>
2022-07-25 15:03:18 +0300

    Added sortStudents function

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,77 @@
 
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[],
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'Name',
+  Surname = 'Surname',
+  Age = 'Age',
+  Married = 'Married',
+  AverageGrade = 'AverageGrade',
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(
+  students: Student[],
+  sortBy:SortType,
+  order: SortOrder,
+): Student[] {
+  let result: Student[] = [...students];
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  result = result.sort((studentOne, studentTwo) => {
+    switch (sortBy) {
+      case SortType.Name:
+        if (order === 'asc') {
+          return studentOne.name.localeCompare(studentTwo.name);
+        }
+
+        return studentOne.name.localeCompare(studentTwo.name);
+
+      case SortType.Age:
+        if (order === 'asc') {
+          return studentOne.age - studentTwo.age;
+        }
+
+        return studentTwo.age - studentOne.age;
+
+      case SortType.Surname:
+        if (order === 'asc') {
+          return studentOne.surname.localeCompare(studentTwo.surname);
+        }
+
+        return studentTwo.surname.localeCompare(studentOne.surname);
+
+      case SortType.Married:
+        if (order === 'asc') {
+          return Number(studentOne.married) - Number(studentTwo.married);
+        }
+
+        return Number(studentTwo.married) - Number(studentOne.married);
+
+      case SortType.AverageGrade: {
+        const studentOneAverageGrade = studentOne.grades
+          .reduce((prev, curr) => prev + curr) / studentOne.grades.length;
+
+        const studentTwoAverageGrade = studentTwo.grades
+          .reduce((prev, curr) => prev + curr) / studentTwo.grades.length;
+
+        if (order === 'asc') {
+          return studentOneAverageGrade - studentTwoAverageGrade;
+        }
+
+        return studentTwoAverageGrade - studentOneAverageGrade;
+      }
+
+      default:
+        throw new Error('Invalid SortType!');
+    }
+  });
+
+  return result;
 }
