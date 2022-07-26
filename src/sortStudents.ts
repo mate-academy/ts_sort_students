75c7c4835af6ac7f914e515dbb16d5c0a5465999
PavloMarkov <r9markovpn@gmail.com>
2022-07-26 16:02:20 +0300

    solution with switch-case

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,95 @@
+// import { type } from "os";
 
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[],
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
 // create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(students: Student[], sortBy: SortType,
+  order: SortOrder): Student[] {
+  const copyOfStudents: Student[] = [];
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  students.forEach((item) => copyOfStudents.push({ ...item }));
+
+  switch (sortBy) {
+    case SortType.Name:
+    case SortType.Surname:
+      copyOfStudents.sort((a: Student, b: Student): number => {
+        if (order === 'asc') {
+          return a[sortBy] >= b[sortBy]
+            ? 1
+            : -1;
+        }
+
+        if (order === 'desc') {
+          return b[sortBy] >= a[sortBy]
+            ? 1
+            : -1;
+        }
+
+        return 0;
+      });
+      break;
+
+    case SortType.Married:
+    case SortType.Age:
+      copyOfStudents.sort((a: Student, b: Student): number => {
+        if (order === 'asc') {
+          return a[sortBy] >= b[sortBy]
+            ? 1
+            : -1;
+        }
+
+        if (order === 'desc') {
+          return b[sortBy] >= a[sortBy]
+            ? 1
+            : -1;
+        }
+
+        return 0;
+      });
+      break;
+
+    case SortType.AverageGrade:
+      copyOfStudents.sort((a: Student, b: Student):number => {
+        const avarSumOne = a.grades.reduce((acc, mark) => acc + mark)
+      / a.grades.length;
+        const avarSumTwo = b.grades.reduce((acc, mark) => acc + mark)
+      / b.grades.length;
+
+        if (order === 'asc') {
+          return avarSumOne >= avarSumTwo
+            ? 1
+            : -1;
+        }
+
+        if (order === 'desc') {
+          return avarSumTwo >= avarSumOne
+            ? 1
+            : -1;
+        }
+
+        return 0;
+      });
+      break;
+
+    default:
+      throw new Error('not correct sort style');
+  }
+
+  return copyOfStudents;
 }
