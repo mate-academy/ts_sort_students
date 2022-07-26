4aafaf713f388e6126dccc297b31114cc848e912
Tania Bukoros <tetiana.bukoros@gmail.com>
2022-07-26 18:05:21 +0300

    Third checklists rule was implement

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,6 +1,5 @@
 
 export interface Student {
-  // describe Student interface
   name: string;
   surname: string;
   age: number;
@@ -9,15 +8,13 @@ export interface Student {
 }
 
 export enum SortType {
-  // describe SortType enum
-  Name,
-  Surname,
-  Age,
-  Married,
-  AverageGrade,
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades'
 }
 
-// create SortOrder type
 export type SortOrder = 'asc' | 'desc';
 
 function AverageGradesValue(student: Student): number {
@@ -33,34 +30,22 @@ export function sortStudents(
 
   switch (sortBy) {
     case SortType.Name:
-      if (order === 'asc') {
-        return studentsCopy.sort((s1, s2) => s1.name.localeCompare(s2.name));
-      }
-
-      return studentsCopy.sort((s1, s2) => s2.name.localeCompare(s1.name));
-
     case SortType.Surname:
       if (order === 'asc') {
         return studentsCopy
-          .sort((s1, s2) => s1.surname.localeCompare(s2.surname));
+          .sort((s1, s2) => s1[sortBy].localeCompare(s2[sortBy]));
       }
 
       return studentsCopy
-        .sort((s1, s2) => s2.surname.localeCompare(s1.surname));
+        .sort((s1, s2) => s2[sortBy].localeCompare(s1[sortBy]));
 
     case SortType.Age:
-      if (order === 'asc') {
-        return studentsCopy.sort((s1, s2) => s1.age - s2.age);
-      }
-
-      return studentsCopy.sort((s1, s2) => s2.age - s1.age);
-
     case SortType.Married:
       if (order === 'asc') {
-        return studentsCopy.sort((s1, s2) => +s1.married - +s2.married);
+        return studentsCopy.sort((s1, s2) => +s1[sortBy] - +s2[sortBy]);
       }
 
-      return studentsCopy.sort((s1, s2) => +s2.married - +s1.married);
+      return studentsCopy.sort((s1, s2) => +s2[sortBy] - +s1[sortBy]);
 
     case SortType.AverageGrade:
       if (order === 'asc') {
