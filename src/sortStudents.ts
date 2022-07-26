7ad933b465d32e9783a78025f6a95a1ae9f0dca0
Olena Rodiontseva <elena.rodiontseva@gmail.com>
2022-07-26 15:33:04 +0200

    used ternary instead of if

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -32,39 +32,29 @@ export function sortStudents(
   return copy.sort((student1: Student, student2: Student) => {
     switch (sortBy) {
       case SortType.Name:
-        if (order === 'asc') {
-          return student1.name.localeCompare(student2.name);
-        }
-
-        return student2.name.localeCompare(student1.name);
+        return order === 'asc'
+          ? student1.name.localeCompare(student2.name)
+          : student2.name.localeCompare(student1.name);
 
       case SortType.Surname:
-        if (order === 'asc') {
-          return student1.surname.localeCompare(student2.surname);
-        }
-
-        return student2.surname.localeCompare(student1.surname);
+        return order === 'asc'
+          ? student1.surname.localeCompare(student2.surname)
+          : student2.surname.localeCompare(student1.surname);
 
       case SortType.Age:
-        if (order === 'asc') {
-          return student1.age - student2.age;
-        }
-
-        return student2.age - student1.age;
+        return order === 'asc'
+          ? student1.age - student2.age
+          : student2.age - student1.age;
 
       case SortType.Married:
-        if (order === 'asc') {
-          return Number(student1.married) - Number(student2.married);
-        }
-
-        return Number(student2.married) - Number(student1.married);
+        return order === 'asc'
+          ? Number(student1.married) - Number(student2.married)
+          : Number(student2.married) - Number(student1.married);
 
       case SortType.AverageGrade:
-        if (order === 'asc') {
-          return getAverageGrade(student1) - getAverageGrade(student2);
-        }
-
-        return getAverageGrade(student2) - getAverageGrade(student1);
+        return order === 'asc'
+          ? getAverageGrade(student1) - getAverageGrade(student2)
+          : getAverageGrade(student2) - getAverageGrade(student1);
 
       default:
         throw new Error('Your input property does not exist on type SortType');
