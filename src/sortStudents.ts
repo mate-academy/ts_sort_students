31608a5162bab5a5fbfe7b1e1db3a451cc048741
Volodya <balyshev.volodya@gmail.com>
2022-07-26 15:30:03 +0300

    solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,67 @@
 
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name,
+  Surname,
+  Age,
+  Married,
+  AverageGrade
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+/* eslint-disable max-len */
+export function sortStudents(students: Student[], sortBy: SortType, order: SortOrder): object[] {
+  const studentsCopy = students.map((student) => ({ ...student }));
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  if (sortBy === SortType.Name && order === 'asc') {
+    return studentsCopy.sort((a, b) => a.name.localeCompare(b.name));
+  }
+
+  if (sortBy === SortType.Name && order === 'desc') {
+    return studentsCopy.sort((a, b) => b.name.localeCompare(a.name));
+  }
+
+  if (sortBy === SortType.Surname && order === 'asc') {
+    return studentsCopy.sort((a, b) => a.surname.localeCompare(b.surname));
+  }
+
+  if (sortBy === SortType.Surname && order === 'desc') {
+    return studentsCopy.sort((a, b) => b.surname.localeCompare(a.surname));
+  }
+
+  if (sortBy === SortType.Age && order === 'asc') {
+    return studentsCopy.sort((a, b) => a.age - b.age);
+  }
+
+  if (sortBy === SortType.Age && order === 'desc') {
+    return studentsCopy.sort((a, b) => b.age - a.age);
+  }
+
+  if (sortBy === SortType.Married && order === 'asc') {
+    return studentsCopy.sort((a, b) => Number(a.married) - Number(b.married));
+  }
+
+  if (sortBy === SortType.Married && order === 'desc') {
+    return studentsCopy.sort((a, b) => Number(b.married) - Number(a.married));
+  }
+
+  if (sortBy === SortType.AverageGrade && order === 'asc') {
+    return studentsCopy
+      .sort((a, b) => (a.grades.reduce((x, y) => x + y) / a.grades.length) - (b.grades.reduce((x, y) => x + y) / b.grades.length));
+  }
+
+  if (sortBy === SortType.AverageGrade && order === 'desc') {
+    return studentsCopy
+      .sort((a, b) => (b.grades.reduce((x, y) => x + y) / b.grades.length) - (a.grades.reduce((x, y) => x + y) / a.grades.length));
+  }
+
+  return studentsCopy;
 }
