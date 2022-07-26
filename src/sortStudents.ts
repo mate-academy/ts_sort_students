b6e529493ca22a052d539bea89b8d07baa3e8a42
Olha Stakhova <olha.stakhova@gmail.com>
2022-07-26 14:58:46 +0300

    solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,64 @@
 
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
+
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name,
+  Surname,
+  Age,
+  Married,
+  AverageGrade,
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc'| 'desc';
 
+export function sortStudents(
+  students:[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const copyStudents: Student[] = [...students];
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  copyStudents.sort((itemA: Student, itemB: Student) => {
+    const a: number = (
+      itemA.grades.reduce((acc: number, item: number) => acc + item, 0)
+    ) / (itemA.grades).length;
+
+    const b: number = (
+      itemB.grades.reduce((acc: number, item: number) => acc + item, 0)
+    ) / (itemB.grades).length;
+
+    switch (sortBy) {
+      case SortType.Name:
+        return order === 'asc'
+          ? itemA.name.localeCompare(itemB.name)
+          : itemB.name.localeCompare(itemA.name);
+
+      case SortType.Surname:
+        return order === 'asc'
+          ? itemA.surname.localeCompare(itemB.surname)
+          : itemB.surname.localeCompare(itemA.surname);
+
+      case SortType.Age:
+        return (order === 'asc')
+          ? itemA.age - itemB.age
+          : itemB.age - itemA.age;
+
+      case SortType.Married:
+        return (order === 'asc')
+          ? Number(itemA.married) - Number(itemB.married)
+          : Number(itemB.married) - Number(itemA.married);
+
+      default:
+        return order === 'asc' ? a - b : b - a;
+    }
+  });
+
+  return copyStudents;
 }
