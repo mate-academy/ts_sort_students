c23ab6284910425e4318a8034434c0f65c10af2a
Yevhen Nikolaienko <illnino380@gmail.com>
2022-07-26 18:01:29 +0300

    Solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,69 @@
 
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[],
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
 // create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function getAverageGrade(grades: number[]): number {
+  return grades.reduce((acc, mark) => acc + mark, 0) / (grades.length || 1);
+}
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const studentsCopy = [...students];
+
+  studentsCopy.sort((studentA, studentB) => {
+    switch (sortBy) {
+      case SortType.Name:
+        return order === 'asc'
+          ? studentA[sortBy].localeCompare(studentB[sortBy])
+          : studentB[sortBy].localeCompare(studentA[sortBy]);
+
+      case SortType.Surname:
+        return order === 'asc'
+          ? studentA[sortBy].localeCompare(studentB[sortBy])
+          : studentB[sortBy].localeCompare(studentA[sortBy]);
+
+      case SortType.Age:
+        return order === 'asc'
+          ? studentA[sortBy] - studentB[sortBy]
+          : studentB[sortBy] - studentA[sortBy];
+
+      case SortType.Married:
+        return order === 'asc'
+          ? Number(studentA[sortBy]) - Number(studentB[sortBy])
+          : Number(studentB[sortBy]) - Number(studentA[sortBy]);
+
+      case SortType.AverageGrade:
+        return order === 'asc'
+          ? getAverageGrade(studentA[sortBy])
+            - getAverageGrade(studentB[sortBy])
+          : getAverageGrade(studentB[sortBy])
+            - getAverageGrade(studentA[sortBy]);
+
+      default:
+        return order === 'asc'
+          ? 1
+          : 0;
+    }
+  });
+
+  return studentsCopy;
 }
