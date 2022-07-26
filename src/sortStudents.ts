5b3e734dbf6e40e746e9dc5f09018b99c86a1ad8
Danil Hodytskyi <danil.hodytskyi1105@gmail.com>
2022-07-26 15:35:21 +0300

    Solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,81 @@
 
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[],
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'firstName',
+  Surname = 'lastName',
+  Age = 'yearsOld',
+  Married = 'isMarried',
+  AverageGrade = 'averageGrade',
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const studentsCopy = [...students];
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  const averageGrade = (grades: number[]): number => grades
+    .reduce((prev: number, curr: number) => prev + curr, 0) / grades.length;
+
+  switch (sortBy) {
+    case SortType.Name:
+      studentsCopy.sort((first: Student, second: Student) => {
+        return order === 'asc'
+          ? first.name.localeCompare(second.name)
+          : second.name.localeCompare(first.name);
+      });
+
+      break;
+
+    case SortType.Surname:
+      studentsCopy.sort((first: Student, second: Student) => {
+        return order === 'asc'
+          ? first.surname.localeCompare(second.surname)
+          : second.surname.localeCompare(first.surname);
+      });
+
+      break;
+
+    case SortType.Age:
+      studentsCopy.sort((first: Student, second: Student) => {
+        return order === 'asc'
+          ? first.age - second.age
+          : second.age - first.age;
+      });
+
+      break;
+
+    case SortType.Married:
+      studentsCopy.sort((first: Student, second: Student) => {
+        return order === 'asc'
+          ? +first.married - +second.married
+          : +second.married - +first.married;
+      });
+
+      break;
+
+    case SortType.AverageGrade:
+      studentsCopy.sort((first: Student, second: Student) => {
+        return order === 'asc'
+          ? averageGrade(first.grades) - averageGrade(second.grades)
+          : averageGrade(second.grades) - averageGrade(first.grades);
+      });
+
+      break;
+
+    default:
+      throw new Error('Wrong Sort Type');
+  }
+
+  return studentsCopy;
 }
