6d3bfc9e46268c4046972b17eb8012b573aec3c0
Olha Chebotarova <chebotarovaolha@gmail.com>
2022-08-04 14:55:28 -0400

    solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,60 @@
-
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name,
+  Surname,
+  Age,
+  Married,
+  AverageGrade,
 }
 
 // create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  switch (sortBy) {
+    case SortType.Age:
+      if (order === 'asc') {
+        return [...students].sort((a, b) => a.age - b.age);
+      }
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+      return [...students].sort((a, b) => b.age - a.age);
+    case SortType.Name:
+      return [...students].sort((a, b) => a.name.localeCompare(b.name));
+    case SortType.Surname:
+      if (order === 'asc') {
+        return [...students].sort((a, b) => a.surname.localeCompare(b.surname));
+      }
+
+      return [...students].sort((a, b) => b.surname.localeCompare(a.surname));
+    case SortType.Married:
+      return [...students]
+        .sort((x, y) => Number(y.married) - Number(x.married));
+    case SortType.AverageGrade:
+      if (order === 'asc') {
+        return [...students].sort(
+          (a, b) => a.grades.reduce((sum, x) => sum + x, 0) / a.grades.length
+            - b.grades.reduce((sum, x) => sum + x, 0) / b.grades.length,
+        );
+      }
+
+      return [...students].sort(
+        (a, b) => b.grades.reduce((sum, x) => sum + x, 0) / b.grades.length
+            - a.grades.reduce((sum, x) => sum + x, 0) / a.grades.length,
+      );
+    default:
+      break;
+  }
+
+  return students;
 }
