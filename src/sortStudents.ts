da28e6bdd5217b9f62cbf28e25d417d9da8a47ea
Vladyslav <vladyslav3177@gmail.com>
2022-08-17 13:15:16 +0300

    Solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,91 @@
 
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[]
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name,
+  Surname,
+  Age,
+  Married,
+  AverageGrade
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+function averagedGrade(items: number[]): number {
+  return items.reduce((initial: number, second: number) => initial + second, 0)
+  / items.length;
+}
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const copyStudents = [...students];
+
+  switch (sortBy) {
+    case SortType.Name:
+      copyStudents.sort((a: Student, b: Student) => {
+        if (order === 'asc') {
+          return a.name.localeCompare(b.name);
+        }
+
+        return b.name.localeCompare(a.name);
+      });
+      break;
+    case SortType.Surname:
+      copyStudents.sort((a: Student, b: Student) => {
+        if (order === 'asc') {
+          return a.surname.localeCompare(b.surname);
+        }
+
+        return b.surname.localeCompare(a.surname);
+      });
+
+      break;
+
+    case SortType.Age:
+      copyStudents.sort((a: Student, b: Student) => {
+        if (order === 'asc') {
+          return a.age - b.age;
+        }
+
+        return b.age - a.age;
+      });
+
+      break;
+
+    case SortType.Married:
+      copyStudents.sort((a: Student, b: Student) => {
+        if (order === 'asc') {
+          return +a.married - +b.married;
+        }
+
+        return +b.married - +a.married;
+      });
+
+      break;
+
+    case SortType.AverageGrade:
+      copyStudents.sort((a: Student, b: Student) => {
+        if (order === 'asc') {
+          return averagedGrade(a.grades) - averagedGrade(b.grades);
+        }
+
+        return averagedGrade(b.grades) - averagedGrade(a.grades);
+      });
+
+      break;
+
+    default:
+      break;
+  }
+
+  return copyStudents;
 }
