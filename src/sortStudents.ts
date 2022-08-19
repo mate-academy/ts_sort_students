017a906d4ec9c3388c47cd00f50f2062739a063b
Oles Partyka <oles.partyka.27@gmail.com>
2022-08-20 00:42:32 +0200

    Solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,63 @@
-
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[],
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  let theSortOrder: number = 1;
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  if (order === 'desc') {
+    theSortOrder = -1;
+  }
+
+  const studentsCopy = [...students];
+
+  switch (sortBy) {
+    case SortType.Name:
+      studentsCopy.sort((a, b) => a.name.localeCompare(b.name));
+      break;
+
+    case SortType.Surname:
+      studentsCopy.sort(
+        (a, b) => a.surname.localeCompare(b.surname) * theSortOrder,
+      );
+      break;
+
+    case SortType.Age:
+      studentsCopy.sort((a, b) => (a.age - b.age) * theSortOrder);
+      break;
+
+    case SortType.Married:
+      studentsCopy.sort((a, b) => (+a.married - +b.married) * theSortOrder);
+      break;
+
+    default:
+      studentsCopy.sort((a, b) => (
+        (a.grades
+          .reduce((sum, current) => sum + current, 0) / a.grades.length)
+        - (b.grades
+          .reduce((
+            sum, current,
+          ) => sum + current, 0) / b.grades.length)) * theSortOrder);
+      break;
+  }
+
+  return studentsCopy;
 }
