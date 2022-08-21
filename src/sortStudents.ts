c951cdc9632b1a4017448d93f2164cf3da5fc7e9
Konstantin <ivanchenkomypost@gmail.com>
2022-08-21 14:00:46 +0300

    Fixes after review

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -17,26 +17,32 @@ export enum SortType {
 
 export type SortOrder = 'asc' | 'desc';
 
-// eslint-disable-next-line
-export function sortStudents(students: Student[], sortBy: SortType, order: SortOrder) {
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
   return [...students].sort((a: Student, b: Student): number => {
     if (sortBy === 'grades') {
       const aAverage = a[sortBy]
-      // eslint-disable-next-line
-        .reduce((sum: number, num: number): number => sum + num) / a[sortBy].length;
+        .reduce((sum: number, num: number): number => sum + num)
+        / a[sortBy].length;
+
       const bAverage = b[sortBy]
-      // eslint-disable-next-line
-        .reduce((sum: number, num: number): number => sum + num) / b[sortBy].length;
+        .reduce((sum: number, num: number): number => sum + num)
+        / b[sortBy].length;
 
       return order === 'asc' ? (aAverage - bAverage) : (bAverage - aAverage);
     }
 
     if (typeof a[sortBy] !== 'string') {
-      return order === 'asc' ? Number(a[sortBy]) - Number(b[sortBy])
+      return order === 'asc'
+        ? Number(a[sortBy]) - Number(b[sortBy])
         : Number(b[sortBy]) - Number(a[sortBy]);
     }
 
-    return order === 'asc' ? String(a[sortBy]).localeCompare(String(b[sortBy]))
+    return order === 'asc'
+      ? String(a[sortBy]).localeCompare(String(b[sortBy]))
       : String(b[sortBy]).localeCompare(String(a[sortBy]));
   });
 }
