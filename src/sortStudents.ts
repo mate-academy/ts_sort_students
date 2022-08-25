944f60aafdcdcd414036eed4b725788690cb34f2
sergii-nosachenko <s.nosachenko@gmail.com>
2022-08-25 17:57:12 +0300

    Task solution code

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,56 @@
-
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[],
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const studentsSorted = [...students];
+  const direction: -1 | 1 = order === 'asc' ? 1 : -1;
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  studentsSorted.sort((studA, studB): number => {
+    const paramA = studA[sortBy];
+    const paramB = studB[sortBy];
+    let diff: number = 0;
+
+    if (typeof paramA === 'number' && typeof paramB === 'number') {
+      diff = paramA - paramB;
+    }
+
+    if (typeof paramA === 'string' && typeof paramB === 'string') {
+      diff = paramA.localeCompare(paramB);
+    }
+
+    if (typeof paramA === 'boolean' && typeof paramB === 'boolean') {
+      diff = Number(paramA) - Number(paramB);
+    }
+
+    if (Array.isArray(paramA) && Array.isArray(paramB)) {
+      const getAverage = (param: number[]): number => (
+        param.reduce((sum, num) => (sum + num), 0) / param.length
+      );
+
+      diff = getAverage(paramA) - getAverage(paramB);
+    }
+
+    return diff * direction;
+  });
+
+  return studentsSorted;
 }
