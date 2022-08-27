a27eba959528020940f8c7559f7d57bb11bc7244
sergii-nosachenko <s.nosachenko@gmail.com>
2022-08-27 08:34:21 +0300

    Code optimization

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -21,36 +21,32 @@ export function sortStudents(
   sortBy: SortType,
   order: SortOrder,
 ): Student[] {
-  const studentsSorted = [...students];
   const direction: -1 | 1 = order === 'asc' ? 1 : -1;
 
-  studentsSorted.sort((studA, studB): number => {
-    const paramA = studA[sortBy];
-    const paramB = studB[sortBy];
-    let diff: number = 0;
+  return [...students]
+    .sort(({ [sortBy]: paramA }, { [sortBy]: paramB }): number => {
+      let diff: number = 0;
 
-    if (typeof paramA === 'number' && typeof paramB === 'number') {
-      diff = paramA - paramB;
-    }
+      if (typeof paramA === 'number' && typeof paramB === 'number') {
+        diff = paramA - paramB;
+      }
 
-    if (typeof paramA === 'string' && typeof paramB === 'string') {
-      diff = paramA.localeCompare(paramB);
-    }
+      if (typeof paramA === 'string' && typeof paramB === 'string') {
+        diff = paramA.localeCompare(paramB);
+      }
 
-    if (typeof paramA === 'boolean' && typeof paramB === 'boolean') {
-      diff = Number(paramA) - Number(paramB);
-    }
+      if (typeof paramA === 'boolean' && typeof paramB === 'boolean') {
+        diff = Number(paramA) - Number(paramB);
+      }
 
-    if (Array.isArray(paramA) && Array.isArray(paramB)) {
-      const getAverage = (param: number[]): number => (
-        param.reduce((sum, num) => (sum + num), 0) / param.length
-      );
+      if (Array.isArray(paramA) && Array.isArray(paramB)) {
+        const getAverage = (param: number[]): number => (
+          param.reduce((sum, num) => sum + num, 0) / param.length
+        );
 
-      diff = getAverage(paramA) - getAverage(paramB);
-    }
+        diff = getAverage(paramA) - getAverage(paramB);
+      }
 
-    return diff * direction;
-  });
-
-  return studentsSorted;
+      return diff * direction;
+    });
 }
