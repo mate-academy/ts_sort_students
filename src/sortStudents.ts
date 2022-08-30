17c5729989bf9052e2715121f233182a9684d4fa
Shahmatov David <davidsahmatov4@gmail.com>
2022-08-30 21:11:10 +0300

    Version 0.1

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,84 @@
 
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[],
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name,
+  Surname,
+  Age,
+  Married,
+  AverageGrade
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+function calculateAverageAge(nums): number {
+  const result = nums.reduce((prev, current) => prev + current);
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  return result / nums.length;
+}
+
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const copy = [...students];
+
+  switch (sortBy) {
+    case SortType.Name:
+      if (order === 'asc') {
+        return copy.sort((a, b) => a.name.localeCompare(b.name));
+      }
+
+      return copy.sort((a, b) => b.name.localeCompare(a.name));
+
+    case SortType.Surname:
+      if (order === 'asc') {
+        return copy.sort((a, b) => a.surname.localeCompare(b.surname));
+      }
+
+      return copy.sort((a, b) => b.surname.localeCompare(a.surname));
+
+    case SortType.Age:
+      if (order === 'asc') {
+        return copy.sort((a, b) => a.age - b.age);
+      }
+
+      return copy.sort((a, b) => b.age - a.age);
+
+    case SortType.Married:
+      if (order === 'asc') {
+        return copy.sort((a, b) => +a.married - +b.married);
+      }
+
+      return copy.sort((a, b) => +b.married - +a.married);
+
+    case SortType.AverageGrade:
+      if (order === 'asc') {
+        return copy
+          .sort(
+            (
+              a,
+              b,
+            ) => calculateAverageAge(a.grades) - calculateAverageAge(b.grades),
+          );
+      }
+
+      return copy
+        .sort(
+          (
+            a,
+            b,
+          ) => calculateAverageAge(b.grades) - calculateAverageAge(a.grades),
+        );
+
+    default:
+      throw new Error('Sorry, wrong arguments');
+  }
 }
