4084a3bdc0e4653c3d3b1347a7351c6dffea35d7
Jamal Amoian <leonidagutin09@gmail.com>
2022-08-30 14:06:46 +0300

    Solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,78 @@
-
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name,
+  Surname,
+  Age,
+  Married,
+  AverageGrade,
 }
 
 // create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'dsc';
 
+function calcSum(numbers): number {
+  const sum = numbers.reduce((prev: number, curr: number) => prev + curr);
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  return sum / numbers.length;
+}
+
+export function sortStudents(
+  students: Student,
+  sortBy: SortType,
+  order: SortOrder,
+): Student {
+  const tempStudents = Object.assign([], students);
+
+  switch (sortBy) {
+    case SortType.Name:
+      if (order === 'asc') {
+        return tempStudents.sort((a, b) => a.name.localeCompare(b.name));
+      }
+
+      return tempStudents.sort((a, b) => b.name.localeCompare(a.name));
+
+    case SortType.Surname:
+      if (order === 'dsc') {
+        return tempStudents.sort((a, b) => b.surname.localeCompare(a.surname));
+      }
+
+      return tempStudents.sort((a, b) => a.surname.localeCompare(b.surname));
+
+    case SortType.Age:
+      if (order === 'asc') {
+        return tempStudents.sort((a, b) => a.age - b.age);
+      }
+
+      return tempStudents.sort((a, b) => b.age - a.age);
+
+    case SortType.Married:
+      if (order === 'asc') {
+        return tempStudents.sort((a, b) => a.married - b.married);
+      }
+
+      return tempStudents.sort((a, b) => b.married - a.married);
+
+    case SortType.AverageGrade:
+      if (order === 'asc') {
+        return tempStudents.sort(
+          (a, b) => calcSum(a.grades) - calcSum(b.grades),
+        );
+      }
+
+      return tempStudents.sort(
+        (a, b) => calcSum(b.grades) - calcSum(a.grades),
+      );
+
+    default:
+      break;
+  }
+
+  return tempStudents;
 }
