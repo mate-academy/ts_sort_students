f8f963a1c48dc584f9d26827f48f314e66e61290
Lesya Korniychuk <korniychuklesya@ukr.net>
2022-08-30 21:24:02 +0300

    solutions

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,64 @@
+// import { resourceLimits } from "worker_threads";
 
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[],
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades'
 }
 
 // create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const studentsCopy = [...students];
+  const orderSwitch: number = (order === 'asc') ? 1 : -1;
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  switch (sortBy) {
+    case SortType.Name:
+      studentsCopy
+        .sort((prev, next) => prev.name.localeCompare(next.name)
+        * orderSwitch);
+      break;
+    case SortType.Surname:
+      studentsCopy
+        .sort((prev, next) => prev.surname.localeCompare(next.surname)
+        * orderSwitch);
+      break;
+    case SortType.Age:
+      studentsCopy
+        .sort((prev, next) => (prev.age - next.age)
+        * orderSwitch);
+      break;
+    case SortType.Married:
+      studentsCopy
+        .sort((prev, next) => (+prev.married - +next.married)
+        * orderSwitch);
+      break;
+    default:
+      studentsCopy
+        .sort((prev, next) => (
+          ((prev.grades
+            .reduce((numPrev, numNext) => numPrev + numNext, 0))
+            / prev.grades.length)
+          - ((next.grades
+            .reduce((numPrev, numNext) => numPrev + numNext, 0))
+            / next.grades.length))
+          * orderSwitch);
+  }
+
+  return studentsCopy;
 }
