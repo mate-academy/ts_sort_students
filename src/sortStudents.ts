da0fd2de640dfc9e95e32fca343ccd1c49c4d8e9
Vasyl Zinchenko <vasyl.zinchenko1991@gmail.com>
2022-08-30 14:40:20 +0300

    Sort students (.ts) task was done

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,52 @@
-
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const averageGrade = (gradesArray:number[]):number => (
+    gradesArray.reduce((sum, num) => sum + num, 0) / gradesArray.length
+  );
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  return [...students].sort((firstStudent, nextStudent) => {
+    switch (sortBy) {
+      case 'age':
+      case 'married':
+        return order === 'asc'
+          ? (+firstStudent[sortBy] - +nextStudent[sortBy])
+          : (+nextStudent[sortBy] - +firstStudent[sortBy]);
+
+      case 'name':
+      case 'surname':
+        return firstStudent[sortBy] > nextStudent[sortBy] ? 1 : -1;
+
+      case 'grades': {
+        const firstStudAverage = averageGrade(firstStudent[sortBy]);
+        const nextStudAverage = averageGrade(nextStudent[sortBy]);
+
+        return order === 'asc'
+          ? firstStudAverage - nextStudAverage
+          : nextStudAverage - firstStudAverage;
+      }
+      default:
+        throw new Error('Something went wrong!');
+    }
+  });
 }
