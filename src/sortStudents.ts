e8e7e0156b4ff24724ab12a410a2cf7d16f967d1
Olha Arama <arama_o_f@ukr.net>
2022-08-30 14:57:42 +0300

    Solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,48 @@
 
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
 // create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(students: Student[],
+  sortBy: SortType, order: SortOrder): Student[] {
+  const arr: Student[] = students.slice();
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  if (sortBy === 'name' || sortBy === 'surname') {
+    return order === 'asc'
+      ? arr.sort((a, b) => a[sortBy].localeCompare(b[sortBy]))
+      : arr.sort((a, b) => b[sortBy].localeCompare(a[sortBy]));
+  }
+
+  if (sortBy === 'age' || sortBy === 'married') {
+    return order === 'asc'
+      ? arr.sort((a, b) => +a[sortBy] - +b[sortBy])
+      : arr.sort((a, b) => +b[sortBy] - +a[sortBy]);
+  }
+
+  function average(array: number[]): number {
+    return array.reduce((a, b) => a + b) / array.length;
+  }
+
+  if (sortBy === 'grades') {
+    return order === 'asc'
+      ? arr.sort((a, b) => average(a.grades) - average(b.grades))
+      : arr.sort((a, b) => average(b.grades) - average(a.grades));
+  }
+
+  return arr;
 }
