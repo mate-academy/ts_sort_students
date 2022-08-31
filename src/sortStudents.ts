014cabf99a777b97b8832d7a9f9c57a593cec46b
Kolia Plemennyk <plemianniknikolay@gmail.com>
2022-08-31 18:26:15 +0300

    Solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,54 @@
-
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[],
+}
+
+export interface StudentCopy extends Student {
+  ag: number,
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'ag',
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] | StudentCopy[] {
+  const studentsCopy: StudentCopy[] = [...students]
+    .map((student) => ({
+      ...student,
+      ag: student.grades
+        .reduce((prev, cur) => prev + cur) / student.grades.length,
+    }));
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  switch (sortBy) {
+    case SortType.Name:
+    case SortType.Surname:
+      return studentsCopy
+        .sort((prev, cur) => (order === 'asc'
+          ? prev[sortBy].localeCompare(cur[sortBy])
+          : cur[sortBy].localeCompare(prev[sortBy])));
+
+    case SortType.Age:
+    case SortType.Married:
+    case SortType.AverageGrade:
+      return studentsCopy
+        .sort((prev, cur) => (order === 'asc'
+          ? +prev[sortBy] - +cur[sortBy]
+          : +cur[sortBy] - +prev[sortBy]));
+
+    default:
+      return students;
+  }
 }
