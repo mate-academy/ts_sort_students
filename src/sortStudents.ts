346f5a703434c9ad09ae9da458e3308e34e2e1d9
Andrii Stiebalov <stiebalov@gmail.com>
2022-08-31 15:28:19 +0300

    sort students in TS was added

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,53 @@
-
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[],
 }
+
+export type SortOrder= 'asc' | 'desc';
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades'
 }
 
-// create SortOrder type
-export type SortOrder;
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const calulateAverge = (grades: number[]): number => {
+    return grades.reduce((a, b) => a + b) / grades.length;
+  };
 
+  switch (sortBy) {
+    case 'name':
+    case 'surname':
+      return order === 'asc'
+        ? [...students].sort((a, b) => a[sortBy].localeCompare(b[sortBy]))
+        : [...students].sort((a, b) => b[sortBy].localeCompare(a[sortBy]));
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+    case 'age':
+    case 'married':
+      return order === 'asc'
+        ? [...students].sort((a, b) => +a[sortBy] - +b[sortBy])
+        : [...students].sort((a, b) => +b[sortBy] - +a[sortBy]);
+
+    case 'grades':
+      return order === 'asc'
+        ? [...students].sort((a, b) => {
+          return calulateAverge(a[sortBy]) - calulateAverge(b[sortBy]);
+        })
+        : [...students].sort((a, b) => {
+          return calulateAverge(b[sortBy]) - calulateAverge(a[sortBy]);
+        });
+
+    default:
+      throw new Error('invalid sort option');
+  }
 }
