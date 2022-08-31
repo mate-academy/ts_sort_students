ea80b810cf90e9c655bcdbf2d9e390908e1c3c97
Zinchenko Anastasiia <zincenkonasta6@gmail.com>
2022-08-31 14:21:17 +0300

    codestyle was changed

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -22,34 +22,55 @@ function averageGrades(grades: number[]): number {
   return grades.reduce((sum, grade) => sum + grade, 0) / grades.length;
 }
 
-export function sortStudents(students: Student[], sortBy: SortType,
-  order: SortOrder)
-  : Student[] {
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
   const copyStudent: Student[] = [...students];
 
   switch (sortBy) {
     case SortType.Name:
     case SortType.Surname:
       return order === 'asc'
-        ? copyStudent.sort((student, nextStudent) => student[sortBy]
-          .localeCompare(nextStudent[sortBy]))
-        : copyStudent.sort((student, nextStudent) => nextStudent[sortBy]
-          .localeCompare(student[sortBy]));
+        ? copyStudent.sort((
+          student,
+          nextStudent,
+        ) => (
+          student[sortBy].localeCompare(nextStudent[sortBy])))
+        : copyStudent.sort((
+          student,
+          nextStudent,
+        ) => (
+          nextStudent[sortBy].localeCompare(student[sortBy])));
+
     case SortType.Age:
     case SortType.Married:
       return order === 'asc'
-        ? copyStudent.sort((student, nextStudent) => Number(student[sortBy])
-          - Number(nextStudent[sortBy]))
-        : copyStudent.sort((student, nextStudent) => Number(nextStudent[sortBy])
-          - Number(student[sortBy]));
+        ? copyStudent.sort((
+          student,
+          nextStudent,
+        ) => (
+          Number(student[sortBy]) - Number(nextStudent[sortBy])))
+        : copyStudent.sort((
+          student,
+          nextStudent,
+        ) => (
+          Number(nextStudent[sortBy]) - Number(student[sortBy])));
+
     case SortType.AverageGrade:
       return order === 'asc'
-        ? copyStudent.sort((student, nextStudent) => (
-          averageGrades(student[sortBy])
-          - averageGrades(nextStudent[sortBy])))
-        : copyStudent.sort((student, nextStudent) => (
-          averageGrades(nextStudent[sortBy])
-          - averageGrades(student[sortBy])));
+        ? copyStudent.sort((
+          student,
+          nextStudent,
+        ) => (
+          averageGrades(student[sortBy]) - averageGrades(nextStudent[sortBy])))
+        : copyStudent.sort((
+          student,
+          nextStudent,
+        ) => (
+          averageGrades(nextStudent[sortBy]) - averageGrades(student[sortBy])));
+
     default:
       return copyStudent;
   }
