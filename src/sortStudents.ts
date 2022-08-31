2dbd5cc6f0472bd135cae0bd14946f99a690e86b
Pavel Ohiiko <timurid346@gmail.com>
2022-08-31 17:26:53 +0300

    Solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,56 @@
 
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[],
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
-// create SortOrder type
-export type SortOrder;
+function calcAverage(points: number[]): number {
+  return points.reduce((sum, point) => sum + point, 0) / points.length;
+}
 
+export type SortOrder = 'asc' | 'desc';
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const copyStudents: Student[] = [...students];
+
+  return copyStudents.sort((a, b) => {
+    const studentA = a[sortBy];
+    const studentB = b[sortBy];
+
+    if (typeof studentA === 'number' && typeof studentB === 'number') {
+      return order === 'asc' ? studentA - studentB : studentB - studentA;
+    }
+
+    if (typeof studentA === 'string' && typeof studentB === 'string') {
+      return order === 'asc' ? studentA.localeCompare(studentB)
+        : studentB.localeCompare(studentA);
+    }
+
+    if (typeof studentA === 'object' && typeof studentB === 'object') {
+      return order === 'asc'
+        ? calcAverage(studentA) - calcAverage(studentB)
+        : calcAverage(studentB) - calcAverage(studentA);
+    }
+
+    if (typeof studentA === 'boolean' && typeof studentB === 'boolean') {
+      return order === 'asc' ? +studentA - +studentB : +studentB - +studentA;
+    }
+
+    return 0;
+  });
 }
