8259483cee9dc1f1733d70f3a2afec998cd94991
Shmelyov Oleg <shmelyov.oleg13@gmail.com>
2022-08-31 15:30:38 +0300

    created function sortStudens

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,61 @@
 
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[]
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name,
+  Surname,
+  Age,
+  Married,
+  AverageGrade,
 }
 
 // create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
-
-export function sortStudents(students, sortBy, order) {
+export function sortStudents(
+  students: Array<Student>,
+  sortBy: SortType,
+  order: SortOrder,
+): Array<Student> {
   // write your function
+  const result: Array<Student> = [...students]
+    .sort((a, b) => {
+      switch (sortBy) {
+        case SortType.Age:
+          return order === 'asc' ? a.age - b.age : b.age - a.age;
+
+        case SortType.Name:
+          return order === 'asc'
+            ? a.name.localeCompare(b.name)
+            : b.name.localeCompare(a.name);
+
+        case SortType.Married:
+          return order === 'asc'
+            ? Number(a.married) - Number(b.married)
+            : Number(b.married) - Number(a.married);
+
+        case SortType.Surname:
+          return order === 'asc'
+            ? a.surname.localeCompare(b.surname)
+            : b.surname.localeCompare(a.surname);
+
+        case SortType.AverageGrade:
+          return order === 'asc'
+            ? a.grades.reduce((acc, curr) => acc + curr, 0) / a.grades.length
+              - b.grades.reduce((acc, curr) => acc + curr, 0) / b.grades.length
+            : b.grades.reduce((acc, curr) => acc + curr, 0) / b.grades.length
+              - a.grades.reduce((acc, curr) => acc + curr, 0) / a.grades.length;
+
+        default:
+          return 0;
+      }
+    });
+
+  return result;
 }
