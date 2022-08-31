2bbd3546c06d3dd23c4b12e835c817c38c62ef86
Kostya Kosyuk <80011474+Kostya7337@users.noreply.github.com>
2022-08-31 23:08:12 +0300

    solution 1

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,55 @@
-
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[],
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'averageGrade',
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+function getAvarageGrade(student: Student): number {
+  return student.grades.reduce((
+    prevSum: number,
+    currentGrade:
+    number,
+  ) => prevSum + currentGrade) / student.grades.length;
+}
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  return [...students].sort((firstStudent: Student, secondStudent: Student) => {
+    switch (sortBy) {
+      case SortType.Name:
+      case SortType.Surname:
+        return order === 'asc'
+          ? firstStudent[sortBy].localeCompare(secondStudent[sortBy])
+          : secondStudent[sortBy].localeCompare(firstStudent[sortBy]);
+
+      case SortType.Age:
+      case SortType.Married:
+        return order === 'asc'
+          ? +firstStudent[sortBy] - +secondStudent[sortBy]
+          : +secondStudent[sortBy] - +firstStudent[sortBy];
+
+      case SortType.AverageGrade: {
+        return order === 'asc'
+          ? getAvarageGrade(firstStudent) - getAvarageGrade(secondStudent)
+          : getAvarageGrade(secondStudent) - getAvarageGrade(firstStudent);
+      }
+      default:
+        return 0;
+    }
+  };)
 }
