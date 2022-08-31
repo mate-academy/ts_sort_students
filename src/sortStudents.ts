d5f2dbabca965582f58eade65a4918bc7ebdecdd
Andrii Kuzmenko <andriikuzmenko35@gmail.com>
2022-08-31 19:57:07 +0300

    solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,76 @@
 
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const studentsCopy: Student[] = JSON.parse(JSON.stringify(students));
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  function countAverage(grades: number[]): number {
+    return grades.reduce((grade1: number,
+      grade2: number) => grade1 + grade2, 0) / grades.length;
+  }
+
+  switch (true) {
+    case ((sortBy === SortType.Name || sortBy === SortType.Surname)
+      && order === 'asc'): {
+      studentsCopy.sort((student1: Student,
+        student2: Student) => String(student1[sortBy])
+        .localeCompare(String(student2[sortBy])));
+      break;
+    }
+
+    case ((sortBy === SortType.Name || sortBy === SortType.Surname)
+      && order === 'desc'): {
+      studentsCopy.sort((student1: Student,
+        student2: Student) => String(student2[sortBy])
+        .localeCompare(String(student1[sortBy])));
+      break;
+    }
+
+    case ((sortBy === SortType.Age || sortBy === SortType.Married)
+      && order === 'asc'): {
+      studentsCopy.sort((student1: Student,
+        student2: Student) => +(student1[sortBy]) - +(student2[sortBy]));
+      break;
+    }
+
+    case ((sortBy === SortType.Age || sortBy === SortType.Married)
+      && order === 'desc'): {
+      studentsCopy.sort((student1: Student,
+        student2: Student) => +(student2[sortBy]) - +(student1[sortBy]));
+      break;
+    }
+
+    case (sortBy === SortType.AverageGrade && order === 'asc'): {
+      studentsCopy.sort((student1: Student,
+        student2: Student) => countAverage(student1.grades)
+        - countAverage(student2.grades));
+      break;
+    }
+
+    default: studentsCopy.sort((student1: Student,
+      student2: Student) => countAverage(student2.grades)
+      - countAverage(student1.grades));
+  }
+
+  return studentsCopy;
 }
