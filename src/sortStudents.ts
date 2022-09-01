1baa391c63331c5892f656c4e36813c8f60233e6
Sasha Gordijchuk <san4ez1717@gmail.com>
2022-09-01 21:48:18 +0300

    sorting was fixed

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -27,42 +27,26 @@ export function sortStudents(
   sortedStudens.forEach((student) => {
     const changedStudent = student;
 
-    changedStudent.averageGrade = student.grades.reduce((a, b) => a + b)
-    / student.grades.length;
+    changedStudent.averageGrade = student.grades
+      .reduce((sum, grade) => sum + grade) / student.grades.length;
   });
 
-  switch (order) {
-    case 'asc':
-      sortedStudens.sort((first: Student, second: Student) => {
-        if (first[sortBy] < second[sortBy]) {
-          return -1;
-        }
+  sortedStudens.sort((firstStudent: Student, secondStudent: Student) => {
+    switch (order) {
+      case 'asc':
+        return firstStudent[sortBy] < secondStudent[sortBy]
+          ? -1
+          : 1;
 
-        if (first[sortBy] > second[sortBy]) {
-          return 1;
-        }
+      case 'desc':
+        return firstStudent[sortBy] > secondStudent[sortBy]
+          ? -1
+          : 1;
 
+      default:
         return 0;
-      });
-      break;
-
-    case 'desc':
-      sortedStudens.sort((first: Student, second: Student) => {
-        if (first[sortBy] > second[sortBy]) {
-          return -1;
-        }
-
-        if (first[sortBy] < second[sortBy]) {
-          return 1;
-        }
-
-        return 0;
-      });
-      break;
-
-    default:
-      return sortedStudens;
-  }
+    }
+  });
 
   return sortedStudens;
 }
