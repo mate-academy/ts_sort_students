8b842a3bfbc2697c23e83a66fcd5c84f103df7fb
DominoSL <artemenko.mik@gmail.com>
2022-09-06 10:42:46 +0300

    create ts sorter

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,80 @@
+function getAverage(arr: number[]): number {
+  return arr.reduce((a, b) => a + b, 0) / arr.length;
+}
 
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: true;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name,
+  Surname,
+  Age,
+  Married,
+  AverageGrade,
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const studentsCopy: Student[] = [...students];
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  switch (sortBy) {
+    case SortType.Name: {
+      return studentsCopy.sort((
+        studentA, studentB,
+      ) => (order === 'asc'
+        ? studentA.name.localeCompare(studentB.name)
+        : studentB.name.localeCompare(studentA.name)
+      ));
+    }
+
+    case SortType.Surname: {
+      return studentsCopy.sort((
+        studentA, studentB,
+      ) => (order === 'asc'
+        ? studentA.surname.localeCompare(studentB.surname)
+        : studentB.surname.localeCompare(studentA.surname)
+      ));
+    }
+
+    case SortType.Age: {
+      return studentsCopy.sort((
+        studentA, studentB,
+      ) => (order === 'asc'
+        ? studentA.age - studentB.age
+        : studentB.age - studentA.age
+      ));
+    }
+
+    case SortType.Married: {
+      return studentsCopy.sort((
+        studentA, studentB,
+      ) => (order === 'asc'
+        ? Number(studentA.married) - Number(studentB.married)
+        : Number(studentB.married) - Number(studentA.married)
+      ));
+    }
+
+    case SortType.AverageGrade: {
+      return studentsCopy.sort((
+        studentA, studentB,
+      ) => (order === 'asc'
+        ? getAverage(studentA.grades) - getAverage(studentB.grades)
+        : getAverage(studentB.grades) - getAverage(studentA.grades)
+      ));
+    }
+
+    default: {
+      throw new Error('Wrong sorting type');
+    }
+  }
 }
