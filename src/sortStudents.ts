036fb00108a6e28cd6eefd4764f32e363f22e356
Anton <shulezhko.antony@gmail.com>
2022-09-13 11:47:14 +0300

    done

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,81 @@
 
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const unsorted: Student[] = [...students];
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  switch (sortBy) {
+    case SortType.Name:
+    case SortType.Surname:
+      if (order === 'asc') {
+        unsorted
+          .sort((a: Student, b: Student) => a[sortBy].localeCompare(b[sortBy]));
+      }
+
+      if (order === 'desc') {
+        unsorted
+          .sort((a: Student, b: Student) => b[sortBy].localeCompare(a[sortBy]));
+      }
+      break;
+
+    case SortType.Age:
+    case SortType.Married:
+      if (order === 'asc') {
+        unsorted.sort((a, b) => {
+          return (+a[sortBy] - +b[sortBy]);
+        });
+      }
+
+      if (order === 'desc') {
+        unsorted.sort((a, b) => {
+          return (+b[sortBy] - +a[sortBy]);
+        });
+      }
+      break;
+
+    case SortType.AverageGrade:
+      if (order === 'asc') {
+        unsorted.sort((a, b) => {
+          const sumA = a[sortBy].reduce((x, y) => x + y);
+          const sumB = b[sortBy].reduce((x, y) => x + y);
+
+          return sumA / a.grades.length - sumB / b.grades.length;
+        });
+      }
+
+      if (order === 'desc') {
+        unsorted.sort((a, b) => {
+          const sumA = a[sortBy].reduce((x, y) => x + y);
+          const sumB = b[sortBy].reduce((x, y) => x + y);
+
+          return sumB / b[sortBy].length - sumA / a[sortBy].length;
+        });
+      }
+      break;
+
+    default:
+      break;
+  }
+
+  return unsorted;
 }
