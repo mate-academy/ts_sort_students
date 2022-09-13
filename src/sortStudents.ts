2d32d97f41ebcebcf034f74239417e6c4529fd4f
HusakOleh <husakoleh91@gmail.com>
2022-09-13 12:37:42 +0300

    implemented solution with switc case

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -25,95 +25,103 @@ export function sortStudents(
   const resArr: Student[] = [...students];
   const sortOrder: boolean = order === 'asc';
 
-  if (sortBy === SortType.Name) {
-    resArr.sort((a: Student, b: Student): number => {
-      if (sortOrder) {
-        return a.name.localeCompare(b.name);
-      }
-
-      return b.name.localeCompare(a.name);
-    });
-  } else if (sortBy === SortType.Surname) {
-    resArr.sort((a: Student, b: Student): number => {
-      if (sortOrder) {
-        return a.surname.localeCompare(b.surname);
-      }
-
-      return b.surname.localeCompare(a.surname);
-    });
-  } else if (sortBy === SortType.Age) {
-    resArr.sort((a: Student, b: Student): number => {
-      let res = 0;
-
-      if (Number(a.age) > Number(b.age)) {
+  switch (sortBy) {
+    case SortType.Name:
+      resArr.sort((a: Student, b: Student): number => {
         if (sortOrder) {
-          res = 1;
-        } else {
-          res = -1;
+          return a.name.localeCompare(b.name);
         }
-      }
 
-      if (Number(a.age) < Number(b.age)) {
-        if (!sortOrder) {
-          res = 1;
-        } else {
-          res = -1;
-        }
-      }
-
-      return res;
-    });
-  } else if (sortBy === SortType.Married) {
-    resArr.sort((a: Student, b: Student): number => {
-      let res = 0;
-
-      if (a.married && !b.married) {
+        return b.name.localeCompare(a.name);
+      });
+      break;
+    case SortType.Surname:
+      resArr.sort((a: Student, b: Student): number => {
         if (sortOrder) {
-          res = 1;
-        } else {
-          res = -1;
+          return a.surname.localeCompare(b.surname);
         }
-      }
 
-      if (!a.married && b.married) {
-        if (!sortOrder) {
-          res = 1;
-        } else {
-          res = -1;
+        return b.surname.localeCompare(a.surname);
+      });
+      break;
+    case SortType.Age:
+      resArr.sort((a: Student, b: Student): number => {
+        let res = 0;
+
+        if (Number(a.age) > Number(b.age)) {
+          if (sortOrder) {
+            res = 1;
+          } else {
+            res = -1;
+          }
         }
-      }
 
-      return res;
-    });
-  } else if (sortBy === SortType.AverageGrade) {
-    resArr.sort((a: Student, b: Student): number => {
-      const averageStudentGrade = (student: Student):number => {
-        return student
-          .grades.reduce((accum: number, grade: number): number => {
-            return accum + grade;
-          }, 0) / student.grades.length;
-      };
-
-      let res = 0;
-
-      if (averageStudentGrade(a) > averageStudentGrade(b)) {
-        if (sortOrder) {
-          res = 1;
-        } else {
-          res = -1;
+        if (Number(a.age) < Number(b.age)) {
+          if (!sortOrder) {
+            res = 1;
+          } else {
+            res = -1;
+          }
         }
-      }
 
-      if (averageStudentGrade(a) < averageStudentGrade(b)) {
-        if (!sortOrder) {
-          res = 1;
-        } else {
-          res = -1;
+        return res;
+      });
+      break;
+    case SortType.Married:
+      resArr.sort((a: Student, b: Student): number => {
+        let res = 0;
+
+        if (a.married && !b.married) {
+          if (sortOrder) {
+            res = 1;
+          } else {
+            res = -1;
+          }
         }
-      }
 
-      return res;
-    });
+        if (!a.married && b.married) {
+          if (!sortOrder) {
+            res = 1;
+          } else {
+            res = -1;
+          }
+        }
+
+        return res;
+      });
+      break;
+    case SortType.AverageGrade:
+      resArr.sort((a: Student, b: Student): number => {
+        const averageStudentGrade = (student: Student):number => {
+          return student
+            .grades.reduce((accum: number, grade: number): number => {
+              return accum + grade;
+            }, 0) / student.grades.length;
+        };
+
+        let res = 0;
+
+        if (averageStudentGrade(a) > averageStudentGrade(b)) {
+          if (sortOrder) {
+            res = 1;
+          } else {
+            res = -1;
+          }
+        }
+
+        if (averageStudentGrade(a) < averageStudentGrade(b)) {
+          if (!sortOrder) {
+            res = 1;
+          } else {
+            res = -1;
+          }
+        }
+
+        return res;
+      });
+      break;
+    default:
+      return resArr;
   }
 
   return resArr;
