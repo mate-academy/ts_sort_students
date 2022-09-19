e2f653dc6828a3f2dc1137c5ecdfe457ea67dafe
Oleksandr Malyshev <sanyokmalyshev@gmail.com>
2022-09-19 21:34:02 +0800

    Task done

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,61 @@
-
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[],
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(
+  students: Student[], sortBy: SortType, order: SortOrder,
+): Student[] {
+  const copyStudents = students.map((student) => {
+    return { ...student };
+  });
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  copyStudents.sort((a: Student, b: Student): number => {
+    let result;
+    let value1 = a[sortBy];
+    let value2 = b[sortBy];
+
+    if (order === 'desc') {
+      value1 = b[sortBy];
+      value2 = a[sortBy];
+    }
+
+    if (typeof value1 === 'number' && typeof value2 === 'number') {
+      result = value1 - value2;
+    }
+
+    if (typeof value1 === 'string' && typeof value2 === 'string') {
+      result = value1.localeCompare(value2);
+    }
+
+    if (typeof value1 === 'boolean' && typeof value2 === 'boolean') {
+      result = Number(value1) - Number(value2);
+    }
+
+    if (Array.isArray(value1) && Array.isArray(value2)) {
+      const average1
+        = value1.reduce((num1, num2) => (num1 + num2)) / value1.length;
+      const average2
+        = value2.reduce((num1, num2) => (num1 + num2)) / value2.length;
+
+      result = average1 - average2;
+    }
+
+    return result;
+  });
+
+  return copyStudents;
 }
