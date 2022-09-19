2ee3af14ab7d2f76fb91c56e53f0c43ee50775aa
Oleksii <oleksii@it.com>
2022-09-19 13:03:02 +0300

    Done

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,70 @@
 
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[],
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+function getAverage(grades: number[]): number {
+  return grades.reduce((sum, value) => sum + value, 0) / grades.length;
+}
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const sortedStudents: Student[] = [...students];
+
+  switch (sortBy) {
+    case SortType.Name:
+    case SortType.Surname:
+      if (order === 'asc') {
+        return sortedStudents.sort((a, b) => {
+          return a[sortBy].localeCompare(b[sortBy]);
+        });
+      }
+
+      return sortedStudents.sort((a, b) => {
+        return b[sortBy].localeCompare(a[sortBy]);
+      });
+
+    case SortType.Age:
+    case SortType.Married:
+      if (order === 'asc') {
+        return sortedStudents.sort((a, b) => {
+          return +a[sortBy] - +b[sortBy];
+        });
+      }
+
+      return sortedStudents.sort((a, b) => {
+        return +b[sortBy] - +a[sortBy];
+      });
+
+    case SortType.AverageGrade:
+      if (order === 'asc') {
+        return sortedStudents.sort((a, b) => {
+          return getAverage(a[sortBy]) - getAverage(b[sortBy]);
+        });
+      }
+
+      return sortedStudents.sort((a, b) => {
+        return getAverage(b[sortBy]) - getAverage(a[sortBy]);
+      });
+
+    default:
+      throw new Error('Invalid parameters received!');
+  }
 }
