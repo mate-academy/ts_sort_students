4d0ff3fc33ec947ce5bd3248d65a6d1603a5e0d1
Saleh BUD <saleh.7734@gmail.com>
2022-09-27 08:19:03 +0330

    try 2

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,90 +1,58 @@
 export interface Student {
-  name: string,
-  surname: string,
-  age: number,
-  married: boolean,
-  grades: number[],
+  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
+  // describe SortType enum
   Name = 'name',
   Surname = 'surname',
   Age = 'age',
   Married = 'married',
-  AverageGrade = 'grades',
+  AverageGrade = 'averageGrade',
 }
 
 // create SortOrder type
 export type SortOrder = 'asc' | 'desc';
 
-export function nameSurnameAge(obj: Student[]): string[] {
-  const res: string[] = [];
-
-  for (let i: number = 0; i < obj.length; i += 1) {
-    const person: string = `${obj[i].name} ${obj[i].surname} ${obj[i].age}`;
-
-    res.push(person);
-  }
-
-  return res;
-}
-
-export function nameSurnameGrades(obj: Student[]): string[] {
-  const res: string[] = [];
-
-  for (let i: number = 0; i < obj.length; i += 1) {
-    const person: string
-    = `${obj[i].name} ${obj[i].surname} [${obj[i].grades}]`;
-
-    res.push(person);
-  }
-
-  return res;
-}
-
-export function avgGrades(grades: number[]): number {
-  let res: number = 0;
-
-  for (let i: number = 0; i < grades.length; i += 1) {
-    res += grades[i];
-  }
-
-  return res / grades.length;
-}
-
 export function sortStudents(
-  std: Student[],
-  srtBy: SortType,
-  ordr: SortOrder,
-): string[] {
-  let res: string[] = [];
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const copyStudents = [...students];
 
-  switch (srtBy) {
+  const averageGrade = (grades: number[]): number => {
+    return grades.reduce((acc, curV) => acc + curV, 0) / grades.length;
+  };
+
+  switch (sortBy) {
     case SortType.Name:
     case SortType.Surname:
-      std.sort((a, b) => (ordr === 'asc'
-        ? a[srtBy].localeCompare(b[srtBy])
-        : b[srtBy].localeCompare(a[srtBy])));
-      res = nameSurnameAge(std);
-      break;
+      return order === 'desc'
+        ? copyStudents.sort((a, b) => b[sortBy].localeCompare(a[sortBy]))
+        : copyStudents.sort((a, b) => a[sortBy].localeCompare(b[sortBy]));
 
-    case SortType.Age:
     case SortType.Married:
-      std.sort((a, b) => (ordr === 'asc'
-        ? Number(a[srtBy]) - Number(b[srtBy])
-        : Number(b[srtBy]) - Number(a[srtBy])));
-      res = nameSurnameAge(std);
-      break;
+    case SortType.Age:
+      return order === 'desc'
+        ? copyStudents.sort((a, b) => +b[sortBy] - +a[sortBy])
+        : copyStudents.sort((a, b) => +a[sortBy] - +b[sortBy]);
 
     case SortType.AverageGrade:
-      std.sort((a, b) => (ordr === 'asc'
-        ? avgGrades(a.grades) - avgGrades(b.grades)
-        : avgGrades(b.grades) - avgGrades(a.grades)));
-      res = nameSurnameGrades(std);
-      break;
+      return order === 'desc'
+        ? copyStudents.sort(
+          (a, b) => averageGrade(b.grades) - averageGrade(a.grades),
+        )
+        : copyStudents.sort(
+          (a, b) => averageGrade(a.grades) - averageGrade(b.grades),
+        );
 
-    default: break;
+    default:
+      return copyStudents;
   }
-
-  return res;
 }
