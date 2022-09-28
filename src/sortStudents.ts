fb15c700162956ee57c3b55c5bb5bbb6218598df
Max Bovtun <maxbovtun@gmail.com>
2022-09-28 22:04:07 +0300

    solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,74 @@
 
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[],
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(
+  students: Student[], sortBy: SortType, order: SortOrder,
+): Student[] {
+  const sortedStudents: Student[] = [...students];
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  // Sort by name and surname and return result:
+  if (sortBy === 'name' || sortBy === 'surname') {
+    sortedStudents.sort((prev: Student, curr: Student) => (
+      prev[sortBy].localeCompare(curr[sortBy])
+    ));
+
+    return order === 'asc' ? sortedStudents : sortedStudents.reverse();
+  }
+
+  // Sort by age:
+  if (sortBy === 'age') {
+    sortedStudents.sort((prev: Student, curr: Student) => (
+      order === 'asc'
+        ? prev[sortBy] - curr[sortBy]
+        : curr[sortBy] - prev[sortBy]
+    ));
+
+  // Sort by married:
+  } else if (sortBy === 'married') {
+    sortedStudents.sort((prev: Student, curr: Student) => (
+      order === 'asc'
+        ? Number(prev[sortBy]) - Number(curr[sortBy])
+        : Number(curr[sortBy]) - Number(prev[sortBy])
+    ));
+
+  // Sort by grades:
+  } else if (sortBy === 'grades') {
+    sortedStudents.sort((prev: Student, curr: Student) => (
+      order === 'asc'
+
+        // ASC:
+        ? (prev[sortBy].reduce(
+          (accum: number, current: number) => accum + current,
+        ) / prev[sortBy].length)
+        - (curr[sortBy].reduce(
+          (accum: number, current: number) => accum + current,
+        ) / curr[sortBy].length)
+
+        // DESC:
+        : (curr[sortBy].reduce(
+          (accum: number, current: number) => accum + current,
+        ) / curr[sortBy].length)
+          - (prev[sortBy].reduce(
+            (accum: number, current: number) => accum + current,
+          ) / prev[sortBy].length)
+    ));
+  }
+
+  return sortedStudents;
 }
