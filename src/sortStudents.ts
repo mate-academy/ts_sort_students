0a1340c1287beeb5c98f8985efc901558ccaafc5
Ihor Andrianov <andrianoia@gmail.com>
2022-09-30 00:32:07 +0300

    solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,76 @@
 
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
+  id?: number;
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'id',
 }
 
 // create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function getResult(
+  students: Student[],
+  criteria: SortType,
+  order: SortOrder,
+): Student[] {
+  const studentsCopy = (criteria === SortType.AverageGrade)
+    ? students.map((student) => (
+      {
+        ...student,
+        id: student.grades.reduce((a, b) => a + b, 0) / student.grades.length,
+      }
+    ))
+    : students.map((student, id) => ({
+      ...student, id,
+    }));
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  if (order === 'asc') {
+    return studentsCopy.sort((currentStud, nextStud) => {
+      if (currentStud[criteria] < nextStud[criteria]) {
+        return -1;
+      }
+
+      if (currentStud[criteria] > nextStud[criteria]) {
+        return 1;
+      }
+
+      return 0;
+    });
+  }
+
+  if (order === 'desc') {
+    return studentsCopy
+      .sort((currentStud, nextStud) => {
+        if (currentStud[criteria] > nextStud[criteria]) {
+          return -1;
+        }
+
+        if (currentStud[criteria] < nextStud[criteria]) {
+          return 1;
+        }
+
+        return 0;
+      });
+  }
+
+  return students;
+}
+
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  return getResult(students, sortBy, order);
 }
