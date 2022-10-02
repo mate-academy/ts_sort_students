6cdd8099dff980a73a7a76e023e7cea7a42d8210
Andriy Shepeha <andrii.shepeha.kn.2021@lpnu.ua>
2022-10-02 18:51:05 +0300

    Solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,84 @@
+// import { type } from "os";
 
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grade',
 }
 
 // create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const studentsCopy: Student[] = [...students];
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  switch (sortBy) {
+    case 'name':
+      studentsCopy.sort((firstPerson: Student, secondPerson: Student) => (
+        order === 'asc'
+          ? firstPerson.name.localeCompare(secondPerson.name)
+          : secondPerson.name.localeCompare(firstPerson.name)
+      ));
+      break;
+
+    case 'surname':
+      studentsCopy.sort((firstPerson: Student, secondPerson: Student) => (
+        order === 'asc'
+          ? firstPerson.surname.localeCompare(secondPerson.surname)
+          : secondPerson.surname.localeCompare(firstPerson.surname)
+      ));
+
+      break;
+
+    case 'age':
+      studentsCopy.sort((firstPerson: Student, secondPerson: Student) => (
+        order === 'asc'
+          ? firstPerson.age - secondPerson.age
+          : secondPerson.age - firstPerson.age
+      ));
+      break;
+
+    case 'married':
+      studentsCopy.sort((firstPerson: Student, secondPerson: Student) => (
+        order === 'asc'
+          ? +firstPerson.married - +secondPerson.married
+          : +secondPerson.married - +firstPerson.married
+      ));
+      break;
+
+    case 'grade':
+      studentsCopy.sort((firstPerson: Student, secondPerson: Student) => {
+        const firstPersonAvgGrade = firstPerson.grades.reduce(
+          (accum: number, curr: number) => accum + curr,
+        ) / firstPerson.grades.length;
+
+        const secondPersonAvgGrade = secondPerson.grades.reduce(
+          (accum: number, curr: number) => accum + curr,
+        ) / secondPerson.grades.length;
+
+        return order === 'asc'
+          ? firstPersonAvgGrade - secondPersonAvgGrade
+          : secondPersonAvgGrade - firstPersonAvgGrade;
+      });
+      break;
+
+    default:
+      break;
+  }
+
+  return studentsCopy;
 }
