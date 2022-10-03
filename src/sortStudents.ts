6ef9fc84cc2a49f6c8c207786ddbe820ddffcc3f
Polly Slipchenko <marmoria.1999science@gmail.com>
2022-10-03 20:04:57 +0300

    Solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,70 @@
-
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[],
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const studentsCopy = [...students];
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  switch (sortBy) {
+    case SortType.Name:
+    case SortType.Surname:
+      studentsCopy.sort((currStudent, nextStudent) => (
+        order === 'asc'
+          ? currStudent[sortBy].localeCompare(nextStudent[sortBy])
+          : nextStudent[sortBy].localeCompare(currStudent[sortBy])
+      ));
+      break;
+
+    case SortType.Age:
+      studentsCopy.sort((currStudent, nextStudent) => (
+        order === 'asc'
+          ? currStudent[sortBy] - nextStudent[sortBy]
+          : nextStudent[sortBy] - currStudent[sortBy]
+      ));
+      break;
+
+    case SortType.Married:
+      studentsCopy.sort((currStudent, nextStudent) => (
+        order === 'asc'
+          ? +currStudent[sortBy] - +nextStudent[sortBy]
+          : +nextStudent[sortBy] - +currStudent[sortBy]
+      ));
+      break;
+
+    case SortType.AverageGrade:
+      studentsCopy.sort((currStudent, nextStudent) => {
+        const currStudentAver = currStudent[sortBy]
+          .reduce((acc, curr) => acc + curr, 0) / currStudent[sortBy].length;
+        const nextStudentAver = nextStudent[sortBy]
+          .reduce((acc, curr) => acc + curr, 0) / nextStudent[sortBy].length;
+
+        return order === 'asc'
+          ? currStudentAver - nextStudentAver
+          : nextStudentAver - currStudentAver;
+      });
+      break;
+
+    default:
+      return studentsCopy.sort();
+  }
+
+  return studentsCopy;
 }
