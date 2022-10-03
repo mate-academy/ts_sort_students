dd5ca1eb4b1b33149810ce306a6c015892de1995
Iryna Borniak <la.aerea.1@gmail.com>
2022-10-03 21:03:39 +0300

    Solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,76 @@
-
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+function calcAverage(grades: number[]): number {
+  return grades.reduce((acc, cur) => acc + cur, 0) / grades.length;
+}
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const studentsCopy = [...students];
+
+  switch (sortBy) {
+    case SortType.Name:
+      studentsCopy.sort((prev: Student, next: Student) => {
+        return order === 'asc'
+          ? prev[sortBy].localeCompare(next[sortBy])
+          : next[sortBy].localeCompare(prev[sortBy]);
+      });
+      break;
+
+    case SortType.Surname:
+      studentsCopy.sort((prev: Student, next: Student) => {
+        return order === 'asc'
+          ? prev[sortBy].localeCompare(next[sortBy])
+          : next[sortBy].localeCompare(prev[sortBy]);
+      });
+      break;
+
+    case SortType.Age:
+      studentsCopy.sort((prev: Student, next: Student) => {
+        return order === 'asc'
+          ? prev[sortBy] - next[sortBy]
+          : next[sortBy] - prev[sortBy];
+      });
+      break;
+
+    case SortType.Married:
+      studentsCopy.sort((prev: Student, next: Student) => {
+        return order === 'asc'
+          ? Number(prev[sortBy]) - Number(next[sortBy])
+          : Number(next[sortBy]) - Number(prev[sortBy]);
+      });
+      break;
+
+    case SortType.AverageGrade:
+      studentsCopy.sort((prev: Student, next: Student) => {
+        return order === 'asc'
+          ? calcAverage(prev[sortBy]) - calcAverage(next[sortBy])
+          : calcAverage(next[sortBy]) - calcAverage(prev[sortBy]);
+      });
+      break;
+
+    default:
+      return studentsCopy;
+  }
+
+  return studentsCopy;
 }
