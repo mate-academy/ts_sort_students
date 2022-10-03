99898be6c65213ce5b0433df7fc5116765922160
Killdex <kdcompany2018@gmail.com>
2022-10-03 14:41:30 +0300

    students have been sorted

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,77 @@
-
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'averageGrade',
 }
 
 // create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+function calculateAverageGrade(student: Student): number {
+  const sumOfMarks
+    = student.grades.reduce((acc: number, prev: number) => acc + prev, 0);
+  const allMarks = student.grades.length;
+  const averageMarks = sumOfMarks / allMarks;
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  return averageMarks;
+}
+
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const outputArray: Student[] = [...students];
+
+  switch (sortBy) {
+    case 'age':
+      outputArray.sort((currentStudent, previousStudent) => (
+        order === 'asc'
+          ? currentStudent.age - previousStudent.age
+          : previousStudent.age - currentStudent.age
+      ));
+      break;
+
+    case 'name':
+    case 'surname':
+      outputArray.sort((currentStudent, previousStudent) => (
+        order === 'asc'
+          ? currentStudent[sortBy].localeCompare(previousStudent[sortBy])
+          : previousStudent[sortBy].localeCompare(currentStudent[sortBy])
+      ));
+      break;
+
+    case 'married':
+      outputArray.sort((currentStudent, previousStudent) => (
+        order === 'asc'
+          ? +currentStudent.married - +previousStudent.married
+          : +previousStudent.married - +currentStudent.married
+      ));
+      break;
+
+    case 'averageGrade':
+      outputArray.sort((currentStudent, previousStudent) => (
+        order === 'asc'
+          ? calculateAverageGrade(currentStudent)
+            - calculateAverageGrade(previousStudent)
+          : calculateAverageGrade(previousStudent)
+            - calculateAverageGrade(currentStudent)
+      ));
+      break;
+
+    default:
+      break;
+  }
+
+  return outputArray;
 }
