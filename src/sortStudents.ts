8e95221099059ac96dfe51a456a8cbb82f0f9fcf
Volodymyr Nosachenko <vovanosachenko@meta.ua>
2022-10-03 15:52:25 +0300

    sort students task

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,77 @@
-
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[]
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades'
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+function averageGrades(grades: number[]): number {
+  return grades.reduce((acc: number, curr: number) => (
+    acc + curr
+  ), 0) / grades.length;
+}
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const studentsList: Student[] = JSON.parse(JSON.stringify(students));
+
+  switch (sortBy) {
+    case SortType.Name:
+    case SortType.Surname:
+      return order === 'asc'
+        ? studentsList
+          .sort((studA: Student, studB: Student) => (
+            studA[sortBy].localeCompare(studB[sortBy])
+          ))
+        : studentsList
+          .sort((studA: Student, studB: Student) => (
+            studA[sortBy].localeCompare(studB[sortBy])
+          ));
+
+    case SortType.Age:
+    case SortType.AverageGrade:
+    case SortType.Married:
+      return order === 'asc'
+        ? studentsList
+          .sort((studA: Student, studB: Student) => {
+            if (sortBy === SortType.Age) {
+              return studA[sortBy] - studB[sortBy];
+            }
+
+            if (sortBy === SortType.Married) {
+              return Number(studA[sortBy]) - Number(studB[sortBy]);
+            }
+
+            return averageGrades(studA[sortBy]) - averageGrades(studB[sortBy]);
+          })
+        : studentsList
+          .sort((studA: Student, studB: Student) => {
+            if (sortBy === SortType.Age) {
+              return studB[sortBy] - studA[sortBy];
+            }
+
+            if (sortBy === SortType.Married) {
+              return Number(studB[sortBy]) - Number(studA[sortBy]);
+            }
+
+            return averageGrades(studB[sortBy]) - averageGrades(studA[sortBy]);
+          });
+
+    default:
+      return studentsList;
+  }
 }
