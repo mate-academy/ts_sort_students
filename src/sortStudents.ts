a247ece5450d0f2cae71f545aed79b895ca19c69
Nikita Ovchinnikov <nikitaovch99@gmail.com>
2022-10-04 12:25:28 +0300

    src/sortStudents.ts

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,68 @@
-
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[],
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): object[] {
+  const copyOfStudents: object[] = students.map(
+    (student: object): object => {
+      return { ...student };
+    },
+  );
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  switch (sortBy) {
+    case SortType.Name:
+    case SortType.Surname:
+      copyOfStudents.sort((studentA: object, stundentB: object) => (
+        studentA[sortBy].localeCompare(stundentB[sortBy])
+      ));
+      break;
+
+    case SortType.Age:
+    case SortType.Married:
+      copyOfStudents.sort((studentA: object, studentB: object) => {
+        return order === 'asc'
+          ? studentA[sortBy] - studentB[sortBy]
+          : studentB[sortBy] - studentA[sortBy];
+      });
+      break;
+
+    case SortType.AverageGrade:
+      copyOfStudents.sort((studentA: object, studentB: object) => {
+        const studentAAverage = studentA[sortBy].reduce(
+          (previous: number, current: number) => previous + current,
+        ) / studentA[sortBy].length;
+
+        const studenBAverage = studentB[sortBy].reduce(
+          (previous: number, current: number) => previous + current,
+        ) / studentB[sortBy].length;
+
+        return order === 'asc'
+          ? studentAAverage - studenBAverage
+          : studenBAverage - studentAAverage;
+      });
+      break;
+
+    default:
+      break;
+  }
+
+  return copyOfStudents;
 }
