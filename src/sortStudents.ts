e25420e3f632a3bd6b941bb75e65a0a20dd656e2
Dima Romanenko <demigod177712@gmail.com>
2022-10-04 13:37:45 +0300

    Done

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,67 @@
 
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[],
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
 // create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+function getAverageGrade(grades: number[]): number {
+  return grades.reduce((acc, current) => acc + current, 0) / grades.length;
+}
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const copy = [...students];
+
+  switch (sortBy) {
+    case SortType.Name:
+    case SortType.Surname:
+      return order === 'asc'
+        ? copy.sort((firstStudent, secondStudent) => {
+          return firstStudent[sortBy].localeCompare(secondStudent[sortBy]);
+        })
+        : copy.sort((firstStudent, secondStudent) => {
+          return secondStudent[sortBy].localeCompare(firstStudent[sortBy]);
+        });
+
+    case SortType.Age:
+    case SortType.Married:
+      return order === 'asc'
+        ? copy.sort((firstStudent, secondStudent) => {
+          return +(firstStudent[sortBy]) - +(secondStudent[sortBy]);
+        })
+        : copy.sort((firstStudent, secondStudent) => {
+          return +(secondStudent[sortBy]) - +(firstStudent[sortBy]);
+        });
+
+    case SortType.AverageGrade:
+      return order === 'asc'
+        ? copy.sort((firstStudent, secondStudent) => {
+          return getAverageGrade(firstStudent[sortBy])
+          - getAverageGrade(secondStudent[sortBy]);
+        })
+        : copy.sort((firstStudent, secondStudent) => {
+          return getAverageGrade(secondStudent[sortBy])
+          - getAverageGrade(firstStudent[sortBy]);
+        });
+
+    default:
+      return copy;
+  }
 }
