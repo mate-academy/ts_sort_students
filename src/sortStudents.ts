2479feea8ca44e804a7dcbe9972fe0495733fece
Ilya Maksymov <ivashthestampede01@gmail.com>
2022-10-04 16:24:20 +0300

    Solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,59 @@
 
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
+  averageGrade?: number;
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'averageGrade'
 }
 
 // create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
-
-export function sortStudents(students, sortBy, order) {
+export function sortStudents(
+  students: Student[], sortBy: SortType, order: SortOrder,
+): Student[] {
   // write your function
+  const studentsWithAverageGrade: Student[] = students.map((student) => {
+    const averageGrade: number = student.grades
+      .reduce((total, current) => total + current) / student.grades.length;
+
+    return {
+      ...student,
+      averageGrade,
+    };
+  });
+
+  let sortingCallback: (a: Student, b: Student) => number;
+
+  switch (sortBy) {
+    case SortType.Name:
+    case SortType.Surname:
+      sortingCallback = order === 'asc'
+        ? (a, b): number => a[sortBy].localeCompare(b[sortBy])
+        : (a, b): number => b[sortBy].localeCompare(a[sortBy]);
+      break;
+
+    case SortType.Age:
+    case SortType.Married:
+    case SortType.AverageGrade:
+      sortingCallback = order === 'asc'
+        ? (a, b): number => Number(a[sortBy]) - Number(b[sortBy])
+        : (a, b): number => Number(b[sortBy]) - Number(a[sortBy]);
+      break;
+
+    default:
+      throw new Error('Wrong type!');
+  }
+
+  return studentsWithAverageGrade.sort(sortingCallback);
 }
