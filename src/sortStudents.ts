b1eb1961e47c2d7d81e519459b54edcc728b2bc2
Artem Musii <musiy.artem@gmail.com>
2022-10-04 14:19:29 +0300

    solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,73 @@
-
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+function getAverageMark(marks: number[]): number {
+  return marks
+    .reduce((acc: number, mark: number) => acc + mark, 0)
+    / marks.length;
+}
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+) : Student[] {
+  const studentsCopy = [...students];
+
+  switch (sortBy) {
+    case SortType.Name:
+    case SortType.Surname:
+      studentsCopy.sort((studentA: Student, studentB: Student) => (
+        order === 'asc'
+          ? studentA[sortBy].localeCompare(studentB[sortBy])
+          : studentB[sortBy].localeCompare(studentA[sortBy])
+      ));
+
+      break;
+
+    case SortType.Age:
+      studentsCopy.sort((studentA: Student, studentB: Student) => (
+        order === 'asc'
+          ? studentA[sortBy] - studentB[sortBy]
+          : studentB[sortBy] - studentA[sortBy]
+      ));
+
+      break;
+
+    case SortType.Married:
+      studentsCopy.sort((studentA: Student, studentB: Student) => (
+        order === 'asc'
+          ? Number(studentA[sortBy]) - Number(studentB[sortBy])
+          : Number(studentB[sortBy]) - Number(studentA[sortBy])
+      ));
+
+      break;
+
+    default:
+      studentsCopy.sort((studentA: Student, studentB: Student) => {
+        const averageMarkA = getAverageMark(studentA[sortBy]);
+        const averageMarkB = getAverageMark(studentB[sortBy]);
+
+        return order === 'asc'
+          ? (averageMarkA - averageMarkB)
+          : (averageMarkB - averageMarkA);
+      });
+  }
+
+  return studentsCopy;
 }
