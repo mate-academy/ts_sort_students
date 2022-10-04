304c5046ceaeba71e814ae8c3045d9f988029b43
Serhii Romaniuk <deepspark@gmail.com>
2022-10-04 21:23:51 +0300

    implemented solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,59 @@
 
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desk';
 
+function averageAge(scores: number[]): number {
+  return scores.reduce((firstValue: number, secondValue: number) => (
+    firstValue + secondValue
+  ), 0) / scores.length;
+}
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const studentsCopy = [...students];
+
+  return studentsCopy.sort((
+    firstStudent: Student,
+    secondStudent: Student,
+  ): number => {
+    switch (sortBy) {
+      case SortType.Name:
+      case SortType.Surname:
+        return (order === 'asc')
+          ? firstStudent[sortBy].localeCompare(secondStudent[sortBy])
+          : secondStudent[sortBy].localeCompare(firstStudent[sortBy]);
+
+      case SortType.Age:
+      case SortType.Married:
+        return (order === 'asc')
+          ? +firstStudent[sortBy] - +secondStudent[sortBy]
+          : +secondStudent[sortBy] - +firstStudent[sortBy];
+
+      case SortType.AverageGrade:
+        return (order === 'asc')
+          ? averageAge(firstStudent.grades) - averageAge(secondStudent.grades)
+          : averageAge(secondStudent.grades) - averageAge(firstStudent.grades);
+
+      default:
+        return 0;
+    }
+  });
 }
