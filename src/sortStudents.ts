bdd8b6f30487550a6783624a953037803b086a4d
Nikita Ovchinnikov <nikitaovch99@gmail.com>
2022-10-04 16:52:33 +0300

    fixed switch case

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -16,6 +16,12 @@ export enum SortType {
 
 export type SortOrder = 'asc' | 'desc';
 
+function getAverage(studentGrades: number[]): number {
+  return studentGrades.reduce((sum: number, curr: number) => (
+    sum + curr
+  ), 0) / studentGrades.length;
+}
+
 export function sortStudents(
   students: Student[],
   sortBy: SortType,
@@ -23,42 +29,25 @@ export function sortStudents(
 ): Student[] {
   const copyOfStudents = [...students];
 
-  switch (sortBy) {
-    case SortType.Name:
-    case SortType.Surname:
-      copyOfStudents.sort((studentA: Student, stundentB: Student) => (
-        studentA[sortBy].localeCompare(stundentB[sortBy])
-      ));
-      break;
+  return copyOfStudents.sort((studentA: Student, studentB: Student) => {
+    switch (sortBy) {
+      case SortType.Name:
+      case SortType.Surname:
+        return studentA[sortBy].localeCompare(studentB[sortBy]);
 
-    case SortType.Age:
-    case SortType.Married:
-      copyOfStudents.sort((studentA: Student, studentB: Student) => {
+      case SortType.Age:
+      case SortType.Married:
         return order === 'asc'
           ? Number(studentA[sortBy]) - Number(studentB[sortBy])
           : Number(studentB[sortBy]) - Number(studentA[sortBy]);
-      });
-      break;
 
-    case SortType.AverageGrade:
-      copyOfStudents.sort((studentA: Student, studentB: Student) => {
-        const studentAAverage = studentA[sortBy].reduce(
-          (previous: number, current: number) => previous + current,
-        ) / studentA[sortBy].length;
-
-        const studenBAverage = studentB[sortBy].reduce(
-          (previous: number, current: number) => previous + current,
-        ) / studentB[sortBy].length;
-
+      case SortType.AverageGrade:
         return order === 'asc'
-          ? studentAAverage - studenBAverage
-          : studenBAverage - studentAAverage;
-      });
-      break;
+          ? getAverage(studentA[sortBy]) - getAverage(studentB[sortBy])
+          : getAverage(studentB[sortBy]) - getAverage(studentA[sortBy]);
 
-    default:
-      break;
-  }
-
-  return copyOfStudents;
+      default:
+        return 0;
+    }
+  });
 }
