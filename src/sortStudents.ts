54fe032bfe508542bfe01b4cd2478906a016ad33
Vadym Mamchur <vmchr344@gmail.com>
2022-10-05 13:15:15 +0300

    func getAverageGrade has been added

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -17,6 +17,12 @@ export enum SortType {
 
 export type SortOrder = 'asc' | 'desc';
 
+function getAverageGrade(grades: number[]): number {
+  return grades.reduce((prev: number, curr: number): number => (
+    prev + curr
+  ), 0) / grades.length;
+}
+
 export function sortStudents(
   students: Student[],
   sortBy: SortType,
@@ -26,51 +32,36 @@ export function sortStudents(
 
   switch (sortBy) {
     case SortType.Name:
-      sortedStudents.sort((person: Student, prevPerson: Student) => (
-        order === 'asc'
-          ? person[sortBy].localeCompare(prevPerson[sortBy])
-          : prevPerson[sortBy].localeCompare(person[sortBy])
-      ));
-      break;
-
     case SortType.Surname:
-      sortedStudents.sort((person: Student, prevPerson: Student) => (
+      sortedStudents.sort((personA: Student, personB: Student) => (
         order === 'asc'
-          ? person[sortBy].localeCompare(prevPerson[sortBy])
-          : prevPerson[sortBy].localeCompare(person[sortBy])
+          ? personA[sortBy].localeCompare(personB[sortBy])
+          : personB[sortBy].localeCompare(personA[sortBy])
       ));
       break;
 
     case SortType.Age:
-      sortedStudents.sort((person: Student, prevPerson: Student) => (
+      sortedStudents.sort((personA: Student, personB: Student) => (
         order === 'asc'
-          ? person[sortBy] - prevPerson[sortBy]
-          : prevPerson[sortBy] - person[sortBy]
+          ? personA[sortBy] - personB[sortBy]
+          : personB[sortBy] - personA[sortBy]
       ));
       break;
 
     case SortType.Married:
-      sortedStudents.sort((person: Student, prevPerson: Student) => (
+      sortedStudents.sort((personA: Student, personB: Student) => (
         order === 'asc'
-          ? Number(person[sortBy]) - Number(prevPerson[sortBy])
-          : Number(prevPerson[sortBy]) - Number(person[sortBy])
+          ? Number(personA[sortBy]) - Number(personB[sortBy])
+          : Number(personB[sortBy]) - Number(personA[sortBy])
       ));
       break;
 
     case SortType.AverageGrade:
-      sortedStudents.sort((person: Student, prevPerson: Student) => {
-        const firstAverage = person[sortBy]
-          .reduce((prev: number, curr: number) => prev + curr, 0)
-            / person[sortBy].length;
-
-        const secondAverage = prevPerson[sortBy]
-          .reduce((prev: number, curr: number) => prev + curr, 0)
-            / prevPerson[sortBy].length;
-
-        return order === 'asc'
-          ? firstAverage - secondAverage
-          : secondAverage - firstAverage;
-      });
+      sortedStudents.sort((personA: Student, personB: Student) => (
+        order === 'asc'
+          ? getAverageGrade(personA[sortBy]) - getAverageGrade(personB[sortBy])
+          : getAverageGrade(personB[sortBy]) - getAverageGrade(personA[sortBy])
+      ));
       break;
 
     default:
