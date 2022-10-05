792fc7681f263c2d64d4deb60e0d586ab8f5fbd1
Iryna Borniak <la.aerea.1@gmail.com>
2022-10-05 23:04:07 +0300

    refactor: follow DRY

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -29,48 +29,35 @@ export function sortStudents(
 
   switch (sortBy) {
     case SortType.Name:
-      studentsCopy.sort((prev: Student, next: Student) => {
-        return order === 'asc'
-          ? prev[sortBy].localeCompare(next[sortBy])
-          : next[sortBy].localeCompare(prev[sortBy]);
-      });
-      break;
-
     case SortType.Surname:
-      studentsCopy.sort((prev: Student, next: Student) => {
+      return studentsCopy.sort((prev: Student, next: Student) => {
         return order === 'asc'
           ? prev[sortBy].localeCompare(next[sortBy])
           : next[sortBy].localeCompare(prev[sortBy]);
       });
-      break;
 
     case SortType.Age:
-      studentsCopy.sort((prev: Student, next: Student) => {
+      return studentsCopy.sort((prev: Student, next: Student) => {
         return order === 'asc'
           ? prev[sortBy] - next[sortBy]
           : next[sortBy] - prev[sortBy];
       });
-      break;
 
     case SortType.Married:
-      studentsCopy.sort((prev: Student, next: Student) => {
+      return studentsCopy.sort((prev: Student, next: Student) => {
         return order === 'asc'
           ? Number(prev[sortBy]) - Number(next[sortBy])
           : Number(next[sortBy]) - Number(prev[sortBy]);
       });
-      break;
 
     case SortType.AverageGrade:
-      studentsCopy.sort((prev: Student, next: Student) => {
+      return studentsCopy.sort((prev: Student, next: Student) => {
         return order === 'asc'
           ? calcAverage(prev[sortBy]) - calcAverage(next[sortBy])
           : calcAverage(next[sortBy]) - calcAverage(prev[sortBy]);
       });
-      break;
 
     default:
       return studentsCopy;
   }
-
-  return studentsCopy;
 }
