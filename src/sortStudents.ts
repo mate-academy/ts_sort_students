680ced65e318682d193fc9b5b59de3d48817216e
Andriy Hladkevych <andriyglad@gmail.com>
2022-10-06 10:13:22 +0300

    Fixed

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -25,46 +25,37 @@ export function sortStudents(
 ): Student[] {
   const sorted: Student[] = [...students];
 
-  switch (sortBy) {
-    case SortType.Name:
-    case SortType.Surname:
-      sorted
-        .sort((person1: Student, person2: Student) => {
-          return person1[sortBy].localeCompare(person2[sortBy]);
-        });
-
-      return order === 'asc'
-        ? sorted
-        : sorted.reverse();
-
-    case SortType.Age:
-      return sorted.sort((person1: Student, person2: Student) => {
+  sorted.sort((person1: Student, person2: Student) => {
+    switch (sortBy) {
+      case SortType.Name:
+      case SortType.Surname:
+        return order === 'asc'
+          ? person1[sortBy].localeCompare(person2[sortBy])
+          : person2[sortBy].localeCompare(person1[sortBy]);
+      case SortType.Age:
         return order === 'asc'
           ? person1[sortBy] - person2[sortBy]
           : person2[sortBy] - person1[sortBy];
-      });
-
-    case SortType.Married:
-      return sorted.sort((person1: Student, person2: Student) => {
+      case SortType.Married:
         return order === 'asc'
           ? Number(person1[sortBy]) - Number(person2[sortBy])
           : Number(person2[sortBy]) - Number(person1[sortBy]);
-      });
-
-    case SortType.AverageGrade:
-      return sorted.sort((person1: Student, person2: Student) => {
+      case SortType.AverageGrade:
+        // eslint-disable-next-line
         const average1: number = person1[sortBy]
           .reduce((a, b) => a + b, 0) / person1[sortBy].length;
+
+        // eslint-disable-next-line
         const average2: number = person2[sortBy]
           .reduce((a, b) => a + b, 0) / person2[sortBy].length;
 
         return order === 'asc'
           ? average1 - average2
           : average2 - average1;
-      });
-
-    default: {
-      return sorted;
+      default:
+        return 0;
     }
-  }
+  });
+
+  return sorted;
 }
