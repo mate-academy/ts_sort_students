d03593c49f8603a1ee1c7dfcb8e9fbd9a4a87166
Dmytro Stepanenko <dimasflacko@gmail.com>
2022-10-10 02:09:08 +0300

    Solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,58 @@
 
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[],
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const sortedStudents: Student[] = [...students];
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  sortedStudents.sort((prev: Student, curr: Student): number => {
+    switch (sortBy) {
+      case SortType.Name:
+      case SortType.Surname:
+        return prev[sortBy].localeCompare(curr[sortBy]);
+
+      case SortType.Age:
+        return order === 'asc'
+          ? prev[sortBy] - curr[sortBy]
+          : curr[sortBy] - prev[sortBy];
+
+      case SortType.Married:
+        return order === 'asc'
+          ? Number(prev[sortBy]) - Number(curr[sortBy])
+          : Number(curr[sortBy]) - Number(prev[sortBy]);
+
+      case SortType.AverageGrade:
+        return order === 'asc'
+          ? prev[sortBy]
+            .reduce((a, b) => a + b) / prev[sortBy].length - curr[sortBy]
+            .reduce((a, b) => a + b) / curr[sortBy].length
+          : curr[sortBy]
+            .reduce((a, b) => a + b) / curr[sortBy].length - prev[sortBy]
+            .reduce((a, b) => a + b) / prev[sortBy].length;
+
+      default:
+        return 0;
+    }
+  });
+
+  return sortedStudents;
 }
