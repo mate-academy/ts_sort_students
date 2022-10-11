5e0318bb30d777841329c1123d3ce7f972dd45f4
Dima <dima3679@MacBook-Air-Marina.local>
2022-10-11 20:59:41 +0300

    done! but sort by married have bug

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,93 @@
-
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[],
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
 // create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+function getAverage(arr: number[]): number {
+  const sum = arr.reduce((acc: number, value: number) => acc + value);
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  return sum / arr.length;
+}
+
+export function sortStudents(
+  students: Student[], sortBy: SortType, order: SortOrder,
+): Student[] {
+  const studentsCopy: Student[] = students.slice();
+  let callback;
+
+  if (order === 'asc') {
+    switch (sortBy) {
+      case 'name':
+      case 'surname':
+        callback = (
+          st1: Student, st2: Student,
+        ): number => st1[sortBy].localeCompare(st2[sortBy]);
+        break;
+
+      case 'age':
+        callback = (
+          st1: Student, st2: Student,
+        ): number => st1[sortBy] - st2[sortBy];
+        break;
+
+      case 'married':
+        callback = (st1: Student): number => (st1[sortBy] ? 1 : -1);
+        break;
+
+      case 'grades':
+        callback = (
+          st1: Student, st2: Student,
+        ): number => getAverage(st1[sortBy]) - getAverage(st2[sortBy]);
+        break;
+
+      default:
+        break;
+    }
+  }
+
+  if (order === 'desc') {
+    switch (sortBy) {
+      case 'name':
+      case 'surname':
+        callback = (
+          st1: Student, st2: Student,
+        ): number => st2[sortBy].localeCompare(st1[sortBy]);
+        break;
+
+      case 'age':
+        callback = (
+          st1: Student, st2: Student,
+        ): number => st2[sortBy] - st1[sortBy];
+        break;
+
+      case 'married':
+        callback = (st1: Student): number => (st1[sortBy] ? -1 : 1);
+        break;
+
+      case 'grades':
+        callback = (
+          st1: Student, st2: Student,
+        ): number => getAverage(st2[sortBy]) - getAverage(st1[sortBy]);
+        break;
+
+      default:
+        break;
+    }
+  }
+
+  return studentsCopy.sort(callback);
 }
