c594b6d81883f61d1d114e6c793a744d22079da9
Oleh Savchuk <sav4uk.oleg@gmail.com>
2022-10-16 02:01:35 +0300

    develop

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,64 @@
-
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[]
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const result: Student[] = [...students];
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  result.sort((a: Student, b: Student): number => {
+    if (sortBy === 'name' || sortBy === 'surname') {
+      if (order === 'desc') {
+        return b[sortBy].localeCompare(a[sortBy]);
+      }
+
+      return a[sortBy].localeCompare(b[sortBy]);
+    }
+
+    if (sortBy === 'married') {
+      if (order === 'desc') {
+        return a[sortBy] <= b[sortBy] ? 1 : -1;
+      }
+
+      return a[sortBy] <= b[sortBy] ? -1 : 1;
+    }
+
+    if (sortBy === 'grades') {
+      const first: number = a[sortBy]
+        .reduce((x: number, y: number): number => x + y) / a[sortBy].length;
+      const second: number = b[sortBy]
+        .reduce((x: number, y: number): number => x + y) / b[sortBy].length;
+
+      if (order === 'desc') {
+        return second - first;
+      }
+
+      return first - second;
+    }
+
+    if (order === 'desc') {
+      return b[sortBy] - a[sortBy];
+    }
+
+    return a[sortBy] - b[sortBy];
+  });
+
+  return result;
 }
