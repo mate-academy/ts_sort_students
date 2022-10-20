e6809acbd95a2519edee2610e7cc65c13c5da2a6
Bradley Matjie <bradleysaint45@gmail.com>
2022-10-20 15:27:13 +0200

    solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -18,7 +18,11 @@ export enum SortType {
 }
 
 // create SortOrder type
-export type SortOrder = 'asc' | 'desc';
+// export type SortOrder = 'asc' | 'desc';
+export enum SortOrder {
+  asc = 'asc',
+  desc = 'desc',
+}
 
 function averageGrades(grades: Array<number>): number {
   return grades.reduce((sum, grade) => sum + grade, 0) / grades.length;
@@ -35,7 +39,7 @@ export function sortStudents(
     case SortType.Name:
     case SortType.Surname:
       studentsCopy.sort((stu1: Student, stu2: Student): number => {
-        return order === 'asc'
+        return order === SortOrder.asc
           ? stu1[sortBy].localeCompare(stu2[sortBy])
           : stu2[sortBy].localeCompare(stu1[sortBy]);
       });
@@ -44,7 +48,7 @@ export function sortStudents(
     case SortType.Age:
     case SortType.Married:
       studentsCopy.sort((stu1: Student, stu2: Student): number => {
-        return order === 'asc'
+        return order === SortOrder.asc
           ? Number(stu1[sortBy]) - Number(stu2[sortBy])
           : Number(stu2[sortBy]) - Number(stu1[sortBy]);
       });
@@ -52,14 +56,14 @@ export function sortStudents(
 
     case SortType.AverageGrade:
       studentsCopy.sort((stu1: Student, stu2: Student): number => {
-        return order === 'asc'
+        return order === SortOrder.asc
           ? averageGrades(stu1[sortBy]) - averageGrades(stu2[sortBy])
           : averageGrades(stu2[sortBy]) - averageGrades(stu1[sortBy]);
       });
       break;
 
     default:
-      return [];
+      return studentsCopy;
   }
 
   return studentsCopy;
