c65ff90f27e01d8c34f9e350fbbcf914637774cd
Oleh Savchuk <sav4uk.oleg@gmail.com>
2022-10-23 15:38:21 +0300

    fixed

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -16,49 +16,54 @@ export enum SortType {
 
 export type SortOrder = 'asc' | 'desc';
 
+function getAverage(array: number[]): number {
+  return array.reduce((x: number, y: number): number => x + y) / array.length;
+}
+
 export function sortStudents(
   students: Student[],
   sortBy: SortType,
   order: SortOrder,
 ): Student[] {
-  const result: Student[] = [...students];
+  return [...students].sort((a: Student, b: Student): number => {
+    switch (sortBy) {
+      case 'name':
+        if (order === 'desc') {
+          return b[sortBy].localeCompare(a[sortBy]);
+        }
 
-  result.sort((a: Student, b: Student): number => {
-    if (sortBy === 'name' || sortBy === 'surname') {
-      if (order === 'desc') {
-        return b[sortBy].localeCompare(a[sortBy]);
-      }
+        return a[sortBy].localeCompare(b[sortBy]);
 
-      return a[sortBy].localeCompare(b[sortBy]);
+      case 'surname':
+        if (order === 'desc') {
+          return b[sortBy].localeCompare(a[sortBy]);
+        }
+
+        return a[sortBy].localeCompare(b[sortBy]);
+
+      case 'married':
+        if (order === 'desc') {
+          return a[sortBy] <= b[sortBy] ? 1 : -1;
+        }
+
+        return a[sortBy] <= b[sortBy] ? -1 : 1;
+
+      case 'age':
+        if (order === 'desc') {
+          return b[sortBy] - a[sortBy];
+        }
+
+        return a[sortBy] - b[sortBy];
+
+      case 'grades':
+        if (order === 'desc') {
+          return getAverage(b[sortBy]) - getAverage(a[sortBy]);
+        }
+
+        return getAverage(a[sortBy]) - getAverage(b[sortBy]);
+
+      default:
+        return 0;
     }
-
-    if (sortBy === 'married') {
-      if (order === 'desc') {
-        return a[sortBy] <= b[sortBy] ? 1 : -1;
-      }
-
-      return a[sortBy] <= b[sortBy] ? -1 : 1;
-    }
-
-    if (sortBy === 'grades') {
-      const first: number = a[sortBy]
-        .reduce((x: number, y: number): number => x + y) / a[sortBy].length;
-      const second: number = b[sortBy]
-        .reduce((x: number, y: number): number => x + y) / b[sortBy].length;
-
-      if (order === 'desc') {
-        return second - first;
-      }
-
-      return first - second;
-    }
-
-    if (order === 'desc') {
-      return b[sortBy] - a[sortBy];
-    }
-
-    return a[sortBy] - b[sortBy];
   });
-
-  return result;
 }
