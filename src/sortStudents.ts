ff7ab40374e3d85e1b815e4993afbbf5ad423d8f
Saleh BUD <saleh.7734@gmail.com>
2022-11-03 21:05:55 +0200

    Fix: SortType.AverageGrade is now outside the switch case

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -19,16 +19,16 @@ export enum SortType {
 // create SortOrder type
 export type SortOrder = 'asc' | 'desc';
 
+const averageGrade = (grades: number[]): number => {
+  return grades.reduce((acc, curV) => acc + curV, 0) / grades.length;
+};
+
 export function sortStudents(
   students: Student[],
   sortBy: SortType,
   order: SortOrder,
 ): Student[] {
   const copyStudents = [...students];
-
-  const averageGrade = (grades: number[]): number => {
-    return grades.reduce((acc, curV) => acc + curV, 0) / grades.length;
-  };
 
   switch (sortBy) {
     case SortType.Name:
@@ -43,16 +43,19 @@ export function sortStudents(
         ? copyStudents.sort((a, b) => +b[sortBy] - +a[sortBy])
         : copyStudents.sort((a, b) => +a[sortBy] - +b[sortBy]);
 
-    case SortType.AverageGrade:
-      return order === 'desc'
-        ? copyStudents.sort(
-          (a, b) => averageGrade(b.grades) - averageGrade(a.grades),
-        )
-        : copyStudents.sort(
-          (a, b) => averageGrade(a.grades) - averageGrade(b.grades),
-        );
-
     default:
-      return copyStudents;
+      break;
   }
+
+  if (order === 'desc') {
+    copyStudents.sort(
+      (a, b) => averageGrade(b.grades) - averageGrade(a.grades),
+    );
+  } else {
+    copyStudents.sort(
+      (a, b) => averageGrade(a.grades) - averageGrade(b.grades),
+    );
+  }
+
+  return copyStudents;
 }
