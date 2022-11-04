5ff2711d9494595154dbd7d546a0049f40cac7cc
Ivan Nevmerzhytskyi <nevmerzhytskyi.ivan@gmail.com>
2022-11-04 23:35:24 +0200

    solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,66 @@
 
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  let sortedStudents = [...students];
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  const getAverage: (grades: number[]) => number = (grades) => {
+    return (grades.reduce((a, b) => a + b, 0) / grades.length);
+  };
+
+  switch (sortBy) {
+    case SortType.Name:
+    case SortType.Surname:
+      sortedStudents = (order === 'asc')
+        ? sortedStudents
+          .sort((studentA, studentB) => studentA[sortBy]
+            .localeCompare(studentB[sortBy]))
+        : sortedStudents
+          .sort((studentA, studentB) => studentB[sortBy]
+            .localeCompare(studentA[sortBy]));
+      break;
+
+    case SortType.Age:
+    case SortType.Married:
+      sortedStudents = (order === 'asc')
+        ? sortedStudents.sort((studentA, studentB) => Number(studentA[sortBy])
+          - Number(studentB[sortBy]))
+        : sortedStudents.sort((studentA, studentB) => Number(studentB[sortBy])
+          - Number(studentA[sortBy]));
+      break;
+
+    case SortType.AverageGrade:
+      sortedStudents = (order === 'asc')
+        ? sortedStudents
+          .sort((studentA, studentB) => getAverage(studentA[sortBy])
+            - getAverage(studentB[sortBy]))
+        : sortedStudents
+          .sort((studentA, studentB) => getAverage(studentB[sortBy])
+            - getAverage(studentA[sortBy]));
+      break;
+
+    default:
+  }
+
+  return sortedStudents;
 }
