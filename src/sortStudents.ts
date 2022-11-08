a08ef9369f7d733f7b5b02c68d5b20a66f6858d5
Sergii Iatsenko <sergii.iatsenko@gmail.com>
2022-11-08 11:13:21 +0200

    Solution after review

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -24,32 +24,37 @@ export function sortStudents(
 ): Student[] {
   const studentsSort: Student[] = [...students];
 
+  const averGrade = (grades: number[]) : number => {
+    return (grades.reduce((prev:number, curr:number) => prev + curr)
+     / grades.length);
+  };
+
   switch (sortBy) {
     case SortType.Name:
-      return studentsSort.sort((a: Student, b: Student) => ((order
-         === 'asc') ? a.name.localeCompare(b.name)
+      return studentsSort.sort((a: Student, b: Student) => (order === 'asc'
+        ? a.name.localeCompare(b.name)
         : b.name.localeCompare(a.name)));
 
     case SortType.Surname:
-      return studentsSort.sort((a: Student, b: Student) => ((order
-         === 'asc') ? a.surname.localeCompare(b.surname)
+      return studentsSort.sort((a: Student, b: Student) => (order === 'asc'
+        ? a.surname.localeCompare(b.surname)
         : b.surname.localeCompare(a.surname)));
 
     case SortType.Age:
-      return studentsSort.sort((a: Student, b: Student) => ((order
-         === 'asc') ? a.age - b.age : b.age - a.age));
+      return studentsSort.sort((a: Student, b: Student) => (order === 'asc'
+        ? a.age - b.age
+        : b.age - a.age));
 
     case SortType.Married:
-      return studentsSort.sort((a: Student, b: Student) => ((order
-         === 'asc') ? Number(a.married) - Number(b.married)
+      return studentsSort.sort((a: Student, b: Student) => (order === 'asc'
+        ? Number(a.married) - Number(b.married)
         : Number(b.married) - Number(a.married)));
 
     case SortType.AverageGrade:
-      return studentsSort.sort((a: Student, b: Student) => ((order === 'asc')
-        ? (a.grades.reduce((prev, curr) => prev + curr) / a.grades.length)
-         - (b.grades.reduce((prev, curr) => prev + curr) / b.grades.length)
-        : (b.grades.reduce((prev, curr) => prev + curr) / b.grades.length)
-         - (a.grades.reduce((prev, curr) => prev + curr) / a.grades.length)));
+      return studentsSort.sort((a: Student, b: Student) => (order === 'asc'
+        ? averGrade(a.grades) - averGrade(b.grades)
+        : averGrade(b.grades) - averGrade(a.grades)
+      ));
 
     default: throw new Error('Error');
   }
