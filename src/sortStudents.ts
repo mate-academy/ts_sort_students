a3d4742e580b5e221a8898043f8251d44d2c4e13
Danyla Krygin <underworldsan@gmail.com>
2022-11-08 17:38:17 +0200

    Solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,56 @@
 
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+function calcAverageGrades(grades: number[]): number {
+  return grades.reduce((sum, elem) => sum + elem, 0) / grades.length;
+}
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const sortedStudents: Student[] = [...students];
+
+  return sortedStudents.sort((item1: Student, item2: Student): number => {
+    switch (sortBy) {
+      case SortType.Name:
+      case SortType.Surname:
+        return order === 'asc'
+          ? item1[sortBy].localeCompare(item2[sortBy])
+          : item2[sortBy].localeCompare(item1[sortBy]);
+
+      case SortType.Age:
+      case SortType.Married:
+        return order === 'asc'
+          ? Number(item1[sortBy]) - Number(item2[sortBy])
+          : Number(item2[sortBy]) - Number(item1[sortBy]);
+
+      case SortType.AverageGrade:
+        return order === 'asc'
+          ? calcAverageGrades(item1[sortBy])
+            - calcAverageGrades(item2[sortBy])
+          : calcAverageGrades(item2[sortBy])
+            - calcAverageGrades(item1[sortBy]);
+
+      default:
+        throw new Error('Invalid sortBy type');
+    }
+  });
 }
