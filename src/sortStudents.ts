d29f6a04396b73dcb66bf54d73f4405b4a488887
Valentyn Ivanenko <Ivanenko1402@gmail.com>
2022-11-08 15:21:03 +0200

    Solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,84 @@
+function averageGrade(array: number[]): number {
+  const result = array.reduce((sum, a) => sum + a, 0);
+
+  return result / array.length;
+}
 
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[],
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'NAME',
+  Surname = 'SURNAME',
+  Age = 'AGE',
+  Married = 'MARRIED',
+  AverageGrade = 'GRADE',
 }
 
 // create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const newArr = [...students];
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  if (sortBy === SortType.Name) {
+    if (order === 'asc') {
+      newArr.sort((a, b) => a.name.localeCompare(b.name));
+    }
+
+    if (order === 'desc') {
+      newArr.sort((a, b) => b.name.localeCompare(a.name));
+    }
+  }
+
+  if (sortBy === SortType.Surname) {
+    if (order === 'asc') {
+      newArr.sort((a, b) => a.surname.localeCompare(b.surname));
+    }
+
+    if (order === 'desc') {
+      newArr.sort((a, b) => b.surname.localeCompare(a.surname));
+    }
+  }
+
+  if (sortBy === SortType.Age) {
+    if (order === 'asc') {
+      newArr.sort((a, b) => a.age - b.age);
+    }
+
+    if (order === 'desc') {
+      newArr.sort((a, b) => b.age - a.age);
+    }
+  }
+
+  if (sortBy === SortType.Married) {
+    if (order === 'asc') {
+      newArr.sort((a, b) => +(a.married) - +(b.married));
+    }
+
+    if (order === 'desc') {
+      newArr.sort((a, b) => +(b.married) - +(a.married));
+    }
+  }
+
+  if (sortBy === SortType.AverageGrade) {
+    if (order === 'asc') {
+      newArr.sort((a, b) => averageGrade(a.grades) - averageGrade(b.grades));
+    }
+
+    if (order === 'desc') {
+      newArr.sort((a, b) => averageGrade(b.grades) - averageGrade(a.grades));
+    }
+  }
+
+  return newArr;
 }
