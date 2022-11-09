703479d3b256e3579caadb2ec0d98e17ea099305
Hlazman <glazman.b@gmail.com>
2022-11-09 14:53:29 +0200

    made sort students with TS

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,89 @@
+// import { type } from "os";
 
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[],
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name,
+  Surname,
+  Age,
+  Married,
+  AverageGrade,
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const result: Student[] = [...students];
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  switch (sortBy) {
+    case SortType.Name:
+      result.sort((a, b) => {
+        if (order === 'asc') {
+          return a.name.localeCompare(b.name);
+        }
+
+        return b.name.localeCompare(a.name);
+      });
+      break;
+
+    case SortType.Surname:
+      result.sort((a, b) => {
+        if (order === 'asc') {
+          return a.surname.localeCompare(b.surname);
+        }
+
+        return b.surname.localeCompare(a.surname);
+      });
+      break;
+
+    case SortType.Age:
+      result.sort((a, b) => {
+        if (order === 'asc') {
+          return a.age - b.age;
+        }
+
+        return b.age - a.age;
+      });
+      break;
+
+    case SortType.Married:
+      result.sort((a, b) => {
+        if (order === 'asc') {
+          return +a.married - +b.married;
+        }
+
+        return +b.married - +a.married;
+      });
+      break;
+
+    case SortType.AverageGrade:
+      result.sort((a, b) => {
+        const itemOne = a.grades.reduce((prev, current) => prev + current, 0)
+        / a.grades.length;
+        const itemTwo = b.grades.reduce((prev, current) => prev + current, 0)
+        / b.grades.length;
+
+        if (order === 'asc') {
+          return itemOne - itemTwo;
+        }
+
+        return itemTwo - itemOne;
+      });
+      break;
+
+    default:
+      break;
+  }
+
+  return result;
 }
