47cdc9a7188a494f7510b407267069482cd9dc11
Taras Pustovyi <taraselnik@gmail.com>
2022-11-10 00:29:36 +0200

    Solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,72 @@
 
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[],
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
 // create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+const arrAvg = (arr:number[]): number => arr
+  .reduce((a, b) => a + b, 0) / arr.length;
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student [] {
+  const toSortStudents: Student[] = [...students];
+
+  switch (sortBy) {
+    case SortType.Name:
+    case SortType.Surname:
+      toSortStudents.sort(
+        (currentStudent: Student,
+          nextStudent: Student): number => {
+          return (order === 'asc')
+            ? currentStudent[sortBy].localeCompare(nextStudent[sortBy])
+            : nextStudent[sortBy].localeCompare(currentStudent[sortBy]);
+        },
+      );
+      break;
+
+    case SortType.Age:
+    case SortType.Married:
+      toSortStudents.sort(
+        (currentStudent: Student,
+          nextStudent: Student): number => {
+          return (order === 'asc')
+            ? Number(currentStudent[sortBy]) - Number(nextStudent[sortBy])
+            : Number(nextStudent[sortBy]) - Number(currentStudent[sortBy]);
+        },
+      );
+      break;
+
+    case SortType.AverageGrade:
+      toSortStudents.sort(
+        (currentStudent: Student,
+          nextStudent: Student): number => {
+          return (order === 'asc')
+            ? arrAvg(currentStudent[sortBy]) - arrAvg(nextStudent[sortBy])
+            : arrAvg(nextStudent[sortBy]) - arrAvg(currentStudent[sortBy]);
+        },
+      );
+      break;
+
+    default:
+      return toSortStudents;
+  }
+
+  return toSortStudents;
 }
