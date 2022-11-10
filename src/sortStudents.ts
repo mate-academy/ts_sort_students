0c1e187bbc6e66e18d928dc4611d8197e6cf8a09
Max Skorokhod <mrzandras@gmail.com>
2022-11-10 14:43:26 +0200

    sortStudents

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,81 @@
 
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+function getTotalMarks(marks : number[]): number {
+  return marks.reduce((total: number, next: number) => total + next, 0)
+   / marks.length;
+}
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const studentCopy: Student[] = [...students];
+
+  switch (sortBy) {
+    case (SortType.Name):
+      return studentCopy.sort((a: Student, b: Student) => {
+        if (order === 'asc') {
+          return a[sortBy].localeCompare(b[sortBy]);
+        }
+
+        return b[sortBy].localeCompare(a[sortBy]);
+      });
+
+    case (SortType.Surname):
+      return studentCopy.sort((a: Student, b: Student) => {
+        if (order === 'asc') {
+          return a[sortBy].localeCompare(b[sortBy]);
+        }
+
+        return b[sortBy].localeCompare(a[sortBy]);
+      });
+
+    case (SortType.Age):
+      return studentCopy.sort((a: Student, b: Student) => {
+        if (order === 'asc') {
+          return a[sortBy] - b[sortBy];
+        }
+
+        return b[sortBy] - a[sortBy];
+      });
+
+    case (SortType.Married):
+      return studentCopy.sort((a: Student, b: Student) => {
+        if (order === 'asc') {
+          return +a[sortBy] - +b[sortBy];
+        }
+
+        return +b[sortBy] - +a[sortBy];
+      });
+
+    case (SortType.AverageGrade):
+      return studentCopy.sort((a: Student, b: Student) => {
+        if (order === 'asc') {
+          return getTotalMarks(a[sortBy]) - getTotalMarks(b[sortBy]);
+        }
+
+        return getTotalMarks(b[sortBy]) - getTotalMarks(a[sortBy]);
+      });
+
+    default:
+      throw new Error('wrong type, pls try again');
+  }
 }
