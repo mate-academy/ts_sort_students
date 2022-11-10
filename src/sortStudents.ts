94c625ff578a568e608fa8d926c33f5fc948961c
Svitlana Yudina <osbka31@gmail.com>
2022-11-10 16:42:51 +0200

    first commit

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,76 @@
 
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[],
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name,
+  Surname,
+  Age,
+  Married,
+  AverageGrade,
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(
+  students: Student[], sortBy: SortType, order: SortOrder,
+): object[] {
+  const stArr: Student[] = students.map((el: Student) => ({ ...el }));
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  switch (sortBy) {
+    case 0:
+      return (order === 'asc')
+        ? stArr
+          .sort((a: Student, b: Student) => a.name.localeCompare(b.name))
+        : stArr
+          .sort((a: Student, b: Student) => b.name.localeCompare(a.name));
+
+    case 1:
+      return (order === 'asc')
+        ? stArr
+          .sort((a: Student, b: Student) => a.surname.localeCompare(b.surname))
+        : stArr
+          .sort((a: Student, b: Student) => b.surname.localeCompare(a.surname));
+
+    case 2:
+      return (order === 'asc')
+        ? stArr.sort((a: Student, b: Student) => a.age - b.age)
+        : stArr.sort((a: Student, b: Student) => b.age - a.age);
+
+    case 3:
+      return (order === 'asc')
+        ? stArr
+          .sort((a: Student, b: Student) => (
+            Number(a.married) - Number(b.married)))
+        : stArr
+          .sort((a: Student, b: Student) => (
+            Number(b.married) - Number(a.married)));
+
+    case 4:
+      return (order === 'asc')
+        ? stArr
+          .sort((a: Student, b: Student) => (
+            (a.grades
+              .reduce((prev: number, el: number) => (
+                prev + el), 0)) / a.grades.length)
+            - (b.grades
+              .reduce((prev: number, el: number) => (
+                prev + el), 0) / b.grades.length))
+        : stArr
+          .sort((a: Student, b: Student) => (
+            (b.grades
+              .reduce((prev: number, el: number) => (
+                prev + el), 0)) / b.grades.length)
+          - (a.grades
+            .reduce((prev: number, el: number) => (
+              prev + el), 0) / a.grades.length));
+
+    default:
+      throw new Error('invalid values!');
+  }
 }
