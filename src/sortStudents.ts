1dc645ce33ae711d7f4ff26b277af4e2dd9f85fa
Bohdan Stepanets <number1xy@gmail.com>
2022-11-10 23:11:18 +0200

    Solution v1.0

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,69 @@
 
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const studentsToSort = [...students];
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  function getAverageGrade(grades: number[]): number {
+    return grades
+      .reduce((mark1: number, mark2: number) => (mark1 + mark2), 0)
+      / grades.length;
+  }
+
+  switch (sortBy) {
+    case SortType.Name:
+    case SortType.Surname:
+      return studentsToSort.sort((first: Student, secind: Student) => {
+        return order === 'asc'
+          ? first[sortBy].localeCompare(secind[sortBy])
+          : secind[sortBy].localeCompare(first[sortBy]);
+      });
+
+    case SortType.Age:
+      return studentsToSort.sort((first: Student, secind: Student) => {
+        return order === 'asc'
+          ? first[sortBy] - secind[sortBy]
+          : secind[sortBy] - first[sortBy];
+      });
+
+    case SortType.Married:
+      return studentsToSort.sort((first: Student, second: Student) => {
+        return order === 'asc'
+          ? Number(first[sortBy]) - Number(second[sortBy])
+          : Number(second[sortBy]) - Number(first[sortBy]);
+      });
+
+    case SortType.AverageGrade:
+      return studentsToSort.sort((first: Student, second: Student) => {
+        return order === 'asc'
+          ? getAverageGrade(first[sortBy])
+            - getAverageGrade(second[sortBy])
+
+          : getAverageGrade(second[sortBy])
+            - getAverageGrade(first[sortBy]);
+      });
+
+    default:
+      throw new Error('There is nothing to sort');
+  }
 }
