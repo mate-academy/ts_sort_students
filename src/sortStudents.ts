473e62ba2b78becd1c893fa796bb8d5239d408be
Svitlana Yudina <osbka31@gmail.com>
2022-11-10 17:37:10 +0200

    fixes by checklist

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,4 +1,3 @@
-
 export interface Student {
   name: string,
   surname: string,
@@ -8,14 +7,19 @@ export interface Student {
 }
 
 export enum SortType {
-  Name,
-  Surname,
-  Age,
-  Married,
-  AverageGrade,
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
 export type SortOrder = 'asc' | 'desc';
+
+export function getAverageGrade(grade: number[]): number {
+  return grade
+    .reduce((prev: number, el: number) => (prev + el), 0) / grade.length;
+}
 
 export function sortStudents(
   students: Student[], sortBy: SortType, order: SortOrder,
@@ -23,52 +27,34 @@ export function sortStudents(
   const stArr: Student[] = students.map((el: Student) => ({ ...el }));
 
   switch (sortBy) {
-    case 0:
-      return (order === 'asc')
-        ? stArr
-          .sort((a: Student, b: Student) => a.name.localeCompare(b.name))
-        : stArr
-          .sort((a: Student, b: Student) => b.name.localeCompare(a.name));
-
-    case 1:
-      return (order === 'asc')
-        ? stArr
-          .sort((a: Student, b: Student) => a.surname.localeCompare(b.surname))
-        : stArr
-          .sort((a: Student, b: Student) => b.surname.localeCompare(a.surname));
-
-    case 2:
-      return (order === 'asc')
-        ? stArr.sort((a: Student, b: Student) => a.age - b.age)
-        : stArr.sort((a: Student, b: Student) => b.age - a.age);
-
-    case 3:
+    case 'name':
+    case 'surname':
       return (order === 'asc')
         ? stArr
           .sort((a: Student, b: Student) => (
-            Number(a.married) - Number(b.married)))
+            a[sortBy].localeCompare(b[sortBy])))
         : stArr
           .sort((a: Student, b: Student) => (
-            Number(b.married) - Number(a.married)));
+            b[sortBy].localeCompare(a[sortBy])));
 
-    case 4:
+    case 'age':
+    case 'married':
       return (order === 'asc')
         ? stArr
           .sort((a: Student, b: Student) => (
-            (a.grades
-              .reduce((prev: number, el: number) => (
-                prev + el), 0)) / a.grades.length)
-            - (b.grades
-              .reduce((prev: number, el: number) => (
-                prev + el), 0) / b.grades.length))
+            Number(a[sortBy]) - Number(b[sortBy])))
         : stArr
           .sort((a: Student, b: Student) => (
-            (b.grades
-              .reduce((prev: number, el: number) => (
-                prev + el), 0)) / b.grades.length)
-          - (a.grades
-            .reduce((prev: number, el: number) => (
-              prev + el), 0) / a.grades.length));
+            Number(b[sortBy]) - Number(a[sortBy])));
+
+    case 'grades':
+      return (order === 'asc')
+        ? stArr
+          .sort((a: Student, b: Student) => (
+            getAverageGrade(a[sortBy]) - getAverageGrade(b[sortBy])))
+        : stArr
+          .sort((a: Student, b: Student) => (
+            getAverageGrade(b[sortBy]) - getAverageGrade(a[sortBy])));
 
     default:
       throw new Error('invalid values!');
