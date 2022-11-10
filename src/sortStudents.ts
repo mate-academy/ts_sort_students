c556861aaa9edf75e401c6f5f3120955b09dc007
Anna Shapovalovaemail <3809.kasabian.00@gmail.com>
2022-11-10 18:00:47 +0200

    solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,67 @@
 
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades'
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+function getAverageGrade(array: number[]): number {
+  return array
+    .reduce((acc: number, val: number) => acc + val, 0) / array.length;
+}
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const studentsCopy: Student[] = [...students];
+
+  switch (sortBy) {
+    case SortType.Name:
+    case SortType.Surname:
+      return studentsCopy.sort(
+        (currentStudent: Student, nextStudent: Student) => {
+          return order === 'asc'
+            ? currentStudent[sortBy].localeCompare(nextStudent[sortBy])
+            : nextStudent[sortBy].localeCompare(currentStudent[sortBy]);
+        },
+      );
+
+    case SortType.Age:
+    case SortType.Married:
+      return studentsCopy.sort(
+        (currentStudent: Student, nextStudent: Student) => {
+          return order === 'asc'
+            ? +currentStudent[sortBy] - +nextStudent[sortBy]
+            : +nextStudent[sortBy] - +currentStudent[sortBy];
+        },
+      );
+
+    case SortType.AverageGrade:
+      return studentsCopy.sort(
+        (currentStudent: Student, nextStudent: Student) => {
+          return order === 'asc'
+            ? getAverageGrade(currentStudent[sortBy])
+              - getAverageGrade(nextStudent[sortBy])
+            : getAverageGrade(nextStudent[sortBy])
+              - getAverageGrade(currentStudent[sortBy]);
+        },
+      );
+
+    default:
+      throw new Error('Something goes wrong - try other sort type');
+  }
 }
