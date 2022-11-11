c85b4b06cabb6d3b00b5a07b44400371278188ec
Roman Maksymuk <romamaksymuk@gmail.com>
2022-11-11 08:15:54 +0200

    first commit

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,14 +1,24 @@
 
 export interface Student {
   // describe Student interface
+  name: 'Jessica',
+  surname: 'Buxton',
+  age: 26,
+  married: true,
+  grades: [5, 5, 4, 5, 4, 4, 4, 4, 5, 4, 5, 4],
 }
 
 export enum SortType {
   // describe SortType enum
+  Name,
+  Surname,
+  Age,
+  Married,
+  AverageGrade,
 }
 
 // create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
 
 export function sortStudents(students, sortBy, order) {
