6c23f8bd139c2696b59522db3d82b7feb40fb415
Roman Maksymuk <romamaksymuk@gmail.com>
2022-11-11 17:54:15 +0200

    moved method outside & combined logic for case SortType.Age and SortType.Married & deleted comments

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,6 +1,5 @@
 
 export interface Student {
-  // describe Student interface
   name: string,
   surname: string,
   age: number,
@@ -9,7 +8,6 @@ export interface Student {
 }
 
 export enum SortType {
-  // describe SortType enum
   Name = 'name',
   Surname = 'surname',
   Age = 'age',
@@ -17,11 +15,10 @@ export enum SortType {
   AverageGrade = 'grades',
 }
 
-function countAverage(arr: number[]): number {
+function getAverageGrade(arr: number[]): number {
   return arr.reduce((prev, item) => prev + item, 0) / arr.length;
 }
 
-// create SortOrder type
 export type SortOrder = 'asc' | 'desc';
 
 export function sortStudents(
@@ -29,39 +26,30 @@ export function sortStudents(
   sortBy: SortType,
   order: SortOrder,
 ): Student[] {
-  // write your function
   const studentsCopy: Student[] = [...students];
 
-  switch (sortBy) {
-    case SortType.Name:
-    case SortType.Surname:
-      return studentsCopy.sort((student: Student, nextStudent: Student) => {
+  return studentsCopy.sort((student: Student, nextStudent: Student) => {
+    switch (sortBy) {
+      case SortType.Name:
+      case SortType.Surname:
         return order === 'asc'
           ? student[sortBy].localeCompare(nextStudent[sortBy])
           : nextStudent[sortBy].localeCompare(student[sortBy]);
-      });
 
-    case SortType.Age:
-      return studentsCopy.sort((student: Student, nextStudent: Student) => {
-        return order === 'asc'
-          ? student[sortBy] - nextStudent[sortBy]
-          : nextStudent[sortBy] - student[sortBy];
-      });
-
-    case SortType.Married:
-      return studentsCopy.sort((student: Student, nextStudent: Student) => {
+      case SortType.Age:
+      case SortType.Married:
         return order === 'asc'
           ? +student[sortBy] - +nextStudent[sortBy]
           : +nextStudent[sortBy] - +student[sortBy];
-      });
 
-    case SortType.AverageGrade:
-      return studentsCopy.sort((student: Student, nextStudent: Student) => {
+      case SortType.AverageGrade:
         return order === 'asc'
-          ? countAverage(student[sortBy]) - countAverage(nextStudent[sortBy])
-          : countAverage(nextStudent[sortBy]) - countAverage(student[sortBy]);
-      });
+          ? getAverageGrade(student[sortBy])
+          - getAverageGrade(nextStudent[sortBy])
+          : getAverageGrade(nextStudent[sortBy])
+          - getAverageGrade(student[sortBy]);
 
-    default: throw new Error('Something went wrong');
-  }
+      default: throw new Error('Something went wrong');
+    }
+  });
 }
