51ac7adabfa88ae917bd8cd69f27a5051d1d3be8
Alex Tsimba <alexander.tsimba@gmail.com>
2022-11-13 16:09:43 +0200

    add solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,56 @@
-
 export interface Student {
   // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
   // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'average',
 }
 
-// create SortOrder type
-export type SortOrder;
+function getAvg(student: Student): number {
+  return student.grades.reduce((ac, mark) => ac + mark) / student.grades.length;
+}
 
+type Sort = (prev: Student, cur: Student, sortBy?: string) => number;
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+const byAvgAsc: Sort = (prev, cur) => getAvg(prev) - getAvg(cur);
+const byAvgDesc: Sort = (prev, cur) => getAvg(cur) - getAvg(prev);
+
+export type SortOrder = 'asc' | 'desc';
+
+export function sortStudents(
+  students: object[], sortBy: SortType, order: SortOrder,
+): object[] {
+  const studCopy: object[] = [...students];
+
+  switch (sortBy) {
+    case 'average':
+      return order === 'asc'
+        ? studCopy.sort(byAvgAsc)
+        : studCopy.sort(byAvgDesc);
+
+    case 'name':
+    case 'surname':
+      return order === 'asc'
+        ? studCopy.sort((prev, cur) => prev[sortBy].localeCompare(cur[sortBy]))
+        : studCopy.sort((prev, cur) => cur[sortBy].localeCompare(prev[sortBy]));
+
+    case 'married':
+    case 'age':
+      return order === 'asc'
+        ? studCopy.sort((prev, cur) => prev[sortBy] - cur[sortBy])
+        : studCopy.sort((prev, cur) => cur[sortBy] - prev[sortBy]);
+
+    default:
+      throw new Error('Error! Check sortBy param');
+  }
 }
