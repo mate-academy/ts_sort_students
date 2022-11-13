e23e45af1f0e68506c919fec41a89601c7c63d8d
Ruslan Nedaiborshch <rnedaiborshch@gmail.com>
2022-11-13 19:39:44 +0200

    cod for sort students

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,53 @@
-
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married?: boolean,
+  grades?: number[],
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export
+function sortStudents(students: [], sortBy: SortType, order: string): [] {
+  const copyArrayStudents = Object.assign([], students);
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  function avarage(array: []): number {
+    return (array.reduce((sum: number, x: number) => sum + x, 0))
+    / array.length;
+  }
+
+  if (sortBy === SortType.Age) {
+    return order === 'asc'
+      ? copyArrayStudents.sort((a, b) => a[sortBy] - b[sortBy])
+      : copyArrayStudents.sort((a, b) => b[sortBy] - a[sortBy]);
+  }
+
+  if (sortBy === SortType.Married) {
+    return order === 'asc'
+      ? copyArrayStudents.sort((a, b) => a[sortBy] - b[sortBy])
+      : copyArrayStudents.sort((a, b) => b[sortBy] - a[sortBy]);
+  }
+
+  if (sortBy === SortType.AverageGrade) {
+    return order === 'asc'
+      ? copyArrayStudents.sort((a, b) => avarage(a.grades) - avarage(b.grades))
+      : copyArrayStudents.sort((a, b) => avarage(b.grades) - avarage(a.grades));
+  }
+
+  if (typeof sortBy === 'string') {
+    return order === 'asc'
+      ? copyArrayStudents.sort((a, b) => a[sortBy].localeCompare(b[sortBy]))
+      : copyArrayStudents.sort((a, b) => b[sortBy].localeCompare(a[sortBy]));
+  }
+
+  return [];
 }
