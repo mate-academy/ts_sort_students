9d74ce96dce032510dd982f14ba8c12e6c9c7943
Alex Tsimba <alexander.tsimba@gmail.com>
2022-11-14 17:33:09 +0200

    minor fixes

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -16,16 +16,15 @@ export enum SortType {
   AverageGrade = 'average',
 }
 
+export type SortOrder = 'asc' | 'desc';
+type Sort = (prev: Student, cur: Student) => number;
+
 function getAvg(student: Student): number {
   return student.grades.reduce((ac, mark) => ac + mark) / student.grades.length;
 }
 
-type Sort = (prev: Student, cur: Student, sortBy?: string) => number;
-
 const byAvgAsc: Sort = (prev, cur) => getAvg(prev) - getAvg(cur);
 const byAvgDesc: Sort = (prev, cur) => getAvg(cur) - getAvg(prev);
-
-export type SortOrder = 'asc' | 'desc';
 
 export function sortStudents(
   students: object[], sortBy: SortType, order: SortOrder,
@@ -33,22 +32,26 @@ export function sortStudents(
   const studCopy: object[] = [...students];
 
   switch (sortBy) {
-    case 'average':
+    case SortType.AverageGrade:
       return order === 'asc'
         ? studCopy.sort(byAvgAsc)
         : studCopy.sort(byAvgDesc);
 
-    case 'name':
-    case 'surname':
-      return order === 'asc'
-        ? studCopy.sort((prev, cur) => prev[sortBy].localeCompare(cur[sortBy]))
-        : studCopy.sort((prev, cur) => cur[sortBy].localeCompare(prev[sortBy]));
+    case SortType.Name:
+    case SortType.Surname:
+      return studCopy.sort((prev, cur) => {
+        return order === 'asc'
+          ? prev[sortBy].localeCompare(cur[sortBy])
+          : cur[sortBy].localeCompare(prev[sortBy]);
+      });
 
-    case 'married':
-    case 'age':
-      return order === 'asc'
-        ? studCopy.sort((prev, cur) => prev[sortBy] - cur[sortBy])
-        : studCopy.sort((prev, cur) => cur[sortBy] - prev[sortBy]);
+    case SortType.Married:
+    case SortType.Age:
+      return studCopy.sort((prev, cur) => {
+        return order === 'asc'
+          ? prev[sortBy] - cur[sortBy]
+          : cur[sortBy] - prev[sortBy];
+      });
 
     default:
       throw new Error('Error! Check sortBy param');
