3aa3e5448964500d8a3772813576850eb6c83241
metin <metinbicaksiz@gmail.com>
2022-11-14 22:42:21 +0300

    solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,57 @@
 
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[],
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
+}
+
+function averageCalculate(grades: number[]): number {
+  return grades.reduce((sum: number, item: number) => sum + item, 0)
+    / grades.length;
 }
 
 // create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(students: Student[], sortBy: SortType,
+  order :SortOrder): Student[] {
+  const newStudents: Student[] = [...students];
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  switch (sortBy) {
+    case SortType.Name:
+    case SortType.Surname:
+      return newStudents.sort((a: Student, b: Student) => {
+        return (order === 'asc')
+          ? a[sortBy].localeCompare(b[sortBy])
+          : b[sortBy].localeCompare(a[sortBy]);
+      });
+
+    case SortType.Age:
+    case SortType.Married:
+      return newStudents.sort((a: Student, b: Student) => {
+        return (order === 'asc')
+          ? +a[sortBy] - +b[sortBy]
+          : +b[sortBy] - +a[sortBy];
+      });
+
+    case SortType.AverageGrade:
+      return newStudents.sort((a: Student, b: Student) => {
+        return (order === 'asc')
+          ? averageCalculate(a[sortBy]) - averageCalculate(b[sortBy])
+          : averageCalculate(b[sortBy]) - averageCalculate(a[sortBy]);
+      });
+
+    default:
+      return newStudents;
+  }
 }
