c6bed47dc51cab2768978c1c43aa00f0588d9c5f
Oleksandr Usov <usov.sasha.93@gmail.com>
2022-11-16 15:28:35 +0200

    Added a task solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,59 @@
 
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
 // create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const result: Student[] = [...students];
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  result.sort((studentA, studentB) => {
+    switch (sortBy) {
+      case SortType.Name:
+      case SortType.Surname:
+        return (order === 'asc')
+          ? studentA[sortBy].localeCompare(studentB[sortBy])
+          : studentB[sortBy].localeCompare(studentA[sortBy]);
+
+      case SortType.Age:
+      case SortType.Married:
+        return (order === 'asc')
+          ? +studentA[sortBy] - +studentB[sortBy]
+          : +studentB[sortBy] - +studentA[sortBy];
+
+      case SortType.AverageGrade:
+        return (order === 'asc')
+          ? studentA[sortBy].reduce((sum, current) => sum + current, 0)
+            / studentA[sortBy].length
+            - studentB[sortBy].reduce((sum, current) => sum + current, 0)
+            / studentB[sortBy].length
+          : studentB[sortBy].reduce((sum, current) => sum + current, 0)
+            / studentB[sortBy].length
+            - studentA[sortBy].reduce((sum, current) => sum + current, 0)
+            / studentA[sortBy].length;
+
+      default:
+        throw new Error('Error');
+    }
+  });
+
+  return result;
 }
