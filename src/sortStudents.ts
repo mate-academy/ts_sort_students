a3232a05f3f5d5548ef2d3f6d4285c07f52f0b77
Yuri Gatskovskyi <yurimoreau@gmail.com>
2022-11-21 23:31:20 +0200

    solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,65 @@
 
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades'
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const studCopy: Student[] = [...students];
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  function getAverage(array: number[]): number {
+    return array.reduce((sum: number, num: number) => sum + num, 0)
+      / array.length;
+  }
+
+  switch (sortBy) {
+    case SortType.Name:
+    case SortType.Surname:
+      return studCopy.sort(
+        (firstStud: Student, secondStud: Student) => {
+          return order === 'asc'
+            ? firstStud[sortBy].localeCompare(secondStud[sortBy])
+            : secondStud[sortBy].localeCompare(firstStud[sortBy]);
+        },
+      );
+
+    case SortType.Age:
+    case SortType.Married:
+      return studCopy.sort(
+        (firstStud: Student, secondStud: Student) => {
+          return order === 'asc'
+            ? +firstStud[sortBy] - +secondStud[sortBy]
+            : +secondStud[sortBy] - +firstStud[sortBy];
+        },
+      );
+
+    case SortType.AverageGrade:
+      return studCopy.sort(
+        (firstStud: Student, secondStud: Student) => {
+          return order === 'asc'
+            ? getAverage(firstStud[sortBy]) - getAverage(secondStud[sortBy])
+            : getAverage(secondStud[sortBy]) - getAverage(firstStud[sortBy]);
+        },
+      );
+
+    default:
+      throw new Error('Sorting failed');
+  }
 }
