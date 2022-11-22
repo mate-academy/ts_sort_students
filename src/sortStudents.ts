a28649bef1f102af4b11ced908d58de38bd2e2eb
Yuri Gatskovskyi <yurimoreau@gmail.com>
2022-11-22 19:38:20 +0200

    solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -33,33 +33,50 @@ export function sortStudents(
     case SortType.Name:
     case SortType.Surname:
       return studCopy.sort(
-        (firstStud: Student, secondStud: Student) => {
-          return order === 'asc'
-            ? firstStud[sortBy].localeCompare(secondStud[sortBy])
-            : secondStud[sortBy].localeCompare(firstStud[sortBy]);
+        (firstStud: Student, secondStud: Student): number => {
+          switch (order) {
+            case 'asc':
+              return firstStud[sortBy].localeCompare(secondStud[sortBy]);
+            case 'desc':
+              return secondStud[sortBy].localeCompare(firstStud[sortBy]);
+            default:
+              throw new Error('Alphabetic sort failed');
+          }
         },
       );
 
     case SortType.Age:
     case SortType.Married:
       return studCopy.sort(
-        (firstStud: Student, secondStud: Student) => {
-          return order === 'asc'
-            ? +firstStud[sortBy] - +secondStud[sortBy]
-            : +secondStud[sortBy] - +firstStud[sortBy];
+        (firstStud: Student, secondStud: Student): number => {
+          switch (order) {
+            case 'asc':
+              return +firstStud[sortBy] - +secondStud[sortBy];
+            case 'desc':
+              return +secondStud[sortBy] - +firstStud[sortBy];
+            default:
+              throw new Error('Numeric sort failed');
+          }
         },
       );
 
     case SortType.AverageGrade:
       return studCopy.sort(
-        (firstStud: Student, secondStud: Student) => {
-          return order === 'asc'
-            ? getAverage(firstStud[sortBy]) - getAverage(secondStud[sortBy])
-            : getAverage(secondStud[sortBy]) - getAverage(firstStud[sortBy]);
+        (firstStud: Student, secondStud: Student): number => {
+          switch (order) {
+            case 'asc':
+              return getAverage(firstStud[sortBy])
+              - getAverage(secondStud[sortBy]);
+            case 'desc':
+              return getAverage(secondStud[sortBy])
+              - getAverage(firstStud[sortBy]);
+            default:
+              throw new Error('Numeric average sort failed');
+          }
         },
       );
 
     default:
-      throw new Error('Sorting failed');
+      throw new Error('Sorting failed. Not a valid sort type');
   }
 }
