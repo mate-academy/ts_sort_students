7179760ebb2c07a26018873850e78b57a951beab
Demut <uddarendeli1@gmail.com>
2022-11-27 00:01:10 +0300

    Solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,61 @@
 
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[],
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
 // create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+// return the average grade of a student
+function averageGrade(student: Student): number {
+  return student.grades.reduce((a, b) => a + b) / student.grades.length || 1;
+}
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+// default compare for three basic types
+function defaultCompare(
+  st1: string | number | boolean,
+  st2: string | number | boolean,
+  order: SortOrder,
+): number {
+  if (st1 > st2) {
+    return order === 'asc'
+      ? 1
+      : -1;
+  }
+
+  if (st1 < st2) {
+    return order === 'asc'
+      ? -1
+      : 1;
+  }
+
+  return 0;
+}
+
+export function sortStudents(
+  students: Student[],
+  sortBy: string,
+  order: SortOrder,
+): Student[] {
+  switch (sortBy) {
+    case SortType.AverageGrade:
+      return [...students].sort((st1, st2) => (order === 'asc'
+        ? averageGrade(st1) - averageGrade(st2)
+        : averageGrade(st2) - averageGrade(st1)));
+    default:
+      return [...students]
+        .sort((st1, st2) => defaultCompare(st1[sortBy], st2[sortBy], order));
+  }
 }
