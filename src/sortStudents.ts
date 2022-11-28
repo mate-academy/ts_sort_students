333596695283cea746020412fe41f7c0fb05d5ea
Dima <dimak119988@gmail.com>
2022-11-29 00:58:34 +0200

    First Solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,78 @@
 
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
+
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name,
+  Surname,
+  Age,
+  Married,
+  AverageGrade
 }
 
 // create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function
+sortStudents(students: Student[], sortBy: SortType, order: SortOrder)
+  : Student[] {
+  const dublicate = [...students];
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  function average(array: number[]): number {
+    const sum = array.reduce((prev, current) => prev + current);
+
+    return sum / array.length;
+  }
+
+  if (order === 'asc') {
+    if (sortBy === SortType.Surname) {
+      dublicate.sort((a, b) => a.surname.localeCompare(b.surname));
+    }
+
+    if (sortBy === SortType.Name) {
+      dublicate.sort((a, b) => a.name.localeCompare(b.name));
+    }
+
+    if (sortBy === SortType.Age) {
+      dublicate.sort((a, b) => a.age - b.age);
+    }
+
+    if (sortBy === SortType.Married) {
+      dublicate
+        .sort((a, b) => String(b.married).localeCompare(String(a.married)));
+    }
+
+    if (sortBy === SortType.AverageGrade) {
+      dublicate.sort((a, b) => average(a.grades) - average(b.grades));
+    }
+  } else {
+    if (sortBy === SortType.Surname) {
+      dublicate.sort((a, b) => b.surname.localeCompare(a.surname));
+    }
+
+    if (sortBy === SortType.Name) {
+      dublicate.sort((a, b) => b.name.localeCompare(a.name));
+    }
+
+    if (sortBy === SortType.Age) {
+      dublicate.sort((a, b) => b.age - a.age);
+    }
+
+    if (sortBy === SortType.Married) {
+      dublicate
+        .sort((a, b) => String(b.married).localeCompare(String(a.married)));
+    }
+
+    if (sortBy === SortType.AverageGrade) {
+      dublicate.sort((a, b) => average(b.grades) - average(a.grades));
+    }
+  }
+
+  return dublicate;
 }
