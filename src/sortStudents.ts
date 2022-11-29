4057d57815dc7bb1ab892382891e7da93aacf33a
Kolya Movchan <kmovchan261@gmail.com>
2022-11-29 20:41:45 +0200

    ts_sort_students

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,66 @@
-
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[],
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades'
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(
+  students: [], sortBy: SortType, order: SortOrder,
+): object[] {
+  const studentCopy: [] = [...students];
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  const getAverage = (value: number[], total: number): number => {
+    return value.reduce((startValue, current) => (
+      current + startValue), 0) / total;
+  };
+
+  switch (sortBy) {
+    case SortType.Name:
+    case SortType.Surname:
+      return studentCopy.sort(
+        (Student1: Student, Student2: Student) => {
+          return order === 'asc'
+            ? Student1[sortBy].localeCompare(Student2[sortBy])
+            : Student2[sortBy].localeCompare(Student1[sortBy]);
+        },
+      );
+
+    case SortType.Age:
+    case SortType.Married:
+      return studentCopy.sort(
+        (Student1: Student, Student2: Student) => {
+          return order === 'asc'
+            ? +Student1[sortBy] - +Student2[sortBy]
+            : +Student2[sortBy] - +Student1[sortBy];
+        },
+      );
+
+    case SortType.AverageGrade:
+      return studentCopy.sort(
+        (Student1: Student, Student2: Student) => {
+          const grades1 = Student1[sortBy];
+          const grades2 = Student2[sortBy];
+          const total1 = grades1.length;
+          const total2 = grades2.length;
+
+          return order === 'asc'
+            ? getAverage(grades1, total1) - getAverage(grades2, total2)
+            : getAverage(grades2, total2) - getAverage(grades1, total1);
+        },
+      );
+
+    default: return studentCopy;
+  }
 }
