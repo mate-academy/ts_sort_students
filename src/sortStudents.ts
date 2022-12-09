6383af3872bc444a1780c367f2864609dc490187
Ninel Levchenko <blackmaplearts@gmail.com>
2022-12-09 16:30:51 +0200

    solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,53 @@
 
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+const getAverageNumber = (arr: number[]): number => {
+  return arr.reduce((prev, current) => prev + current, 0) / arr.length;
+};
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+):Student[] {
+  return [...students].sort((firstPerson, secondPerson) => {
+    const a = order === 'asc'
+      ? firstPerson
+      : secondPerson;
+    const b = order === 'asc'
+      ? secondPerson
+      : firstPerson;
+
+    switch (sortBy) {
+      case SortType.Name:
+      case SortType.Surname:
+        return a[sortBy].localeCompare(b[sortBy]);
+
+      case SortType.Age:
+      case SortType.Married:
+        return Number(a[sortBy]) - Number(b[sortBy]);
+
+      case SortType.AverageGrade:
+        return getAverageNumber(a.grades) - getAverageNumber(b.grades);
+
+      default:
+        throw Error('This type doesn\'t exist');
+    }
+  });
 }
