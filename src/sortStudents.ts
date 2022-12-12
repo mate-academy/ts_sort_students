153c5cf0849d4b92d41f1292a17e71d03da54021
ysrckr <dyt.yasarcakir@gmail.com>
2022-12-12 13:34:53 +0300

    after the review fixes

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,4 +1,9 @@
 
+function averageGrade(grades: number[]): number {
+  return grades.reduce((accum: number, curr: number) => {
+    return accum + curr;
+  }, 0) / grades.length;
+}
 export interface Student {
   name: string
   surname: string
@@ -25,87 +30,52 @@ export function sortStudents(
 ): Student[] {
   switch (sortBy) {
     case SortType.Name:
-      if (order === 'asc') {
-        return students
-          .map((student: Student) => student)
-          .sort((a: Student, b: Student) => a.name.localeCompare(b.name));
-      }
-
-      return students
-        .map((student: Student) => student)
-        .sort((a: Student, b: Student) => b.name.localeCompare(a.name));
-    case SortType.Surname:
-      if (order === 'asc') {
-        return students
-          .map((student: Student) => student)
-          .sort((a: Student, b: Student) => a.surname.localeCompare(b.surname));
-      }
-
-      return students
-        .map((student: Student) => student)
-        .sort((a: Student, b: Student) => b.surname.localeCompare(a.surname));
-    case SortType.Age:
-      if (order === 'asc') {
-        return students
-          .map((student: Student) => student)
-          .sort((a: Student, b: Student) => a.age - b.age);
-      }
-
-      return students
-        .map((student: Student) => student)
-        .sort((a: Student, b: Student) => b.age - a.age);
-    case SortType.Married:
-      if (order === 'asc') {
-        return students
-          .map((student: Student) => student)
-          .sort((a: Student, b: Student) => {
-            return Number(a.married) - Number(b.married);
-          });
-      }
-
       return students
         .map((student: Student) => student)
         .sort((a: Student, b: Student) => {
-          return Number(b.married) - Number(a.married);
+          return order === 'asc'
+            ? a.name.localeCompare(b.name)
+            : b.name.localeCompare(a.name);
         });
+
+    case SortType.Surname:
+      return students
+        .map((student: Student) => student)
+        .sort((a: Student, b: Student) => {
+          return order === 'asc'
+            ? a.surname.localeCompare(b.surname)
+            : b.surname.localeCompare(a.surname);
+        });
+
+    case SortType.Age:
+      return students
+        .map((student: Student) => student)
+        .sort((a: Student, b: Student) => {
+          return order === 'asc'
+            ? a.age - b.age
+            : b.age - a.age;
+        });
+
+    case SortType.Married:
+      return students
+        .map((student: Student) => student)
+        .sort((a: Student, b: Student) => {
+          return order === 'asc'
+            ? Number(a.married) - Number(b.married)
+            : Number(b.married) - Number(a.married);
+        });
+
     case SortType.AverageGrade:
-      if (order === 'asc') {
-        return students
-          .map((student: Student) => student)
-          .sort(
-            (a: Student, b: Student) => {
-              const averageGradeA: number = a.grades
-                .reduce((accum: number, curr: number) => {
-                  return accum + curr;
-                }, 0) / a.grades.length;
-
-              const averageGradeB = b.grades
-                .reduce((accum: number, curr: number) => {
-                  return accum + curr;
-                }, 0) / b.grades.length;
-
-              return averageGradeA - averageGradeB;
-            },
-          );
-      }
-
       return students
         .map((student: Student) => student)
         .sort(
           (a: Student, b: Student) => {
-            const averageGradeA = a.grades
-              .reduce((accum: number, curr: number) => {
-                return accum + curr;
-              }, 0) / a.grades.length;
-
-            const averageGradeB = b.grades
-              .reduce((accum: number, curr: number) => {
-                return accum + curr;
-              }, 0) / b.grades.length;
-
-            return averageGradeB - averageGradeA;
+            return order === 'asc'
+              ? averageGrade(a.grades) - averageGrade(b.grades)
+              : averageGrade(b.grades) - averageGrade(a.grades);
           },
         );
+
     default:
       return students;
   }
