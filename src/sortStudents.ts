b5eee8662f18e6560fd6f0ea0f36e60d65bd41cb
ysrckr <dyt.yasarcakir@gmail.com>
2022-12-13 17:48:28 +0300

    after the third  review

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,9 +1,3 @@
-
-function getAverageGrade(grades: number[]): number {
-  return grades.reduce((accum: number, curr: number) => {
-    return accum + curr;
-  }, 0) / grades.length;
-}
 export interface Student {
   name: string
   surname: string
@@ -20,98 +14,71 @@ export enum SortType {
   AverageGrade = 'average grade',
 }
 
-// create SortOrder type
+export type StudentProperties = number | string | boolean;
+
 export type SortOrder = 'asc' | 'desc';
+
+function sortByProperty(
+  propertyA: StudentProperties,
+  propertyB: StudentProperties,
+  order: SortOrder,
+): number {
+  switch (order) {
+    case 'asc':
+      if (propertyA > propertyB) {
+        return 1;
+      }
+
+      if (propertyA < propertyB) {
+        return -1;
+      }
+
+      return 0;
+
+    case 'desc':
+      if (propertyA > propertyB) {
+        return -1;
+      }
+
+      if (propertyA < propertyB) {
+        return 1;
+      }
+
+      return 0;
+
+    default:
+      return 0;
+  }
+}
+
+function getAverageGrade(grades: number[]): number {
+  return grades.reduce((accum: number, curr: number) => {
+    return accum + curr;
+  }, 0) / grades.length;
+}
 
 export function sortStudents(
   students: Student[],
   sortBy: SortType,
   order: SortOrder,
 ): Student[] {
-  switch (sortBy) {
-    case SortType.Name:
-      return students
-        .map((student: Student) => student)
-        .sort((a: Student, b: Student) => {
-          switch (order) {
-            case 'asc':
-              return a.name.localeCompare(b.name);
-
-            case 'desc':
-              return b.name.localeCompare(a.name);
-
-            default:
-              return 0;
-          }
-        });
-
-    case SortType.Surname:
-      return students
-        .map((student: Student) => student)
-        .sort((a: Student, b: Student) => {
-          switch (order) {
-            case 'asc':
-              return a.surname.localeCompare(b.surname);
-
-            case 'desc':
-              return b.surname.localeCompare(a.surname);
-
-            default:
-              return 0;
-          }
-        });
-
-    case SortType.Age:
-      return students
-        .map((student: Student) => student)
-        .sort((a: Student, b: Student) => {
-          switch (order) {
-            case 'asc':
-              return a.age - b.age;
-
-            case 'desc':
-              return b.age - a.age;
-
-            default:
-              return 0;
-          }
-        });
-
-    case SortType.Married:
-      return students
-        .map((student: Student) => student)
-        .sort((a: Student, b: Student) => {
-          switch (order) {
-            case 'asc':
-              return Number(a.married) - Number(b.married);
-
-            case 'desc':
-              return Number(b.married) - Number(a.married);
-
-            default:
-              return 0;
-          }
-        });
-
-    case SortType.AverageGrade:
-      return students
-        .map((student: Student) => student)
-        .sort(
-          (a: Student, b: Student) => {
-            switch (order) {
-              case 'asc':
-                return getAverageGrade(a.grades) - getAverageGrade(b.grades);
-
-              case 'desc':
-                return getAverageGrade(b.grades) - getAverageGrade(a.grades);
-
-              default:
-                return 0;
-            }
-          },
-        );
-
-    default:
-      return students;
+  if (sortBy === SortType.AverageGrade) {
+    return students
+      .map((student: Student) => student)
+      .sort(
+        (a: Student, b: Student) => {
+          return sortByProperty(
+            getAverageGrade(a.grades),
+            getAverageGrade(b.grades),
+            order,
+          );
+        },
+      );
   }
+
+  return students
+    .map((student: Student) => student)
+    .sort((a: Student, b: Student) => {
+      return sortByProperty(a[sortBy], b[sortBy], order);
+    });
 }
