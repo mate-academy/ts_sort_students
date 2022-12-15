a2973da03ff7e71badba3afe6fb908769d8bd15b
Lefteris Loizides <loizides.lef@gmail.com>
2022-12-15 03:28:19 +0200

    solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -20,45 +20,43 @@ export type SortFunc = (a: Student[], b: SortType, c: SortOrder) => Student[];
 export const sortStudents: SortFunc = (students, sortBy, order) => {
   type Num = number;
 
-  const sortCallback: Function = () => {
-    return (a: Student, b: Student): Num => {
-      let studentA: Student = a;
-      let studentB: Student = b;
+  const sortCallback = (a: Student, b: Student): Num => {
+    let studentA: Student = a;
+    let studentB: Student = b;
 
-      if (order === 'desc') {
-        studentA = b;
-        studentB = a;
-      }
+    if (order === 'desc') {
+      studentA = b;
+      studentB = a;
+    }
 
-      if (sortBy === SortType.AverageGrade) {
-        const getSum: Function = () => (x: Num, y: Num): Num => x + y;
-        const sumA: Num = studentA.grades.reduce(getSum());
-        const sumB: Num = studentB.grades.reduce(getSum());
-        const averageA: Num = sumA / studentA.grades.length;
-        const averageB: Num = sumB / studentB.grades.length;
+    if (sortBy === SortType.AverageGrade) {
+      const getSum = (x: Num, y: Num): Num => x + y;
+      const sumA: Num = studentA.grades.reduce(getSum);
+      const sumB: Num = studentB.grades.reduce(getSum);
+      const averageA: Num = sumA / studentA.grades.length;
+      const averageB: Num = sumB / studentB.grades.length;
 
-        return averageA - averageB;
-      }
+      return averageA - averageB;
+    }
 
-      switch (sortBy) {
-        case SortType.Name:
-          return studentA.name.localeCompare(studentB.name);
+    switch (sortBy) {
+      case SortType.Name:
+        return studentA.name.localeCompare(studentB.name);
 
-        case SortType.Surname:
-          return studentA.surname.localeCompare(studentB.surname);
+      case SortType.Surname:
+        return studentA.surname.localeCompare(studentB.surname);
 
-        case SortType.Age:
-          return studentA.age - studentB.age;
+      case SortType.Age:
+        return studentA.age - studentB.age;
 
-        case SortType.Married:
-          return !studentA.married && studentB.married
-            ? -1
-            : 1;
+      case SortType.Married:
+        return !studentA.married && studentB.married
+          ? -1
+          : 1;
 
-        default: return 0;
-      }
-    };
+      default: return 0;
+    }
   };
 
-  return [...students].sort(sortCallback());
+  return [...students].sort(sortCallback);
 };
