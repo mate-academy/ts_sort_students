ae90fa72a0f0a1408c9539a9632c25deb58a15e0
Illia Shcherbak <illiashcherbak6@gmail.com>
2022-12-16 23:24:15 +0200

    solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,74 @@
 
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades'
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+function getAverageGrades(gradesArray: number[]): number {
+  const sumOfGrades = gradesArray.reduce((acc, curr) => acc + curr, 0);
+  const avarageAge = sumOfGrades / gradesArray.length;
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  return avarageAge;
+}
+
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  let sortedStudentsList = [...students];
+
+  switch (sortBy) {
+    case SortType.Name:
+    case SortType.Surname:
+      sortedStudentsList = sortedStudentsList
+        .sort((currStudent, nextStudent) => {
+          return order === 'asc'
+            ? currStudent[sortBy].localeCompare(nextStudent[sortBy])
+            : nextStudent[sortBy].localeCompare(currStudent[sortBy]);
+        });
+
+      break;
+
+    case SortType.Age:
+    case SortType.Married:
+      sortedStudentsList = sortedStudentsList
+        .sort((currStudent, nextStudent) => {
+          return order === 'asc'
+            ? Number(currStudent[sortBy]) - Number(nextStudent[sortBy])
+            : Number(nextStudent[sortBy]) - Number(currStudent[sortBy]);
+        });
+
+      break;
+
+    case SortType.AverageGrade:
+      sortedStudentsList = sortedStudentsList
+        .sort((currStudent, nextStudent) => {
+          return order === 'asc'
+            ? getAverageGrades(currStudent.grades)
+              - getAverageGrades(nextStudent.grades)
+            : getAverageGrades(nextStudent.grades)
+              - getAverageGrades(currStudent.grades);
+        });
+
+      break;
+
+    default:
+      break;
+  }
+
+  return sortedStudentsList;
 }
