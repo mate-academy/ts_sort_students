6fcce7a95e3ea1d6c9f7c0901b2f419cc63bb0f9
Levente Krotos <krotos.levi@gmail.com>
2022-12-16 20:59:25 +0100

    fix

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -31,25 +31,17 @@ export function sortStudents(
   const studentsCopy = [...students];
 
   switch (sortBy) {
-    case (SortType.Name):
+    case SortType.Name:
+    case SortType.Surname:
       return (order === 'asc')
-        ? studentsCopy.sort((a, b) => a.name.localeCompare(b.name))
-        : studentsCopy.sort((a, b) => b.name.localeCompare(a.name));
-
-    case (SortType.Surname):
-      return (order === 'asc')
-        ? studentsCopy.sort((a, b) => a.surname.localeCompare(b.surname))
-        : studentsCopy.sort((a, b) => b.surname.localeCompare(a.surname));
+        ? studentsCopy.sort((a, b) => a[sortBy].localeCompare(b[sortBy]))
+        : studentsCopy.sort((a, b) => b[sortBy].localeCompare(a[sortBy]));
 
     case (SortType.Age):
-      return (order === 'asc')
-        ? studentsCopy.sort((a, b) => a.age - b.age)
-        : studentsCopy.sort((a, b) => b.age - a.age);
-
     case (SortType.Married):
       return (order === 'asc')
-        ? studentsCopy.sort((a, b) => Number(a.married) - Number(b.married))
-        : studentsCopy.sort((a, b) => Number(b.married) - Number(a.married));
+        ? studentsCopy.sort((a, b) => Number(a[sortBy]) - Number(b[sortBy]))
+        : studentsCopy.sort((a, b) => Number(b[sortBy]) - Number(a[sortBy]));
 
     case (SortType.AverageGrade):
       return (order === 'asc')
