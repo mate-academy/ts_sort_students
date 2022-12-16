658bc8bf3a20b67299f153c0cb7668b7f774391f
Olha Mamai <olgga.ju@gmail.com>
2022-12-16 21:11:50 +0200

    Solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,55 @@
 
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[],
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+function calculateAvarageMark(allMarks: number[]): number {
+  return allMarks.reduce((total, current) => total + current) / allMarks.length;
+}
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const copiedStudents = [...students];
+
+  return copiedStudents.sort((student1: Student, student2: Student) => {
+    switch (sortBy) {
+      case SortType.Name:
+      case SortType.Surname:
+        return order === 'asc'
+          ? student1[sortBy].localeCompare(student2[sortBy])
+          : student2[sortBy].localeCompare(student1[sortBy]);
+
+      case SortType.Age:
+      case SortType.Married:
+        return order === 'asc'
+          ? +student1[sortBy] - +student2[sortBy]
+          : +student2[sortBy] - +student1[sortBy];
+
+      case SortType.AverageGrade:
+        return order === 'asc'
+          ? calculateAvarageMark(student1[sortBy])
+          - calculateAvarageMark(student2[sortBy])
+          : calculateAvarageMark(student2[sortBy])
+          - calculateAvarageMark(student1[sortBy]);
+
+      default: return 0;
+    }
+  });
 }
