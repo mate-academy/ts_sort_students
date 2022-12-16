f8d417ba44ff5db8ac5f44eca2f9b59ceea49e39
Sveta Kryukova <kryukova.sv.13@gmail.com>
2022-12-16 19:45:14 +0200

    solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,62 @@
 
 export interface Student {
-  // describe Student interface
+  name:string;
+  surname:string;
+  age:number;
+  married:boolean;
+  grades:number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
 // create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+function getAverageGrade(grades:number[]):number {
+  return grades.reduce((previous:number, current:number) => previous
+  + current, 0) / grades.length;
+}
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+export function sortStudents(
+  students:Student[],
+  sortBy:SortType,
+  order:SortOrder,
+):Student[] {
+  const sortedStudents = [...students];
+
+  switch (sortBy) {
+    case SortType.Name:
+    case SortType.Surname:
+      return order === 'asc'
+        // eslint-disable-next-line max-len
+        ? sortedStudents.sort((first, second) => first[sortBy].localeCompare(second[sortBy]))
+        // eslint-disable-next-line max-len
+        : sortedStudents.sort((first, second) => second[sortBy].localeCompare(first[sortBy]));
+
+    case SortType.Age:
+    case SortType.Married:
+      return order === 'asc'
+        // eslint-disable-next-line max-len
+        ? sortedStudents.sort((first, second) => Number(first[sortBy]) - Number((second[sortBy])))
+        // eslint-disable-next-line max-len
+        : sortedStudents.sort((first, second) => Number(second[sortBy]) - Number(first[sortBy]));
+
+    case SortType.AverageGrade:
+      return order === 'asc'
+        // eslint-disable-next-line max-len
+        ? sortedStudents.sort((first, second) => getAverageGrade(first[sortBy]) - getAverageGrade(second[sortBy]))
+        // eslint-disable-next-line max-len
+        : sortedStudents.sort((first, second) => getAverageGrade(second[sortBy]) - getAverageGrade(first[sortBy]));
+
+    default:
+      break;
+  }
+
+  return sortedStudents;
 }
