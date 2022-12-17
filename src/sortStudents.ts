1d8ff17f6b9c204e8416915dc9ef8865496f0052
Vladimir Arkanov <vladimirarkanov@icloud.com>
2022-12-17 23:29:13 +0200

    solution try 1

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,77 @@
 
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[],
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+function countMarks(grades:number[]):number {
+  return grades.reduce((a, b) => a + b, 0) / grades.length;
+}
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+export function sortStudents(
+  students:Student[],
+  sortBy:SortType,
+  order:SortOrder,
+):Student[] {
+  const sortedStudents:Student[] = [...students];
+
+  switch (sortBy) {
+    case
+      SortType.Name:
+      if (order === 'asc') {
+        return sortedStudents.sort((a, b) => (
+          a[sortBy].localeCompare(b[sortBy])));
+      }
+
+      return sortedStudents.sort((a, b) => b[sortBy].localeCompare(a[sortBy]));
+
+    case SortType.Surname:
+      if (order === 'asc') {
+        return sortedStudents.sort((a, b) => (
+          a[sortBy].localeCompare(b[sortBy])));
+      }
+
+      return sortedStudents.sort((a, b) => b[sortBy].localeCompare(a[sortBy]));
+
+    case SortType.Age:
+      if (order === 'asc') {
+        return sortedStudents.sort((a, b) => a[sortBy] - b[sortBy]);
+      }
+
+      return sortedStudents.sort((a, b) => b[sortBy] - a[sortBy]);
+
+    case SortType.Married:
+      if (order === 'asc') {
+        return sortedStudents.sort((a, b) => a[sortBy] - b[sortBy]);
+      }
+
+      return sortedStudents.sort((a, b) => b[sortBy] - a[sortBy]);
+
+    case SortType.AverageGrade:
+      if (order === 'asc') {
+        return sortedStudents.sort((a, b) => (
+          countMarks(a[sortBy]) - countMarks(b[sortBy])));
+      }
+
+      return sortedStudents.sort((a, b) => (
+        countMarks(b[sortBy]) - countMarks(a[sortBy])));
+
+    default:
+      break;
+  }
+
+  return sortedStudents;
 }
