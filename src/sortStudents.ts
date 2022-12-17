7c35f279c2ae0cd6a3bc2f80a21b274d699a52d9
Maksym Levchenko <llllewvvaa@gmail.com>
2022-12-17 20:17:19 +0200

    Solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,54 @@
 
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades'
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+const getAverageGrade = (arrayOfGrades: number[]): number => {
+  return arrayOfGrades.reduce((acc, curr) => acc + curr, 0)
+  / arrayOfGrades.length;
+};
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  return [...students].sort((firstStudent, secondStudent) => {
+    const a = order === 'asc'
+      ? firstStudent
+      : secondStudent;
+    const b = order === 'asc'
+      ? secondStudent
+      : firstStudent;
+
+    switch (sortBy) {
+      case SortType.Name:
+      case SortType.Surname:
+        return a[sortBy].localeCompare(b[sortBy]);
+
+      case SortType.Age:
+      case SortType.Married:
+        return Number(a[sortBy]) - Number(b[sortBy]);
+
+      case SortType.AverageGrade:
+        return getAverageGrade(a.grades) - getAverageGrade(b.grades);
+
+      default:
+        throw new Error('Error: Unknown sort type');
+    }
+  });
 }
