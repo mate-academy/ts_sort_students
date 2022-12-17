615317a121bca6ca8a6c7f7e6afaa70eda6d7571
Anastasiia <anastasianauk44@gmail.com>
2022-12-17 19:08:34 +0200

    Solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,60 @@
 
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
 // create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+function getAverageGreade(grades: number[]): number {
+  return grades.reduce((accum, grade) => accum + grade, 0) / grades.length;
+}
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  return [...students]
+    .sort((previous: Student, next: Student) => {
+      switch (sortBy) {
+        case (SortType.Name):
+        case (SortType.Surname):
+          return order === 'asc'
+            ? previous[sortBy].localeCompare(next[sortBy])
+            : next[sortBy].localeCompare(previous[sortBy]);
+
+        case (SortType.Age):
+          return order === 'asc'
+            ? previous[sortBy] - next[sortBy]
+            : next[sortBy] - previous[sortBy];
+
+        case (SortType.Married):
+          return order === 'asc'
+            ? Number(previous[sortBy]) - Number(next[sortBy])
+            : Number(next[sortBy]) - Number(previous[sortBy]);
+
+        case (SortType.AverageGrade):
+          return order === 'asc'
+            ? getAverageGreade(previous[sortBy])
+              - getAverageGreade(next[sortBy])
+            : getAverageGreade(next[sortBy])
+              - getAverageGreade(previous[sortBy]);
+
+        default:
+          return 0;
+      }
+    });
 }
