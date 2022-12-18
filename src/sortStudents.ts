11409f6110510a49cbbeb0744ca254edfe70ae78
Mykhailyk Vadym <ihappyspam@gmail.com>
2022-12-18 14:29:21 +0200

    Finished fixing

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -12,18 +12,18 @@ export enum SortType {
   Surname = 'surname',
   Age = 'age',
   Married = 'married',
-  AverageGrades = 'grades',
+  AverageGrade = 'grades',
 }
 
-export type SortOrder = 'desc' | 'asc';
-
-function getAverageGrade(studentGrades: (number)[]): number {
+function getAverageGrade(studentGrades: number[]): number {
   const sumOfGrades = studentGrades.reduce((sum: number, grade: number) => {
     return sum + grade;
   }, 0);
 
   return sumOfGrades / studentGrades.length;
 }
+
+export type SortOrder = 'desc' | 'asc';
 
 export function sortStudents(
   students: Student[],
@@ -35,11 +35,11 @@ export function sortStudents(
   switch (sortBy) {
     case SortType.Name:
     case SortType.Surname:
-      studentsCopy = studentsCopy.sort((stud1, stud2) =>
-        { return order === 'asc'
+      studentsCopy = studentsCopy.sort((stud1, stud2) => {
+        return order === 'asc'
           ? stud1[sortBy].localeCompare(stud2[sortBy])
           : stud2[sortBy].localeCompare(stud1[sortBy]);
-        });
+      });
 
       break;
 
@@ -53,18 +53,20 @@ export function sortStudents(
 
       break;
 
-    case SortType.AverageGrades:
+    case SortType.AverageGrade:
       studentsCopy = studentsCopy.sort((stud1, stud2) => {
         return order === 'asc'
-          ? getAverageGrade(stud1.grades) - getAverageGrade(stud2.grades)
-          : getAverageGrade(stud2.grades) - getAverageGrade(stud1.grades);
+          ? getAverageGrade(stud1.grades)
+            - getAverageGrade(stud2.grades)
+          : getAverageGrade(stud2.grades)
+            - getAverageGrade(stud1.grades);
       });
 
       break;
 
     default:
       break;
-  };
+  }
 
   return studentsCopy;
-};
+}
