fb4f17881ad5f02a2908314f2a22b23662e123bc
Kris Planka <kristyaplanka@gmail.com>
2022-12-19 15:38:23 +0200

    fixed code-style

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -30,35 +30,30 @@ export function sortStudents(
   sortBy: SortType,
   order: SortOrder,
 ): Student[] {
-  const studentsCopy = [...students];
+  return [...students]
+    .sort((student1: Student, student2: Student) => {
+      switch (sortBy) {
+        case SortType.Name:
+        case SortType.Surname:
+          return student1[sortBy].localeCompare(student2[sortBy]);
 
-  switch (sortBy) {
-    case SortType.Name:
-    case SortType.Surname:
-      return studentsCopy.sort((student1, student2) => {
-        return order === 'asc'
-          ? student1[sortBy].localeCompare(student2[sortBy])
-          : student2[sortBy].localeCompare(student1[sortBy]);
-      });
+        case SortType.Age:
+          return order === 'asc'
+            ? student1.age - student2.age
+            : student2.age - student1.age;
 
-    case SortType.Age:
-    case SortType.Married:
-      return studentsCopy.sort((student1, student2) => {
-        return order === 'asc'
-          ? Number(student1[sortBy]) - Number(student2[sortBy])
-          : Number(student2[sortBy]) - Number(student1[sortBy]);
-      });
+        case SortType.Married:
+          return Number(student2.married) - Number(student1.married);
 
-    case SortType.AverageGrade:
-      return studentsCopy.sort((student1, student2) => {
-        return order === 'asc'
-          ? countAverageGrade(student1.grades)
+        case SortType.AverageGrade:
+          return order === 'asc'
+            ? countAverageGrade(student1.grades)
             - countAverageGrade(student2.grades)
-          : countAverageGrade(student2.grades)
+            : countAverageGrade(student2.grades)
             - countAverageGrade(student1.grades);
-      });
 
-    default:
-      return studentsCopy;
-  }
+        default:
+          return 0;
+      }
+    });
 }
