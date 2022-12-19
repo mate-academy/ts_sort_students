e9c62f01c2661a11da534e2d190063498fedf3c9
Ivan Melnychuk <jacksmilereacher@gmail.com>
2022-12-19 14:28:44 +0200

    added my solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,60 @@
 
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+function getAverage(numbers: number[]): number {
+  return numbers.reduce((sum: number, currValue: number) => {
+    return sum + currValue;
+  }, 0) / numbers.length;
+}
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+export function sortStudents(
+  students: Student[], sortBy: SortType, order: SortOrder,
+): Student[] {
+  return [...students].sort((firstStudent: Student, secondStudent: Student) => {
+    switch (sortBy) {
+      case SortType.Married:
+        return order === 'asc'
+          ? Number(firstStudent.married) - Number(secondStudent.married)
+          : Number(secondStudent.married) - Number(firstStudent.married);
+
+      case SortType.Age:
+        return order === 'asc'
+          ? firstStudent.age - secondStudent.age
+          : secondStudent.age - firstStudent.age;
+
+      case SortType.Name:
+        return order === 'asc'
+          ? firstStudent.name.localeCompare(secondStudent.name)
+          : secondStudent.name.localeCompare(firstStudent.name);
+
+      case SortType.Surname:
+        return order === 'asc'
+          ? firstStudent.surname.localeCompare(secondStudent.surname)
+          : secondStudent.surname.localeCompare(firstStudent.surname);
+
+      case SortType.AverageGrade:
+        return order === 'asc'
+          ? getAverage(firstStudent.grades) - getAverage(secondStudent.grades)
+          : getAverage(secondStudent.grades) - getAverage(firstStudent.grades);
+
+      default:
+        return 0;
+    }
+  });
 }
