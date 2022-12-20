26e971c5553764b701165bb141aec49a47544d8e
Roman Bliashuk <romanblyashuk94@gmail.com>
2022-12-20 13:03:46 +0200

    Fixed studentCopy array

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -25,36 +25,38 @@ export function sortStudents(
   sortBy: SortType,
   order: SortOrder,
 ): Student[] {
+  const studentsCopy = [...students];
+
   switch (sortBy) {
     case SortType.Name:
     case SortType.Surname:
       return (order === 'asc')
-        ? [...students].sort((prevStud, currentStud) => prevStud[sortBy]
+        ? studentsCopy.sort((prevStud, currentStud) => prevStud[sortBy]
           .localeCompare(currentStud[sortBy]))
-        : [...students].sort((prevStud, currentStud) => currentStud[sortBy]
+        : studentsCopy.sort((prevStud, currentStud) => currentStud[sortBy]
           .localeCompare(prevStud[sortBy]));
 
     case SortType.Age:
     case SortType.Married:
       return (order === 'asc')
-        ? [...students].sort((prevStud, currentStud) => (
+        ? studentsCopy.sort((prevStud, currentStud) => (
           Number(prevStud[sortBy]) - Number(currentStud[sortBy])
         ))
-        : [...students].sort((prevStud, currentStud) => (
+        : studentsCopy.sort((prevStud, currentStud) => (
           Number(currentStud[sortBy]) - Number(prevStud[sortBy])
         ));
 
     case SortType.AverageGrade:
       return (order === 'asc')
-        ? [...students].sort((prevStud, currentStud) => (
+        ? studentsCopy.sort((prevStud, currentStud) => (
           getAvarageGrade(prevStud[sortBy])
             - getAvarageGrade(currentStud[sortBy])
         ))
-        : [...students].sort((prevStud, currentStud) => (
+        : studentsCopy.sort((prevStud, currentStud) => (
           getAvarageGrade(currentStud[sortBy])
             - getAvarageGrade(prevStud[sortBy])
         ));
     default:
-      return [...students];
+      return studentsCopy;
   }
 }
