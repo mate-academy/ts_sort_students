088d6e37458dc5a945f371517c8c1ece34730e1f
JzSol <111354281+JzSol@users.noreply.github.com>
2022-12-20 19:17:14 +0200

    Finished

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,54 @@
-
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'avarageGrade',
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+const avarageGrade = (grades: number[]): number => {
+  return grades.reduce((x, y) => x + y) / grades.length || 1;
+};
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const studentCopy = [...students];
+
+  switch (sortBy) {
+    case SortType.Name:
+    case SortType.Surname:
+      return order === 'desc'
+        ? studentCopy.sort((a, b) => b[sortBy].localeCompare(a[sortBy]))
+        : studentCopy.sort((a, b) => a[sortBy].localeCompare(b[sortBy]));
+
+    case SortType.Married:
+    case SortType.Age:
+      return order === 'desc'
+        ? studentCopy.sort((a, b) => Number(b[sortBy]) - Number(a[sortBy]))
+        : studentCopy.sort((a, b) => Number(a[sortBy]) - Number(b[sortBy]));
+
+    default:
+      break;
+  }
+
+  if (SortType.AverageGrade && order === 'desc') {
+    studentCopy.sort((a, b) => avarageGrade(b.grades) - avarageGrade(a.grades));
+  } else if (SortType.AverageGrade) {
+    studentCopy.sort((a, b) => avarageGrade(a.grades) - avarageGrade(b.grades));
+  }
+
+  return studentCopy;
 }
