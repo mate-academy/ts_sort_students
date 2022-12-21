487a453a8ecd9214365a48079d6af6e11828c16d
Iorga Nicu <ioghigithublab@gmail.com>
2022-12-21 18:01:07 +0200

    done

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,75 @@
 
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[],
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name,
+  Surname,
+  Age,
+  Married,
+  AverageGrade,
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const sortedStudents = [...students];
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  if (sortBy === SortType.Age) {
+    sortedStudents.sort((prevS, student) => {
+      return order === 'desc'
+        ? student.age - prevS.age
+        : prevS.age - student.age;
+    });
+  }
+
+  if (sortBy === SortType.Surname) {
+    sortedStudents.sort((prevS, student) => {
+      return order === 'desc'
+        ? student.surname.localeCompare(prevS.surname)
+        : prevS.surname.localeCompare(student.surname);
+    });
+  }
+
+  if (sortBy === SortType.Name) {
+    sortedStudents.sort((prevS, student) => {
+      return order === 'desc'
+        ? student.name.localeCompare(prevS.name)
+        : prevS.name.localeCompare(student.name);
+    });
+  }
+
+  if (sortBy === SortType.Married) {
+    sortedStudents.sort((prevS, student) => {
+      return order === 'desc'
+        ? Number(student.married) - Number(prevS.married)
+        : Number(prevS.married) - Number(student.married);
+    });
+  }
+
+  if (sortBy === SortType.AverageGrade) {
+    sortedStudents
+      .sort((prevS, student) => {
+        const prevGrades = prevS.grades
+          .reduce((acc, grade) => acc + grade) / prevS.grades.length;
+
+        const currentGrades = student.grades
+          .reduce((acc, grade) => acc + grade) / student.grades.length;
+
+        return order === 'desc'
+          ? currentGrades - prevGrades
+          : prevGrades - currentGrades;
+      });
+  }
+
+  return sortedStudents;
 }
