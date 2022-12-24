67233d248803edce397c1b6de713d9eea8d57d4d
Dmytro Kravchenko <dmytro318@gmail.com>
2022-12-25 01:02:03 +0200

    Solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,52 @@
 
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+const avgGrades = (grades: number[]): number => {
+  return grades.reduce((a, b) => a + b) / grades.length;
+};
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const copy = [...students];
+
+  switch (sortBy) {
+    case SortType.Name:
+    case SortType.Surname:
+      return order === 'desc'
+        ? copy.sort((a, b) => b[sortBy].localeCompare(a[sortBy]))
+        : copy.sort((a, b) => a[sortBy].localeCompare(b[sortBy]));
+
+    case SortType.Age:
+    case SortType.Married:
+      return order === 'desc'
+        ? copy.sort((a, b) => +b[sortBy] - +a[sortBy])
+        : copy.sort((a, b) => +a[sortBy] - +b[sortBy]);
+
+    case SortType.AverageGrade:
+      return order === 'desc'
+        ? copy.sort((a, b) => avgGrades(b[sortBy]) - avgGrades(a[sortBy]))
+        : copy.sort((a, b) => avgGrades(a[sortBy]) - avgGrades(b[sortBy]));
+
+    default:
+      return copy;
+  }
 }
