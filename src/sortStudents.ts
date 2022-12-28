b19f15a74891d0315182b2670f2fb3a03217d6be
Dima Korol <korovaidima@gmail.com>
2022-12-28 10:31:36 +0100

    solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,74 @@
 
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[],
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'averageGrade',
 }
 
 // create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(students: Student[],
+  sortBy: SortType, order: SortOrder): Student[] {
+  const copy = [...students];
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  function averageGr(grades: number[]): number {
+    return grades.reduce((a, b) => a + b, 0) / grades.length;
+  }
+
+  switch (sortBy) {
+    case SortType.Name:
+      copy.sort((studA, studB) => studA.name.localeCompare(studB.name));
+
+      if (order === 'desc') {
+        copy.sort((studA, studB) => studB.name.localeCompare(studA.name));
+      }
+      break;
+    case SortType.Surname:
+      copy.sort((studA, studB) => studA.surname.localeCompare(studB.surname));
+
+      if (order === 'desc') {
+        copy.sort((studA, studB) => studB.surname.localeCompare(studA.surname));
+      }
+      break;
+    case SortType.Age:
+      copy.sort((studA, studB) => studA.age - studB.age);
+
+      if (order === 'desc') {
+        copy.sort((studA, studB) => studB.age - studA.age);
+      }
+      break;
+    case SortType.AverageGrade:
+      copy.sort((studA, studB) => averageGr(studA.grades)
+        - averageGr(studB.grades));
+
+      if (order === 'desc') {
+        copy.sort((studA, studB) => averageGr(studB.grades)
+          - averageGr(studA.grades));
+      }
+      break;
+    case SortType.Married:
+      copy.sort((studA, studB) => Number(studA.married)
+        - Number(studB.married));
+
+      if (order === 'desc') {
+        copy.sort((studA, studB) => Number(studB.married)
+          - Number(studA.married));
+      }
+      break;
+
+    default:
+  }
+
+  return copy;
 }
