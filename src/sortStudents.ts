d2adbdfdfa9dc4b646666454f6bbcaae7677883c
Andrii Medintsev <medintsev.andrew@gmail.com>
2023-01-06 14:38:56 +0200

    Switch/case statement was fixed and  moved inside sort function

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -16,43 +16,38 @@ export enum SortType {
 
 export type SortOrder = 'asc' | 'desc';
 
+function calculateAverageGrade(gradeArray: number[]): number {
+  return gradeArray.reduce((a: number, b: number) => a + b) / gradeArray.length;
+}
+
 export function sortStudents(
   students: Student[], sortBy: SortType, order: SortOrder,
 ): Student[] {
   const newStudentsArr: Student[] = [...students];
 
-  switch (sortBy) {
-    case 'surname':
-    case 'name':
-      newStudentsArr.sort((a, b) => {
+  newStudentsArr.sort((a, b) => {
+    switch (sortBy) {
+      case SortType.Surname:
+      case SortType.Name:
         return order === 'asc'
           ? a[sortBy].localeCompare(b[sortBy])
           : b[sortBy].localeCompare(a[sortBy]);
-      });
-      break;
 
-    case 'age':
-    case 'married':
-      newStudentsArr.sort((a, b) => {
+      case SortType.Age:
+      case SortType.Married:
         return order === 'asc'
           ? +a[sortBy] - +b[sortBy]
           : +b[sortBy] - +a[sortBy];
-      });
-      break;
 
-    default:
-      newStudentsArr.sort((a, b) => {
-        const firstAverageGrade
-          = a.grades.reduce((q, w) => q + w) / a.grades.length;
-        const secondAverageGrade
-          = b.grades.reduce((e, r) => e + r) / b.grades.length;
-
+      case SortType.AverageGrade:
         return order === 'asc'
-          ? firstAverageGrade - secondAverageGrade
-          : secondAverageGrade - firstAverageGrade;
-      });
-      break;
-  }
+          ? calculateAverageGrade(a.grades) - calculateAverageGrade(b.grades)
+          : calculateAverageGrade(b.grades) - calculateAverageGrade(a.grades);
+
+      default:
+        throw new Error(`Sort type ${sortBy} not supported`);
+    }
+  });
 
   return newStudentsArr;
 }
