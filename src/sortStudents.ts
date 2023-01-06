31528661a8f11e8f705020af425a3225dff73572
Mykyta Krainik <nikinta2003@gmail.com>
2023-01-06 14:46:09 +0200

    Readability was improved

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -20,6 +20,32 @@ function calculateAverageGrade(grades: readonly number[]): number {
   return grades.reduce((sum, grade) => sum + grade, 0) / grades.length;
 }
 
+function sortByProperty(
+  studentA: Student,
+  studentB: Student,
+  sortBy: SortType,
+): number {
+  switch (sortBy) {
+    case SortType.Age:
+    case SortType.Married:
+      return Number(studentA[sortBy]) - Number(studentB[sortBy]);
+
+    case SortType.AverageGrade: {
+      const averageGradeA = calculateAverageGrade(studentA[sortBy]);
+      const averageGradeB = calculateAverageGrade(studentB[sortBy]);
+
+      return averageGradeA - averageGradeB;
+    }
+
+    case SortType.Name:
+    case SortType.Surname:
+      return studentA[sortBy].localeCompare(studentB[sortBy]);
+
+    default:
+      throw new Error('Wrong property name');
+  }
+}
+
 export function sortStudents(
   students: readonly Student[],
   sortBy: SortType,
@@ -27,43 +53,11 @@ export function sortStudents(
 ): Student[] {
   const studentsCopy = [...students];
 
-  switch (sortBy) {
-    case SortType.Age:
-    case SortType.Married:
-      return order === 'asc'
-        ? studentsCopy.sort((studentA, studentB) => {
-          return Number(studentA[sortBy]) - Number(studentB[sortBy]);
-        })
-        : studentsCopy.sort((studentA, studentB) => {
-          return Number(studentB[sortBy]) - Number(studentA[sortBy]);
-        });
-
-    case SortType.AverageGrade:
-      return order === 'asc'
-        ? studentsCopy.sort((studentA, studentB) => {
-          const averageGradeA = calculateAverageGrade(studentA[sortBy]);
-          const averageGradeB = calculateAverageGrade(studentB[sortBy]);
-
-          return averageGradeA - averageGradeB;
-        })
-        : studentsCopy.sort((studentA, studentB) => {
-          const averageGradeA = calculateAverageGrade(studentA[sortBy]);
-          const averageGradeB = calculateAverageGrade(studentB[sortBy]);
-
-          return averageGradeB - averageGradeA;
-        });
-
-    case SortType.Name:
-    case SortType.Surname:
-      return order === 'asc'
-        ? studentsCopy.sort((studentA, studentB) => {
-          return studentA[sortBy].localeCompare(studentB[sortBy]);
-        })
-        : studentsCopy.sort((studentA, studentB) => {
-          return studentB[sortBy].localeCompare(studentA[sortBy]);
-        });
-
-    default:
-      throw new Error('Wrong property name');
-  }
+  return order === 'asc'
+    ? studentsCopy.sort((studentA, studentB) => {
+      return sortByProperty(studentA, studentB, sortBy);
+    })
+    : studentsCopy.sort((studentA, studentB) => {
+      return sortByProperty(studentB, studentA, sortBy);
+    });
 }
