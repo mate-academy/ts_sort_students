c6a836b99370dfbbd8afe81e6bdc8819ecd610c5
Konstantin Burnazaki <kostas.burnazaki@gmail.com>
2023-01-07 17:41:51 +0200

    initial

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,85 @@
 
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[],
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = '' // required an initializer by lint
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  let sortFunc: Function;
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  switch (true) {
+    case sortBy === SortType.Name || sortBy === SortType.Surname:
+      sortFunc = (a: Student, b: Student): number => {
+        return a[sortBy].localeCompare(b[sortBy]);
+      };
+      break;
+
+    case sortBy === SortType.Age:
+      sortFunc = (a: Student, b: Student): number => {
+        return a[sortBy] - b[sortBy];
+      };
+      break;
+
+    case sortBy === SortType.Married:
+      sortFunc = (a: Student, b: Student): number => {
+        let res = -1;
+
+        if (a[sortBy] === b[sortBy]) {
+          res = 0;
+        } else if (a[sortBy]) {
+          res = 1;
+        }
+
+        return res;
+      };
+      break;
+
+    case sortBy === SortType.AverageGrade:
+      sortFunc = (a: Student, b: Student): number => {
+        const sumPrev = a.grades.reduce(
+          (prevGrade, nextGrade) => prevGrade + nextGrade, 0,
+        );
+        const avgPrev = (sumPrev / a.grades.length);
+        const sumNext = b.grades.reduce(
+          (prevGrade, nextGrade) => prevGrade + nextGrade, 0,
+        );
+        const avgNext = (sumNext / b.grades.length);
+
+        return avgPrev - avgNext;
+      };
+      break;
+
+    default: // default case required by linter
+      throw new Error('ERROR');
+  }
+
+  return students.sort(
+    (prevStudent: Student, nextStudent: Student): number => {
+      let studentA: Student = prevStudent;
+      let studentB: Student = nextStudent;
+
+      if (order === 'desc') {
+        [studentA, studentB] = [studentB, studentA];
+      }
+
+      return sortFunc(studentA, studentB);
+    },
+  );
 }
