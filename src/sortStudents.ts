0fbb928c251dc541f38412cad46d92417545ec82
Ivan Vyhinnyi <ivan665303@gmail.com>
2023-01-12 00:12:55 +0200

    Solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,101 @@
+import { Student } from './sortStudents';
+import { Key } from "readline";
 
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
 // create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+function averageGrade(numbers: number[]): number {
+  const sum = [...numbers].reduce((acc: number, current: number) => {
+    return acc + current;
+  }, 0);
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  return sum / numbers.length;
+}
+
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const copiedStudents = [...students];
+
+  switch (sortBy) {
+    case SortType.Name:
+    case SortType.Surname:
+      if (order === 'asc') {
+        return copiedStudents.sort(
+          (a, b) => a[sortBy].localeCompare(b[sortBy]),
+        );
+      }
+
+      if (order === 'desc') {
+        return copiedStudents.sort(
+          (a, b) => b[sortBy].localeCompare(a[sortBy]),
+        );
+      }
+      break;
+
+    case SortType.Age:
+      if (order === 'asc') {
+        return copiedStudents.sort(
+          (a, b) => a[sortBy] - b[sortBy],
+        );
+      }
+
+      if (order === 'desc') {
+        return copiedStudents.sort(
+          (a, b) => b[sortBy] - a[sortBy],
+        );
+      }
+      break;
+
+    case SortType.Married:
+      if (order === 'asc') {
+        return copiedStudents.sort(
+          (a, b) => +a[sortBy] - +b[sortBy],
+        );
+      }
+
+      if (order === 'desc') {
+        return copiedStudents.sort(
+          (a, b) => +b[sortBy] - +a[sortBy],
+        );
+      }
+      break;
+
+    case SortType.AverageGrade:
+      if (order === 'asc') {
+        return copiedStudents.sort(
+          (a, b) => averageGrade(a[sortBy]) - averageGrade(b[sortBy]),
+        );
+      }
+
+      if (order === 'desc') {
+        return copiedStudents.sort(
+          (a, b) => averageGrade(b[sortBy]) - averageGrade(a[sortBy]),
+        );
+      }
+      break;
+
+    default:
+      return copiedStudents;
+  }
+
+  return copiedStudents;
 }
