05959460f8f826686a3020089db7beb833426f84
Yuliia Andrieieva <andrieieva.y@gmail.com>
2023-01-16 00:31:13 +0200

    Solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,69 @@
 
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const copiedStudents = [...students];
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  switch (sortBy) {
+    case SortType.Age:
+      return copiedStudents.sort(
+        (a: Student, b: Student) => {
+          return order === 'asc'
+            ? +a[sortBy] - b[sortBy]
+            : +b[sortBy] - +a[sortBy];
+        },
+      );
+
+    case SortType.Name:
+    case SortType.Surname:
+      return copiedStudents.sort(
+        (a: Student, b: Student) => {
+          return order === 'asc'
+            ? String(a[sortBy]).localeCompare(String(b[sortBy]))
+            : String(b[sortBy]).localeCompare(String(a[sortBy]));
+        },
+      );
+
+    case SortType.AverageGrade:
+      return copiedStudents.sort(
+        (a: Student, b: Student) => {
+          function averageValue(arr: number[]): number {
+            return arr.reduce((prevNum, nextNum) => prevNum + nextNum)
+            / arr.length;
+          }
+
+          return order === 'asc'
+            ? averageValue(a[sortBy]) - averageValue(b[sortBy])
+            : averageValue(b[sortBy]) - averageValue(a[sortBy]);
+        },
+      );
+
+    case SortType.Married:
+      return copiedStudents
+        .filter((x) => x[sortBy] === true)
+        .concat(copiedStudents.filter((x) => x[sortBy] === false));
+
+    default:
+      return copiedStudents;
+  }
 }
