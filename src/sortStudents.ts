df183e501c572c9e64ea26cfa4dd9b04bb86a8f2
Igor Kashchenko <blackangryrabbit@gmail.com>
2023-01-16 15:33:25 +0200

    solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,58 @@
 
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+function getAverageGrade({ grades }: Student): number {
+  return grades.reduce((sum, grade) => sum + grade) / grades.length;
+}
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+export function sortStudents(
+  students: Student[], sortBy: SortType, order: SortOrder,
+): Student[] {
+  const copiedStudents: Student[] = [...students];
+
+  return copiedStudents.sort((a, b) => {
+    switch (sortBy) {
+      case (SortType.Name):
+      case (SortType.Surname):
+        if (order === 'desc') {
+          return b[sortBy].localeCompare(a[sortBy]);
+        }
+
+        return a[sortBy].localeCompare(b[sortBy]);
+
+      case (SortType.Age):
+      case (SortType.Married):
+        if (order === 'desc') {
+          return +b[sortBy] - +a[sortBy];
+        }
+
+        return +a[sortBy] - +b[sortBy];
+
+      case (SortType.AverageGrade):
+        if (order === 'desc') {
+          return getAverageGrade(b) - getAverageGrade(a);
+        }
+
+        return getAverageGrade(a) - getAverageGrade(b);
+
+      default:
+        throw new Error('Error! Invalid arguments');
+    }
+  });
 }
