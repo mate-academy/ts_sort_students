a41f2cafbb6f81f1e6a8d9675dee9d11c3097a17
Oksana Shvets <ksenyasvatko1993@gmail.com>
2023-01-17 12:31:45 +0100

    Solution2

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,68 @@
 
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[],
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name,
+  Surname,
+  Age,
+  Married,
+  AverageGrade,
 }
 
 // create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+function averageGradeFunc(grades: number[]): number {
+  let avarage: number = 0;
+  let sum: number = 0;
 
-export function sortStudents(students, sortBy, order) {
+  for (let i: number = 0; i < grades.length; i += 1) {
+    sum += grades[i];
+    avarage = sum / grades.length;
+  }
+
+  return avarage;
+}
+
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): object[] {
   // write your function
+  let newArr: object[] = [];
+
+  if (sortBy === 0) {
+    newArr = [...students].sort((a, b) => (a.name).localeCompare(b.name));
+  }
+
+  if (sortBy === 1) {
+    newArr = [...students].sort((a, b) => (a.surname).localeCompare(b.surname));
+  }
+
+  if (sortBy === 2) {
+    newArr = [...students].sort((a, b) => (a.age) - (b.age));
+  }
+
+  if (sortBy === 3) {
+    newArr = [...students].sort((a, b) => Number(b.married)
+    - Number(a.married));
+  }
+
+  if (sortBy === 4) {
+    newArr = [...students].sort((a, b) => (
+      averageGradeFunc(a.grades)) - (averageGradeFunc(b.grades)));
+  }
+
+  if (order === 'desc') {
+    return newArr.reverse();
+  }
+
+  return newArr;
 }
