558ad610f1ad411d654ac98324368df854983296
Anna Koval <anny.koval@icloud.com>
2023-01-17 19:44:58 +0200

    added changes

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,85 +1,62 @@
 
 export interface Student {
-  name: string,
-  surname: string,
-  age: number,
-  married: boolean,
-  grades: number[],
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  Name,
-  Surname,
-  Age,
-  Married,
-  AverageGrade,
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
 export type SortOrder = 'asc' | 'desc';
 
-type Callback = (studentA: Student, studentB: Student) => number;
+export function getAverage(data: number[]): number {
+  const result = data.reduce(
+    (sum: number, current: number) => sum + current, 0,
+  );
 
-function getAvarageGrade(grades: number[]): number {
-  return grades.reduce((sum, grade) => sum + grade, 0) / grades.length;
+  return result / data.length;
 }
 
-function callbackFactory(sortBy: SortType, order: SortOrder): Callback {
-  let callBack: Callback;
+export function sortStudents(
+  students: Student[], sortBy: SortType, order: SortOrder,
+): Student[] {
+  const copy = [...students];
 
   switch (sortBy) {
     case SortType.Name:
-      callBack = (studentA: Student, studentB: Student): number => (
-        order === 'asc'
-          ? studentA.name.localeCompare(studentB.name)
-          : studentB.name.localeCompare(studentA.name)
-      );
-      break;
-
     case SortType.Surname:
-      callBack = (studentA: Student, studentB: Student): number => (
-        order === 'asc'
-          ? studentA.surname.localeCompare(studentB.surname)
-          : studentB.surname.localeCompare(studentA.surname)
-      );
+      copy.sort((a, b) => {
+        return order === 'asc'
+          ? a[sortBy].localeCompare(b[sortBy])
+          : b[sortBy].localeCompare(a[sortBy]);
+      });
       break;
-
-    case SortType.Age:
-      callBack = (studentA: Student, studentB: Student): number => (
-        order === 'asc'
-          ? studentA.age - studentB.age
-          : studentB.age - studentA.age
-      );
-      break;
-
     case SortType.Married:
-      callBack = (studentA: Student, studentB: Student): number => (
-        order === 'asc'
-          ? Number(studentA.married) - Number(studentB.married)
-          : Number(studentB.married) - Number(studentA.married)
-      );
+    case SortType.Age:
+      copy.sort((a, b) => {
+        return order === 'asc'
+          ? +a[sortBy] - +b[sortBy]
+          : +b[sortBy] - +a[sortBy];
+      });
       break;
-
     case SortType.AverageGrade:
-      callBack = (studentA: Student, studentB: Student): number => (
-        order === 'asc'
-          ? getAvarageGrade(studentA.grades) - getAvarageGrade(studentB.grades)
-          : getAvarageGrade(studentB.grades) - getAvarageGrade(studentA.grades)
-      );
+      copy.sort((a, b) => {
+        return order === 'asc'
+          ? getAverage(a[sortBy]) - getAverage(b[sortBy])
+          : getAverage(b[sortBy]) - getAverage(a[sortBy]);
+      });
       break;
-
     default:
-      throw new Error('Unknown sort type!');
+      break;
   }
 
-  return callBack;
-}
-
-export function sortStudents(students: Student[],
-  sortBy: SortType,
-  order: SortOrder): Student[] {
-  const studentsCopy: Student[] = [...students];
-
-  const sortCallback: Callback = callbackFactory(sortBy, order);
-
-  return studentsCopy.sort(sortCallback);
+  return copy;
 }
