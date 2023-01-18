260abccec63ec34358dbb321b4d2aec39e2b6988
Oleksandr Prokopenkop <alex.prokop2206@gmail.com>
2023-01-18 19:23:25 +0200

    completed

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,53 @@
 
 export interface Student {
   // describe Student interface
+  name: string;
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[],
 }
 
 export enum SortType {
   // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'AverageGrade',
 }
 
 // create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  return [...students].sort((current, next) => {
+    switch (sortBy) {
+      case SortType.Name:
+      case SortType.Surname:
+        return order === 'asc'
+          ? current[sortBy].localeCompare(next[sortBy])
+          : next[sortBy].localeCompare(current[sortBy]);
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+      case SortType.Age:
+      case SortType.Married:
+        return order === 'asc'
+          ? +current[sortBy] - +next[sortBy]
+          : +next[sortBy] - +current[sortBy];
+
+      case SortType.AverageGrade:
+        return order === 'asc'
+          ? current.grades.reduce((a, b) => a + b) / current.grades.length
+            - next.grades.reduce((a, b) => a + b) / next.grades.length
+          : next.grades.reduce((a, b) => a + b) / next.grades.length
+            - current.grades.reduce((a, b) => a + b) / current.grades.length;
+
+      default:
+        throw new Error('verify parametrs');
+    }
+  });
 }
