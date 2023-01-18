8229a541278c59ad77e615f82a3f00e4eb548277
Yaroslav <yaroslavfoorgala@gmail.com>
2023-01-18 08:52:58 +0200

    Initial commit

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,56 @@
 
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[],
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
 // create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+function getAveregeGrade(student: Student): number {
+  return student.grades
+    .reduce((sum, grade) => sum + grade, 0) / student.grades.length;
+}
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  return [...students].sort((student1, student2) => {
+    switch (sortBy) {
+      case SortType.Name:
+      case SortType.Surname:
+        return order === 'asc'
+          ? student1[sortBy].localeCompare(student2[sortBy])
+          : student2[sortBy].localeCompare(student1[sortBy]);
+
+      case SortType.Age:
+      case SortType.Married:
+        return order === 'asc'
+          ? +student1[sortBy] - +student2[sortBy]
+          : +student2[sortBy] - +student1[sortBy];
+
+      case SortType.AverageGrade:
+        return order === 'asc'
+          ? getAveregeGrade(student1)
+            - getAveregeGrade(student2)
+          : getAveregeGrade(student2)
+            - getAveregeGrade(student1);
+
+      default:
+        throw new Error('Enter valid sort type');
+    }
+  });
 }
