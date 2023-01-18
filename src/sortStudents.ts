9685471468f287da105761aa9d0e27a08da7607b
Bohdan Kutasevich <obvinmusic@gmail.com>
2023-01-18 17:16:24 +0200

    task done

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,59 @@
-
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc'|'desc';
 
+function getAverageGrade(grades:number[]):number {
+  const sumOfGrades = grades.reduce((sum, grade) => sum + grade, 0);
+  const gradesCount = grades.length;
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  return sumOfGrades / gradesCount;
+}
+
+export function sortStudents(
+  students:Student[],
+  sortBy:SortType,
+  order:SortOrder,
+):Student[] {
+  const sortedStudents = [...students];
+  const currentOrder = order === 'asc';
+
+  sortedStudents.sort((a, b) => {
+    switch (sortBy) {
+      case SortType.Name:
+      case SortType.Surname:
+        return currentOrder
+          ? a[sortBy].localeCompare(b[sortBy])
+          : b[sortBy].localeCompare(b[sortBy]);
+
+      case SortType.AverageGrade:
+        return currentOrder
+          ? getAverageGrade(a[sortBy]) - getAverageGrade(b[sortBy])
+          : getAverageGrade(b[sortBy]) - getAverageGrade(a[sortBy]);
+
+      case SortType.Age:
+      case SortType.Married:
+        return currentOrder
+          ? +a[sortBy] - +b[sortBy]
+          : +b[sortBy] - +a[sortBy];
+
+      default:
+        throw new Error('incorrect input data');
+    }
+  });
+
+  return sortedStudents;
 }
