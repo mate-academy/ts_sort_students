959a889912ff20c2d5f3ab68c1f7ce6e26d47ef8
Anton Zhytonbaiev <chipchilinka11@gmail.com>
2023-01-18 15:27:22 +0200

    solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,55 @@
 
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+const getAver = (marks:number[]): number => {
+  const averageMark:number = marks
+    .reduce((prev: number, curr: number): number => prev + curr, 0)
+    / marks.length;
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  return averageMark;
+};
+
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  return [...students].sort((a, b) => {
+    switch (sortBy) {
+      case SortType.Name:
+      case SortType.Surname:
+        return order === 'asc'
+          ? a[sortBy].localeCompare(b[sortBy])
+          : b[sortBy].localeCompare(a[sortBy]);
+
+      case SortType.Age:
+      case SortType.Married:
+        return order === 'asc'
+          ? +a[sortBy] - +b[sortBy]
+          : +b[sortBy] - +a[sortBy];
+
+      case SortType.AverageGrade:
+        return order === 'asc'
+          ? getAver(a[sortBy]) - getAver(b[sortBy])
+          : getAver(b[sortBy]) - getAver(a[sortBy]);
+
+      default: throw Error('Invalid input data');
+    }
+  });
 }
