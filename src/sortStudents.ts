c4a138602470415e006437e9d951c695ee66a4df
PolinaSukhorukova <sukhorukovapolina03@gmail.com>
2023-01-19 15:30:19 +0200

    fixed solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -4,7 +4,7 @@ export interface Student {
   surname: string;
   age: number;
   married: boolean;
-  grades: [];
+  grades: number[];
 }
 
 export enum SortType {
@@ -24,29 +24,31 @@ export function sortStudents(
   order: SortOrder,
 ): Student[] {
   const studentsCopy: Student[] = [...students];
-  const getMark = (
+  const getAverageGrades = (
     grades: number[],
   ): number => grades.reduce((a, b) => a + b, 0) / grades.length;
 
-  switch (sortBy) {
-    case SortType.Name:
-    case SortType.Surname:
-      return order === 'asc'
-        ? studentsCopy.sort((a, b) => a[sortBy].localeCompare(b[sortBy]))
-        : studentsCopy.sort((a, b) => b[sortBy].localeCompare(a[sortBy]));
+  return studentsCopy.sort((a, b) => {
+    switch (sortBy) {
+      case SortType.Name:
+      case SortType.Surname:
+        return order === 'asc'
+          ? a[sortBy].localeCompare(b[sortBy])
+          : b[sortBy].localeCompare(a[sortBy]);
 
-    case SortType.Age:
-    case SortType.Married:
-      return order === 'asc'
-        ? studentsCopy.sort((a, b) => +a[sortBy] - +b[sortBy])
-        : studentsCopy.sort((a, b) => +b[sortBy] - +a[sortBy]);
+      case SortType.Age:
+      case SortType.Married:
+        return order === 'asc'
+          ? +a[sortBy] - +b[sortBy]
+          : +b[sortBy] - +a[sortBy];
 
-    case SortType.AverageGrade:
-      return order === 'asc'
-        ? studentsCopy.sort((a, b) => getMark(a[sortBy]) - getMark(b[sortBy]))
-        : studentsCopy.sort((a, b) => getMark(b[sortBy]) - getMark(a[sortBy]));
+      case SortType.AverageGrade:
+        return order === 'asc'
+          ? getAverageGrades(a[sortBy]) - getAverageGrades(b[sortBy])
+          : getAverageGrades(b[sortBy]) - getAverageGrades(a[sortBy]);
 
-    default:
-      return students;
-  }
+      default:
+        throw new Error('Sort are not valid');
+    }
+  });
 }
