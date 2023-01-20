f9469d56ee226f6a60af62e5f53bfc7df804ae89
Artem_Sobolevskiy <artemsobolevskuy@gmail.com>
2023-01-20 11:06:57 +0200

    small refactoring

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -27,26 +27,27 @@ export function sortStudents(
     return array.reduce((a, b) => a + b, 0) / array.length;
   }
 
-  const studentsClone = [...students].sort((obj1, obj2) => {
+  return [...students].sort((student1, student2) => {
     switch (sortBy) {
       case SortType.Name:
       case SortType.Surname:
         return order === 'asc'
-          ? obj1[sortBy].localeCompare(obj2[sortBy])
-          : obj2[sortBy].localeCompare(obj1[sortBy]);
+          ? student1[sortBy].localeCompare(student2[sortBy])
+          : student2[sortBy].localeCompare(student1[sortBy]);
+
       case SortType.Age:
       case SortType.Married:
         return order === 'asc'
-          ? +obj1[sortBy] - +obj2[sortBy]
-          : +obj2[sortBy] - +obj1[sortBy];
+          ? +student1[sortBy] - +student2[sortBy]
+          : +student2[sortBy] - +student1[sortBy];
+
       case SortType.AverageGrade:
         return order === 'asc'
-          ? getAvarageGrade(obj1.grades) - getAvarageGrade(obj2.grades)
-          : getAvarageGrade(obj2.grades) - getAvarageGrade(obj1.grades);
+          ? getAvarageGrade(student1.grades) - getAvarageGrade(student2.grades)
+          : getAvarageGrade(student2.grades) - getAvarageGrade(student1.grades);
+
       default:
         throw new Error('Unexpected sort type');
     }
   });
-
-  return studentsClone;
 }
