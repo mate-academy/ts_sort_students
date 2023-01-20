f515b776076b83e4697f3151980add1045aeae49
Bohdan Rudenko <RudBeee@gmail.com>
2023-01-20 11:34:07 +0200

    new test

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -22,7 +22,7 @@ export type SortOrder = 'asc' | 'desc';
 export function sortStudents(
   students: Student[], sortBy: SortType, order: SortOrder,
 ): Student[] {
-  const res: Student[] = students.map(
+  const result: Student[] = students.map(
     (person: Student) => ({
       ...person,
     }),
@@ -30,41 +30,41 @@ export function sortStudents(
 
   switch (sortBy) {
     case SortType.Name:
-      res.sort((a: Student, b: Student) => (a.name > b.name ? 1 : -1));
+      result.sort((a: Student, b: Student) => (a.name > b.name ? 1 : -1));
 
       break;
 
     case SortType.Surname:
-      res.sort((a: Student, b: Student) => (a.surname > b.surname ? 1 : -1));
+      result.sort((a: Student, b: Student) => (a.surname > b.surname ? 1 : -1));
 
       if (order === 'desc') {
-        res.reverse();
+        result.reverse();
       }
 
-      return res;
+      return result;
 
     case SortType.Age:
-      res.sort((a: Student, b: Student) => (a.age > b.age ? 1 : -1));
+      result.sort((a: Student, b: Student) => (a.age > b.age ? 1 : -1));
 
       if (order === 'desc') {
-        res.reverse();
+        result.reverse();
       }
 
       break;
 
     case SortType.Married:
-      res.sort((a: Student, b: Student) => (
+      result.sort((a: Student, b: Student) => (
         Number(a.married) > Number(b.married) ? 1 : -1));
 
       if (order === 'desc') {
-        res.reverse();
+        result.reverse();
       }
 
       break;
 
     case SortType.AverageGrade:
       if (order === 'asc') {
-        res.sort((a: Student, b: Student) => (
+        result.sort((a: Student, b: Student) => (
           a.grades.reduce((z: number, q: number) => z + q)
           / a.grades.length
           >= b.grades.reduce((z: number, q: number) => z + q)
@@ -72,7 +72,7 @@ export function sortStudents(
       }
 
       if (order === 'desc') {
-        res.sort((a: Student, b: Student) => (
+        result.sort((a: Student, b: Student) => (
           a.grades.reduce((z: number, q: number) => z + q)
           / a.grades.length
           > b.grades.reduce((z: number, q: number) => z + q)
@@ -84,5 +84,5 @@ export function sortStudents(
       break;
   }
 
-  return res;
+  return result;
 }
