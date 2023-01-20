12ef019b084d61f7978628befa1ffda7fc83ed96
Denis Bokov <denisbokov1703@gmail.com>
2023-01-20 16:28:10 +0100

    lint fixed

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -17,38 +17,45 @@ export enum SortType {
 
 export type SortOrder = 'asc' | 'desc';
 
-export function sortStudents(students: Student[], sortBy: SortType, order: SortOrder): Student[] {
-  function getAverageGerage(students: Student): number {
-    return students.grades.reduce((previous: number, current: number) => previous + current, 0) 
-    / students.grades.length;
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  function getAverageGerage(student): number {
+    return student.grades.reduce(
+      (previous: number, current: number) => previous + current, 0,
+    )
+    / student.grades.length;
   }
 
-  switch(sortBy) {
+  switch (sortBy) {
     case SortType.Name:
     case SortType.Surname:
-    if (order === 'asc') {
-      return [...students].sort((a, b) => a[sortBy].localeCompare(b[sortBy]));
-    } else {
-      return [...students].sort((a, b) => b[sortBy].localeCompare(a[sortBy]));
-      }
+
+      return (order === 'asc')
+        ? [...students].sort((a, b) => a[sortBy].localeCompare(b[sortBy]))
+        : [...students].sort((a, b) => b[sortBy].localeCompare(a[sortBy]));
 
     case SortType.Age:
     case SortType.Married:
-    if (order === 'asc') {
-      return [...students].sort((a, b) => +a[sortBy] - +b[sortBy]);
-    } else {
-        return [...students].sort((a, b) => +b[sortBy] - +a[sortBy]);
-      }
+
+      return (order === 'asc')
+        ? [...students].sort((a, b) => +a[sortBy] - +b[sortBy])
+        : [...students].sort((a, b) => +b[sortBy] - +a[sortBy]);
 
     case SortType.AverageGrade:
-      if (order === 'asc') {
-        return [...students].sort((a, b) => getAverageGerage(a) - getAverageGerage(b));
-      } else {
-        return [...students].sort((a, b) => getAverageGerage(b) - getAverageGerage(a));
 
-      }
+      return (order === 'asc')
+        ? [...students].sort(
+          (a, b) => getAverageGerage(a) - getAverageGerage(b),
+        )
 
-      default:
-        throw Error('Not valid data');
+        : [...students].sort(
+          (a, b) => getAverageGerage(b) - getAverageGerage(a),
+        );
+
+    default:
+      throw Error('Not valid data');
   }
 }
