925c2038531cad1f059fe200c20b8f0b7d2cda08
Oleksandr Prykhodko <olexandr.prykhodko@gmail.com>
2023-01-20 16:34:13 +0100

    Sort_Students were implemented

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,57 @@
 
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[],
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'averageGrade'
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(students: Student[],
+  sortBy: SortType,
+  order: SortOrder): Student[] {
+  const newStudents = [...students];
+  const orderValue = order === 'asc' ? 1 : -1;
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  switch (sortBy) {
+    case SortType.Name:
+    case SortType.Surname:
+      newStudents.sort((a, b) => {
+        return a[sortBy].localeCompare(b[sortBy]) * orderValue;
+      });
+      break;
+
+    case SortType.Age:
+    case SortType.Married:
+      newStudents.sort((a, b) => {
+        return (+a[sortBy] - (+b[sortBy])) * orderValue;
+      });
+      break;
+
+    case SortType.AverageGrade:
+      newStudents.sort((a, b) => {
+        const averageA = a.grades.reduce((sum, grade) => sum + grade, 0)
+          / a.grades.length;
+        const averageB = b.grades.reduce((sum, grade) => sum + grade, 0)
+          / b.grades.length;
+
+        return (averageA - averageB) * orderValue;
+      });
+      break;
+
+    default:
+      throw new Error('Check parameters one more time');
+  }
+
+  return newStudents;
 }
