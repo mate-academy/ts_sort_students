00dad745ebda9dde517c55210f696391ebc88aec
Ihor Mariash <imariash86@gmail.com>
2023-01-22 16:56:49 +0200

    enum for switch cases was used, repeats were removed, info to error was added

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -26,41 +26,31 @@ export function sortStudents(
   order: SortOrder,
 ): Student[] {
   // write your function
-  let res: Student[] = [];
+  function calc(arr: number[]): number {
+    return arr.reduce((acc, cur) => acc + cur, 0) / arr.length;
+  }
 
-  switch (sortBy) {
-    case 'age':
-    case 'married':
-      res = [...students].sort((a: Student, b: Student): number => {
+  return [...students].sort((a: Student, b: Student): number => {
+    switch (sortBy) {
+      case SortType.Age:
+      case SortType.Married:
         return order === 'desc'
           ? Number(b[sortBy]) - Number(a[sortBy])
           : Number(a[sortBy]) - Number(b[sortBy]);
-      });
-      break;
 
-    case 'name':
-    case 'surname':
-      res = [...students].sort((a: Student, b: Student): number => {
+      case SortType.Name:
+      case SortType.Surname:
         return order === 'desc'
           ? b[sortBy].localeCompare(a[sortBy])
           : a[sortBy].localeCompare(b[sortBy]);
-      });
-      break;
 
-    case 'grades':
-      res = [...students].sort((a: Student, b: Student): number => {
-        function calc(arr: number[]): number {
-          return arr.reduce((acc, cur) => acc + cur, 0) / arr.length;
-        }
-
+      case SortType.AverageGrade:
         return order === 'desc'
           ? calc(b[sortBy]) - calc(a[sortBy])
           : calc(a[sortBy]) - calc(b[sortBy]);
-      });
-      break;
-    default:
-      throw new Error('There is an error!');
-  }
 
-  return res;
+      default:
+        throw new Error('There is no such category to sort!');
+    }
+  });
 }
