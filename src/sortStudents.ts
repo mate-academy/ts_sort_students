a2134d1ce3b54b004c7651fb91884e141b562068
dmitriy-kovalenko <kovalenkodmitrij@gmail.com>
2023-01-26 13:11:53 +0200

    all tesk Ok

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,66 @@
-
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
 // create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(
+  students:Student[],
+  sortBy:SortType,
+  order:SortOrder,
+):Student[] {
+  let result:Student[] = [];
+  const result1:Student[] = students.slice();
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  switch (sortBy) {
+    case SortType.Name:
+      result = result1.sort((a:Student, b:Student) => {
+        return order === 'asc'
+          ? a[sortBy].localeCompare(b[sortBy])
+          : b[sortBy].localeCompare(a[sortBy]);
+      });
+      break;
+    case SortType.Surname:
+      result = result1.sort((a:Student, b:Student) => {
+        return order === 'asc'
+          ? a[sortBy].localeCompare(b[sortBy])
+          : b[sortBy].localeCompare(a[sortBy]);
+      });
+      break;
+    case SortType.Age:
+    case SortType.Married:
+      result = result1.sort((a:Student, b:Student) => {
+        return order === 'asc'
+          ? +a[sortBy] - +b[sortBy]
+          : +b[sortBy] - +a[sortBy];
+      });
+      break;
+    case SortType.AverageGrade:
+      result = result1.sort((a:Student, b:Student) => {
+        const A = a[sortBy].reduce((prev, item) => prev + item
+          , 0) / a[sortBy].length;
+        const B = b[sortBy].reduce((prev, item) => prev + item
+          , 0) / b[sortBy].length;
+
+        return order === 'asc' ? A - B : B - A;
+      });
+      break;
+    default:
+      break;
+  }
+
+  return result;
 }
