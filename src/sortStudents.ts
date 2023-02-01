84794a3afce3d385b235cffe6087b8b28a8595cb
YouRa Allakhverdov <allakhverdovyuriy@gmail.com>
2023-02-01 10:48:01 +0200

    Finish task

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,65 @@
-
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name,
+  Surname,
+  Age,
+  Married,
+  AverageGrade,
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  return [...students].sort((studentA: Student, studentB: Student) => {
+    switch (sortBy) {
+      case SortType.Name:
+        return order === 'asc'
+          ? studentA.name.localeCompare(studentB.name)
+          : studentB.name.localeCompare(studentA.name);
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+      case SortType.Surname:
+        return order === 'asc'
+          ? studentA.surname.localeCompare(studentB.surname)
+          : studentB.surname.localeCompare(studentA.surname);
+      case SortType.Age:
+        return order === 'asc'
+          ? studentA.age - studentB.age
+          : studentB.age - studentA.age;
+      case SortType.Married:
+        return order === 'asc'
+          ? Number(studentA.married) - Number(studentB.married)
+          : Number(studentB.married) - Number(studentA.married);
+
+      case SortType.AverageGrade:
+      {
+        const callback: (
+          acc: number,
+          grade: number,
+          index: number,
+          arr: number[]
+        ) => number = (acc: number, grade: number, index, arr) => {
+          return acc + grade / arr.length;
+        };
+
+        return order === 'asc'
+          ? studentA.grades.reduce(callback, 0)
+          - studentB.grades.reduce(callback, 0)
+          : studentB.grades.reduce(callback, 0)
+          - studentA.grades.reduce(callback, 0);
+      }
+      default:
+        return 0;
+    }
+  });
 }
