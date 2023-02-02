b697db27bc55718ff159783a56d59b0bc4b8b4e2
Oleksii Mishchenko <alex.mishch@gmail.com>
2023-02-02 12:06:53 +0200

    Students Sorting Implemented

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,55 @@
+function getAvg(arr: number[]): number {
+  if (arr.length === 0) {
+    return 0;
+  }
+
+  return arr.reduce((a, b) => a + b) / arr.length;
+}
 
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] | string {
+  return [...students].sort((studentA, studentB) => {
+    switch (sortBy) {
+      case SortType.Name:
+      case SortType.Surname:
+        return order === 'asc'
+          ? studentA[sortBy].localeCompare(studentB[sortBy])
+          : studentB[sortBy].localeCompare(studentA[sortBy]);
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+      case SortType.Age:
+      case SortType.Married:
+        return order === 'asc'
+          ? +studentA[sortBy] - +studentB[sortBy]
+          : +studentB[sortBy] - +studentA[sortBy];
+
+      case SortType.AverageGrade:
+        return order === 'asc'
+          ? getAvg(studentA[sortBy]) - getAvg(studentB[sortBy])
+          : getAvg(studentB[sortBy]) - getAvg(studentA[sortBy]);
+
+      default:
+        throw new Error('Define sort parameters');
+    }
+  });
 }
