d0fb6171a73e2131459e9dd203c967b40804620b
Nadiia Kotliar <eknakaron@gmail.com>
2023-02-04 14:01:18 +0200

    solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,53 @@
 
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades'
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+function getAverage(student: Student): number {
+  return student.grades
+    .reduce((a: number, b: number) => a + b, 0) / student.grades.length;
+}
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  return [...students].sort((student1, student2) => {
+    switch (sortBy) {
+      case SortType.Name:
+      case SortType.Surname:
+        return order === 'asc'
+          ? student1[sortBy].localeCompare(student2[sortBy])
+          : student2[sortBy].localeCompare(student1[sortBy]);
+
+      case SortType.Married:
+      case SortType.Age:
+        return order === 'asc'
+          ? +student1[sortBy] - +student2[sortBy]
+          : +student2[sortBy] - +student1[sortBy];
+
+      case SortType.AverageGrade:
+        return order === 'asc'
+          ? getAverage(student1) - getAverage(student2)
+          : getAverage(student2) - getAverage(student1);
+
+      default:
+        throw Error('Enter a valid sort value.');
+    }
+  });
 }
