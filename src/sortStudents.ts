759fffb04c8d03519b41da84fe233d239694e3dc
Taras Baran <baranprivat22@gmail.com>
2023-02-10 16:37:37 +0200

    finished

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,57 @@
 
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[],
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'averageGrade',
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'desk' | 'asc';
 
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const countAverage = (x: number[]): number => {
+    return x
+      .reduce((prev, curr) => prev + curr, 0) / x.length;
+  };
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  return [...students].sort((x, y) => {
+    switch (sortBy) {
+      case 'married':
+      case 'age':
+        if (order === 'asc') {
+          return (+x[sortBy]) - (+y[sortBy]);
+        }
+
+        return (+y[sortBy]) - (+x[sortBy]);
+
+      case 'name':
+      case 'surname':
+        if (order === 'asc') {
+          return x[sortBy].localeCompare(y[sortBy]);
+        }
+
+        return y[sortBy].localeCompare(x[sortBy]);
+
+      case 'averageGrade':
+      default:
+        if (order === 'asc') {
+          return countAverage(x.grades) - countAverage(y.grades);
+        }
+
+        return countAverage(y.grades) - countAverage(x.grades);
+    }
+  });
 }
