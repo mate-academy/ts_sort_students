6bb387cf09b940d994998029aa18e851ce2fe246
Olena Hapon <olenagapon90gmail.com>
2023-02-12 18:40:32 +0100

    Solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,55 @@
 
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
 // create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+const getAvarage = (grades: number[]): number => {
+  return grades.reduce((a, b) => a + b, 0) / (grades.length || 1);
+};
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student [] {
+  const copy = [...students];
+
+  return copy.sort((a, b) => {
+    switch (sortBy) {
+      case SortType.Name:
+      case SortType.Surname:
+        return order === 'asc'
+          ? a[sortBy].localeCompare(b[sortBy])
+          : b[sortBy].localeCompare(a[sortBy]);
+
+      case SortType.Age:
+      case SortType.Married:
+        return order === 'asc'
+          ? +(a[sortBy]) - +(b[sortBy])
+          : +(b[sortBy]) - +(a[sortBy]);
+
+      case SortType.AverageGrade:
+        return order === 'asc'
+          ? getAvarage(a.grades) - getAvarage(b.grades)
+          : getAvarage(b.grades) - getAvarage(a.grades);
+
+      default:
+        throw Error('Enter a valid sort value.');
+    }
+  });
 }
