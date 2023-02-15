3674818801f321b5cd407585b20b425c7a3683e3
Oleksii Pishchuhin <aliliolek@gmail.com>
2023-02-15 23:29:55 +0100

    optimized sol 1.1

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,6 +1,4 @@
-
 export interface Student {
-  // describe Student interface
   name: string;
   surname: string;
   age: number;
@@ -9,33 +7,28 @@ export interface Student {
 }
 
 export enum SortType {
-  // describe SortType enum
-  Name,
-  Surname,
-  Age,
-  Married,
-  AverageGrade
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
-// create SortOrder type
 export type SortOrder = 'asc' | 'desc';
 
 export function sortStudents(
   students: Student[], sortBy: SortType, order: SortOrder,
 ): Student[] {
-  // write your function
   if (order === 'asc') {
     switch (sortBy) {
-      case SortType.Name:
+      case 'name':
+      case 'surname':
+        return [...students].sort((a, b) => a[sortBy].localeCompare(b[sortBy]));
+      case 'age':
+      case 'married':
+        return [...students].sort((a, b) => +a[sortBy] - +b[sortBy]);
+      case 'grades':
       default:
-        return [...students].sort((a, b) => a.name.localeCompare(b.name));
-      case SortType.Surname:
-        return [...students].sort((a, b) => a.surname.localeCompare(b.surname));
-      case SortType.Age:
-        return [...students].sort((a, b) => a.age - b.age);
-      case SortType.Married:
-        return [...students].sort((a, b) => +a.married - +b.married);
-      case SortType.AverageGrade:
         return [...students].sort((a, b) => (a.grades.reduce((x, y) => x + y, 0)
           / a.grades.length)
           - (b.grades.reduce((m, n) => m + n, 0) / b.grades.length));
@@ -43,16 +36,14 @@ export function sortStudents(
   }
 
   switch (sortBy) {
-    case SortType.Name:
+    case 'name':
+    case 'surname':
+      return [...students].sort((a, b) => b[sortBy].localeCompare(a[sortBy]));
+    case 'age':
+    case 'married':
+      return [...students].sort((a, b) => +b[sortBy] - +a[sortBy]);
+    case 'grades':
     default:
-      return [...students].sort((a, b) => b.name.localeCompare(a.name));
-    case SortType.Surname:
-      return [...students].sort((a, b) => b.surname.localeCompare(a.surname));
-    case SortType.Age:
-      return [...students].sort((a, b) => b.age - a.age);
-    case SortType.Married:
-      return [...students].sort((a, b) => +b.married - +a.married);
-    case SortType.AverageGrade:
       return [...students].sort((a, b) => (b.grades.reduce((x, y) => x + y, 0)
         / b.grades.length)
         - (a.grades.reduce((m, n) => m + n, 0) / a.grades.length));
