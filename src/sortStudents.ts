f7e0a54f5f4a9bfd463db2e9491633078b076b82
Anastasiia Kuznetsova <an.stsii.k@gmail.com>
2023-02-19 13:38:11 +0100

    solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,67 @@
 
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[],
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'averageGrade'
 }
 
 // create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const studentsCopy = [...students];
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  studentsCopy.sort((student1: Student, student2: Student) => {
+    switch (sortBy) {
+      case SortType.Name:
+        return order === 'asc'
+          ? student1[sortBy].localeCompare(student2[sortBy])
+          : student2[sortBy].localeCompare(student2[sortBy]);
+
+      case SortType.Surname:
+        return order === 'asc'
+          ? student1[sortBy].localeCompare(student2[sortBy])
+          : student2[sortBy].localeCompare(student2[sortBy]);
+
+      case SortType.Age:
+        return order === 'asc'
+          ? Number(student1[sortBy]) - Number(student2[sortBy])
+          : Number(student2[sortBy]) - Number(student1[sortBy]);
+
+      case SortType.Married:
+        return order === 'asc'
+          ? Number(student1[sortBy]) - Number(student2[sortBy])
+          : Number(student2[sortBy]) - Number(student1[sortBy]);
+
+      case SortType.AverageGrade:
+        return order === 'asc'
+          ? (student1.grades.reduce((a, b) => a + b, 0)
+            / student1.grades.length)
+            - (student2.grades.reduce((a, b) => a + b, 0)
+              / student2.grades.length)
+          : (student2.grades.reduce((a, b) => a + b, 0)
+            / student2.grades.length)
+            - (student1.grades.reduce((a, b) => a + b, 0)
+              / student1.grades.length);
+
+      default:
+        return 0;
+    }
+  });
+
+  return studentsCopy;
 }
