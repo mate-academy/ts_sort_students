36407266ae096c2856943a568f51d27a2e5b33a4
Petro Lozynskyi <petrolozynskyi01@gmail.com>
2023-02-22 12:53:24 +0200

    update

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,5 +1,4 @@
 
-// describe Student interface
 export interface Student {
   name: string;
   surname: string;
@@ -11,11 +10,11 @@ export interface Student {
 
 // describe SortType enum
 export enum SortType {
-  name,
-  surname,
+  Name,
+  Surname,
   Age,
-  married,
-  averageGrade
+  Married,
+  AverageGrade
 }
 
 // create SortOrder type
@@ -26,12 +25,52 @@ sortStudents(students: Student[], sortBy: SortType, order: SortOrder)
   : Student[] |void {
   const newobj = JSON.parse(JSON.stringify(students));
 
+  function Sum(array: number[]): number {
+    return array.reduce((accumulator: number, value:number) => {
+      return accumulator + value;
+    }, 0);
+  }
+
   if (sortBy === SortType.Age && order === 'desc') {
     return newobj.sort((a: Student, b: Student) => b.age - a.age);
   }
 
   if (sortBy === SortType.Age && order === 'asc') {
     return newobj.sort((a: Student, b: Student) => a.age - b.age);
+  }
+
+  if (sortBy === SortType.AverageGrade && order === 'desc') {
+    return newobj.sort((
+      a: Student,
+      b: Student,
+    ) => Sum(b.grades) / b.grades.length
+      - Sum(a.grades) / a.grades.length);
+  }
+
+  if (sortBy === SortType.AverageGrade && order === 'asc') {
+    return newobj.sort((
+      a: Student,
+      b: Student,
+    ) => Sum(a.grades) / a.grades.length
+      - Sum(b.grades) / b.grades.length);
+  }
+
+  if (sortBy === SortType.Name && order === 'asc') {
+    return newobj.sort((a:Student, b:Student) => a.name.localeCompare(b.name));
+  }
+
+  if (sortBy === SortType.Surname && order === 'asc') {
+    return newobj.sort((
+      a:Student,
+      b:Student,
+    ) => a.surname.localeCompare(b.surname));
+  }
+
+  if (sortBy === SortType.Married && order === 'desc') {
+    return newobj.sort((
+      a:Student,
+      b:Student,
+    ) => Number(b.married) - Number(a.married));
   }
 
   return [];
