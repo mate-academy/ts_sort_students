e3a4ead083ef1688457670074c9471a28e42b45e
Anastasiia Kuznetsova <an.stsii.k@gmail.com>
2023-02-26 10:19:22 +0100

    improved solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -18,6 +18,10 @@ export enum SortType {
 // create SortOrder type
 export type SortOrder = 'asc' | 'desc';
 
+function getAverageGrade({ grades }: Student): number {
+  return grades.reduce((acc, grade) => acc + grade, 0) / grades.length;
+}
+
 export function sortStudents(
   students: Student[],
   sortBy: SortType,
@@ -25,38 +29,24 @@ export function sortStudents(
 ): Student[] {
   const studentsCopy = [...students];
 
-  studentsCopy.sort((student1: Student, student2: Student) => {
+  studentsCopy.sort((prevStudent: Student, nextStudent: Student) => {
     switch (sortBy) {
       case SortType.Name:
-        return order === 'asc'
-          ? student1[sortBy].localeCompare(student2[sortBy])
-          : student2[sortBy].localeCompare(student2[sortBy]);
-
       case SortType.Surname:
         return order === 'asc'
-          ? student1[sortBy].localeCompare(student2[sortBy])
-          : student2[sortBy].localeCompare(student2[sortBy]);
+          ? prevStudent[sortBy].localeCompare(nextStudent[sortBy])
+          : nextStudent[sortBy].localeCompare(prevStudent[sortBy]);
 
       case SortType.Age:
-        return order === 'asc'
-          ? Number(student1[sortBy]) - Number(student2[sortBy])
-          : Number(student2[sortBy]) - Number(student1[sortBy]);
-
       case SortType.Married:
         return order === 'asc'
-          ? Number(student1[sortBy]) - Number(student2[sortBy])
-          : Number(student2[sortBy]) - Number(student1[sortBy]);
+          ? Number(prevStudent[sortBy]) - Number(nextStudent[sortBy])
+          : Number(nextStudent[sortBy]) - Number(prevStudent[sortBy]);
 
       case SortType.AverageGrade:
         return order === 'asc'
-          ? (student1.grades.reduce((a, b) => a + b, 0)
-            / student1.grades.length)
-            - (student2.grades.reduce((a, b) => a + b, 0)
-              / student2.grades.length)
-          : (student2.grades.reduce((a, b) => a + b, 0)
-            / student2.grades.length)
-            - (student1.grades.reduce((a, b) => a + b, 0)
-              / student1.grades.length);
+          ? getAverageGrade(prevStudent) - getAverageGrade(nextStudent)
+          : getAverageGrade(nextStudent) - getAverageGrade(prevStudent);
 
       default:
         return 0;
