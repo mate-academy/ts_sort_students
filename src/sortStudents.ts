94f62987a2e436a975982cac01a385c5d7d7baa7
Petro Lozynskyi <petrolozynskyi01@gmail.com>
2023-02-27 16:22:22 +0200

    Update

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -31,71 +31,62 @@ sortStudents(students: Student[], sortBy: SortType, order: SortOrder)
     }, 0);
   }
 
-  if (order === 'desc') {
-    if (sortBy === SortType.Age) {
-      return newobj.sort((a: Student, b: Student) => b.age - a.age);
-    }
+  switch (order) {
+    case 'desc':
+      switch (sortBy) {
+        case SortType.Age:
+          return newobj.sort((a: Student, b: Student) => b.age - a.age);
+        case SortType.AverageGrade:
+          return newobj.sort((
+            a: Student,
+            b: Student,
+          ) => Sum(b.grades) / b.grades.length
+            - Sum(a.grades) / a.grades.length);
+        case SortType.Married:
+          return newobj.sort((
+            a: Student,
+            b: Student,
+          ) => Number(b.married) - Number(a.married));
+        case SortType.Name:
+          return newobj.sort((
+            a: Student, b: Student,
+          ) => b.name.localeCompare(a.name));
+        case SortType.Surname:
+          return newobj.sort((
+            a: Student, b: Student,
+          ) => a.surname.localeCompare(b.surname));
+        default:
+          return [];
+      }
 
-    if (sortBy === SortType.AverageGrade) {
-      return newobj.sort((
-        a: Student,
-        b: Student,
-      ) => Sum(b.grades) / b.grades.length
-        - Sum(a.grades) / a.grades.length);
-    }
-
-    if (sortBy === SortType.Married) {
-      return newobj.sort((
-        a:Student,
-        b:Student,
-      ) => Number(b.married) - Number(a.married));
-    }
-
-    if (sortBy === SortType.Name) {
-      return newobj.sort((a:Student,
-        b:Student) => b.name.localeCompare(a.name));
-    }
-
-    if (sortBy === SortType.Surname) {
-      return newobj.sort((
-        a:Student,
-        b:Student,
-      ) => a.surname.localeCompare(b.surname));
-    }
+    case 'asc':
+      switch (sortBy) {
+        case SortType.Age:
+          return newobj.sort((a: Student, b: Student) => a.age - b.age);
+        case SortType.AverageGrade:
+          return newobj.sort((
+            a: Student,
+            b: Student,
+          ) => Sum(a.grades) / a.grades.length
+           - Sum(b.grades) / b.grades.length);
+        case SortType.Name:
+          return newobj.sort((
+            a: Student, b: Student,
+          ) => a.name.localeCompare(b.name));
+        case SortType.Surname:
+          return newobj.sort((
+            a: Student,
+            b: Student,
+          ) => a.surname.localeCompare(b.surname));
+        case SortType.Married:
+          return newobj.sort((
+            a: Student,
+            b: Student,
+          ) => Number(b.married) - Number(a.married));
+        default:
+          return [];
+      }
+    default:
+      return [];
   }
-
-  if (order === 'asc') {
-    if (sortBy === SortType.Age) {
-      return newobj.sort((a: Student, b: Student) => a.age - b.age);
-    }
-
-    if (sortBy === SortType.AverageGrade) {
-      return newobj.sort((
-        a: Student,
-        b: Student,
-      ) => Sum(a.grades) / a.grades.length
-        - Sum(b.grades) / b.grades.length);
-    }
-
-    if (sortBy === SortType.Name) {
-      return newobj.sort((a:Student,
-        b:Student) => a.name.localeCompare(b.name));
-    }
-
-    if (sortBy === SortType.Surname) {
-      return newobj.sort((
-        a:Student,
-        b:Student,
-      ) => a.surname.localeCompare(b.surname));
-    }
-
-    if (sortBy === SortType.Married) {
-      return newobj.sort((
-        a:Student,
-        b:Student,
-      ) => Number(b.married) - Number(a.married));
-    }
-  }
-
-  return [];
 }
