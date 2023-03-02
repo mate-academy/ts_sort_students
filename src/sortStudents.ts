583af34300cf3f7a8df51820d54d7b5196a20832
Maksym Tsarenko <makstsar94@gmail.com>
2023-03-02 13:34:09 +0200

    solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -18,40 +18,51 @@ export enum SortType {
 export type SortOrder = 'asc' | 'desc';
 
 function getAverageGrade(student: Student): number {
-  return student.grades.reduce((prev: number, curr: number) => prev + curr, 0)
-   / student.grades.length;
+  const { grades } = student;
+
+  return grades.reduce((prev: number, curr: number) => prev + curr, 0)
+    / grades.length;
 }
 
 export function sortStudents(
-  students:Student[],
+  students: Student[],
   sortBy: SortType,
   order: SortOrder,
 ): Student[] {
   const sortededStudents = [...students];
 
-  sortededStudents.sort((a: Student, b: Student) => {
-    switch (sortBy) {
-      case SortType.Name:
-      case SortType.Surname:
-        return order === 'asc'
+  switch (sortBy) {
+    default:
+      return sortededStudents;
+
+    case SortType.Name:
+    case SortType.Surname:
+      sortededStudents.sort((a: Student, b: Student) => (
+        order === 'asc'
           ? a[sortBy].localeCompare(b[sortBy])
-          : b[sortBy].localeCompare(a[sortBy]);
+          : b[sortBy].localeCompare(a[sortBy])
+      ));
 
-      case SortType.Age:
-      case SortType.Married:
-        return order === 'asc'
+      break;
+
+    case SortType.Age:
+    case SortType.Married:
+      sortededStudents.sort((a: Student, b: Student) => (
+        order === 'asc'
           ? Number(a[sortBy]) - Number(b[sortBy])
-          : Number(b[sortBy]) - Number(a[sortBy]);
+          : Number(b[sortBy]) - Number(a[sortBy])
+      ));
+      break;
 
-      case SortType.AverageGrade:
-        return order === 'asc'
+    case SortType.AverageGrade:
+      sortededStudents.sort((a: Student, b: Student) => (
+        order === 'asc'
           ? getAverageGrade(a) - getAverageGrade(b)
-          : getAverageGrade(b) - getAverageGrade(a);
+          : getAverageGrade(b) - getAverageGrade(a)
+      ));
 
-      default:
-        return 0;
-    }
-  });
+      break;
+  }
 
   return sortededStudents;
 }
