b49a6caba55d27e5941e53ffdc696cd979d3b56f
Ruslan Liapin <joy.esenin@gmail.com>
2023-03-03 15:56:54 +0200

    Solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,77 @@
 
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'Name',
+  Surname = 'Surname',
+  Age = 'Age',
+  Married = 'Married',
+  AverageGrade = 'AverageGrade',
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(students: Student[],
+  sortBy: SortType,
+  order: SortOrder): Student[] {
+  const studentsCopy = [...students];
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  studentsCopy.sort((a, b) => {
+    let aGrade;
+    let bGrade;
+
+    switch (sortBy) {
+      case SortType.Name:
+        return a.name.localeCompare(b.name);
+      case SortType.Surname:
+        return a.surname.localeCompare(b.surname);
+      case SortType.Age:
+        return order === 'asc' ? a.age - b.age : b.age - a.age;
+
+      case SortType.Married:
+        switch (order) {
+          case 'asc':
+            if (a.married === b.married) {
+              return 0;
+            }
+
+            if (a.married) {
+              return 1;
+            }
+
+            return -1;
+
+          case 'desc':
+            if (b.married === a.married) {
+              return 0;
+            }
+
+            if (b.married) {
+              return 1;
+            }
+
+            return -1;
+
+          default:
+            return -1;
+        }
+
+      case SortType.AverageGrade:
+        aGrade = a.grades.reduce((x, y) => x + y, 0) / a.grades.length;
+        bGrade = b.grades.reduce((x, y) => x + y, 0) / b.grades.length;
+
+        return order === 'asc' ? aGrade - bGrade : bGrade - aGrade;
+
+      default:
+        return a.name.localeCompare(b.name);
+    }
+  });
+
+  return studentsCopy;
 }
