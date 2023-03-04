3d87387273e83929bc3a84aaa8d7a64dec68d0ae
timoxa2t <alex2t172@gmail.com>
2023-03-04 11:18:46 +0200

    Solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,67 @@
+/* eslint-disable comma-dangle */
 
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'averageGrade',
 }
 
 // create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+function getAvgGrade(student: Student): number {
+  const total: number = student.grades.reduce(
+    (sum: number, grade: number) => sum + grade, 0
+  );
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  return total / student.grades.length;
+}
+
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder
+): Student[] {
+  type Callback = (first: Student, second: Student) => number;
+
+  const sortCallback : Callback = (first, second) => {
+    let difrence: number;
+
+    switch (sortBy) {
+      case SortType.Name:
+        difrence = first.name < second.name ? -1 : 1;
+        break;
+      case SortType.Surname:
+        difrence = first.surname < second.surname ? -1 : 1;
+        break;
+      case SortType.Age:
+        difrence = first.age - second.age;
+        break;
+      case SortType.Married:
+        difrence = Number(first.married) - Number(second.married);
+        break;
+      case SortType.AverageGrade:
+        difrence = getAvgGrade(first) - getAvgGrade(second);
+        break;
+      default:
+        difrence = 0;
+    }
+
+    return order === 'asc' ? difrence : -difrence;
+  };
+  const studentsCopy: Student[] = [...students];
+
+  studentsCopy.sort(sortCallback);
+
+  return studentsCopy;
 }
