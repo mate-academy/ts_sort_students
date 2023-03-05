703299192ec339dad4527ab717e7c45a7c9d77db
Serhii Zelinskyi <zelinskyi.serh@gmail.com>
2023-03-05 12:40:31 +0200

    solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,112 @@
 
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[],
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name,
+  Surname,
+  Age,
+  Married,
+  AverageGrade,
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' |'desc';
 
+export function sortStudents(
+  students: Student, sortBy: SortType, order: SortOrder,
+): string[] {
+  const copy: string[] = [...students];
+  const copyObject = copy.map((el) => ({ ...el }));
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  switch (sortBy) {
+    case (SortType.Name):
+      if (order === 'asc') {
+        copyObject.sort((a, b) => a.name.localeCompare(b.name));
+      }
+
+      if (order === 'desc') {
+        copyObject.sort((a, b) => b.name.localeCompare(a.name));
+      }
+      break;
+
+    case (SortType.Surname):
+      if (order === 'asc') {
+        copyObject.sort((a, b) => a.surname.localeCompare(b.surname));
+      }
+
+      if (order === 'desc') {
+        copyObject.sort((a, b) => b.surname.localeCompare(a.surname));
+      }
+      break;
+
+    case (SortType.Age):
+      if (order === 'asc') {
+        copyObject.sort((a, b) => a.age - b.age);
+      }
+
+      if (order === 'desc') {
+        copyObject.sort((a, b) => b.age - a.age);
+      }
+      break;
+
+    case (SortType.Married):
+      if (order === 'desc') {
+        copyObject.sort((a, b) => {
+          if (a.married === b.married) {
+            return 0;
+          }
+
+          return a.married ? -1 : 1;
+        });
+      }
+
+      if (order === 'asc') {
+        copyObject.sort((a, b) => {
+          if (b.married === a.married) {
+            return 0;
+          }
+
+          return b.married ? -1 : 1;
+        });
+      }
+      break;
+
+    case (SortType.AverageGrade):
+      if (order === 'asc') {
+        copyObject.sort((a, b) => {
+          const aSum = a.grades.reduce((acc: number, num: number) => {
+            return acc + num;
+          }, 0);
+          const bSum = b.grades.reduce((acc: number, num: number) => {
+            return acc + num;
+          }, 0);
+
+          return aSum / a.grades.length - bSum / b.grades.length;
+        });
+      }
+
+      if (order === 'desc') {
+        copyObject.sort((a, b) => {
+          const aSum = a.grades.reduce((acc: number, num: number) => {
+            return acc + num;
+          }, 0);
+          const bSum = b.grades.reduce((acc: number, num: number) => {
+            return acc + num;
+          }, 0);
+
+          return bSum / b.grades.length - aSum / a.grades.length;
+        });
+      }
+      break;
+
+    default:
+      throw new Error('uncorrect parameters');
+  }
+
+  return copyObject;
 }
