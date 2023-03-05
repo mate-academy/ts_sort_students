23a0b2238e9b03c63e7495da00bc35ef4669ac2c
Vladyslav Mosor <mosorvlad@gmail.com>
2023-03-05 15:02:53 +0100

    solution for sortStudents

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,55 @@
-
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[],
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
 // create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+// calculating average grade function
+function getAverageGrade({ grades }: Student): number {
+  return grades.reduce((grade, sum) => grade + sum, 0) / grades.length;
+}
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const copiedStudents = [...students];
+
+  return copiedStudents.sort((previousStudent, currentStudent) => {
+    switch (sortBy) {
+      case SortType.Name:
+      case SortType.Surname:
+        return order === 'asc'
+          ? previousStudent[sortBy].localeCompare(currentStudent[sortBy])
+          : currentStudent[sortBy].localeCompare(previousStudent[sortBy]);
+
+      case SortType.Age:
+      case SortType.Married:
+        return order === 'asc'
+          ? +(previousStudent[sortBy]) - +(currentStudent[sortBy])
+          : +(currentStudent[sortBy]) - +(previousStudent[sortBy]);
+
+      case SortType.AverageGrade:
+        return order === 'asc'
+          ? getAverageGrade(previousStudent) - getAverageGrade(currentStudent)
+          : getAverageGrade(currentStudent) - getAverageGrade(previousStudent);
+
+      default:
+        throw new Error('Provided type is not valid');
+    }
+  });
 }
