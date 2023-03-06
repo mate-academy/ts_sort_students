f680447698c34a3b1de86658e7d8a0364cf746d1
Ukrainian_panda <94929618+Ukrainiane-panda@users.noreply.github.com>
2023-03-06 19:12:42 +0200

    Solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,84 @@
 
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[],
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name,
+  Surname,
+  Age,
+  Married,
+  AverageGrade,
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(
+  students: Student[], sortBy: SortType, order: SortOrder,
+): Student[] {
+  const sortedStudents = [...students];
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  sortedStudents.sort((a: Student, b: Student) => {
+    let compareValue: number;
+    let avgGradeA: number;
+    let avgGradeB: number;
+
+    switch (sortBy) {
+      case SortType.Name:
+        compareValue = a.name.localeCompare(b.name);
+        break;
+
+      case SortType.Surname:
+        compareValue = a.surname.localeCompare(b.surname);
+        break;
+
+      case SortType.Age:
+        compareValue = a.age - b.age;
+        break;
+
+      case SortType.Married:
+        if (a.married === b.married) {
+          compareValue = 0;
+        } else if (a.married) {
+          compareValue = 1;
+        } else {
+          compareValue = -1;
+        }
+        break;
+
+      case SortType.AverageGrade:
+        avgGradeA = a.grades.reduce(
+          (sum: number, x: number) => (sum + x), 0,
+        ) / a.grades.length;
+
+        avgGradeB = b.grades.reduce(
+          (sum: number, x: number) => (sum + x), 0,
+        ) / b.grades.length;
+
+        if (avgGradeA === avgGradeB) {
+          compareValue = 0;
+        } else if (avgGradeA > avgGradeB) {
+          compareValue = 1;
+        } else {
+          compareValue = -1;
+        }
+
+        break;
+
+      default:
+        throw new Error('Invalid SortType');
+    }
+
+    if (order === 'asc') {
+      return compareValue;
+    }
+
+    return -compareValue;
+  });
+
+  return sortedStudents;
 }
