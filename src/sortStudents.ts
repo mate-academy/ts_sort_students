da831200f7f8daa1adf86800e4544a16a53cafca
Bogdan <bodia-00wot@ukr.net>
2023-03-07 17:47:14 +0200

    solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,57 @@
 
 export interface Student {
-  // describe Student interface
+  name : string,
+  surname : string,
+  age : number,
+  married : boolean,
+  grades : number[],
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'averageGrade',
 }
 
-// create SortOrder type
-export type SortOrder;
+export enum SortOrder {
+  Ascending = 'asc',
+  Descending = 'desc',
+}
 
+type AverageGrade = (x : Student) => number;
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+const callback = (
+  prev : number, grade: number,
+): number => prev + grade;
+
+const getAverageGrade: AverageGrade
+= (object) => object.grades.reduce(callback, 0) / object.grades.length;
+
+export function sortStudents(
+  students : Student[], sortBy : SortType, order : SortOrder,
+) : Student[] {
+  const arrayOfStudents = [...students];
+
+  arrayOfStudents.sort((prev : Student, curr: Student) => {
+    switch (sortBy) {
+      case SortType.Name: case SortType.Surname:
+        return order === SortOrder.Ascending
+          ? prev[sortBy].localeCompare(curr[sortBy])
+          : curr[sortBy].localeCompare(prev[sortBy]);
+      case SortType.Age: case SortType.Married:
+        return order === SortOrder.Ascending
+          ? (+prev[sortBy]) - (+curr[sortBy])
+          : (+curr[sortBy]) - (+prev[sortBy]);
+      case SortType.AverageGrade:
+        return order === SortOrder.Ascending
+          ? getAverageGrade(prev) - getAverageGrade(curr)
+          : getAverageGrade(curr) - getAverageGrade(prev);
+      default:
+        throw new Error('Invalid SortType');
+    }
+  });
+
+  return arrayOfStudents;
 }
