7dc78cbc2f9281bceaaa25f259255cf9e5d1f051
Nazarov Oleksiy <112445970+nazarovoleksiy@users.noreply.github.com>
2023-03-07 09:50:19 +0200

    Solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,52 @@
 
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[],
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+function calcAverage({ grades }: Student): number {
+  return grades.reduce((grade, sum) => grade + sum, 0) / grades.length;
+}
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  return [...students].sort((previous, current) => {
+    switch (sortBy) {
+      case SortType.Surname:
+      case SortType.Name:
+        return order === 'asc'
+          ? previous[sortBy].localeCompare(current[sortBy])
+          : current[sortBy].localeCompare(previous[sortBy]);
+
+      case SortType.Married:
+      case SortType.Age:
+        return order === 'asc'
+          ? Number(previous[sortBy]) - Number(current[sortBy])
+          : Number(current[sortBy]) - Number(previous[sortBy]);
+
+      case SortType.AverageGrade:
+        return order === 'asc'
+          ? calcAverage(previous) - calcAverage(current)
+          : calcAverage(current) - calcAverage(previous);
+
+      default:
+        throw new Error('Something going wrong...');
+    }
+  });
 }
