0a2030214145525012ee9ec6813067fcd5fcd84d
Yevhenii Hura <yevheniihura@gmail.com>
2023-03-07 17:06:42 +0200

    solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,52 @@
 
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[],
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name,
+  Surname,
+  Age,
+  Married,
+  AverageGrade,
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+export function sortStudents(students: Student[], sortBy: SortType, order: SortOrder): Student[] {
+  const arrayForSorting = [...students];
+  const orderForSorting: number = order === 'asc' ? 1 : -1;
+
+  return arrayForSorting.sort((studentA: Student, studentB: Student) => {
+    switch(sortBy) {
+      case SortType.Name:
+        return studentA.name.localeCompare(studentB.name) * orderForSorting;
+
+      case SortType.Surname:
+        return studentA.surname.localeCompare(studentB.surname) * orderForSorting;
+
+      case SortType.Age:
+        return (studentA.age - studentB.age) * orderForSorting;
+
+      case SortType.Married:
+        return (Number(studentA.married) - Number(studentB.married)) * orderForSorting;
+
+      case SortType.AverageGrade:
+        return (getAverageGrade(studentA) - getAverageGrade(studentB)) * orderForSorting;
+
+    }
+  });
+}
+
+function getAverageGrade(student: Student): number {
+  const sum: number = student.grades.reduce((accumulator: number, currentValue: number) => {
+    return accumulator + currentValue;
+  }, 0)
+
+  return sum / student.grades.length;
 }
