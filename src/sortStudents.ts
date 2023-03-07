4a054cd0c96f2b60ca65f788ae23f3594e09ec16
Mark Babich <morak.bobama@gmail.com>
2023-03-07 15:45:12 +0200

    Sort Students task done

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,75 @@
-
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
-// create SortOrder type
-export type SortOrder;
+export enum SortOrder {
+  Asc = 'asc',
+  Desc = 'desc',
+}
 
+function getAverageGrade(student: Student): number {
+  return student.grades.reduce((total, grade) => total + grade)
+    / student.grades.length;
+}
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const studentCopy: Student[] = [...students];
+
+  switch (sortBy) {
+    case SortType.Name:
+    case SortType.Surname:
+      studentCopy.sort((firstStudent, secondStudent): number => {
+        const isAscending = order === SortOrder.Asc;
+
+        return isAscending
+          ? firstStudent[sortBy].localeCompare(secondStudent[sortBy])
+          : secondStudent[sortBy].localeCompare(firstStudent[sortBy]);
+      });
+
+      break;
+
+    case SortType.Age:
+    case SortType.Married:
+      studentCopy.sort((firstStudent, secondStudent): number => {
+        const isAscending = order === SortOrder.Asc;
+
+        return isAscending
+          ? +firstStudent[sortBy] - +secondStudent[sortBy]
+          : +secondStudent[sortBy] - +firstStudent[sortBy];
+      });
+
+      break;
+
+    case SortType.AverageGrade:
+      studentCopy.sort((firstStudent, secondStudent): number => {
+        const isAscending = order === SortOrder.Asc;
+
+        return isAscending
+          ? getAverageGrade(firstStudent) - getAverageGrade(secondStudent)
+          : getAverageGrade(secondStudent) - getAverageGrade(firstStudent);
+      });
+
+      break;
+
+    default:
+      throw new Error('Invalid SortBy value');
+  }
+
+  return studentCopy;
 }
