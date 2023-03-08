e425d9b9009f6482dd7f461195ea77aa7cb3e56a
Oleh Chernenko <oleh.chernenko97@gmail.com>
2023-03-08 09:15:20 +0200

    solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,56 @@
 
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[],
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades'
 }
 
-// create SortOrder type
-export type SortOrder;
+export enum SortOrder {
+  Asc = 'asc',
+  Desc = 'desc'
+}
 
+function averageGrades({ grades }: Student): number {
+  return grades.reduce((grade, sum) => grade + sum, 0) / grades.length;
+}
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const arrayOfStudents = [...students];
+
+  return arrayOfStudents.sort((student1: Student, student2: Student) => {
+    switch (sortBy) {
+      case SortType.Name:
+      case SortType.Surname:
+        return order === SortOrder.Asc
+          ? student1[sortBy].localeCompare(student2[sortBy])
+          : student2[sortBy].localeCompare(student1[sortBy]);
+
+      case SortType.Age:
+      case SortType.Married:
+        return order === SortOrder.Asc
+          ? Number(student1[sortBy]) - Number(student2[sortBy])
+          : Number(student2[sortBy]) - Number(student1[sortBy]);
+
+      case SortType.AverageGrade:
+        return order === SortOrder.Asc
+          ? averageGrades(student1) - averageGrades(student2)
+          : averageGrades(student2) - averageGrades(student1);
+      default:
+        throw new Error('It isn`t a valid type');
+    }
+  });
 }
