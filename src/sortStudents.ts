a7587baebba110eb10aab9d9eeb53292439266fd
Vadym Tsyndra <Mr.Tsyndra@gmail.com>
2023-03-08 22:01:00 +0200

    Solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,110 @@
-
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[],
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name,
+  Surname,
+  Age,
+  Married,
+  AverageGrade,
 }
 
 // create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+function averageGrade(arr: number[]): number {
+  let sum: number = 0;
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  for (let i: number = 0; i < arr.length; i + 1) {
+    sum += arr[i];
+  }
+
+  return sum / arr.length;
+}
+
+export function sortStudents(
+  students: Student[], sortBy: SortType, order: SortOrder,
+): Student[] {
+  if (sortBy === SortType.Name && order === 'asc') {
+    const result: Student[] = [...students].sort(
+      (a: Student, b: Student) => a.name.localeCompare(b.name),
+    );
+
+    return result;
+  }
+
+  if (sortBy === SortType.Name && order === 'desc') {
+    const result: Student[] = [...students].sort(
+      (a: Student, b: Student) => b.name.localeCompare(a.name),
+    );
+
+    return result;
+  }
+
+  if (sortBy === SortType.Surname && order === 'asc') {
+    const result: Student[] = [...students].sort(
+      (a: Student, b: Student) => a.surname.localeCompare(b.surname),
+    );
+
+    return result;
+  }
+
+  if (sortBy === SortType.Surname && order === 'desc') {
+    const result: Student[] = [...students].sort(
+      (a: Student, b: Student) => b.surname.localeCompare(a.surname),
+    );
+
+    return result;
+  }
+
+  if (sortBy === SortType.Age && order === 'asc') {
+    const result: Student[] = [...students].sort(
+      (a: Student, b: Student) => a.age - b.age,
+    );
+
+    return result;
+  }
+
+  if (sortBy === SortType.Age && order === 'desc') {
+    const result: Student[] = [...students].sort(
+      (a: Student, b: Student) => b.age - a.age,
+    );
+
+    return result;
+  }
+
+  if (sortBy === SortType.Married && order === 'desc') {
+    const result: Student[] = [...students].sort(
+      (a: Student, b: Student) => +b.married - +a.married,
+    );
+
+    return result;
+  }
+
+  if (sortBy === SortType.AverageGrade && order === 'asc') {
+    const result: Student[] = [...students].sort(
+      (a: Student, b: Student) => averageGrade(
+        a.grades,
+      ) - averageGrade(b.grades),
+    );
+
+    return result;
+  }
+
+  if (sortBy === SortType.AverageGrade && order === 'desc') {
+    const result: Student[] = [...students].sort(
+      (a: Student, b: Student) => averageGrade(
+        b.grades,
+      ) - averageGrade(a.grades),
+    );
+
+    return result;
+  }
+
+  return students;
 }
