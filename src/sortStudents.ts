e57747206784b9ee798950c3d59b2194c133e397
Sasha Kononenko <nurchiksasha@gmail.com>
2023-03-08 14:16:07 +0200

    sortStudent

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,56 @@
-
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grade',
 }
 
-// create SortOrder type
-export type SortOrder;
+export function getAverage({ grades }: Student): number {
+  return grades.reduce((startValue, currentValue) => startValue + currentValue)
+    / grades.length;
+}
 
+export enum SortOrder {
+  Asc = 'asc',
+  Desc = 'desc',
+}
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  return [...students].sort((firstPupil, secondPupil) => {
+    const sort = order === SortOrder.Asc
+      ? 1
+      : -1;
+
+    switch (sortBy) {
+      case SortType.Name:
+      case SortType.Surname:
+        return (firstPupil[sortBy].localeCompare(secondPupil[sortBy]))
+        * sort;
+
+      case SortType.Age:
+      case SortType.Married:
+        return (Number(firstPupil[sortBy]) - Number(secondPupil[sortBy]))
+        * sort;
+
+      case SortType.AverageGrade:
+        return (getAverage(firstPupil) - getAverage(secondPupil))
+        * sort;
+
+      default:
+        throw new Error();
+    }
+  });
 }
