014612212d942d31903382f3d4e6ff08674f4375
andriibezkorovainyi <122180564+andriibezkorovainyi@users.noreply.github.com>
2023-03-08 12:29:50 +0200

    Task complete

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,59 @@
-
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[],
+  averageGrade: number,
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'averageGrade',
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const studentsOut = students.map((student) => (
+    {
+      ...student,
+      averageGrade: student.grades.reduce(
+        (prevGrade, currGrade) => prevGrade + currGrade,
+      ) / student.grades.length,
+    }
+  ));
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  function compareFn(aStudent: Student, bStudent: Student): number {
+    switch (sortBy) {
+      case SortType.Name:
+      case SortType.Surname:
+        return order === 'desc'
+          ? bStudent[sortBy].localeCompare(aStudent[sortBy])
+          : aStudent[sortBy].localeCompare(bStudent[sortBy]);
+
+      case SortType.Age:
+      case SortType.AverageGrade:
+        return order === 'desc'
+          ? bStudent[sortBy] - aStudent[sortBy]
+          : aStudent[sortBy] - bStudent[sortBy];
+
+      case SortType.Married:
+        return order === 'desc'
+          ? Number(bStudent.married) - Number(aStudent.married)
+          : Number(aStudent.married) - Number(bStudent.married);
+
+      default:
+        return 0;
+    }
+  }
+
+  return studentsOut.sort(compareFn);
 }
