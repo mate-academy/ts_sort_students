b1413475f0ce88e986e636231ae048d00ccf2363
kulakostas <92936150+kulakostas@users.noreply.github.com>
2023-03-08 09:56:33 +0200

    solution for sortStudents task

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,53 @@
 
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
 // create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+function averageGrade(grades: number[]): number {
+  return grades.reduce((acc, curr) => acc + curr) / grades.length;
+}
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  return [...students].sort((previous: Student, next: Student) => {
+    switch (sortBy) {
+      case SortType.Name:
+      case SortType.Surname:
+        return order === 'desc'
+          ? next[sortBy].localeCompare(previous[sortBy])
+          : previous[sortBy].localeCompare(next[sortBy]);
+
+      case SortType.Age:
+      case SortType.Married:
+        return order === 'desc'
+          ? +(next[sortBy]) - +(previous[sortBy])
+          : +(previous[sortBy]) - +(next[sortBy]);
+
+      case SortType.AverageGrade:
+        return order === 'desc'
+          ? averageGrade(next[sortBy]) - averageGrade(previous[sortBy])
+          : averageGrade(previous[sortBy]) - averageGrade(next[sortBy]);
+
+      default:
+        throw new Error('SortType Error');
+    }
+  });
 }
