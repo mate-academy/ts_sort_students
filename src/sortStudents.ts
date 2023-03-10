3a1003c958f1a9610a4ebdeac790e314bc67383c
Arthur Krepko <krepko2003krepko@gmail.com>
2023-03-10 05:22:02 +0200

    task completed

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,52 @@
 
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[],
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+function calculateAverageGrade({ grades }: Student): number {
+  return grades.reduce((acc, curr) => acc + curr, 0) / grades.length;
+}
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  return [...students].sort((prev: Student, curr: Student) => {
+    switch (sortBy) {
+      case SortType.AverageGrade:
+        return order === 'asc'
+          ? calculateAverageGrade(prev) - calculateAverageGrade(curr)
+          : calculateAverageGrade(curr) - calculateAverageGrade(prev);
+      case SortType.Age:
+        return order === 'asc'
+          ? +prev[sortBy] - +curr[sortBy]
+          : +curr[sortBy] - +prev[sortBy];
+      case SortType.Married:
+        return order === 'asc'
+          ? +prev[sortBy] - +curr[sortBy]
+          : +curr[sortBy] - +prev[sortBy];
+      case SortType.Name:
+      case SortType.Surname:
+        return order === 'asc'
+          ? prev[sortBy].localeCompare(curr[sortBy])
+          : curr[sortBy].localeCompare(prev[sortBy]);
+      default:
+        throw new Error('Set another type');
+    }
+  });
 }
