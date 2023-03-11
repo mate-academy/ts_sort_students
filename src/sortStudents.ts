195cd898cba1ad366a54febaf2c3e18b02f5d867
Nataliia Batiukova <batiukova.nataliia@gmail.com>
2023-03-11 15:07:18 +0000

    switch case is used now

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -20,47 +20,30 @@ export type SortOrder = 'asc' | 'desc';
 
 export function sortStudents(students: Student[],
   sortBy: SortType, order: SortOrder): Student[] {
-  let resultArray: Student[] = [];
+  return [...students].sort((a, b): number => {
+    switch (sortBy) {
+      case SortType.Age:
+      case SortType.Married:
+        return order === 'asc' ? a[sortBy] - b[sortBy]
+          : b[sortBy] - a[sortBy];
 
-  if (order === 'asc' && sortBy === SortType.Age) {
-    resultArray = [...students].sort((a, b) => a.age - b.age);
-  } else if (order === 'desc' && sortBy === SortType.Age) {
-    resultArray = [...students].sort((a, b) => b.age - a.age);
-  }
+      case SortType.Name:
+      case SortType.Surname:
+        return order === 'asc' ? a[sortBy].localeCompare(b[sortBy])
+          : b[sortBy].localeCompare(a[sortBy]);
 
-  if (order === 'asc' && sortBy === SortType.Name) {
-    resultArray = [...students].sort((a, b) => a.name.localeCompare(b.name));
-  } else if (order === 'desc' && sortBy === SortType.Name) {
-    resultArray = [...students].sort((a, b) => b.name.localeCompare(a.name));
-  }
+      case SortType.AverageGrade:
+        return order === 'asc' ? a[sortBy]
+          .reduce((prev, current) => prev + current) / a[sortBy].length
+          - b[sortBy]
+            .reduce((prev, current) => prev + current) / b[sortBy].length
+          : b[sortBy]
+            .reduce((prev, current) => prev + current) / b[sortBy].length
+          - a[sortBy]
+            .reduce((prev, current) => prev + current) / a[sortBy].length;
 
-  if (order === 'asc' && sortBy === SortType.Surname) {
-    resultArray = [...students]
-      .sort((a, b) => a.surname.localeCompare(b.surname));
-  } else if (order === 'desc' && sortBy === SortType.Surname) {
-    resultArray = [...students]
-      .sort((a, b) => b.surname.localeCompare(a.surname));
-  }
-
-  if (order === 'asc' && sortBy === SortType.AverageGrade) {
-    resultArray = [...students]
-      .sort((a, b) => a.grades
-        .reduce((prev, current) => prev + current) / a.grades.length
-        - b.grades
-          .reduce((prev, current) => prev + current) / b.grades.length);
-  } else if (order === 'desc' && sortBy === SortType.AverageGrade) {
-    resultArray = [...students]
-      .sort((a, b) => b.grades
-        .reduce((prev, current) => prev + current) / b.grades.length
-        - a.grades
-          .reduce((prev, current) => prev + current) / a.grades.length);
-  }
-
-  if (order === 'asc' && sortBy === SortType.Married) {
-    resultArray = [...students].sort((a, b) => a.married - b.married);
-  } else if (order === 'desc' && sortBy === SortType.Married) {
-    resultArray = [...students].sort((a, b) => b.married - a.married);
-  }
-
-  return resultArray;
+      default:
+        return 0;
+    }
+  });
 }
