f5ee9af4b449c3d4f80859be834a9287464cba9f
Vadim Drobyazko <volandemortuwka@gmail.com>
2023-03-12 13:16:05 +0200

    TS Sort

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,48 @@
 
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: true;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'ave',
+  Married = 'married',
+  AverageGrade = 'AverageGrade',
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
+
+function AverageGrade({grades}: Student): number {
+  return grades.reduce((a, b) => a + b / grades.length);
+}
 
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+export function sortStudents(students: Student[], sortBy: SortType, order: SortOrder): Student[] {
+  const studentCopy = [...students];
+  const orderName = order === 'asc'
+    ? 1
+    : -1;
+
+  return studentCopy.sort((prevStudent, currentStudent): number => {
+    switch (sortBy) {
+      case SortType.Name:
+      case SortType.Surname:
+        return orderName * prevStudent[sortBy].localeCompare(currentStudent[sortBy]);
+
+      case SortType.Age:
+      case SortType.Married:
+        return orderName * (Number(prevStudent[sortBy]) - Number(currentStudent[sortBy]));
+
+      case SortType.AverageGrade:
+        return orderName * (AverageGrade(prevStudent) - AverageGrade(currentStudent));
+
+      default: 
+        throw new Error('OrderName is wrong');
+    }
+  })
 }
