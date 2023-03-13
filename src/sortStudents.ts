289bd6eaaf0da030759ba0fe07adf2fa6c20d95e
Oleh Chernenko <oleh.chernenko97@gmail.com>
2023-03-13 14:03:49 +0200

    bugs fixed

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -29,28 +29,27 @@ export function sortStudents(
   sortBy: SortType,
   order: SortOrder,
 ): Student[] {
-  const arrayOfStudents = [...students];
+  return [...students].sort((student1: Student, student2: Student) => {
+    const direction = order === SortOrder.Asc
+      ? 1
+      : -1;
 
-  return arrayOfStudents.sort((student1: Student, student2: Student) => {
     switch (sortBy) {
       case SortType.Name:
       case SortType.Surname:
-        return order === SortOrder.Asc
-          ? student1[sortBy].localeCompare(student2[sortBy])
-          : student2[sortBy].localeCompare(student1[sortBy]);
+        return student1[sortBy].localeCompare(student2[sortBy])
+        * direction;
 
       case SortType.Age:
       case SortType.Married:
-        return order === SortOrder.Asc
-          ? Number(student1[sortBy]) - Number(student2[sortBy])
-          : Number(student2[sortBy]) - Number(student1[sortBy]);
+        return (Number(student1[sortBy]) - Number(student2[sortBy]))
+        * direction;
 
       case SortType.AverageGrade:
-        return order === SortOrder.Asc
-          ? averageGrades(student1) - averageGrades(student2)
-          : averageGrades(student2) - averageGrades(student1);
+        return (averageGrades(student1) - averageGrades(student2))
+        * direction;
       default:
-        throw new Error('It isn`t a valid type');
+        throw new Error(`${sortBy} isn't a valid sorting type`);
     }
   });
 }
