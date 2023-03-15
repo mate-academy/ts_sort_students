00b02975f8d5719a66e018dbed9c657b89f77516
Vladyslav Kanaiev <nordex83@gmail.com>
2023-03-15 11:50:11 +0200

    optimization

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -17,46 +17,41 @@ export enum SortType {
 
 export type SortOrder = 'asc' | 'desc';
 
+export function calcAverage(arrOfGrades: number[]): number {
+  return arrOfGrades.reduce((acc, grade) => {
+    return acc + grade;
+  }, 0) / arrOfGrades.length;
+}
+
 export function sortStudents(
   students: Student[],
   sortBy: SortType,
   order: SortOrder,
 ): object[] {
-  switch (sortBy) {
-    case SortType.Name:
-    case SortType.Surname:
-      return order === 'asc'
-        ? [...students]
-          .sort((firstStudent, secondStudent) => firstStudent[sortBy]
-            .localeCompare(secondStudent[sortBy]))
-        : [...students]
-          .sort((firstStudent, secondStudent) => secondStudent[sortBy]
-            .localeCompare(firstStudent[sortBy]));
+  return [...students].sort((firstStudent, secondStudent) => {
+    const direction = order === 'asc' ? 1 : -1;
 
-    case SortType.Age:
-    case SortType.Married:
-      return order === 'asc'
-        ? [...students]
-          .sort((firstStudent, secondStudent) => Number(firstStudent[sortBy])
-            - Number(secondStudent[sortBy]))
-        : [...students]
-          .sort((firstStudent, secondStudent) => Number(secondStudent[sortBy])
-            - Number(firstStudent[sortBy]));
+    switch (sortBy) {
+      case SortType.Name:
+      case SortType.Surname:
+        return (
+          firstStudent[sortBy].localeCompare(secondStudent[sortBy]) * direction
+        );
 
-    case SortType.AverageGrade:
-      return order === 'asc'
-        ? [...students]
-          .sort((firstStudent, secondStudent) => (firstStudent.grades
-            .reduce((acc, num) => acc + num, 0) / firstStudent.grades.length)
-              - (secondStudent.grades
-                .reduce((acc, num) => acc + num)) / secondStudent.grades.length)
-        : [...students]
-          .sort((firstStudent, secondStudent) => (secondStudent.grades
-            .reduce((acc, num) => acc + num, 0) / secondStudent.grades.length)
-              - (firstStudent.grades
-                .reduce((acc, num) => acc + num)) / firstStudent.grades.length);
+      case SortType.Age:
+      case SortType.Married:
+        return (
+          (Number(firstStudent[sortBy]) - Number(secondStudent[sortBy]))
+            * direction
+        );
 
-    default:
-      throw new Error('unknown sort type');
-  }
+      case SortType.AverageGrade:
+        return (
+          calcAverage(firstStudent.grades) - calcAverage(secondStudent.grades)
+        ) * direction;
+
+      default:
+        throw new Error('unknown sort type');
+    }
+  });
 }
