22c0f86ba9628c64e091ee2191b9887995f3b6e2
OlyaNikora <nikoraolya@gmail.com>
2023-03-17 15:55:29 +0100

    Solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,56 @@
+function averageGrade(grades: number[]): number {
+  return grades.reduce((acc: number, grade: number) => acc + grade, 0)
+    / grades.length;
+}
 
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const copyStudents: Student[] = [...students];
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  switch (sortBy) {
+    case SortType.Age:
+    case SortType.Married:
+      return order === 'asc'
+        ? copyStudents.sort((a, b) => Number(a[sortBy]) - Number(b[sortBy]))
+        : copyStudents.sort((a, b) => Number(b[sortBy]) - Number(a[sortBy]));
+
+    case SortType.Name:
+    case SortType.Surname:
+      return order === 'asc'
+        // eslint-disable-next-line max-len
+        ? copyStudents.sort((a: Student, b: Student) => a[sortBy].localeCompare(b[sortBy]))
+        // eslint-disable-next-line max-len
+        : copyStudents.sort((a: Student, b: Student) => b[sortBy].localeCompare(a[sortBy]));
+
+    case SortType.AverageGrade:
+      return order === 'asc'
+        // eslint-disable-next-line max-len
+        ? copyStudents.sort((a: Student, b: Student) => averageGrade(a[sortBy]) - averageGrade(b[sortBy]))
+        // eslint-disable-next-line max-len
+        : copyStudents.sort((a: Student, b: Student) => averageGrade(b[sortBy]) - averageGrade(a[sortBy]));
+
+    default:
+      throw new Error('Sort is impossible');
+  }
 }
