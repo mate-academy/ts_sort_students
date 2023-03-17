a2e792564f73143b45a3fc56c508cc507cee8a33
EhorSaprykin <astralik1993@gmail.com>
2023-03-17 18:11:32 +0200

    tsc done

--- src/sortStudents.js
+++ src/sortStudents.js
@@ -9,41 +9,24 @@ var SortType;
     SortType["Married"] = "married";
     SortType["AverageGrade"] = "grade";
 })(SortType = exports.SortType || (exports.SortType = {}));
+function average(item) {
+    return item.reduce(function (a, b) { return a + b; }) / item.length;
+}
 function sortStudents(students, sortBy, order) {
     var direction = order === 'asc' ? 1 : -1;
-    if (sortBy === 'name' || sortBy === 'surname') {
-        return students.sort(function (a, b) {
-            var aString = a[sortBy].toLowerCase();
-            var bString = b[sortBy].toLowerCase();
-            if (aString > bString) {
-                return 1 * direction;
-            }
-            if (aString < bString) {
-                return -1 * direction;
-            }
-            return 0;
-        });
-    }
-    if (sortBy === 'age') {
-        return students.sort(function (a, b) { return (a.age - b.age) * direction; });
-    }
-    if (sortBy === 'married') {
-        return students.sort(function (a, b) {
-            if (b.married) {
-                return -1 * direction;
-            }
-            if (a.married) {
-                return 1 * direction;
-            }
-            return 0;
-        });
-    }
-    function average(item) {
-        return item.reduce(function (a, b) { return a + b; }) / item.length;
-    }
-    if (sortBy === 'grade') {
-        return students.sort(function (a, b) { return (average(a.grades) - average(b.grades)) * direction; });
-    }
-    return students;
+    return students.sort(function (a, b) {
+        switch (sortBy) {
+            case 'name':
+            case 'surname':
+                return a[sortBy].localeCompare(b[sortBy]) * direction;
+            case 'age':
+            case 'married':
+                return (+b.age - +a.age) * direction;
+            case 'grade':
+                return (average(a.grades) - average(b.grades));
+            default:
+                return 0;
+        }
+    });
 }
 exports.sortStudents = sortStudents;
