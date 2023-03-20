8e1e5b07ba13acafd94bd460a3b11de4952d7789
Oleksandr <98667890az@gmail.com>
2023-03-20 17:24:46 +0200

    ts_sort_students

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,76 @@
-
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[]
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name,
+  Surname,
+  Age,
+  Married,
+  AverageGrade
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+function average(array: number[]): number {
+  const sum = array.reduce(
+    (accamulator, currentValue) => accamulator + currentValue,
+  );
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  return sum / array.length;
+}
+
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const newArray: Student[] = [...students];
+
+  switch (sortBy) {
+    case SortType.Name:
+      if (order === 'asc') {
+        return newArray.sort((a, b) => a.name.localeCompare(b.name));
+      }
+
+      return newArray.sort((a, b) => b.name.localeCompare(a.name));
+
+    case SortType.Surname:
+      if (order === 'asc') {
+        return newArray.sort((a, b) => a.surname.localeCompare(b.surname));
+      }
+
+      return newArray.sort((a, b) => b.surname.localeCompare(a.surname));
+
+    case SortType.Age:
+      if (order === 'asc') {
+        return newArray.sort((a, b) => a.age - b.age);
+      }
+
+      return newArray.sort((a, b) => b.age - a.age);
+
+    case SortType.Married:
+      if (order === 'asc') {
+        return newArray.sort((a, b) => +a.married - +b.married);
+      }
+
+      return newArray.sort((a, b) => +b.married - +a.married);
+
+    case SortType.AverageGrade:
+      if (order === 'asc') {
+        return newArray.sort(
+          (a, b) => (average(a.grades)) - (average(b.grades)),
+        );
+      }
+
+      return newArray.sort(
+        (a, b) => (average(b.grades)) - average(a.grades),
+      );
+
+    default: return newArray;
+  }
 }
