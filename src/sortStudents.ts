a24aa97af6b592f437042ee282a087bb3edbf9f9
Serhii Nikolin <sergeynikolin@gmail.com>
2023-03-23 17:43:51 +0200

    Ready

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,84 @@
 
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades'
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(
+  students: Student[], sortBy: SortType, order: SortOrder,
+): Student[] {
+  const copyArr = [...students];
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  function aveGrade(arr: number[]): number {
+    const ave = arr.reduce((prev: number, curr: number): number => {
+      return prev + curr;
+    }, 0);
+
+    return ave / arr.length;
+  }
+
+  copyArr.sort((studentA: Student, studentB: Student): number => {
+    if (sortBy === 'name' || sortBy === 'surname') {
+      if (order === 'asc') {
+        return studentA[sortBy].localeCompare(studentB[sortBy]);
+      }
+
+      if (order === 'desc') {
+        return studentB[sortBy].localeCompare(studentA[sortBy]);
+      }
+    }
+
+    if (sortBy === 'grades') {
+      const aveGradeStudA = aveGrade(studentA[sortBy]);
+      const aveGradeStudB = aveGrade(studentB[sortBy]);
+
+      if (order === 'asc') {
+        return aveGradeStudA - aveGradeStudB;
+      }
+
+      if (order === 'desc') {
+        return aveGradeStudB - aveGradeStudA;
+      }
+    }
+
+    if (sortBy === 'age') {
+      if (order === 'asc') {
+        return studentA[sortBy] - studentB[sortBy];
+      }
+
+      if (order === 'desc') {
+        return studentB[sortBy] - studentA[sortBy];
+      }
+    }
+
+    if (sortBy === 'married') {
+      const marriedStatusA: number = studentA[sortBy] === true ? 1 : 0;
+      const marriedStatusB: number = studentB[sortBy] === true ? 1 : 0;
+
+      if (order === 'asc') {
+        return marriedStatusA - marriedStatusB;
+      }
+
+      if (order === 'desc') {
+        return marriedStatusB - marriedStatusA;
+      }
+    }
+
+    return 0;
+  });
+
+  return copyArr;
 }
