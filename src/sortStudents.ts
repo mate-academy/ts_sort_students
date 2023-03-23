f4c13d46d0dc905348b9ee530d9aec6324839ef7
Serhii Nikolin <sergeynikolin@gmail.com>
2023-03-23 20:04:52 +0200

    Code edited

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-case-declarations */
 
 export interface Student {
   name: string;
@@ -18,63 +19,62 @@ export enum SortType {
 export type SortOrder = 'asc' | 'desc';
 
 export function sortStudents(
-  students: Student[], sortBy: SortType, order: SortOrder,
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
 ): Student[] {
   const copyArr = [...students];
 
-  function aveGrade(arr: number[]): number {
-    const ave = arr.reduce((prev: number, curr: number): number => {
+  function averageGrade(arr: number[]): number {
+    const sumOfGrades = arr.reduce((prev: number, curr: number): number => {
       return prev + curr;
     }, 0);
 
-    return ave / arr.length;
+    return sumOfGrades / arr.length;
+  }
+
+  function compareNumberType(prop1: number, prop2: number): number {
+    if (order === 'asc') {
+      return prop1 - prop2;
+    }
+
+    if (order === 'desc') {
+      return prop2 - prop1;
+    }
+
+    return 0;
   }
 
   copyArr.sort((studentA: Student, studentB: Student): number => {
-    if (sortBy === 'name' || sortBy === 'surname') {
-      if (order === 'asc') {
-        return studentA[sortBy].localeCompare(studentB[sortBy]);
-      }
+    switch (sortBy) {
+      case SortType.Name:
+      case SortType.Surname:
+        if (order === 'asc') {
+          return studentA[sortBy].localeCompare(studentB[sortBy]);
+        }
 
-      if (order === 'desc') {
-        return studentB[sortBy].localeCompare(studentA[sortBy]);
-      }
-    }
+        if (order === 'desc') {
+          return studentB[sortBy].localeCompare(studentA[sortBy]);
+        }
+        break;
 
-    if (sortBy === 'grades') {
-      const aveGradeStudA = aveGrade(studentA[sortBy]);
-      const aveGradeStudB = aveGrade(studentB[sortBy]);
+      case SortType.AverageGrade:
+        const aveGradeStudA = averageGrade(studentA[sortBy]);
+        const aveGradeStudB = averageGrade(studentB[sortBy]);
 
-      if (order === 'asc') {
-        return aveGradeStudA - aveGradeStudB;
-      }
+        return compareNumberType(aveGradeStudA, aveGradeStudB);
 
-      if (order === 'desc') {
-        return aveGradeStudB - aveGradeStudA;
-      }
-    }
+      case SortType.Age:
+        return compareNumberType(studentA.age, studentB.age);
 
-    if (sortBy === 'age') {
-      if (order === 'asc') {
-        return studentA[sortBy] - studentB[sortBy];
-      }
+      case SortType.Married:
+        const marriedStatusA: number = studentA[sortBy] === true ? 1 : 0;
+        const marriedStatusB: number = studentB[sortBy] === true ? 1 : 0;
 
-      if (order === 'desc') {
-        return studentB[sortBy] - studentA[sortBy];
-      }
-    }
+        return compareNumberType(marriedStatusA, marriedStatusB);
 
-    if (sortBy === 'married') {
-      const marriedStatusA: number = studentA[sortBy] === true ? 1 : 0;
-      const marriedStatusB: number = studentB[sortBy] === true ? 1 : 0;
-
-      if (order === 'asc') {
-        return marriedStatusA - marriedStatusB;
-      }
-
-      if (order === 'desc') {
-        return marriedStatusB - marriedStatusA;
-      }
+      default:
+        break;
     }
 
     return 0;
