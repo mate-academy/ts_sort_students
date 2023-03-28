2fe9a675c053ab6a4d542cc96ea9b9d3e0b98ecc
Vlad Ptashnichenko <vp.vlad.vp@gmail.com>
2023-03-28 10:40:12 +0300

    Solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,61 @@
 
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name,
+  Surname,
+  Age,
+  Married,
+  AverageGrade,
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const copyStudents = [...students];
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  switch (sortBy) {
+    case SortType.Name:
+      copyStudents.sort((a, b) => (order === 'asc'
+        ? a.name.localeCompare(b.name)
+        : b.name.localeCompare(a.name)));
+      break;
+    case SortType.Surname:
+      copyStudents.sort((a, b) => (order === 'asc'
+        ? a.surname.localeCompare(b.surname)
+        : b.surname.localeCompare(a.surname)));
+      break;
+    case SortType.Age:
+      copyStudents.sort((a, b) => (order === 'asc'
+        ? a.age - b.age
+        : b.age - a.age));
+      break;
+    case SortType.Married:
+      copyStudents.sort((a, b) => (order === 'asc'
+        ? +a.married - +b.married
+        : +b.married - +a.married
+      ));
+      break;
+    default:
+      copyStudents.sort((a, b) => {
+        const firstEl = a.grades
+          .reduce((sum, item) => sum + item, 0) / a.grades.length;
+        const secondEl = b.grades
+          .reduce((sum, item) => sum + item, 0) / b.grades.length;
+
+        return order === 'asc' ? firstEl - secondEl : secondEl - firstEl;
+      });
+  }
+
+  return copyStudents;
 }
