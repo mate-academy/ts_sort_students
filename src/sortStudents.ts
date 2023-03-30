8e58a0c3756d1b54b0239fb61be29278f57e80ac
Kocherha Roman <f.your.d.2017@gmail.com>
2023-03-30 12:09:40 +0300

    done

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -31,15 +31,14 @@ export function sortStudents(
 
     function calcFunc(student): number {
       return student.grades
-        .reduce((sum, grade) => sum + grade, 0) / student.grades.length;
+        .reduce((sum: number, grade: number) => sum + grade, 0)
+        / student.grades.length;
     }
 
     switch (sortBy) {
       case SortType.Name:
-        result = firstStudent.name.localeCompare(secondStudent.name);
-        break;
       case SortType.Surname:
-        result = firstStudent.surname.localeCompare(secondStudent.surname);
+        result = firstStudent[sortBy].localeCompare(secondStudent[sortBy]);
         break;
       case SortType.Age:
         result = firstStudent.age - secondStudent.age;
