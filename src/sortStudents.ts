d6739391b3d20137c67868267de230284c5c59c3
Rostyslav Hirin <hirin.rostyslav@gmail.com>
2023-04-03 15:40:49 +0300

    Sorting function implemented

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,66 @@
 
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name,
+  Surname,
+  Age,
+  Married,
+  AverageGrade,
 }
 
 // create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(
+  students: object[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const sortedStudents = JSON.parse(JSON.stringify(students));
+  const orderDirection = order === 'asc' ? 1 : -1;
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  switch (sortBy) {
+    case SortType.Name:
+      return sortedStudents.sort((a: Student, b: Student) => (
+        orderDirection * a.name.localeCompare(b.name)
+      ));
+
+    case SortType.Surname:
+      return sortedStudents.sort((a: Student, b: Student) => (
+        orderDirection * a.surname.localeCompare(b.surname)
+      ));
+
+    case SortType.Age:
+      return sortedStudents.sort((a: Student, b: Student) => (
+        orderDirection * (a.age - b.age)
+      ));
+
+    case SortType.Married:
+      return sortedStudents.sort((a: Student, b: Student) => (
+        a.married > b.married ? orderDirection : orderDirection * -1
+      ));
+
+    case SortType.AverageGrade:
+      return sortedStudents.sort((a: Student, b: Student) => {
+        const averageGradesA: number = a.grades
+          .reduce((acc: number, grade: number) => acc + grade, 0)
+          / a.grades.length;
+
+        const averageGradesB: number = b.grades
+          .reduce((acc: number, grade: number) => acc + grade, 0)
+          / b.grades.length;
+
+        return orderDirection * (averageGradesA - averageGradesB);
+      });
+
+    default:
+      throw new Error('error');
+  }
 }
