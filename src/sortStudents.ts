7bb616eb3ab5b7b9c9ed9ff709b1562f22a844d8
Oleksandr Fenchuk <ofenchuk@proton.me>
2023-04-03 16:56:45 +0300

    sorting students

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,55 @@
-
 export interface Student {
-  // describe Student interface
+  // describe Student interface'
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[],
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
 // create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+function sorted(
+  studentArray: Student[],
+  sortBy: SortType,
+  order: string,
+): object[] {
+  return studentArray
+    .sort((student1: Student, student2: Student) => {
+      const output = typeof student1[sortBy] === 'string'
+        ? student1[sortBy].toString().localeCompare(student2[sortBy].toString())
+        : Number(student1[sortBy]) - Number(student2[sortBy]);
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+      return order === 'asc' ? output : -output;
+    });
+}
+
+export function sortStudents(
+  // eslint-disable-next-line @typescript-eslint/no-shadow
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): object[] {
+  if (sortBy === 'grades') {
+    const average = (array: number[]): number => {
+      return array.reduce((a: number, b: number) => a + b, 0) / array.length;
+    };
+
+    return students.sort((student1: Student, student2: Student) => {
+      const output = average(student1.grades) - average(student2.grades);
+
+      return order === 'asc' ? output : -output;
+    });
+  }
+
+  return sorted(students, sortBy, order);
 }
