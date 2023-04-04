9193be584aea7d85a53c118f4adf4bf600c575ac
Maksym Didyk <didyk.maksym@gmail.com>
2023-04-05 00:26:07 +0300

    Solution getStudent

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,79 @@
 
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[],
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name,
+  Surname,
+  Age,
+  Married,
+  AverageGrade,
 }
 
 // create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const copiedStudents: Student[] = [...students];
+  const sortOrder = order === 'asc' ? 1 : -1;
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  switch (sortBy) {
+    case SortType.Name:
+
+      return copiedStudents.sort(
+        (studentA: Student, studentB: Student) => {
+          return studentA.name.localeCompare(studentB.name) * sortOrder;
+        },
+      );
+
+    case SortType.Surname:
+
+      return copiedStudents.sort(
+        (studentA: Student, studentB: Student) => {
+          return studentA.surname.localeCompare(studentB.surname) * sortOrder;
+        },
+      );
+
+    case SortType.Age:
+
+      return copiedStudents.sort(
+        (studentA: Student, studentB: Student) => {
+          return (studentA.age - studentB.age) * sortOrder;
+        },
+      );
+
+    case SortType.Married:
+
+      return copiedStudents.sort(
+        (studentA: Student, studentB: Student) => {
+          return (Number(studentA.married) - Number(studentB.married))
+          * sortOrder;
+        },
+      );
+
+    case SortType.AverageGrade:
+
+      return copiedStudents.sort(
+        (studentA: Student, studentB: Student) => {
+          const avgA = studentA.grades.reduce((sum, grade) => sum + grade)
+          / studentA.grades.length;
+          const avgB = studentB.grades.reduce((sum, grade) => sum + grade)
+          / studentB.grades.length;
+
+          return (avgA - avgB) * sortOrder;
+        },
+      );
+
+    default:
+      throw new Error('Error');
+  }
 }
