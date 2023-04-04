153c0c50088f7ff2e2df438f00d78afd4964d8ba
EvgenBabin <ebabin000@gmail.com>
2023-04-04 13:50:10 +0300

    sortered students use TypeScript

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,84 @@
-
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name,
+  Surname,
+  Age,
+  Married,
+  AverageGrade,
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
+type Values = string | number | boolean;
 
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const compareFunction = (
+    firstPerson: Student,
+    secondPerson: Student,
+  ): number => {
+    let firstValue: Values;
+    let secondValue: Values;
+    let firstAverage: Values;
+    let secondAverage: Values;
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+    switch (sortBy) {
+      case SortType.Name:
+        firstValue = firstPerson.name;
+        secondValue = secondPerson.name;
+        break;
+
+      case SortType.Surname:
+        firstValue = firstPerson.surname;
+        secondValue = secondPerson.surname;
+        break;
+
+      case SortType.Age:
+        firstValue = firstPerson.age;
+        secondValue = secondPerson.age;
+        break;
+
+      case SortType.Married:
+        firstValue = firstPerson.married;
+        secondValue = secondPerson.married;
+        break;
+
+      case SortType.AverageGrade:
+        firstAverage = firstPerson.grades
+          .reduce((sum, grade) => sum + grade, 0) / firstPerson.grades.length;
+
+        secondAverage = secondPerson.grades
+          .reduce((sum, grade) => sum + grade, 0) / secondPerson.grades.length;
+
+        firstValue = firstAverage;
+        secondValue = secondAverage;
+        break;
+
+      default:
+        throw new Error(`Invalid SortType: ${sortBy}`);
+    }
+
+    if (firstValue < secondValue) {
+      return order === 'asc' ? -1 : 1;
+    }
+
+    if (firstValue > secondValue) {
+      return order === 'asc' ? 1 : -1;
+    }
+
+    return students.indexOf(firstPerson) - students.indexOf(secondPerson);
+  };
+
+  const sortedStudents = [...students].sort(compareFunction);
+
+  return sortedStudents;
 }
