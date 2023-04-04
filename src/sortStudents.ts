c38df5f306c2ad7863f694c104934d2b34a8f62b
glots-a <glots@ukr.net>
2023-04-04 13:38:27 +0300

    solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,55 @@
 
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades'
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  return [...students].sort((a: Student, b: Student) => {
+    if (typeof a[sortBy] === 'string') {
+      if (order === 'desc') {
+        return b[sortBy].toString().localeCompare(a[sortBy].toString());
+      }
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+      return a[sortBy].toString().localeCompare(b[sortBy].toString());
+    }
+
+    if (sortBy === 'grades') {
+      const quantityA: number = a.grades.length;
+      const quantityB: number = b.grades.length;
+
+      const averageA: number = a.grades
+        .reduce((acum, el) => (acum + el), 0) / quantityA;
+
+      const averageB: number = b.grades
+        .reduce((acum, el) => (acum + el), 0) / quantityB;
+
+      if (order === 'desc') {
+        return averageB - averageA;
+      }
+
+      return averageA - averageB;
+    }
+
+    return order === 'desc'
+      ? Number(b[sortBy]) - Number(a[sortBy])
+      : Number(a[sortBy]) - Number(b[sortBy]);
+  });
 }
