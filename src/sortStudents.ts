c5d3201d4589e43bdabff65ab558e733485f24b7
Andrew Litkovets <andriylao@gmail.com>
2023-04-04 18:09:06 +0300

    task done

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,75 @@
 
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[],
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name,
+  Surname,
+  Age,
+  Married,
+  AverageGrade,
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+function average(grades: number[]): number {
+  return grades.reduce((sum, el) => sum + el, 0) / grades.length;
+}
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const sortedStudents = [...students];
+
+  sortedStudents.sort((a, b) => {
+    let aValue;
+    let bValue;
+
+    switch (sortBy) {
+      case SortType.Name:
+        aValue = a.name;
+        bValue = b.name;
+        break;
+
+      case SortType.Surname:
+        aValue = a.surname;
+        bValue = b.surname;
+        break;
+
+      case SortType.Age:
+        aValue = a.age;
+        bValue = b.age;
+        break;
+
+      case SortType.Married:
+        aValue = a.married;
+        bValue = b.married;
+        break;
+
+      case SortType.AverageGrade:
+        aValue = average(a.grades);
+        bValue = average(b.grades);
+        break;
+
+      default:
+        throw new Error(`Invalid SortType '${sortBy}'`);
+    }
+
+    if (aValue === bValue) {
+      return 0;
+    }
+
+    const result = aValue > bValue ? 1 : -1;
+
+    return order === 'asc' ? result : -result;
+  });
+
+  return sortedStudents;
 }
