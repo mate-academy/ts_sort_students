97401c102e96d8af2af5f15a8f1c96764af6e144
Hamuud <artemlask2014@gmail.com>
2023-04-04 19:59:26 +0300

    Big red lines... Create sort for students

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,80 @@
 
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name,
+  Surname,
+  Age,
+  Married,
+  AverageGrade,
 }
 
 // create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: string,
+): Student[] {
+  const copyStudents: Student[] = [...students];
+  const sortByOrder = order === 'asc'
+    ? 1
+    : -1;
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  type Sum = (sum: number, mark: number) => number;
+  type Average = (grades: number[]) => number;
+
+  const sumMarks: Sum = (sum, mark) => sum + mark;
+  const avgMark: Average = (grades) => (
+    grades.reduce(sumMarks, 0) / grades.length
+  );
+
+  switch (sortBy) {
+    case SortType.Name:
+      copyStudents.sort((
+        { name: personA },
+        { name: personB },
+      ) => personA.localeCompare(personB) * sortByOrder);
+      break;
+
+    case SortType.Surname:
+      copyStudents.sort((
+        { surname: personA },
+        { surname: personB },
+      ) => personA.localeCompare(personB) * sortByOrder);
+      break;
+
+    case SortType.Age:
+      copyStudents.sort((
+        { age: personA },
+        { age: personB },
+      ) => (personA - personB) * sortByOrder);
+      break;
+
+    case SortType.Married:
+      copyStudents.sort((
+        { married: personA },
+        { married: personB },
+      ) => (Number(personA) - Number(personB)) * sortByOrder);
+      break;
+
+    case SortType.AverageGrade:
+      copyStudents.sort((
+        { grades: personA },
+        { grades: personB },
+      ) => (avgMark(personA) - avgMark(personB)) * sortByOrder);
+      break;
+
+    default:
+      throw new Error('Some value is wrong');
+  }
+
+  return copyStudents;
 }
