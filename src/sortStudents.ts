de471e3bafca20dd275448d392ecdb4cdcd9a5d9
Oleh Kolodii <bigknife4@gmail.com>
2023-04-04 18:43:30 +0300

    fixes after student review

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -28,31 +28,19 @@ export function sortStudents(
     case 'asc':
       switch (sortBy) {
         case (SortType.Name):
-          newStudentsArray.sort((
-            student1: Student,
-            student2: Student,
-          ) => student1.name.localeCompare(student2.name));
-          break;
-
         case (SortType.Surname):
           newStudentsArray.sort((
             student1: Student,
             student2: Student,
-          ) => student1.surname.localeCompare(student2.surname));
+          ) => student1[sortBy].localeCompare(student2[sortBy]));
           break;
 
+        case (SortType.Married):
         case (SortType.Age):
           newStudentsArray.sort((
             student1: Student,
             student2: Student,
-          ) => student1.age - student2.age);
-          break;
-
-        case (SortType.Married):
-          newStudentsArray.sort((
-            student1: Student,
-            student2: Student,
-          ) => Number(student1.married) - Number(student2.married));
+          ) => Number(student1[sortBy]) - Number(student2[sortBy]));
           break;
 
         case (SortType.AverageGrade):
@@ -79,31 +67,19 @@ export function sortStudents(
     case 'desc':
       switch (sortBy) {
         case (SortType.Name):
-          newStudentsArray.sort((
-            student1: Student,
-            student2: Student,
-          ) => student2.name.localeCompare(student1.name));
-          break;
-
         case (SortType.Surname):
           newStudentsArray.sort((
             student1: Student,
             student2: Student,
-          ) => student2.surname.localeCompare(student1.surname));
+          ) => student2[sortBy].localeCompare(student1[sortBy]));
           break;
 
+        case (SortType.Married):
         case (SortType.Age):
           newStudentsArray.sort((
             student1: Student,
             student2: Student,
-          ) => student2.age - student1.age);
-          break;
-
-        case (SortType.Married):
-          newStudentsArray.sort((
-            student1: Student,
-            student2: Student,
-          ) => Number(student2.married) - Number(student1.married));
+          ) => Number(student2[sortBy]) - Number(student1[sortBy]));
           break;
 
         case (SortType.AverageGrade):
@@ -112,6 +88,7 @@ export function sortStudents(
               acc: number,
               val: number,
             ) => acc + val, 0) / student1.grades.length;
+
             const student2AverageGrade: number = student2.grades.reduce((
               acc: number,
               val: number,
