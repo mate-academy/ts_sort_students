60bdb0d19e4d638614eeb0ed9ab6b6f9988da6f4
Anna Kuzmenko <mogilevs.anna@gmail.com>
2023-04-04 20:27:33 +0300

    Solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,63 @@
 
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+function countAvg(array: number[]): number {
+  return array.reduce((count: number, num: number) => count + num, 0)
+  / array.length;
+}
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const newStudents: Student[] = [...students];
+
+  newStudents.sort((student1: Student, student2: Student) => {
+    switch (sortBy) {
+      case SortType.Name:
+      case SortType.Surname:
+        if (order === 'asc') {
+          return student1[sortBy].localeCompare(student2[sortBy]);
+        }
+
+        return student2[sortBy].localeCompare(student1[sortBy]);
+
+      case SortType.Age:
+      case SortType.Married:
+        if (order === 'asc') {
+          return Number(student1[sortBy]) - Number(student2[sortBy]);
+        }
+
+        return Number(student2[sortBy]) - Number(student1[sortBy]);
+
+      case SortType.AverageGrade:
+        if (order === 'asc') {
+          return countAvg(student1[sortBy]) - countAvg(student2[sortBy]);
+        }
+
+        return countAvg(student2[sortBy]) - countAvg(student1[sortBy]);
+
+      default:
+        throw new Error('error');
+    }
+  });
+
+  return newStudents;
 }
