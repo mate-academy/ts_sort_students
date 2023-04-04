f2bdec2b15377c123cd726675fb1262e502b6b79
Myla Crystal <code.crystal.008@gmail.com>
2023-04-04 23:17:49 +0300

    Solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,61 @@
 
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[],
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+function countAverage(grades: number[]): number {
+  return grades.reduce((sum, grade) => sum + grade, 0) / grades.length;
+}
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const copiedStudents = [...students];
+
+  switch (sortBy) {
+    case SortType.Name:
+    case SortType.Surname:
+      return copiedStudents
+        .sort((studentA, studentB) => (
+          order === 'asc'
+            ? studentA[sortBy].localeCompare(studentB[sortBy])
+            : studentB[sortBy].localeCompare(studentA[sortBy])
+        ));
+
+    case SortType.Age:
+    case SortType.Married:
+      return copiedStudents
+        .sort((studentA, studentB) => (
+          order === 'asc'
+            ? Number(studentA[sortBy]) - Number(studentB[sortBy])
+            : Number(studentB[sortBy]) - Number(studentA[sortBy])
+        ));
+
+    case SortType.AverageGrade:
+      return copiedStudents
+        .sort((studentA, studentB) => (
+          order === 'asc'
+            ? countAverage(studentA[sortBy]) - countAverage(studentB[sortBy])
+            : countAverage(studentB[sortBy]) - countAverage(studentA[sortBy])
+        ));
+
+    default:
+      throw new Error('Error. Check your input data types.');
+  }
 }
