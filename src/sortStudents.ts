fbb289795a67b15b7f44170bc62f758ff48373bf
Mykola Chernysh <nanoteck7@gmail.com>
2023-04-05 18:02:17 +0300

    Solution sortStudents

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,57 @@
 
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name,
+  Surname,
+  Age,
+  Married,
+  AverageGrade
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+export function sortStudents(students: Student[], sortBy: SortType, order: SortOrder): Student[] {
+  const studentstArray: Student[] = [...students];
+
+  switch(sortBy) {
+    case SortType.Name:
+    case SortType.Surname:
+      return studentstArray.sort((firstStudent: Student, secondStudents: Student): number => (
+        order === 'asc'
+          ? firstStudent[sortBy].localeCompare(secondStudents[sortBy])
+          : secondStudents[sortBy].localeCompare(firstStudent[sortBy])
+        )
+      );
+
+    case SortType.Age:
+    case SortType.AverageGrade:
+      return studentstArray.sort((firstStudent: Student, secondStudents: Student): number => (
+        order === 'asc'
+          ? +firstStudent[sortBy] - secondStudents[sortBy]
+          : +secondStudents[sortBy] - firstStudent[sortBy]
+        )
+      );
+
+      case SortType.AverageGrade:
+        studentstArray.sort(
+          (order === 'asc'
+            ? (firstStudent: Student, secondStudents: Student): number => getAverage(firstStudent[sortBy]) - getAverage(secondStudents[sortBy])
+            : (firstStudent: Student, secondStudents: Student): number => getAverage(secondStudents[sortBy]) - getAverage(firstStudent[sortBy]))
+          );
+
+      default:
+        break;
+  }
+}
+
+function getAverage(grades: number[]): number {
+  return grades.reduce((prev: number, cur: number) => prev + cur, 0) / grades.length;
 }
