bf88b0a09605b38e3decd8fa906d70d1b5370b74
Goto-Light <vadim.tkachenko.1997@gmail.com>
2023-04-05 16:56:46 +0300

    solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,74 @@
-
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name,
+  Surname,
+  Age,
+  Married,
+  AverageGrade,
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const copierStudents = [...students];
+  const sortOrder = order === 'asc' ? 1 : -1;
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  type Callback = (prev: number, cur: number) => number;
+
+  const sumCallback: Callback = (prev, cur) => prev + cur;
+
+  switch (sortBy) {
+    case SortType.Name:
+      copierStudents.sort(
+        (a: Student, b: Student) => a.name.localeCompare(b.name) * sortOrder,
+      );
+      break;
+
+    case SortType.Surname:
+      copierStudents.sort(
+        (a: Student, b: Student) => a.surname.localeCompare(b.surname)
+        * sortOrder,
+      );
+      break;
+
+    case SortType.Age:
+      copierStudents.sort(
+        (a: Student, b: Student) => (a.age - b.age) * sortOrder,
+      );
+      break;
+
+    case SortType.Married:
+      copierStudents.sort(
+        (a: Student, b: Student) => ((a.married > b.married)
+          ? sortOrder : -sortOrder),
+      );
+      break;
+
+    case SortType.AverageGrade:
+      copierStudents.sort(
+        (a: Student, b: Student) => {
+          const aGrade = a.grades.reduce(sumCallback) / a.grades.length;
+          const bGrade = b.grades.reduce(sumCallback) / b.grades.length;
+
+          return (aGrade - bGrade) * sortOrder;
+        },
+      );
+      break;
+
+    default:
+      throw new Error(`Not correct type for sort: '${sortBy}'`);
+  }
+
+  return copierStudents;
 }
