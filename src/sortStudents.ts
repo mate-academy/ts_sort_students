1ce5e804e41c3dfa086bbb8da8db0fe4fce3ed33
Ivan Tkachenko <ivantkachenko3301@gmail.com>
2023-04-05 09:59:38 +0300

    remove --  (eslint-disable-next-line max-len)

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -28,38 +28,40 @@ export function sortStudents(
     case SortType.Name:
 
       sortedStudents.sort(
-        // eslint-disable-next-line max-len
-        (firstStudent: Student, secondStudent: Student) => firstStudent.name.localeCompare(secondStudent.name),
+        (firstStudent: Student, secondStudent: Student) => (
+          firstStudent.name.localeCompare(secondStudent.name)
+        ),
       );
       break;
 
     case SortType.Surname:
       return sortedStudents.sort(
-        // eslint-disable-next-line max-len
-        (firstStudent: Student, secondStudent: Student) => firstStudent.surname.localeCompare(secondStudent.surname),
+        (firstStudent: Student, secondStudent: Student) => (
+          firstStudent.surname.localeCompare(secondStudent.surname)
+        ),
       );
 
     case SortType.Age:
       return sortedStudents.sort(
-        // eslint-disable-next-line max-len
-        (firstStudent: Student, secondStudent: Student) => sortDirection * (firstStudent.age - secondStudent.age),
+        (firstStudent: Student, secondStudent: Student) => sortDirection * (
+          firstStudent.age - secondStudent.age),
       );
 
     case SortType.Married:
       sortedStudents.sort((firstStudent: Student, secondStudent: Student) => (
-        // eslint-disable-next-line max-len
-        firstStudent.married > secondStudent.married ? sortDirection : sortDirection * -1
+        firstStudent.married > secondStudent.married
+          ? sortDirection : sortDirection * -1
       ));
       break;
 
     case SortType.AverageGrade:
       sortedStudents.sort((firstStudent: Student, secondStudent: Student) => {
-        // eslint-disable-next-line max-len
-        const avgFirstStudent = firstStudent.grades.reduce((acc, curr) => acc + curr, 0)
-          / firstStudent.grades.length;
-        // eslint-disable-next-line max-len
-        const avgSecondStudent = secondStudent.grades.reduce((acc, curr) => acc + curr, 0)
-          / secondStudent.grades.length;
+        const avgFirstStudent = firstStudent.grades.reduce(
+          (acc, curr) => acc + curr, 0,
+        ) / firstStudent.grades.length;
+        const avgSecondStudent = secondStudent.grades.reduce(
+          (acc, curr) => acc + curr, 0,
+        ) / secondStudent.grades.length;
 
         return sortDirection * (avgFirstStudent - avgSecondStudent);
       });
