a10476302d9d0c3a0d3ecacf4e6776ccd7dda28a
Kyryl <kika_domination@yahoo.com>
2023-04-05 12:33:05 +0300

    make my code shorter, add function for average, change + to Number

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,4 +1,3 @@
-
 export interface Student {
   name: string;
   surname: string;
@@ -15,8 +14,24 @@ export enum SortType {
   AverageGrade = 'averageGrade',
 }
 
-// create SortOrder type
 export type SortOrder = 'asc' | 'desc';
+
+function getAverage(students: Student[], order: SortOrder): Student[] {
+  return students.sort((
+    prev: Student, current: Student,
+  ) => {
+    const prevAverageGrades: number = prev.grades.reduce((
+      gradesAcc: number, grade: number,
+    ) => gradesAcc + grade, 0) / prev.grades.length;
+    const currentAverageGrades: number = current.grades.reduce((
+      gradesAcc: number, grade: number,
+    ) => gradesAcc + grade, 0) / current.grades.length;
+
+    return order === 'asc'
+      ? prevAverageGrades - currentAverageGrades
+      : currentAverageGrades - prevAverageGrades;
+  });
+}
 
 export function sortStudents(
   students: Student[],
@@ -25,97 +40,30 @@ export function sortStudents(
 ): Student[] {
   const copiedStudents = [...students];
 
-  if (order === 'asc') {
-    switch (sortBy) {
-      case SortType.Name:
-        copiedStudents.sort((
-          prev: Student, current: Student,
-        ) => prev.name.localeCompare(current.name));
-        break;
+  switch (sortBy) {
+    case SortType.Name:
+    case SortType.Surname:
+      return copiedStudents.sort((
+        prev: Student, current: Student,
+      ) => (
+        order === 'asc'
+          ? prev[sortBy].localeCompare(current[sortBy])
+          : current[sortBy].localeCompare(prev[sortBy])
+      ));
 
-      case SortType.Surname:
-        copiedStudents.sort((
-          prev: Student, current: Student,
-        ) => prev.surname.localeCompare(current.surname));
-        break;
+    case SortType.Age:
+    case SortType.Married:
+      return copiedStudents.sort((
+        prev: Student, current: Student,
+      ) => (order === 'asc'
+        ? Number(prev[sortBy]) - Number(current[sortBy])
+        : Number(current[sortBy]) - Number(prev[sortBy])
+      ));
 
-      case SortType.Age:
-        copiedStudents.sort((
-          prev: Student, current: Student,
-        ) => prev.age - current.age);
-        break;
+    case SortType.AverageGrade:
+      return getAverage(copiedStudents, order);
 
-      case SortType.Married:
-        copiedStudents.sort((
-          prev: Student, current: Student,
-        ) => (+prev.married) - (+current.married));
-        break;
-
-      case SortType.AverageGrade:
-        copiedStudents.sort((
-          prev: Student, current: Student,
-        ) => {
-          const prevAverageGrades: number = prev.grades.reduce((
-            gradesAcc: number, grade: number,
-          ) => gradesAcc + grade, 0) / prev.grades.length;
-          const currentAverageGrades: number = current.grades.reduce((
-            gradesAcc: number, grade: number,
-          ) => gradesAcc + grade, 0) / current.grades.length;
-
-          return prevAverageGrades - currentAverageGrades;
-        });
-        break;
-
-      default:
-        throw new Error(`Wrong value ${order} for sorting`);
-    }
+    default:
+      throw new Error(`Wrong value ${order} for sorting`);
   }
-
-  if (order === 'desc') {
-    switch (sortBy) {
-      case SortType.Name:
-        copiedStudents.sort((
-          prev: Student, current: Student,
-        ) => current.name.localeCompare(prev.name));
-        break;
-
-      case SortType.Surname:
-        copiedStudents.sort((
-          prev: Student, current: Student,
-        ) => current.surname.localeCompare(prev.surname));
-        break;
-
-      case SortType.Age:
-        copiedStudents.sort((
-          prev: Student, current: Student,
-        ) => current.age - prev.age);
-        break;
-
-      case SortType.Married:
-        copiedStudents.sort((
-          prev: Student, current: Student,
-        ) => (+current.married) - (+prev.married));
-        break;
-
-      case SortType.AverageGrade:
-        copiedStudents.sort((
-          prev: Student, current: Student,
-        ) => {
-          const prevAverageGrades: number = prev.grades.reduce((
-            gradesAcc: number, grade: number,
-          ) => gradesAcc + grade, 0) / prev.grades.length;
-          const currentAverageGrades: number = current.grades.reduce((
-            gradesAcc: number, grade: number,
-          ) => gradesAcc + grade, 0) / current.grades.length;
-
-          return currentAverageGrades - prevAverageGrades;
-        });
-        break;
-
-      default:
-        throw new Error(`Wrong value ${order} for sorting`);
-    }
-  }
-
-  return copiedStudents;
 }
