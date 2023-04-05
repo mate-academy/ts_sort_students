32c9807c6c70df0e6fe436ab575e0a11159480f8
Nikita Zadorozhnyi <nikita.zadorozhnyi@nure.ua>
2023-04-05 12:08:51 +0300

    Solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -7,11 +7,11 @@ export interface Student {
 }
 
 export enum SortType {
-  Name,
-  Surname,
-  Age,
-  Married,
-  AverageGrade,
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
 export type SortOrder = 'asc' | 'desc';
@@ -23,24 +23,24 @@ export function sortStudents(
 ): Student[] {
   const callback = (a: Student, b:Student): number => {
     let result: number = 0;
-    const avgA = a.grades.reduce((s, gr) => s + gr, 0) / a.grades.length;
-    const avgB = b.grades.reduce((s, gr) => s + gr, 0) / b.grades.length;
+
+    const avgA = a.grades.reduce(
+      (sum, grade) => sum + grade, 0,
+    ) / a.grades.length;
+
+    const avgB = b.grades.reduce(
+      (sum, grade) => sum + grade, 0,
+    ) / b.grades.length;
 
     switch (sortBy) {
       case SortType.Name:
-        result = a.name.localeCompare(b.name);
-        break;
-
       case SortType.Surname:
-        result = a.surname.localeCompare(b.surname);
+        result = a[sortBy].localeCompare(b[sortBy]);
         break;
 
       case SortType.Age:
-        result = a.age - b.age;
-        break;
-
       case SortType.Married:
-        result = Number(a.married) - Number(b.married);
+        result = Number(a[sortBy]) - Number(b[sortBy]);
         break;
 
       case SortType.AverageGrade:
