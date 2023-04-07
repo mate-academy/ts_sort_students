df1ec0065dd2fc5f8489e1becb2f7a00fd8d8d32
DanielSorokowski <sorokowskidaniel0@gmail.com>
2023-04-07 12:27:53 +0200

    solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,71 @@
 
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name,
+  Surname,
+  Age,
+  Married,
+  AverageGrade,
 }
 
 // create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const cloneStudens = [...students];
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  switch (sortBy) {
+    case SortType.Name: {
+      cloneStudens.sort((a, b) => a.name.localeCompare(b.name));
+      break;
+    }
+
+    case SortType.Surname: {
+      cloneStudens.sort((a, b) => a.surname.localeCompare(b.surname));
+      break;
+    }
+
+    case SortType.Age: {
+      cloneStudens.sort((a, b) => b.age - a.age);
+      break;
+    }
+
+    case SortType.Married: {
+      cloneStudens.sort((a, b) => Number(b.married) - Number(a.married));
+      break;
+    }
+
+    case SortType.AverageGrade: {
+      /* eslint-disable max-len */
+      switch (order) {
+        case 'asc': {
+          cloneStudens.sort((a, b) => (a.grades.reduce((sum, num) => sum + num, 0) / a.grades.length) - (b.grades.reduce((sum, num) => sum + num, 0) / b.grades.length));
+          break;
+        }
+
+        case 'desc': {
+          cloneStudens.sort((a, b) => (b.grades.reduce((sum, num) => sum + num, 0) / b.grades.length) - (a.grades.reduce((sum, num) => sum + num, 0) / a.grades.length));
+          break;
+        }
+
+        default:
+      }
+      break;
+    }
+
+    default:
+  }
+
+  return cloneStudens;
 }
