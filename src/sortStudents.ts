ff03fd58b71b2080d8898b99930b9e8693ad240d
Alex Rog <sasharg303@gmail.com>
2023-04-07 14:44:11 +0200

    WorkedVersion

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,56 @@
-
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[],
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(students: Student[], sortBy: SortType, order: SortOrder): Student[] { // eslint-disable-line
+  const studentsRes = JSON.parse(JSON.stringify(students));
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  switch (sortBy) {
+    case 'name' || 'surname':
+      return studentsRes.sort((student1: Student, student2: Student) => {
+        if (order === 'asc') {
+          return student1[sortBy].localeCompare(student2[sortBy]);
+        }
+
+        return student2[sortBy].localeCompare(student1[sortBy]);
+      });
+
+    case 'age' || 'married':
+      return studentsRes.sort((student1: Student, student2: Student) => {
+        if (order === 'asc') {
+          return student1[sortBy] - student2[sortBy];
+        }
+
+        return student2[sortBy] - student1[sortBy];
+      });
+
+    case 'grades':
+      return studentsRes.sort((student1: Student, student2: Student) => {
+        const firstAvarage = student1[sortBy].reduce((elem1, elem2) => elem1 + elem2) / student1[sortBy].length; // eslint-disable-line
+        const secondAvarage = student2[sortBy].reduce((elem1, elem2) => elem1 + elem2) / student2[sortBy].length; // eslint-disable-line
+
+        if (order === 'asc') {
+          return firstAvarage - secondAvarage;
+        }
+
+        return secondAvarage - firstAvarage;
+      });
+
+    default:
+      return studentsRes;
+  }
 }
