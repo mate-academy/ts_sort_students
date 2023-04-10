3a3b6075440abae71039df01ff45df6d047d4bf1
Serhii Melnychenko <8melya8@gmail.com>
2023-04-10 17:55:12 +0300

    Solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,58 @@
 
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[],
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'averageGrade',
 }
 
 // create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const copystydent = [...students];
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  const AvgGrades = (grade: Student): number => {
+    return grade.grades.reduce(
+      (sum: number, num: number) => sum + num, 0,
+    )
+      / grade.grades.length;
+  };
+
+  return copystydent.sort((a: Student, b: Student) => {
+    switch (sortBy) {
+      case SortType.Name:
+      case SortType.Surname:
+      case SortType.Married:
+        return order === 'asc'
+          ? a[sortBy].toString().localeCompare(b[sortBy].toString())
+          : b[sortBy].toString().localeCompare(a[sortBy].toString());
+
+      case SortType.Age:
+        return order === 'asc'
+          ? a.age - b.age
+          : b.age - a.age;
+
+      case SortType.AverageGrade:
+        return order === 'asc'
+          ? AvgGrades(a) - AvgGrades(b)
+          : AvgGrades(b) - AvgGrades(a);
+
+      default:
+        throw new Error('Error');
+    }
+  });
 }
