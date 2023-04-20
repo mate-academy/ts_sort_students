ab1fae6fa90e99efbeb8e8954584ba8770d59161
Natali <swetswet1234@gmail.com>
2023-04-20 21:00:27 +0300

    develop

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,82 @@
 
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[],
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name,
+  Surname,
+  Age,
+  Married,
+  AverageGrade,
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(students: Student[], sortBy: SortType,
+  order: SortOrder): Student[] {
+  const newArray = [...students];
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  switch (sortBy) {
+    case SortType.Name:
+      if (order === 'asc') {
+        newArray.sort((a, b) => a.name.localeCompare(b.name));
+      } else {
+        newArray.sort((a, b) => b.name.localeCompare(a.name));
+      }
+      break;
+
+    case SortType.Surname:
+      if (order === 'asc') {
+        newArray.sort((a, b) => a.surname.localeCompare(b.surname));
+      } else {
+        newArray.sort((a, b) => b.surname.localeCompare(a.surname));
+      }
+      break;
+
+    case SortType.Age:
+      if (order === 'asc') {
+        newArray.sort((a, b) => a.age - b.age);
+      } else {
+        newArray.sort((a, b) => b.age - a.age);
+      }
+      break;
+
+    case SortType.Married:
+      if (order === 'asc') {
+        newArray.sort((a, b) => Number(a.married) - Number(b.married));
+      } else {
+        newArray.sort((a, b) => Number(b.married) - Number(a.married));
+      }
+      break;
+
+    case SortType.AverageGrade:
+      if (order === 'asc') {
+        newArray.sort((a, b) => {
+          const averegePrev = a.grades.reduce((sum, x) => sum + x, 0)
+            / a.grades.length;
+          const averegeNext = b.grades.reduce((sum, x) => sum + x, 0)
+            / b.grades.length;
+
+          return averegePrev - averegeNext;
+        });
+      } else {
+        newArray.sort((a, b) => {
+          const averegePrev = a.grades.reduce((sum, x) => sum + x, 0)
+            / a.grades.length;
+          const averegeNext = b.grades.reduce((sum, x) => sum + x, 0)
+            / b.grades.length;
+
+          return averegeNext - averegePrev;
+        });
+      }
+      break;
+    default:
+  }
+
+  return newArray;
 }
