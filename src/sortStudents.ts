d88cc7bb413995ad3ceb209ae80013909010bd49
Amadeusz Michalowski <amadeusz.michalowski@gmail.com>
2023-04-20 19:31:55 +0200

    solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,99 @@
 
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: Array<number>,
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name,
+  Surname,
+  Age,
+  Married,
+  AverageGrade,
 }
 
 // create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+function getAverageGrade(student: Student): number {
+  return student.grades.reduce(
+    (acc, grade) => acc + grade, 0,
+  ) / student.grades.length;
+}
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Array<object> | undefined {
+  const newStudentArray = [...students];
+
+  if (sortBy === SortType.Name) {
+    return order === 'asc'
+      ? newStudentArray.sort(
+        (student, nextStudent) => student.name.localeCompare(nextStudent.name),
+      )
+      : newStudentArray.sort(
+        (student, nextStudent) => nextStudent.name.localeCompare(student.name),
+      );
+  }
+
+  if (sortBy === SortType.Surname) {
+    return order === 'asc'
+      ? newStudentArray.sort((
+        student,
+        nextStudent,
+      ) => student.surname.localeCompare(nextStudent.surname))
+      : newStudentArray.sort((
+        student,
+        nextStudent,
+      ) => nextStudent.surname.localeCompare(student.surname));
+  }
+
+  if (sortBy === SortType.Age) {
+    return order === 'asc'
+      ? newStudentArray.sort(
+        (student, nextStudent) => student.age - nextStudent.age,
+      )
+      : newStudentArray.sort(
+        (student, nextStudent) => nextStudent.age - student.age,
+      );
+  }
+
+  if (sortBy === SortType.Married) {
+    return order === 'asc'
+      ? newStudentArray.sort(
+        (
+          student,
+          nextStudent,
+        ) => Number(student.married) - Number(nextStudent.married),
+      )
+      : newStudentArray.sort(
+        (
+          student,
+          nextStudent,
+        ) => Number(nextStudent.married) - Number(student.married),
+      );
+  }
+
+  if (sortBy === SortType.AverageGrade) {
+    return order === 'asc'
+      ? newStudentArray.sort(
+        (
+          student,
+          nextStudent,
+        ) => getAverageGrade(student) - getAverageGrade(nextStudent),
+      )
+      : newStudentArray.sort(
+        (
+          student,
+          nextStudent,
+        ) => getAverageGrade(nextStudent) - getAverageGrade(student),
+      );
+  }
+
+  return undefined;
 }
