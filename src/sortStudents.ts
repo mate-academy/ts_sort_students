469996452382e1607895c0c9e2a8a69256c0718e
AnastassiaH <nastyagurska@gmail.com>
2023-04-21 21:43:47 +0300

    solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,72 @@
 
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[],
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name,
+  Surname,
+  Age,
+  Married,
+  AverageGrade,
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const copy = students.map((item) => {
+    const temp = { ...item };
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+    return temp;
+  });
+
+  const result = copy.sort((a: Student, b: Student): number => {
+    let x = { ...a };
+    let y = { ...b };
+
+    if (order === 'desc') {
+      const temp = { ...a };
+
+      x = { ...b };
+      y = { ...temp };
+    }
+
+    function compareString(str1: string, str2: string): number {
+      if (str1 > str2) {
+        return 1;
+      }
+
+      if (str1 < str2) {
+        return -1;
+      }
+
+      return 0;
+    }
+
+    switch (sortBy) {
+      case SortType.Name:
+        return compareString(x.name, y.name);
+      case SortType.Surname:
+        return compareString(x.surname, y.surname);
+      case SortType.Age:
+        return x.age - y.age;
+      case SortType.AverageGrade:
+        return x.grades.reduce((acc, item) => acc + item, 0) / x.grades.length
+            - y.grades.reduce((acc, item) => acc + item, 0) / y.grades.length;
+      case SortType.Married:
+        return +(+x.married - +y.married);
+      default:
+        return 0;
+    }
+  });
+
+  return result;
 }
