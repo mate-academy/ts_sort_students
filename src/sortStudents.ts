1a5026f9b2927b33d12af3e2e46b48915bd64e77
sylcym <prusak.syl@gmail.com>
2023-04-22 12:11:05 +0200

    solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,79 @@
-
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name,
+  Surname,
+  Age,
+  Married,
+  AverageGrade,
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(students: Student[],
+  sortBy: SortType,
+  order: SortOrder): Student[] {
+  const copyStudent = [...students];
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  switch (sortBy) {
+    case SortType.Name: {
+      copyStudent.sort((a, b) => {
+        return order === 'asc'
+          ? a.name.localeCompare(b.name)
+          : b.name.localeCompare(a.name);
+      });
+      break;
+    }
+
+    case SortType.Surname: {
+      copyStudent.sort((a, b) => {
+        return order === 'asc'
+          ? a.surname.localeCompare(b.surname)
+          : b.surname.localeCompare(a.surname);
+      });
+      break;
+    }
+
+    case SortType.Age: {
+      copyStudent.sort((a, b) => {
+        return order === 'asc'
+          ? a.age - b.age
+          : b.age - a.age;
+      });
+      break;
+    }
+
+    case SortType.Married: {
+      copyStudent.sort((a, b) => {
+        return order === 'asc'
+          ? +(a.married) - +(b.married)
+          : +(b.married) - +(a.married);
+      });
+      break;
+    }
+
+    case SortType.AverageGrade: {
+      copyStudent.sort((a, b) => {
+        const averageA = a.grades.reduce((p, c) => p + c, 0) / a.grades.length;
+        const averageB = b.grades.reduce((p, c) => p + c, 0) / b.grades.length;
+
+        return order === 'asc'
+          ? averageA - averageB
+          : averageB - averageA;
+      });
+      break;
+    }
+
+    default: {
+      throw new Error('Error');
+    }
+  }
+
+  return copyStudent;
 }
