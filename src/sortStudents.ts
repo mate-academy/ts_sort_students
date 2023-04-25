25a60ec6bd2f383dc0af40b35ca234902de0787a
Barbara Jeziorek <barbarajeziorek@gmail.com>
2023-04-25 12:53:54 +0200

    fixes

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -7,11 +7,11 @@ export interface Student {
 }
 
 export enum SortType {
-  Name,
-  Surname,
-  Age,
-  Married,
-  AverageGrade
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades'
 }
 
 export type SortOrder = 'asc' | 'desc';
@@ -21,72 +21,82 @@ export function sortStudents(
   sortBy: SortType,
   order: SortOrder,
 ): Student[] {
-  const copyStudents = students.map((el: Student) => ({ ...el }));
+  const copyStudents = [...students];
+
+  const field = sortBy;
 
   switch (sortBy) {
     case SortType.Name:
       return (copyStudents
         .sort((
-          student1: Student,
-          student2: Student,
+          firstStudent: Student,
+          secondStudent: Student,
         ) => {
           return order === 'asc'
-            ? student1.name.localeCompare(student2.name)
-            : +!student1.name.localeCompare(student2.name);
+            ? firstStudent.name.localeCompare(secondStudent.name)
+            : Number(!firstStudent.name.localeCompare(secondStudent.name));
         }));
 
     case SortType.Surname:
       return (copyStudents
         .sort((
-          student1: Student,
-          student2: Student,
+          firstStudent,
+          secondStudent,
         ) => {
-          return order === 'asc'
-            ? student1.surname.localeCompare(student2.surname)
-            : +!student1.surname.localeCompare(student2.surname);
+          const aValue = firstStudent[field];
+          const bValue = secondStudent[field];
+
+          if (typeof aValue === 'string' && typeof bValue === 'string') {
+            return order === 'asc'
+              ? aValue.localeCompare(bValue)
+              : Number(!aValue.localeCompare(bValue));
+          }
+
+          return 0;
         }));
 
     case SortType.Age:
       return (copyStudents
         .sort((
-          student1: Student,
-          student2: Student,
+          firstStudent: Student,
+          secondStudent: Student,
         ) => {
           return order === 'asc'
-            ? student1.age - student2.age
-            : student2.age - student1.age;
+            ? firstStudent.age - secondStudent.age
+            : secondStudent.age - firstStudent.age;
         }));
 
     case SortType.Married:
       return (copyStudents
         .sort((
-          student1: Student,
-          student2: Student,
+          firstStudent: Student,
+          secondStudent: Student,
         ) => {
           return order === 'asc'
-            ? +student1.married - +student2.married
-            : +student2.married - +student1.married;
+            ? Number(firstStudent.married) - Number(secondStudent.married)
+            : Number(secondStudent.married) - Number(firstStudent.married);
         }));
 
     case SortType.AverageGrade:
       return (copyStudents
         .sort((
-          student1: Student,
-          student2: Student,
+          firstStudent: Student,
+          secondStudent: Student,
         ) => {
-          const student1Avarage = student1
-            .grades
-            .reduce((acc: number, grade: number) => acc + grade, 0)
-            / student1.grades.length;
+          function average(student: Student): number {
+            return student
+              .grades
+              .reduce((acc: number, grade: number) => acc + grade, 0)
+              / student.grades.length;
+          }
 
-          const student2Avarage = student2
-            .grades
-            .reduce((acc: number, grade: number) => acc + grade, 0)
-            / student2.grades.length;
+          const firstStudentAverage = average(firstStudent);
+
+          const secondStudentAverage = average(secondStudent);
 
           return order === 'asc'
-            ? student1Avarage - student2Avarage
-            : student2Avarage - student1Avarage;
+            ? firstStudentAverage - secondStudentAverage
+            : secondStudentAverage - firstStudentAverage;
         }));
 
     default:
