66464f1c6d75358d62597dc31b5f2f2bbf63d8ec
Daniel <henio09@wp.pl>
2023-04-25 10:28:49 +0200

    joined similar cases

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -8,11 +8,11 @@ export interface Student {
 }
 
 export enum SortType {
-  Name,
-  Surname,
-  Age,
-  Married,
-  AverageGrade,
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'averageGrade',
 }
 
 export type SortOrder = 'asc' | 'desc';
@@ -45,29 +45,15 @@ export function sortStudents(
       : (b[key] as string).localeCompare(a[key] as string);
   };
 
-  switch (sortBy) {
-    case SortType.Name:
-      studentsCopy.sort((a, b) => compare(a, b, 'name'));
-      break;
-    case SortType.Surname:
-      studentsCopy.sort((a, b) => compare(a, b, 'surname'));
-      break;
-    case SortType.Age:
-      studentsCopy.sort((a, b) => compare(a, b, 'age'));
-      break;
-    case SortType.Married:
-      studentsCopy.sort((a, b) => compare(a, b, 'married'));
-      break;
-    case SortType.AverageGrade:
-      studentsCopy.sort((a, b) => {
-        const first = a.grades.reduce((c, d) => c + d) / a.grades.length;
-        const second = b.grades.reduce((c, d) => c + d) / b.grades.length;
+  if (sortBy === 'averageGrade') {
+    studentsCopy.sort((a, b) => {
+      const first = a.grades.reduce((c, d) => c + d) / a.grades.length;
+      const second = b.grades.reduce((c, d) => c + d) / b.grades.length;
 
-        return isAsc ? first - second : second - first;
-      });
-      break;
-    default:
-      throw new Error('Wrong sort inner value');
+      return isAsc ? first - second : second - first;
+    });
+  } else {
+    studentsCopy.sort((a, b) => compare(a, b, sortBy));
   }
 
   return studentsCopy;
