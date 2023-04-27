4fc578b328b4bf5a825f145591e487b8bf990a51
Yulia Borshovska <yulya.borshovska@gmail.com>
2023-04-26 21:58:05 -0700

    ts_sort_student solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -17,39 +17,66 @@ export enum SortType {
 
 export type SortOrder = 'asc' | 'desc';
 
-function countAvGrades(grades: number[]): number {
-  return grades.reduce((sum, grade) => sum + grade, 0) / grades.length;
-}
-
-export function sortStudents(
-  [...students]: Student[],
+export function sortStudents(students: Student[],
   sortBy: SortType,
-  order: SortOrder,
-): Student[] {
+  order: SortOrder): Student[] {
+  const studentsCopy = [...students];
+
   switch (sortBy) {
-    case SortType.Name:
-    case SortType.Surname:
-      return order === 'asc'
-        ? students.sort((st1, st2) => st1[sortBy].localeCompare(st2[sortBy]))
-        : students.sort((st1, st2) => st2[sortBy].localeCompare(st1[sortBy]));
-
-    case SortType.Age:
-    case SortType.Married:
-      return order === 'asc'
-        ? students.sort((st1, st2) => +st1[sortBy] - +st2[sortBy])
-        : students.sort((st1, st2) => +st2[sortBy] - +st1[sortBy]);
-
-    case SortType.AverageGrade:
-      return order === 'asc'
-        ? students.sort(
-          (st1, st2) => countAvGrades(st1[sortBy]) - countAvGrades(st2[sortBy]),
-        )
-        : students.sort(
-          (st1, st2) => countAvGrades(st2[sortBy]) - countAvGrades(st1[sortBy]),
-        );
-    default:
+    case SortType.Name: {
+      studentsCopy.sort((firstStudent, secondStudent) => {
+        return order === 'asc'
+          ? firstStudent.name.localeCompare(secondStudent.name)
+          : secondStudent.name.localeCompare(firstStudent.name);
+      });
       break;
+    }
+
+    case SortType.Surname: {
+      studentsCopy.sort((firstStudent, secondStudent) => {
+        return order === 'asc'
+          ? firstStudent.surname.localeCompare(secondStudent.surname)
+          : secondStudent.surname.localeCompare(firstStudent.surname);
+      });
+      break;
+    }
+
+    case SortType.Age: {
+      studentsCopy.sort((firstStudent, secondStudent) => {
+        return order === 'asc'
+          ? firstStudent.age - secondStudent.age
+          : secondStudent.age - firstStudent.age;
+      });
+      break;
+    }
+
+    case SortType.Married: {
+      studentsCopy.sort((firstStudent, secondStudent) => {
+        return order === 'asc'
+          ? +(firstStudent.married) - +(secondStudent.married)
+          : +(secondStudent.married) - +(firstStudent.married);
+      });
+      break;
+    }
+
+    case SortType.AverageGrade: {
+      studentsCopy.sort((firstStudent, secondStudent) => {
+        const averageA = firstStudent.grades.reduce((p, c) => p + c, 0)
+        / firstStudent.grades.length;
+        const averageB = secondStudent.grades.reduce((p, c) => p + c, 0)
+         / secondStudent.grades.length;
+
+        return order === 'asc'
+          ? averageA - averageB
+          : averageB - averageA;
+      });
+      break;
+    }
+
+    default: {
+      throw new Error('Invalid sort type');
+    }
   }
 
-  return students;
+  return studentsCopy;
 }
