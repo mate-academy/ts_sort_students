af7889f30d5a90051c4c673a496138d98e36ef5c
Nadiein Oleh <olegnadein1994@gmail.com>
2023-05-02 14:54:56 +0300

    Solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,78 @@
+// import { SortOrder } from './sortStudents';
 
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name,
+  Surname,
+  Age,
+  Married,
+  AverageGrade,
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+function getAverageGrades(grades: number[]): number {
+  return grades.reduce((acc, val) => acc + val) / grades.length;
+}
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+export function sortStudents(students: Student[],
+  sortBy: SortType, order: SortOrder): Student[] {
+  switch (sortBy) {
+    case (SortType.Name):
+    default:
+      if (order === 'asc') {
+        return [...students]
+          .sort((prev, cur) => prev.name.localeCompare(cur.name));
+      }
+
+      return [...students]
+        .sort((prev, cur) => cur.name.localeCompare(prev.name));
+
+    case (SortType.Surname):
+      if (order === 'asc') {
+        return [...students]
+          .sort((prev, cur) => prev.surname.localeCompare(cur.surname));
+      }
+
+      return [...students]
+        .sort((prev, cur) => cur.surname.localeCompare(prev.surname));
+
+    case (SortType.Age):
+      if (order === 'asc') {
+        return [...students]
+          .sort((prev, cur) => prev.age - cur.age);
+      }
+
+      return [...students]
+        .sort((prev, cur) => cur.age - prev.age);
+
+    case (SortType.AverageGrade):
+      if (order === 'asc') {
+        return [...students]
+          .sort((prev, cur) => {
+            return getAverageGrades(prev.grades) - getAverageGrades(cur.grades);
+          });
+      }
+
+      return [...students]
+        .sort((prev, cur) => {
+          return getAverageGrades(cur.grades) - getAverageGrades(prev.grades);
+        });
+
+    case (SortType.Married):
+      if (order === 'asc') {
+        return [...students]
+          .sort((prev, cur) => +prev.married - +cur.married);
+      }
+
+      return [...students]
+        .sort((prev, cur) => +cur.married - +prev.married);
+  }
 }
