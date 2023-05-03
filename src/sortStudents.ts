88a447ff3e141b3bd39debde969250381b8bf55a
anton_chornobai <antonchornobajj@gmail.com>
2023-05-03 23:41:55 +0200

    done

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,52 @@
-
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[],
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
 // create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const avarageGrade = (grades: number[]): number => {
+    return grades.reduce((prev, cur) => cur + prev) / grades.length;
+  };
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  return [...students].sort((student1, student2) => {
+    switch (sortBy) {
+      case SortType.Name:
+      case SortType.Surname:
+        return order === 'asc'
+          ? student1[sortBy].localeCompare(student2[sortBy])
+          : student2[sortBy].localeCompare(student1[sortBy]);
+
+      case SortType.Age:
+      case SortType.Married:
+        return order === 'asc'
+          ? Number(student1[sortBy]) - Number(student2[sortBy])
+          : Number(student2[sortBy]) - Number(student1[sortBy]);
+
+      case SortType.AverageGrade:
+        return order === 'asc'
+          ? avarageGrade(student1[sortBy]) - avarageGrade(student2[sortBy])
+          : avarageGrade(student2[sortBy]) - avarageGrade(student1[sortBy]);
+
+      default:
+        throw new Error('Something went wrong...');
+    }
+  });
 }
