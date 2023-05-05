a85139e24ca600d9dad1f886dc5dac1ee34dc48e
meltourme <Meltourme@outlook.com>
2023-05-05 10:17:34 +0300

    add task solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,58 @@
-
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
 // create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const copyStudents = [...students];
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  switch (sortBy) {
+    case SortType.Name:
+    case SortType.Surname:
+      copyStudents.sort((a, b) => (order === 'asc'
+        ? a[sortBy].localeCompare(b[sortBy])
+        : b[sortBy].localeCompare(a[sortBy])
+      ));
+      break;
+    case SortType.Age:
+      copyStudents.sort((a, b) => (order === 'asc'
+        ? a.age - b.age : b.age - a.age
+      ));
+      break;
+    case SortType.Married:
+      copyStudents.sort((a, b) => (order === 'asc'
+        ? +a.married - +b.married
+        : +b.married - +a.married
+      ));
+      break;
+    default:
+      copyStudents.sort((a, b) => {
+        const firstEl = a.grades
+          .reduce((sum, item) => sum + item, 0) / a.grades.length;
+        const secondEl = b.grades
+          .reduce((sum, item) => sum + item, 0) / b.grades.length;
+
+        return order === 'asc' ? firstEl - secondEl : secondEl - firstEl;
+      });
+  }
+
+  return copyStudents;
 }
