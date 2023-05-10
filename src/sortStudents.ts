e89caf90c552f5157a43a4ba447d9a583a4597f7
Maksym Avramenko <nonste@outlook.com>
2023-05-10 14:20:35 +0300

    solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,67 @@
 
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const sortedStudents = [...students];
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  function calculateAverage(numbers: number[]): number {
+    return numbers.reduce((acc, curr) => acc + curr, 0) / numbers.length;
+  }
+
+  switch (sortBy) {
+    case SortType.Name:
+    case SortType.Surname:
+      return sortedStudents.sort((a: Student, b: Student) => {
+        return order === 'desc'
+          ? b[sortBy].localeCompare(a[sortBy])
+          : a[sortBy].localeCompare(b[sortBy]);
+      });
+
+    case SortType.Age:
+      return sortedStudents.sort((a: Student, b: Student) => {
+        return order === 'asc'
+          ? a[sortBy] - b[sortBy]
+          : b[sortBy] - a[sortBy];
+      });
+
+    case SortType.AverageGrade:
+      return sortedStudents.sort((a: Student, b: Student) => {
+        return order === 'asc'
+          ? calculateAverage(a[sortBy]) - calculateAverage(b[sortBy])
+          : calculateAverage(b[sortBy]) - calculateAverage(a[sortBy]);
+      });
+
+    case SortType.Married: {
+      const married
+        = sortedStudents.filter((student: Student) => student[sortBy]);
+      const unmarried
+        = sortedStudents.filter((student: Student) => !student[sortBy]);
+
+      return order === 'asc' ? unmarried.concat(married)
+        : married.concat(unmarried);
+    }
+
+    default:
+      return sortedStudents;
+  }
 }
