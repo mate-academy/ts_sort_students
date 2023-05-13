40a3263c1cd1264387e96f81d3395d2e438f757b
Emil Sukiennik <ewilemil@gmail.com>
2023-05-13 12:05:59 +0200

    more requested changes applied

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,60 @@
 
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[],
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'averageGrade',
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const setOrder = (): number => {
+    return order === 'asc' ? 1 : -1;
+  };
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  type GetProp = (student: Student) => string | number | boolean;
+
+  const getProp: GetProp = (student) => {
+    switch (sortBy) {
+      case SortType.Name:
+      case SortType.Surname:
+        return student[sortBy].toUpperCase();
+      case SortType.Age:
+      case SortType.Married:
+        return student[sortBy];
+      case SortType.AverageGrade:
+      default:
+        return student.grades.reduce((a, b) => a + b, 0)
+        / student.grades.length;
+    }
+  };
+
+  return [...students].sort((a, b) => {
+    const aProp = getProp(a);
+    const bProp = getProp(b);
+
+    if (aProp > bProp) {
+      return setOrder();
+    }
+
+    if (aProp < bProp) {
+      return -setOrder();
+    }
+
+    return 0;
+  });
 }
