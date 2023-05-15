e3e6a093f8d3e54a40e7ac6ace2b73696c8f2841
Victor Buldenko <gookidoo@gmail.com>
2023-05-15 22:42:05 +0300

    rework

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -21,69 +21,66 @@ export function sortStudents(
   sortBy: SortType,
   order: SortOrder,
 ): Student[] {
-  function copyObj(obj: Student): Student {
-    return Object.assign(obj);
-  }
+  const copy: Student[] = [...students];
 
-  const copy = students.reduce((res: Student[], el) => {
-    res.push(copyObj(el));
+  switch (sortBy) {
+    case SortType.Name:
+      copy.sort((a, b) => {
+        if (order === 'asc') {
+          return a.name.localeCompare(b.name);
+        }
 
-    return res;
-  }, [] as Student[]);
+        return b.name.localeCompare(a.name);
+      });
 
-  if (sortBy === SortType.Name) {
-    copy.sort((a, b) => {
-      if (order === 'asc') {
-        return a.name.localeCompare(b.name);
-      }
+      break;
 
-      return b.name.localeCompare(a.name);
-    });
-  }
+    case SortType.Surname:
+      copy.sort((a, b) => {
+        if (order === 'asc') {
+          return a.surname.localeCompare(b.surname);
+        }
 
-  if (sortBy === SortType.Surname) {
-    copy.sort((a, b) => {
-      if (order === 'asc') {
-        return a.surname.localeCompare(b.surname);
-      }
+        return b.surname.localeCompare(a.surname);
+      });
 
-      return b.surname.localeCompare(a.surname);
-    });
-  }
+      break;
 
-  if (sortBy === SortType.Age) {
-    copy.sort((a, b) => {
-      if (order === 'asc') {
-        return a.age - b.age;
-      }
+    case SortType.Age:
+      copy.sort((a, b) => {
+        if (order === 'asc') {
+          return a.age - b.age;
+        }
 
-      return b.age - a.age;
-    });
-  }
+        return b.age - a.age;
+      });
 
-  if (sortBy === SortType.Married) {
-    copy.sort((a, b) => {
-      if (order === 'asc') {
-        return Number(a.married) - Number(b.married);
-      }
+      break;
+    case SortType.Married:
+      copy.sort((a, b) => {
+        if (order === 'asc') {
+          return Number(a.married) - Number(b.married);
+        }
 
-      return Number(b.married) - Number(a.married);
-    });
-  }
+        return Number(b.married) - Number(a.married);
+      });
 
-  if (sortBy === SortType.AverageGrade) {
-    copy.sort((a, b) => {
-      const aAverage: number = a.grades.reduce((res: number, el) => res + el, 0)
-        / a.grades.length;
-      const bAverage: number = b.grades.reduce((res: number, el) => res + el, 0)
-        / b.grades.length;
+      break;
+    default:
+      copy.sort((a, b) => {
+        const aAverage: number
+          = a.grades.reduce((res: number, el) => res + el, 0) / a.grades.length;
+        const bAverage: number
+          = b.grades.reduce((res: number, el) => res + el, 0) / b.grades.length;
 
-      if (order === 'asc') {
-        return aAverage - bAverage;
-      }
+        if (order === 'asc') {
+          return aAverage - bAverage;
+        }
 
-      return bAverage - aAverage;
-    });
+        return bAverage - aAverage;
+      });
+
+      break;
   }
 
   return copy;
