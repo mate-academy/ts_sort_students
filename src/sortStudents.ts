50c830c3dda63e7a55a6c8dc682b91e97b55f6f8
Victor Buldenko <gookidoo@gmail.com>
2023-05-15 19:06:19 +0300

    TS... pain

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,90 @@
-
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name,
+  Surname,
+  Age,
+  Married,
+  AverageGrade,
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  function copyObj(obj: Student): Student {
+    return Object.assign(obj);
+  }
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  const copy = students.reduce((res: Student[], el) => {
+    res.push(copyObj(el));
+
+    return res;
+  }, [] as Student[]);
+
+  if (sortBy === SortType.Name) {
+    copy.sort((a, b) => {
+      if (order === 'asc') {
+        return a.name.localeCompare(b.name);
+      }
+
+      return b.name.localeCompare(a.name);
+    });
+  }
+
+  if (sortBy === SortType.Surname) {
+    copy.sort((a, b) => {
+      if (order === 'asc') {
+        return a.surname.localeCompare(b.surname);
+      }
+
+      return b.surname.localeCompare(a.surname);
+    });
+  }
+
+  if (sortBy === SortType.Age) {
+    copy.sort((a, b) => {
+      if (order === 'asc') {
+        return a.age - b.age;
+      }
+
+      return b.age - a.age;
+    });
+  }
+
+  if (sortBy === SortType.Married) {
+    copy.sort((a, b) => {
+      if (order === 'asc') {
+        return Number(a.married) - Number(b.married);
+      }
+
+      return Number(b.married) - Number(a.married);
+    });
+  }
+
+  if (sortBy === SortType.AverageGrade) {
+    copy.sort((a, b) => {
+      const aAverage: number = a.grades.reduce((res: number, el) => res + el, 0)
+        / a.grades.length;
+      const bAverage: number = b.grades.reduce((res: number, el) => res + el, 0)
+        / b.grades.length;
+
+      if (order === 'asc') {
+        return aAverage - bAverage;
+      }
+
+      return bAverage - aAverage;
+    });
+  }
+
+  return copy;
 }
