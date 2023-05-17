9deb170d1186a172f73166e5fcddf220cf7e9f93
mariuszzawierucha <mariusz.zawierucha@gmail.com>
2023-05-17 19:34:19 +0200

    solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,54 @@
+/* eslint-disable max-len */
+/* eslint-disable semi */
 
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[],
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades'
+}
+
+function countAverageGrades(grades: number[]): number {
+  const sum = grades.reduce((total, grade) => total + grade, 0);
+
+  return sum / grades.length;
 }
 
 // create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(students: Student[], sortBy: SortType, order: SortOrder): Student[] {
+  const result = [...students];
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  result.sort((prev, next) => {
+    switch (sortBy) {
+      case 'age':
+      case 'married':
+        return (order === 'asc')
+          ? Number(prev[sortBy]) - Number(next[sortBy])
+          : Number(next[sortBy]) - Number(prev[sortBy]);
+      case 'name':
+      case 'surname':
+        return (order === 'asc')
+          ? prev[sortBy].localeCompare(next[sortBy])
+          : next[sortBy].localeCompare(prev[sortBy]);
+      case 'grades':
+        return (order === 'asc')
+          ? countAverageGrades(prev[sortBy]) - countAverageGrades(next[sortBy])
+          : countAverageGrades(next[sortBy]) - countAverageGrades(prev[sortBy]);
+      default:
+        throw new Error('Sort type doesn\'t exist');
+    }
+  });
+
+  return result;
 }
