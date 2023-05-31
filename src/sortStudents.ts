c9e189ec35614730352af6d05d6dc1d335cd57ed
Katya Zaremska <unnarummel@gmail.com>
2023-05-31 22:25:23 +0300

    add task solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -16,6 +16,10 @@ export enum SortType {
 
 export type SortOrder = 'asc' | 'desc';
 
+function getAverageGrade(grades: number[]): number {
+  return grades.reduce((acc, grade) => acc + grade, 0) / grades.length;
+}
+
 export function sortStudents(
   students: Student[],
   sortBy: SortType,
@@ -23,34 +27,30 @@ export function sortStudents(
 ): Student[] {
   const copiedStudents = [...students];
 
-  return copiedStudents
-    .sort((studentOne: Student, studentTwo: Student) => {
-      const studentOneAverage = (studentOne.grades)
-        .reduce((sum, num) => sum + num, 0) / studentOne.grades.length;
+  return copiedStudents.sort((studentOne: Student, studentTwo: Student) => {
+    const studentOneAverage = getAverageGrade(studentOne.grades);
+    const studentTwoAverage = getAverageGrade(studentTwo.grades);
 
-      const studentTwoAverage = (studentTwo.grades)
-        .reduce((sum, num) => sum + num, 0) / studentTwo.grades.length;
+    switch (sortBy) {
+      case SortType.Name:
+      case SortType.Surname:
+        return order === 'asc'
+          ? studentOne[sortBy].localeCompare(studentTwo[sortBy])
+          : studentTwo[sortBy].localeCompare(studentOne[sortBy]);
 
-      switch (sortBy) {
-        case SortType.Name:
-        case SortType.Surname:
-          return order === 'asc'
-            ? studentOne[sortBy].localeCompare(studentTwo[sortBy])
-            : studentTwo[sortBy].localeCompare(studentOne[sortBy]);
+      case SortType.Age:
+      case SortType.Married:
+        return order === 'asc'
+          ? Number(studentOne[sortBy]) - Number(studentTwo[sortBy])
+          : Number(studentTwo[sortBy]) - Number(studentOne[sortBy]);
 
-        case SortType.Age:
-        case SortType.Married:
-          return order === 'asc'
-            ? Number(studentOne[sortBy]) - Number(studentTwo[sortBy])
-            : Number(studentTwo[sortBy]) - Number(studentOne[sortBy]);
+      case SortType.AverageGrade:
+        return order === 'asc'
+          ? studentOneAverage - studentTwoAverage
+          : studentTwoAverage - studentOneAverage;
 
-        case SortType.AverageGrade:
-          return order === 'asc'
-            ? studentOneAverage - studentTwoAverage
-            : studentTwoAverage - studentOneAverage;
-
-        default:
-          throw new Error('This order is not suported!');
-      }
-    });
+      default:
+        throw new Error('This order is not supported!');
+    }
+  });
 }
