bfb386694cafc5d0414e1f4942a9dd033c2cd967
Maksym Bobryk <booobryyyk@gmail.com>
2023-05-31 18:54:19 +0300

    add sortStudents solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,57 @@
 
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grade',
 }
 
 // create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+function countAverage(student: Student): number {
+  return student.grades.reduce((sum, grade) => sum + grade, 0)
+    / student.grades.length;
+}
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const copiedStudents: Student[] = [...students];
+
+  return copiedStudents
+    .sort((studentFirst, studentSecond) => {
+      switch (sortBy) {
+        case SortType.Name:
+        case SortType.Surname:
+          return order === 'asc'
+            ? studentFirst[sortBy].localeCompare(studentSecond[sortBy])
+            : studentSecond[sortBy].localeCompare(studentFirst[sortBy]);
+
+        case SortType.Age:
+        case SortType.Married:
+          return order === 'asc'
+            ? +studentFirst[sortBy] - +studentSecond[sortBy]
+            : +studentSecond[sortBy] - +studentFirst[sortBy];
+
+        case SortType.AverageGrade:
+          return order === 'asc'
+            ? countAverage(studentFirst) - countAverage(studentSecond)
+            : countAverage(studentSecond) - countAverage(studentFirst);
+
+        default:
+          throw new Error(`Sorry, but your type ${sortBy} wasn't found`);
+      }
+    });
 }
