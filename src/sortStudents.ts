c5ed6a07ef1322275806ba140364978581709f6b
Vania Yarovyi <yarovyi@gmail.com>
2023-05-31 16:28:35 +0300

    add solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,70 @@
 
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades'
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const copyStudents = [...students];
+  const sortedStudents = copyStudents.sort((student, nextStudent) => {
+    let studentData: string | number | boolean | number[] = student[sortBy];
+    let nextStudentData
+    : string
+    | number
+    | boolean
+    | number[]
+    = nextStudent[sortBy];
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+    if (sortBy === SortType.AverageGrade) {
+      studentData = student[sortBy]
+        .reduce((total, mark) => total + mark) / student[sortBy].length;
+
+      nextStudentData = nextStudent[sortBy]
+        .reduce((total, mark) => total + mark) / nextStudent[sortBy].length;
+
+      return order === 'asc'
+        ? studentData - nextStudentData
+        : nextStudentData - studentData;
+    }
+
+    if (typeof studentData === 'number'
+    && typeof nextStudentData === 'number') {
+      return order === 'asc'
+        ? studentData - nextStudentData
+        : nextStudentData - studentData;
+    }
+
+    if (typeof studentData === 'string'
+    && typeof nextStudentData === 'string') {
+      return order === 'asc'
+        ? studentData.localeCompare(nextStudentData)
+        : nextStudentData.localeCompare(studentData);
+    }
+
+    const studentDataBooleanToNum = studentData ? 1 : 0;
+    const nextStudentDataBooleanToNum = nextStudentData ? 1 : 0;
+
+    return order === 'asc'
+      ? studentDataBooleanToNum - nextStudentDataBooleanToNum
+      : nextStudentDataBooleanToNum - studentDataBooleanToNum;
+  });
+
+  return sortedStudents;
 }
