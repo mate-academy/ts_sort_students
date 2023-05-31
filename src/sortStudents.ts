98f4140ef69375912a39da171ae479867fbf9bfc
Stepan Haida <stepanhaida@gmail.com>
2023-05-31 17:57:37 +0300

    add solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -18,7 +18,39 @@ export enum SortType {
 // create SortOrder type
 export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const sortedStudents = [...students];
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  const getAvg = (arrNum: number[]): number => (
+    arrNum.reduce((a, b) => a + b) / arrNum.length
+  );
+
+  sortedStudents.sort(
+    (student1: Student, student2: Student) => {
+      switch (sortBy) {
+        case SortType.Name:
+        case SortType.Surname:
+          return order === 'asc'
+            ? student1[sortBy].localeCompare(student2[sortBy])
+            : student2[sortBy].localeCompare(student2[sortBy]);
+        case SortType.Age:
+        case SortType.Married:
+          return order === 'asc'
+            ? +student1[sortBy] - +student2[sortBy]
+            : +student2[sortBy] - +student1[sortBy];
+        case SortType.AverageGrade:
+          return order === 'asc'
+            ? getAvg(student1[sortBy]) - getAvg(student2[sortBy])
+            : getAvg(student2[sortBy]) - getAvg(student1[sortBy]);
+        default:
+          throw new Error('Something wert wrong(invalid sort by)');
+      }
+    },
+  );
+
+  return sortedStudents;
 }
