1e7d0eed5eca82d95fa8d18a6242ec441f4ed126
Viktor Panikar <vpany97@gmail.com>
2023-06-01 00:16:58 +0300

    Solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,74 @@
 
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades'
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+function calculateAverageGrade(grades: number[]): number {
+  return grades.reduce((total, grade) => total + grade, 0) / grades.length;
+}
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const sortedStudents = [...students];
+
+  sortedStudents.sort((a: Student, b: Student): number => {
+    let aValue: string | number | boolean;
+    let bValue: string | number | boolean;
+
+    switch (sortBy) {
+      case SortType.Name:
+        aValue = a.name;
+        bValue = b.name;
+        break;
+      case SortType.Surname:
+        aValue = a.surname;
+        bValue = b.surname;
+        break;
+      case SortType.Age:
+        aValue = a.age;
+        bValue = b.age;
+        break;
+      case SortType.Married:
+        aValue = a.married;
+        bValue = b.married;
+        break;
+      case SortType.AverageGrade:
+        aValue = calculateAverageGrade(a.grades);
+        bValue = calculateAverageGrade(b.grades);
+        break;
+      default:
+        aValue = '';
+        bValue = '';
+        break;
+    }
+
+    if (aValue === bValue) {
+      return 0;
+    }
+
+    if (order === 'asc') {
+      return aValue < bValue ? -1 : 1;
+    }
+
+    return aValue > bValue ? -1 : 1;
+  });
+
+  return sortedStudents;
 }
