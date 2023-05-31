dbe78e481dee8aeb57d2a2debf210963f385c469
Kateryna Sokur <k.o.sokur@gmail.com>
2023-05-31 12:41:29 +0200

    add solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,64 @@
 
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const newStudentsArr: Student[] = [...students];
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  switch (sortBy) {
+    case SortType.Name:
+    case SortType.Surname:
+      newStudentsArr.sort((a, b) => {
+        return order === 'asc'
+          ? a[sortBy].localeCompare(b[sortBy])
+          : b[sortBy].localeCompare(a[sortBy]);
+      });
+      break;
+
+    case SortType.Age:
+    case SortType.Married:
+      newStudentsArr.sort((a, b) => {
+        return order === 'asc'
+          ? +a[sortBy] - +b[sortBy]
+          : +b[sortBy] - +a[sortBy];
+      });
+      break;
+
+    case SortType.AverageGrade:
+      newStudentsArr.sort((a, b) => {
+        const averageStudentA: number = a.grades
+          .reduce((sum, grade) => sum + grade) / a.grades.length;
+        const averageStudentB: number = b.grades
+          .reduce((sum, grade) => sum + grade) / b.grades.length;
+
+        return order === 'asc'
+          ? averageStudentA - averageStudentB
+          : averageStudentB - averageStudentA;
+      });
+      break;
+
+    default:
+      return newStudentsArr;
+  }
+
+  return newStudentsArr;
 }
