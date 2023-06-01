5f14576af1564fd446d14dc2754d586192838c8b
Yehor Myronov <2726048@gmail.com>
2023-06-01 14:24:22 +0300

    moved switch func into reduce callback

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -19,9 +19,7 @@ export type SortOrder = 'asc' | 'desc';
 
 function getAverageGrade(grades: number[]): number {
   return grades.reduce(
-    (gradesSum: number, grade: number) => {
-      return grade + gradesSum;
-    },
+    (gradesSum: number, grade: number) => grade + gradesSum,
   ) / grades.length;
 }
 
@@ -32,40 +30,30 @@ export function sortStudents(
 ): Student[] {
   const studentsCopy: Student[] = [...students];
 
-  switch (sortBy) {
-    case SortType.Name:
-    case SortType.Surname:
-      return studentsCopy.sort(
-        (currentStudent: Student, nextStudent: Student): number => {
-          return order === 'asc'
-            ? (currentStudent[sortBy]).localeCompare(nextStudent[sortBy])
-            : (nextStudent[sortBy].localeCompare(currentStudent[sortBy]));
-        },
-      );
+  return studentsCopy.sort((currentStudent: Student, nextStudent: Student) => {
+    switch (sortBy) {
+      case SortType.Name:
+      case SortType.Surname:
+        return order === 'asc'
+          ? (currentStudent[sortBy]).localeCompare(nextStudent[sortBy])
+          : (nextStudent[sortBy].localeCompare(currentStudent[sortBy]));
 
-    case SortType.Age:
-    case SortType.Married:
-      return studentsCopy.sort(
-        (currentStudent: Student, nextStudent: Student) => {
-          return order === 'asc'
-            ? (+currentStudent[sortBy]) - (+nextStudent[sortBy])
-            : (+nextStudent[sortBy]) - (+currentStudent[sortBy]);
-        },
-      );
+      case SortType.Age:
+      case SortType.Married:
+        return order === 'asc'
+          ? (+currentStudent[sortBy]) - (+nextStudent[sortBy])
+          : (+nextStudent[sortBy]) - (+currentStudent[sortBy]);
 
-    case SortType.AverageGrade:
-      return studentsCopy.sort(
-        (currentStudent: Student, nextStudent: Student) => {
-          return order === 'asc'
-            ? getAverageGrade(currentStudent.grades)
-              - getAverageGrade(nextStudent.grades)
+      case SortType.AverageGrade:
+        return order === 'asc'
+          ? getAverageGrade(currentStudent.grades)
+                - getAverageGrade(nextStudent.grades)
 
-            : getAverageGrade(nextStudent.grades)
-              - getAverageGrade(currentStudent.grades);
-        },
-      );
+          : getAverageGrade(nextStudent.grades)
+                - getAverageGrade(currentStudent.grades);
 
-    default:
-      throw new Error('Invalid sorting parameters');
-  }
+      default:
+        throw new Error('Invalid sorting parameters');
+    }
+  });
 }
