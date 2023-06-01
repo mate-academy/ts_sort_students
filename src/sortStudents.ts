f08a768cdf2aa77663d3ff732686fbd62bd6e50c
Antonio S <soi4.toxa@ukr.net>
2023-06-01 12:13:05 +0300

    Sort-students-1

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,60 @@
 
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[],
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'averageGrade',
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export type Comparison = (
+  start: Student,
+  end: Student,
+  sortBy: SortType) => number;
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+export type SortStudents = (
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder) => Student[];
+
+export function getAverageGrades(student: Student): number {
+  const sumStart = student.grades.reduce((sum: number, n: number) => sum + n);
+
+  return sumStart / student.grades.length;
 }
+
+export const comparison: Comparison = (start, end, sortBy) => {
+  if (sortBy === 'age') {
+    return start[sortBy] - end[sortBy];
+  }
+
+  if (sortBy === 'averageGrade') {
+    return getAverageGrades(start) - getAverageGrades(end);
+  }
+
+  if (sortBy === 'married') {
+    return (start[sortBy] ? 1 : 0) - (end[sortBy] ? 1 : 0);
+  }
+
+  return start[sortBy].localeCompare(end[sortBy]);
+};
+
+export const sortStudents: SortStudents = (students, sortBy, order) => {
+  const copyStudents = [...students];
+
+  return copyStudents.sort((a: Student, b: Student): number => {
+    return (order === 'asc')
+      ? comparison(a, b, sortBy)
+      : comparison(b, a, sortBy);
+  });
+};
