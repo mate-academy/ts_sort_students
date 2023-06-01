981c3fb739c04d9c3044a1e8e3bc93f7f979df79
Yura Enttsel <equikmla@gmail.com>
2023-06-01 12:28:23 +0300

    Solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,54 @@
 
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+function getAvarageGrade(student: Student): number {
+  return student.grades.reduce((sum, grade) => sum + grade, 0)
+    / student.grades.length;
+}
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+export function sortStudents(
+  students: Student[], sortBy: SortType, order: SortOrder,
+): Student[] {
+  const copyStudents: Student[] = [...students];
+
+  return copyStudents
+    .sort((firstStudent, secondStudent) => {
+      switch (sortBy) {
+        case SortType.Name:
+        case SortType.Surname:
+          return (order === 'asc')
+            ? firstStudent[sortBy].localeCompare(secondStudent[sortBy])
+            : firstStudent[sortBy].localeCompare(secondStudent[sortBy]);
+
+        case SortType.Age:
+        case SortType.Married:
+          return (order === 'asc')
+            ? Number(firstStudent[sortBy]) - Number(secondStudent[sortBy])
+            : Number(secondStudent[sortBy]) - Number(firstStudent[sortBy]);
+
+        case SortType.AverageGrade:
+          return (order === 'asc')
+            ? getAvarageGrade(firstStudent) - getAvarageGrade(secondStudent)
+            : getAvarageGrade(secondStudent) - getAvarageGrade(firstStudent);
+
+        default:
+          throw new Error(`Function can't sort by ${sortBy} type!`);
+      }
+    });
 }
