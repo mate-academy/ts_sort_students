14575586cef0e3ddc19454a0337ef0534bd730ce
Mager <magerazp@gmail.com>
2023-06-02 07:43:47 +0300

    Students sorted

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,60 @@
 
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc'| 'desc';
 
+const getAverageGrades = (grades: number[]): number => {
+  return grades.length
+    ? grades.reduce((acc, cur) => acc + cur, 0) / grades.length
+    : 0;
+};
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const sortedStudents:Student[] = [...students];
+
+  return sortedStudents.sort((firstStud, secondStud) => {
+    const studAField = firstStud[sortBy];
+    const studBField = secondStud[sortBy];
+
+    switch (sortBy) {
+      case SortType.Surname:
+      case SortType.Name:
+
+        return order === 'asc'
+          ? studAField.localeCompare(studBField)
+          : studBField.localeCompare(studAField);
+
+      case SortType.Age:
+      case SortType.Married:
+        return order === 'asc'
+          ? Number(studAField) - Number(studBField)
+          : Number(studBField) - Number(studAField);
+
+      case SortType.AverageGrade:
+        return order === 'asc'
+          ? getAverageGrades(studAField) - getAverageGrades(studBField)
+          : getAverageGrades(studBField) - getAverageGrades(studAField);
+
+      default:
+        throw new Error('No such sort type');
+    }
+  });
 }
