0799c5d9cf62f8d335e1d1cd4005c191a22c1dcc
Sonya Liubchenko <conilub@gmail.com>
2023-06-02 13:59:29 +0300

    fixed

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -8,11 +8,11 @@ export interface Student {
 }
 
 export enum SortType {
-  Name,
-  Surname,
-  Age,
-  Married,
-  AverageGrade,
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
 export type SortOrder = 'asc' | 'desc';
@@ -28,37 +28,29 @@ export function sortStudents(
 ): Student[] {
   const sortedStudents = [...students];
 
-  return order === 'asc'
-    ? sortedStudents.sort((a: Student, b: Student) => {
-      switch (sortBy) {
-        case SortType.Name:
-          return a.name.localeCompare(b.name);
-        case SortType.Surname:
-          return a.surname.localeCompare(b.surname);
-        case SortType.Age:
-          return a.age - b.age;
-        case SortType.Married:
-          return Number(a.married) - Number(b.married);
-        case SortType.AverageGrade:
-          return getAverageGrade(a.grades) - getAverageGrade(b.grades);
-        default:
-          throw new Error('Not valid data');
-      }
-    })
-    : sortedStudents.sort((a: Student, b: Student) => {
-      switch (sortBy) {
-        case SortType.Name:
-          return b.name.localeCompare(a.name);
-        case SortType.Surname:
-          return b.surname.localeCompare(a.surname);
-        case SortType.Age:
-          return b.age - a.age;
-        case SortType.Married:
-          return Number(b.married) - Number(a.married);
-        case SortType.AverageGrade:
-          return getAverageGrade(b.grades) - getAverageGrade(a.grades);
-        default:
-          throw new Error('Not valid data');
-      }
-    });
+  sortedStudents.sort((a: Student, b: Student) => {
+    switch (sortBy) {
+      case SortType.Name:
+      case SortType.Surname:
+        return order === 'asc'
+          ? a[sortBy].localeCompare(b[sortBy])
+          : b[sortBy].localeCompare(a[sortBy]);
+
+      case SortType.Age:
+      case SortType.Married:
+        return order === 'asc'
+          ? Number(a[sortBy]) - Number(b[sortBy])
+          : Number(b[sortBy]) - Number(a[sortBy]);
+
+      case SortType.AverageGrade:
+        return order === 'asc'
+          ? getAverageGrade(a.grades) - getAverageGrade(b.grades)
+          : getAverageGrade(b.grades) - getAverageGrade(a.grades);
+
+      default:
+        throw new Error('Not valid data');
+    }
+  });
+
+  return sortedStudents;
 }
