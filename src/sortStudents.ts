d3d27eaedc13e510f2780aec50b481929fb0e7e0
Marek Derlukiewicz <marek.derlukiewicz@gmail.com>
2023-06-12 11:02:31 +0200

    single quotes change, other changes for test purposes

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -16,20 +16,28 @@ export enum SortType {
 }
 
 // defining possible soritng ways
-export type SortOrder = "asc" | "desc";
+export type SortOrder = 'asc' | 'desc';
 
-// takes an array of students, the SortType - fields to sort by, and the SortOrder (asc or desc)
-// returns a new array of students sorted according to the given parameters, without modifying the original array.
+// takes an array of students, the SortType - fields to sort by,
+// and the SortOrder (asc or desc)
+// returns a new array of students sorted according to the given parameters,
+// without modifying the original array.
 
 export function sortStudents(
   students: Student[],
   sortBy: SortType,
-  order: SortOrder
+  order: SortOrder,
 ): Student[] {
   // logic based on the SortType specified
 
-  const compareFunction = (a: Student, b: Student) => {
-    let aValue, bValue;
+  function calculateAverageGrade(grades: number[]): number {
+    const sum = grades.reduce((total, grade) => total + grade, 0);
+    return sum / grades.length;
+  }
+
+  const compareFunction = (a: Student, b: Student): number => {
+    let aValue;
+    let bValue;
 
     switch (sortBy) {
       case SortType.Name:
@@ -56,8 +64,14 @@ export function sortStudents(
         return 0;
     }
 
-    if (aValue < bValue) return order === "asc" ? -1 : 1;
-    if (aValue > bValue) return order === "asc" ? 1 : -1;
+    if (aValue < bValue) {
+      return order === 'asc' ? -1 : 1;
+    }
+
+    if (aValue > bValue) {
+      return order === 'asc' ? 1 : -1;
+    }
+
     return 0;
   };
 
@@ -69,78 +83,73 @@ export function sortStudents(
 
 // calculate the average grade from the grades array
 
-function calculateAverageGrade(grades: number[]): number {
-  const sum = grades.reduce((total, grade) => total + grade, 0);
-  return sum / grades.length;
-}
-
 const students = [
   {
-    name: "Diana",
-    surname: "Dorsey",
+    name: 'Diana',
+    surname: 'Dorsey',
     age: 24,
     married: false,
     grades: [3, 3, 4, 5, 4, 3, 5, 5],
   },
   {
-    name: "Christina",
-    surname: "Branscome",
+    name: 'Christina',
+    surname: 'Branscome',
     age: 23,
     married: true,
     grades: [4, 4, 4, 5, 5, 5, 5, 5],
   },
   {
-    name: "Willie",
-    surname: "Barrera",
+    name: 'Willie',
+    surname: 'Barrera',
     age: 22,
     married: false,
     grades: [3, 5, 5, 3, 3, 5, 4, 4],
   },
   {
-    name: "Douglas",
-    surname: "Paez",
+    name: 'Douglas',
+    surname: 'Paez',
     age: 23,
     married: true,
     grades: [5, 5, 5, 4, 5, 5, 5, 5],
   },
   {
-    name: "Richard",
-    surname: "Hall",
+    name: 'Richard',
+    surname: 'Hall',
     age: 23,
     married: false,
     grades: [3, 2, 4, 5, 4, 3, 3, 3],
   },
   {
-    name: "Dale",
-    surname: "Gandy",
+    name: 'Dale',
+    surname: 'Gandy',
     age: 23,
     married: false,
     grades: [5, 3, 3, 3, 3, 5, 4, 3, 4],
   },
   {
-    name: "Lillian",
-    surname: "Quinn",
+    name: 'Lillian',
+    surname: 'Quinn',
     age: 23,
     married: false,
     grades: [3, 4, 3, 4, 4, 4, 5, 2, 3],
   },
   {
-    name: "Jessica",
-    surname: "Buxton",
+    name: 'Jessica',
+    surname: 'Buxton',
     age: 26,
     married: true,
     grades: [5, 5, 4, 5, 4, 4, 4, 4, 5, 4, 5, 4],
   },
   {
-    name: "Pamela",
-    surname: "Casillas",
+    name: 'Pamela',
+    surname: 'Casillas',
     age: 24,
     married: false,
     grades: [4, 5, 4, 5, 5, 4, 3, 2, 3, 3, 3, 2],
   },
   {
-    name: "Glenn",
-    surname: "Thompson",
+    name: 'Glenn',
+    surname: 'Thompson',
     age: 22,
     married: false,
     grades: [5, 5, 5, 5, 5, 5, 5, 5, 4, 4, 3, 2],
@@ -149,4 +158,6 @@ const students = [
 
 // my checks:
 // console.log(sortStudents(students, SortType.Name, "asc"));
-// console.log('calculateAverageGrade',calculateAverageGrade(students[0].grades));
+
+// console.log('calculateAverageGrade',
+// calculateAverageGrade(students[0].grades));
