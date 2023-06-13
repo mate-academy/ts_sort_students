1d2d081f4646cf3f1c9ffc76dcb52b0318a88e92
Vo7kov <jegor.volkov.a@gmail.com>
2023-06-13 16:55:56 +0300

    fix

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -24,73 +24,53 @@ export function sortStudents(
   order: SortOrder,
 ): Student[] {
   const copyStudents: Student[] = [...students];
-  let sortedStudents: Student[] = copyStudents;
 
-  // eslint-disable-next-line default-case
-  switch (sortBy) {
-    case SortType.Name: {
-      sortedStudents
-        = copyStudents.sort((student1: Student, student2: Student) => {
-          return order === 'asc'
-            ? student1.name.localeCompare(student2.name)
-            : student2.name.localeCompare(student1.name);
-        });
+  copyStudents.sort((student1: Student, student2: Student) => {
+    // eslint-disable-next-line default-case
+    switch (sortBy) {
+      case SortType.Name: {
+        return order === 'asc'
+          ? student1.name.localeCompare(student2.name)
+          : student2.name.localeCompare(student1.name);
+      }
 
-      break;
+      case SortType.Surname: {
+        return order === 'asc'
+          ? student1.surname.localeCompare(student2.surname)
+          : student2.surname.localeCompare(student1.surname);
+      }
+
+      case SortType.Age: {
+        return order === 'asc'
+          ? student1.age - student2.age
+          : student2.age - student1.age;
+      }
+
+      case SortType.Married: {
+        return order === 'asc'
+          ? Number(student1.married) - Number(student2.married)
+          : Number(student2.married) - Number(student1.married);
+      }
+
+      case SortType.AverageGrade: {
+        const student1AverageGrade
+          = student1.grades
+            .reduce((a: number, b: number) => a + b) / student1.grades.length;
+
+        const student2AverageGrade
+          = student2.grades
+            .reduce((a: number, b: number) => a + b) / student2.grades.length;
+
+        return order === 'asc'
+          ? student1AverageGrade - student2AverageGrade
+          : student2AverageGrade - student1AverageGrade;
+      }
+
+      default: {
+        return 0;
+      }
     }
+  });
 
-    case SortType.Surname: {
-      sortedStudents
-        = copyStudents.sort((student1: Student, student2: Student) => {
-          return order === 'asc'
-            ? student1.surname.localeCompare(student2.surname)
-            : student2.surname.localeCompare(student1.surname);
-        });
-
-      break;
-    }
-
-    case SortType.Age: {
-      sortedStudents
-        = copyStudents.sort((student1: Student, student2: Student) => {
-          return order === 'asc'
-            ? student1.age - student2.age
-            : student2.age - student1.age;
-        });
-
-      break;
-    }
-
-    case SortType.Married: {
-      sortedStudents
-        = copyStudents.sort((student1: Student, student2: Student) => {
-          return order === 'asc'
-            ? Number(student1.married) - Number(student2.married)
-            : Number(student2.married) - Number(student1.married);
-        });
-
-      break;
-    }
-
-    case SortType.AverageGrade: {
-      sortedStudents
-        = copyStudents.sort((student1: Student, student2: Student) => {
-          const student1AverageGrade
-            = student1.grades
-              .reduce((a: number, b: number) => a + b) / student1.grades.length;
-
-          const student2AverageGrade
-            = student2.grades
-              .reduce((a: number, b: number) => a + b) / student2.grades.length;
-
-          return order === 'asc'
-            ? student1AverageGrade - student2AverageGrade
-            : student2AverageGrade - student1AverageGrade;
-        });
-
-      break;
-    }
-  }
-
-  return sortedStudents;
+  return copyStudents;
 }
