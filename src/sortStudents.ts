bcd4da0807a92ca735b42b6be5f22f81587e6adf
AndriiYelieva <andrii.yelieva@gmail.com>
2023-06-13 16:38:21 +0300

    add task solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,59 @@
 
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number,
+  married: boolean,
+  grades: number[],
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades'
+}
+
+function getAverageScore(students: Student): number {
+  const averegSum = students.grades.reduce((a, b) => a + b, 0);
+
+  return averegSum / students.grades.length;
 }
 
 // create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const notOriginalStudents = [...students];
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  notOriginalStudents.sort((a, b) => {
+    switch (sortBy) {
+      case SortType.Name:
+      case SortType.Surname:
+        return order === 'asc'
+          ? a[sortBy].localeCompare(b[sortBy])
+          : b[sortBy].localeCompare(a[sortBy]);
+
+      case SortType.Age:
+      case SortType.Married:
+        return order === 'asc'
+          ? +a[sortBy] - +b[sortBy]
+          : +b[sortBy] - +a[sortBy];
+
+      case SortType.AverageGrade:
+        return order === 'asc'
+          ? getAverageScore(a) - getAverageScore(b)
+          : getAverageScore(b) - getAverageScore(a);
+
+      default:
+        throw new Error('Error');
+    }
+  });
+
+  return notOriginalStudents;
 }
