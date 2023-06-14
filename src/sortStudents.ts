197ca83081e6d531c82e00704756d657b3d698c0
Aleks <offsset@gmail.com>
2023-06-14 10:26:12 +0300

    Solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,72 @@
 
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number [];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades'
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(
+  students: Student [],
+  sortBy: SortType,
+  order: SortOrder,
+):Student [] {
+  const studentsNew: Student [] = [...students];
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  switch (sortBy) {
+    case 'age': {
+      const result: Student[] = studentsNew.sort((x, y) => {
+        return (order === 'asc' ? x[sortBy] - y[sortBy]
+          : y[sortBy] - x[sortBy]);
+      });
+
+      return result;
+    }
+
+    case 'name':
+    /* falls through */
+
+    case 'surname': {
+      const result: Student[] = studentsNew.sort((x, y) => {
+        return (order === 'asc' ? x[sortBy].localeCompare(y[sortBy])
+          : y[sortBy].localeCompare(x[sortBy]));
+      });
+
+      return result;
+    }
+
+    case 'grades': {
+      const result: Student[] = studentsNew.sort((x, y) => {
+        return (
+          order === 'asc'
+            ? (x[sortBy].reduce((a, summ) => a + summ)) / x[sortBy].length
+          - (y[sortBy].reduce((b, summ) => b + summ)) / y[sortBy].length
+            : (y[sortBy].reduce((b, summ) => b + summ)) / y[sortBy].length
+          - (x[sortBy].reduce((a, summ) => a + summ)) / x[sortBy].length
+        );
+      });
+
+      return result;
+    }
+
+    default: {
+      const result: Student[] = studentsNew.sort((x, y) => {
+        return (order === 'asc' ? Number(x[sortBy]) - Number(y[sortBy])
+          : Number(y[sortBy]) - Number(x[sortBy]));
+      });
+
+      return result;
+    }
+  }
 }
