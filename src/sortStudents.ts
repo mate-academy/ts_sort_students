864708b5b155375cf91b7c9bd9cea69fad951487
Vladyslav Orlenko <vlad.pos1bl@gmail.com>
2023-06-18 16:17:44 +0300

    task done

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,55 @@
-
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name,
+  Surname,
+  Age,
+  Married,
+  AverageGrade
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
-
-export function sortStudents(students, sortBy, order) {
-  // write your function
+export function sortStudents(students: Student[],
+  sortBy: SortType, order: SortOrder): Student[] {
+  switch (sortBy) {
+    case SortType.Name:
+      return order === 'asc'
+        ? students
+          .sort((std1, std2) => std1.name.localeCompare(std2.name))
+        : students
+          .sort((std1, std2) => std2.name.localeCompare(std1.name));
+    case SortType.Surname:
+      return order === 'asc'
+        ? students
+          .sort((std1, std2) => std1.surname.localeCompare(std2.surname))
+        : students
+          .sort((std1, std2) => std2.surname.localeCompare(std1.surname));
+    case SortType.Age:
+      return order === 'asc'
+        ? students.sort((std1, std2) => std1.age - std2.age)
+        : students.sort((std1, std2) => std2.age - std1.age);
+    case SortType.Married:
+      return order === 'asc'
+        ? students.sort((std1, std2) => +std1.married - +std2.married)
+        : students.sort((std1, std2) => +std2.married - +std1.married);
+    case SortType.AverageGrade:
+      return order === 'asc'
+        ? students.sort((std1, std2) => {
+          return (std1.grades.reduce((a, b) => a + b) / std1.grades.length)
+            - (std2.grades.reduce((a, b) => a + b) / std2.grades.length);
+        })
+        : students.sort((std1, std2) => {
+          return (std2.grades.reduce((a, b) => a + b) / std2.grades.length)
+            - (std1.grades.reduce((a, b) => a + b) / std1.grades.length);
+        });
+    default:
+      return students;
+  }
 }
