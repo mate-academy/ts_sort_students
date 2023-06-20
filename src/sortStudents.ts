241fce940f4708c25aa49ac9d36fcba8ac0356b4
Adrian Yevchuk <adrianyevchuk@MacBook-Air-Adrian.local>
2023-06-21 00:27:33 +0300

    solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,75 @@
-
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name,
+  Surname,
+  Age,
+  Married,
+  AverageGrade,
 }
 
 // create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+function calculateAvarage(student: Student): number {
+  const sumGrades = student.grades.reduce((sum, grade) => sum + grade);
+  const avg = sumGrades / student.grades.length;
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  return avg;
+}
+
+type SortStudentsType = string | boolean | number;
+
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const sortedStudents = [...students].sort((a, b) => {
+    let aVal: SortStudentsType;
+    let bVal: SortStudentsType;
+
+    switch (sortBy) {
+      case SortType.Name:
+        aVal = a.name;
+        bVal = b.name;
+        break;
+      case SortType.Surname:
+        aVal = a.surname;
+        bVal = b.surname;
+        break;
+      case SortType.Age:
+        aVal = a.age;
+        bVal = b.age;
+        break;
+      case SortType.Married:
+        aVal = a.married;
+        bVal = b.married;
+        break;
+      case SortType.AverageGrade:
+        aVal = calculateAvarage(a);
+        bVal = calculateAvarage(b);
+        break;
+      default:
+        break;
+    }
+
+    if (aVal < bVal) {
+      return order === 'asc' ? -1 : 1;
+    }
+
+    if (aVal > bVal) {
+      return order === 'asc' ? 1 : -1;
+    }
+
+    return 0;
+  });
+
+  return sortedStudents;
 }
