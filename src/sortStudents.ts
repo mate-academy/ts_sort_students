f7b81d39c4240469d556def7167f74d9fa93f6a6
Artem Shuhailo <artem0462@gmail.com>
2023-06-21 14:55:16 +0300

    solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,73 @@
 
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[],
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name,
+  Surname,
+  Age,
+  Married,
+  AverageGrade,
 }
 
 // create SortOrder type
-export type SortOrder;
+export type SortOrder = {
+  asc: string,
+  desc: string,
+};
 
+function calculateAverageGrade(grades: number[]): number {
+  if (grades.length === 0) {
+    return 0;
+  }
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  const sum = grades.reduce((total, grade) => total + grade, 0);
+
+  return sum / grades.length;
+}
+
+export function sortStudents(students: Student[], sortBy: SortType,
+  order: string): Student[] {
+  const newStudents: Student[] = [...students];
+
+  newStudents.sort((firstStudent: Student, secondStudent: Student) => {
+    switch (sortBy) {
+      case SortType.Name:
+        return order === 'asc'
+          ? firstStudent.name.localeCompare(secondStudent.name)
+          : secondStudent.name.localeCompare(firstStudent.name);
+
+      case SortType.Surname:
+        return order === 'asc'
+          ? firstStudent.surname.localeCompare(secondStudent.surname)
+          : secondStudent.surname.localeCompare(firstStudent.surname);
+
+      case SortType.Age:
+        return order === 'asc'
+          ? firstStudent.age - secondStudent.age
+          : secondStudent.age - firstStudent.age;
+
+      case SortType.Married:
+        return order === 'asc'
+          ? +firstStudent.married - +secondStudent.married
+          : +secondStudent.married - +firstStudent.married;
+
+      case SortType.AverageGrade:
+        return order === 'asc'
+          ? calculateAverageGrade(firstStudent.grades)
+          - calculateAverageGrade(secondStudent.grades)
+          : calculateAverageGrade(secondStudent.grades)
+          - calculateAverageGrade(firstStudent.grades);
+
+      default:
+        return 0;
+    }
+  });
+
+  return newStudents;
 }
