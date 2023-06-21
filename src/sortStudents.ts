9a843c0854b82b71f10633ab8d935f78f0a87c6d
Vitalii Fedusov <vitalii.fedusov@gmail.com>
2023-06-21 21:22:34 +0300

    Solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,91 @@
 
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name,
+  Surname,
+  Age,
+  Married,
+  AverageGrade,
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const copy = [...students];
+  const callback = (acc: number, current: number): number => {
+    return acc + current;
+  };
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  switch (sortBy) {
+    case SortType.Name:
+      if (order === 'asc') {
+        return copy.sort((a, b) => {
+          return a.name.localeCompare(b.name);
+        });
+      }
+
+      return copy.sort((a, b) => {
+        return b.name.localeCompare(a.name);
+      });
+
+    case SortType.Surname:
+      if (order === 'asc') {
+        return copy.sort((a, b) => {
+          return a.surname.localeCompare(b.surname);
+        });
+      }
+
+      return copy.sort((a, b) => {
+        return b.surname.localeCompare(a.surname);
+      });
+
+    case SortType.Age:
+      if (order === 'asc') {
+        return copy.sort((a, b) => {
+          return a.age - b.age;
+        });
+      }
+
+      return copy.sort((a, b) => {
+        return b.age - a.age;
+      });
+
+    case SortType.Married:
+      if (order === 'asc') {
+        return copy.sort((a, b) => {
+          return a.married - b.married;
+        });
+      }
+
+      return copy.sort((a, b) => {
+        return b.married - a.married;
+      });
+
+    case SortType.AverageGrade:
+      if (order === 'asc') {
+        return copy.sort((a, b) => {
+          return (a.grades.reduce(callback, 0) / a.grades.length)
+          - (b.grades.reduce(callback, 0) / b.grades.length);
+        });
+      }
+
+      return copy.sort((a, b) => {
+        return (b.grades.reduce(callback, 0) / b.grades.length)
+        - (a.grades.reduce(callback, 0) / a.grades.length);
+      });
+
+    default:
+      return students;
+  }
 }
