b5d1bfa128af8bfa24467acb3c7a2eb2b661b7c3
Melnychuk Vova <kivertsivova@gmail.com>
2023-06-23 17:45:56 +0300

    add task solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,86 @@
 
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[]
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades'
 }
 
 // create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const studentsArr: Student[] = [...students];
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  let averageA: number;
+  let averageB: number;
+
+  studentsArr.sort((a: Student, b: Student) => {
+    switch (sortBy) {
+      case SortType.Name:
+        if (order === 'asc') {
+          return a.name.localeCompare(b.name);
+        }
+
+        if (order === 'desc') {
+          return b.name.localeCompare(a.name);
+        }
+
+        return 0;
+
+      case SortType.Surname:
+        if (order === 'asc') {
+          return a.surname.localeCompare(b.surname);
+        }
+
+        if (order === 'desc') {
+          return b.surname.localeCompare(a.surname);
+        }
+
+        return 0;
+
+      case SortType.Age:
+        if (order === 'asc') {
+          return a.age - b.age;
+        }
+
+        return b.age - a.age;
+
+      case SortType.Married:
+        return order === 'asc'
+          ? +(a[sortBy]) - +(b[sortBy])
+          : +(b[sortBy]) - +(a[sortBy]);
+
+      case SortType.AverageGrade:
+        averageA
+        = a.grades.reduce((acc, value) => acc + value, 0) / a.grades.length;
+
+        averageB
+        = b.grades.reduce((acc, value) => acc + value, 0) / b.grades.length;
+
+        if (order === 'asc') {
+          return averageA - averageB;
+        }
+
+        return averageB - averageA;
+
+      default:
+        return 0;
+    }
+  });
+
+  return studentsArr;
 }
