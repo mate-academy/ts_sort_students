32de6bfdad204415df1a71cb47ec058948f9af4a
denys2 <brownexpert@gmail.com>
2023-06-27 20:13:22 +0300

    add task solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,79 @@
-
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name,
+  Surname,
+  Age,
+  Married,
+  AverageGrade,
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(
+  students: Student[], sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  // Create a new array to hold the sorted students
+  const sortedStudents = [...students];
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  // Define the sorting logic based on the provided sortBy parameter
+  switch (sortBy) {
+    case SortType.Name:
+      sortedStudents.sort((a, b) => a.name.localeCompare(b.name));
+      break;
+    case SortType.Surname:
+      sortedStudents.sort((a, b) => a.surname.localeCompare(b.surname));
+      break;
+    case SortType.Age:
+      sortedStudents.sort((a, b) => a.age - b.age);
+      break;
+    case SortType.Married:
+      sortedStudents.sort((a, b) => {
+        if (a.married === b.married) {
+          return 0;
+        }
+
+        if (order === 'asc') {
+          return a.married ? -1 : 1;
+        // eslint-disable-next-line no-else-return
+        } else {
+          return a.married ? 1 : -1;
+        }
+      });
+      break;
+    case SortType.AverageGrade:
+      sortedStudents.sort((a, b) => {
+        // eslint-disable-next-line @typescript-eslint/no-use-before-define
+        const averageGradeA = calculateAverageGrade(a);
+        // eslint-disable-next-line @typescript-eslint/no-use-before-define
+        const averageGradeB = calculateAverageGrade(b);
+
+        return averageGradeA - averageGradeB;
+      });
+      break;
+    default:
+      break;
+  }
+
+  // Reverse the array if the order is 'desc'
+  if (order === 'desc') {
+    sortedStudents.reverse();
+  }
+
+  return sortedStudents;
+}
+
+function calculateAverageGrade(student: Student): number {
+  const sum = student.grades.reduce(
+    (total, grade) => total + grade, 0,
+  );
+
+  return sum / student.grades.length;
 }
