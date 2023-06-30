dadb91d1d2258f78c34c291fc60649711774e373
denys2 <brownexpert@gmail.com>
2023-06-30 13:10:58 +0300

    add task solution 2

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,79 +1,54 @@
+
 export interface Student {
-  name: string;
-  surname: string;
-  age: number;
-  married: boolean;
-  grades: number[];
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[],
 }
 
 export enum SortType {
-  Name,
-  Surname,
-  Age,
-  Married,
-  AverageGrade,
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
 export type SortOrder = 'asc' | 'desc';
 
-export function sortStudents(
-  students: Student[], sortBy: SortType,
-  order: SortOrder,
-): Student[] {
-  // Create a new array to hold the sorted students
-  const sortedStudents = [...students];
-
-  // Define the sorting logic based on the provided sortBy parameter
-  switch (sortBy) {
-    case SortType.Name:
-      sortedStudents.sort((a, b) => a.name.localeCompare(b.name));
-      break;
-    case SortType.Surname:
-      sortedStudents.sort((a, b) => a.surname.localeCompare(b.surname));
-      break;
-    case SortType.Age:
-      sortedStudents.sort((a, b) => a.age - b.age);
-      break;
-    case SortType.Married:
-      sortedStudents.sort((a, b) => {
-        if (a.married === b.married) {
-          return 0;
-        }
-
-        if (order === 'asc') {
-          return a.married ? -1 : 1;
-        // eslint-disable-next-line no-else-return
-        } else {
-          return a.married ? 1 : -1;
-        }
-      });
-      break;
-    case SortType.AverageGrade:
-      sortedStudents.sort((a, b) => {
-        // eslint-disable-next-line @typescript-eslint/no-use-before-define
-        const averageGradeA = calculateAverageGrade(a);
-        // eslint-disable-next-line @typescript-eslint/no-use-before-define
-        const averageGradeB = calculateAverageGrade(b);
-
-        return averageGradeA - averageGradeB;
-      });
-      break;
-    default:
-      break;
-  }
-
-  // Reverse the array if the order is 'desc'
-  if (order === 'desc') {
-    sortedStudents.reverse();
-  }
-
-  return sortedStudents;
+function averageGradeCulc(grades: number[]): number {
+  return grades.reduce((acc, grade) => acc + grade, 0) / (grades.length || 1);
 }
 
-function calculateAverageGrade(student: Student): number {
-  const sum = student.grades.reduce(
-    (total, grade) => total + grade, 0,
-  );
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const sortedStudents = [...students];
 
-  return sum / student.grades.length;
+  switch (sortBy) {
+    case SortType.Name:
+    case SortType.Surname:
+      return order === 'asc'
+        ? sortedStudents.sort((a, b) => a[sortBy].localeCompare(b[sortBy]))
+        : sortedStudents.sort((a, b) => b[sortBy].localeCompare(a[sortBy]));
+
+    case SortType.Married:
+    case SortType.Age:
+      return order === 'asc'
+        ? sortedStudents.sort((a, b) => +a[sortBy] - +b[sortBy])
+        : sortedStudents.sort((a, b) => +b[sortBy] - +a[sortBy]);
+
+    case SortType.AverageGrade:
+      return order === 'asc'
+        ? sortedStudents.sort((a, b) => averageGradeCulc(a[sortBy])
+          - averageGradeCulc(b[sortBy]))
+        : sortedStudents.sort((a, b) => averageGradeCulc(b[sortBy])
+          - averageGradeCulc(a[sortBy]));
+
+    default:
+      return sortedStudents;
+  }
 }
