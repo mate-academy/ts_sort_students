876ec8585266f735a1d4303f85d15c2f1e395e3d
Andrii Khomiak <andr.hamster@gmail.com>
2023-07-01 16:47:01 +0300

    task done

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,60 @@
-
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+function calcAvg(grades: number[]): number {
+  return grades.reduce((acc, item) => acc + item, 0) / grades.length;
+}
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const sortedStudents: Student[] = [...students];
+
+  // eslint-disable-next-line default-case
+  switch (sortBy) {
+    case SortType.Name:
+    case SortType.Surname:
+      if (order === 'asc') {
+        sortedStudents.sort((a, b) => a[sortBy].localeCompare(b[sortBy]));
+      } else {
+        sortedStudents.sort((a, b) => b[sortBy].localeCompare(a[sortBy]));
+      }
+      break;
+
+    case SortType.Age:
+    case SortType.Married:
+      if (order === 'asc') {
+        sortedStudents.sort((a, b) => +a[sortBy] - +b[sortBy]);
+      } else {
+        sortedStudents.sort((a, b) => +b[sortBy] - +a[sortBy]);
+      }
+      break;
+
+    case SortType.AverageGrade:
+      if (order === 'asc') {
+        sortedStudents.sort((a, b) => calcAvg(a[sortBy]) - calcAvg(b[sortBy]));
+      } else {
+        sortedStudents.sort((a, b) => calcAvg(b[sortBy]) - calcAvg(a[sortBy]));
+      }
+      break;
+  }
+
+  return sortedStudents;
 }
