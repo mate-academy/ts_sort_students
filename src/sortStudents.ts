f746ece9f0ce3335bc49981f6e4b0830a8597131
alex <cebanoleksandr@gmail.com>
2023-07-03 15:01:41 +0300

    solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,73 @@
 
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[],
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
 // create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder
+): Student[] {
+  const sortedStudents = [...students];
+
+  switch (sortBy) {
+    case 'name':
+    case 'surname':
+      return sortedStudents
+        .sort((student1, student2) => {
+          return order === 'asc'
+            ? student1[sortBy].localeCompare(student2[sortBy])
+            : student2[sortBy].localeCompare(student1[sortBy]);
+        });
+
+    case 'age':
+      return sortedStudents
+        .sort((student1, student2) => {
+          return order === 'asc'
+            ? student1[sortBy] - student2[sortBy]
+            : student2[sortBy] - student1[sortBy]
+        });
+
+    case 'married':
+      const marriedStudents = sortedStudents
+        .filter(student => student[sortBy] === true);
+      const notMarriedStudents = sortedStudents
+        .filter(student => student[sortBy] === false);
+
+      return order === 'asc'
+        ? [...marriedStudents, ...notMarriedStudents]
+        : [...notMarriedStudents, ...marriedStudents];
+
+    case 'grades':
+      return sortedStudents
+        .sort((student1, student2) => {
+          const first = ([...student1[sortBy]]
+            .reduce((a, b) => a + b, 0) / student1[sortBy].length) || 0;
+          const second = ([...student2[sortBy]]
+            .reduce((a, b) => a + b, 0) / student1[sortBy].length) || 0;
+
+          return order === 'asc'
+            ? first - second
+            : second - first;
+        });
+
+    default:
+      return sortedStudents;
+  }
 }
