85358ba257432d5833633846fe03df7fb78a9a88
Antonina Klishch <tonya91t@gmail.com>
2023-07-04 16:01:01 +0300

    Solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,71 @@
 
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean,
+  grades: number[],
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name,
+  Surname,
+  Age,
+  Married,
+  AverageGrade,
 }
 
 // create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+function getAverageGrade(grades: number[]): number {
+  return grades.reduce((sum, mark) => sum + mark, 0) / grades.length;
+}
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+function sortArray(firstData: string | number, secondData: string | number,
+  direction: string): number {
+  let result: number = 0;
+
+  if (typeof firstData === 'string' && typeof secondData === 'string') {
+    if (direction === 'asc') {
+      result = firstData.localeCompare(secondData);
+    } else {
+      result = secondData.localeCompare(secondData);
+    }
+  }
+
+  if (typeof firstData === 'number' && typeof secondData === 'number') {
+    if (direction === 'asc') {
+      result = firstData - secondData;
+    } else {
+      result = secondData - firstData;
+    }
+  }
+
+  return result;
+}
+
+export function sortStudents(students: Student[], sortBy: SortType,
+  order: SortOrder): Student[] {
+  const newStudents: Student[] = [...students];
+
+  newStudents.sort((firstStudent: Student, secondStudent: Student) => {
+    switch (sortBy) {
+      case SortType.Name:
+        return sortArray(firstStudent.name, secondStudent.name, order);
+      case SortType.Surname:
+        return sortArray(firstStudent.surname, secondStudent.surname, order);
+      case SortType.Age:
+        return sortArray(firstStudent.age, secondStudent.age, order);
+      case SortType.Married:
+        return sortArray(+firstStudent.married, +secondStudent.married, order);
+      case SortType.AverageGrade:
+        return sortArray(getAverageGrade(firstStudent.grades),
+          getAverageGrade(secondStudent.grades), order);
+      default:
+        return 0;
+    }
+  });
+
+  return newStudents;
 }
