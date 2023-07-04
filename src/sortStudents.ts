07aa400b3edc5432ae1782fd548d3da4c90e58bd
domkab <dominykas.bobinas@gmail.com>
2023-07-04 17:50:12 +0300

    1.0.0

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,51 @@
+import { log } from 'console';
 
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades'
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(students: Student[], sortBy: SortType, order: SortOrder) {
+  const studArr = [...students];
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
-}
+  const sorter = studArr.sort((a: Student, b: Student) => {
+    if (sortBy === SortType.AverageGrade) {
+      const avgA = a.grades.reduce((sum, val) => sum + val, 0) / a.grades.length;
+      const avgB = b.grades.reduce((sum, val) => sum + val, 0) / b.grades.length;
+
+      console.log(a, avgA);
+
+      return order === 'asc' ? avgA - avgB : avgB - avgA;
+    }
+    const [valA, valB] = [a[sortBy], b[sortBy]];
+
+    if (typeof valA === 'number' && typeof valB === 'number') {
+      return order === 'asc' ? valA - valB : valB - valA;
+    }
+
+    if (typeof valA === 'string' && typeof valB === 'string') {
+      return order === 'asc' ? valA.localeCompare(valB) : valB.localeCompare(valA);
+    }
+
+    if (typeof valA === 'boolean' && typeof valB === "boolean") {
+      return order === 'asc' ? Number(valA) - Number(valB) : Number(valB) - Number(valA) ;
+    }
+
+    return 0;
+  });
+
+  return sorter;
+};
