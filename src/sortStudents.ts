be5f6570a56b8e037661a0bf93ef5b7011c1d430
Serhii <gashysh@protonmail.com>
2023-07-07 00:05:30 +0300

    Solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,88 @@
 
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: Array<number>,
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name,
+  Surname,
+  Age,
+  Married,
+  AverageGrade,
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(
+  students: Array<Student>,
+  sortBy: SortType,
+  order: SortOrder,
+): Array<Student> {
+  const copy: Array<Student> = [...students];
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  switch (sortBy) {
+    case SortType.Name:
+      if (order === 'asc') {
+        copy.sort((a: Student, b: Student) => a.name.localeCompare(b.name));
+      } else {
+        copy.sort((a: Student, b: Student) => b.name.localeCompare(a.name));
+      }
+      break;
+    case SortType.Surname:
+      copy.sort((a: Student, b: Student) => {
+        if (order === 'asc') {
+          return a.surname.localeCompare(b.surname);
+        }
+
+        if (order === 'desc') {
+          return b.surname.localeCompare(a.surname);
+        }
+
+        return 0;
+      });
+      break;
+    case SortType.Age:
+      if (order === 'asc') {
+        copy.sort((a: Student, b: Student) => a.age - b.age);
+      } else {
+        copy.sort((a: Student, b: Student) => b.age - a.age);
+      }
+      break;
+    case SortType.Married:
+      if (order === 'asc') {
+        copy.sort((a: Student, b: Student) => a.married - b.married);
+      } else {
+        copy.sort((a: Student, b: Student) => b.married - a.married);
+      }
+      break;
+    case SortType.AverageGrade:
+      copy.sort((a: Student, b: Student) => {
+        const averageA = a.grades.reduce((prev, grade) => {
+          return grade + prev;
+        }, a.grades[0]) / a.grades.length;
+
+        const averageB = b.grades.reduce((prev, grade) => {
+          return grade + prev;
+        }, b.grades[0]) / b.grades.length;
+
+        if (order === 'asc') {
+          return averageA - averageB;
+        }
+
+        if (order === 'desc') {
+          return averageB - averageA;
+        }
+
+        return 0;
+      });
+      break;
+    default:
+      return copy;
+  }
+
+  return copy;
 }
