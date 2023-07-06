ad086e4f474aaa9da3b95a70889a17fddd632e4a
Iryna Shtynda <ira.shtynda@gmail.com>
2023-07-06 22:33:41 +0300

    add Solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,52 @@
 
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[],
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+function calcAvgGrades({ grades }: Student): number {
+  return grades.reduce((avgAcc, currGrade) => (
+    avgAcc + currGrade)) / grades.length;
+}
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const orderCondition = (order === 'asc' ? 1 : -1);
+
+  return [...students].sort((currStud, prevStud): number => {
+    switch (sortBy) {
+      case SortType.Name:
+      case SortType.Surname:
+        return currStud[sortBy].localeCompare(prevStud[sortBy])
+          * orderCondition;
+
+      case SortType.Age:
+      case SortType.Married:
+        return (+(currStud[sortBy]) - +(prevStud[sortBy]))
+          * orderCondition;
+
+      case SortType.AverageGrade:
+        return (calcAvgGrades(currStud) - calcAvgGrades(prevStud))
+          * orderCondition;
+
+      default:
+        throw new Error('Sort type is invalid');
+    }
+  });
 }
