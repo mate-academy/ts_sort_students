905fe7b62b1db25664862f3fd276721de78eb953
Ivan Bilotserkivskiy <ivanbilotserkivskiy56@gmail.com>
2023-07-06 19:14:47 +0300

    sortSolution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,73 @@
 
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[],
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'average',
 }
 
 // create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+function calculateAverageGrades(grades: number[]): number {
+  return grades.reduce((acc, cur) => acc + cur, 0) / grades.length;
+}
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const preparedStudents = [...students];
+
+  if (sortBy && order === 'asc') {
+    preparedStudents.sort((student1, student2) => {
+      switch (sortBy) {
+        case SortType.Name:
+          return student1.name.localeCompare(student2.name);
+        case SortType.Surname:
+          return student1.surname.localeCompare(student2.surname);
+        case SortType.Age:
+          return student1.age - student2.age;
+        case SortType.Married:
+          return Number(student1.married) - Number(student2.married);
+        case SortType.AverageGrade:
+          return calculateAverageGrades(student1.grades)
+            - calculateAverageGrades(student2.grades);
+        default:
+          return 0;
+      }
+    });
+  }
+
+  if (sortBy && order === 'desc') {
+    preparedStudents.sort((student1, student2) => {
+      switch (sortBy) {
+        case SortType.Name:
+          return student2.name.localeCompare(student1.name);
+        case SortType.Surname:
+          return student2.surname.localeCompare(student1.surname);
+        case SortType.Age:
+          return student2.age - student1.age;
+        case SortType.Married:
+          return Number(student2.married) - Number(student1.married);
+        case SortType.AverageGrade:
+          return calculateAverageGrades(student2.grades)
+            - calculateAverageGrades(student1.grades);
+        default:
+          return 0;
+      }
+    });
+  }
+
+  return preparedStudents;
 }
