51153cc7ae077557d88fc5ce4e743c3677b4b7c2
Daniil Postoiuk <daniil.postoyuk@gmail.com>
2023-07-08 12:57:45 +0300

    add task solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,59 @@
-
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+function averageMark({ grades }: { grades: number[] }): number {
+  return grades.reduce((acc, mark) => acc + mark, 0) / grades.length;
+}
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const copyStudents = [...students];
+
+  return copyStudents.sort((a, b): number => {
+    switch (sortBy) {
+      case SortType.Name:
+      case SortType.Surname:
+        if (order === 'asc') {
+          return a[sortBy].localeCompare(b[sortBy]);
+        }
+
+        return a[sortBy].localeCompare(b[sortBy]);
+
+      case SortType.Age:
+      case SortType.Married:
+        if (order === 'asc') {
+          return Number(a[sortBy]) - Number(b[sortBy]);
+        }
+
+        return Number(b[sortBy]) - Number(a[sortBy]);
+
+      case SortType.AverageGrade:
+        if (order === 'asc') {
+          return averageMark(a) - averageMark(b);
+        }
+
+        return averageMark(b) - averageMark(a);
+
+      default:
+        throw new Error('Enter valid sort type');
+    }
+  });
 }
