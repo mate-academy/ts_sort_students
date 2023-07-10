5db16d257cab048682952b6c38af2b6e63eab327
Nazar Baraban <barabannazar44@gmail.com>
2023-07-10 14:02:11 +0300

    made code dryer)

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,9 +1,9 @@
 export enum SortType {
-  Name,
-  Surname,
-  Age,
-  Married,
-  AverageGrade,
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
 export type SortOrder = 'asc' | 'desc';
@@ -28,33 +28,25 @@ export function sortStudents(
   order: SortOrder,
 ): Student[] {
   const sortedStudents = [...students];
+  const sortOrder: number = order === 'asc' ? 1 : -1;
 
-  switch (sortBy) {
-    case SortType.Name:
-      return order === 'asc'
-        ? sortedStudents.sort((a, b) => a.name.localeCompare(b.name))
-        : sortedStudents.sort((a, b) => b.name.localeCompare(a.name));
-    case SortType.Surname:
-      return order === 'asc'
-        ? sortedStudents.sort((a, b) => a.surname.localeCompare(b.surname))
-        : sortedStudents.sort((a, b) => b.surname.localeCompare(a.surname));
-    case SortType.Age:
-      return order === 'asc'
-        ? sortedStudents.sort((a, b) => +a.age - +b.age)
-        : sortedStudents.sort((a, b) => +b.age - +a.age);
-    case SortType.Married:
-      return order === 'asc'
-        ? sortedStudents.sort((a, b) => +a.married - +b.married)
-        : sortedStudents.sort((a, b) => +b.married - +a.married);
-    case SortType.AverageGrade:
-      return order === 'asc'
-        ? sortedStudents.sort(
-          (a, b) => getAverageGrade(a.grades) - getAverageGrade(b.grades),
-        )
-        : sortedStudents.sort(
-          (a, b) => getAverageGrade(b.grades) - getAverageGrade(a.grades),
-        );
-    default:
-      return sortedStudents;
-  }
+  return sortedStudents.sort((a, b): number => {
+    switch (sortBy) {
+      case SortType.Name:
+      case SortType.Surname:
+        return a[sortBy].localeCompare(b[sortBy]) * sortOrder;
+
+      case SortType.Age:
+      case SortType.Married:
+        return (+a[sortBy] - +b[sortBy]) * sortOrder;
+
+      case SortType.AverageGrade:
+        return (
+          getAverageGrade(a.grades) - getAverageGrade(b.grades)
+        ) * sortOrder;
+
+      default:
+        return 0;
+    }
+  });
 }
