19342af519f0ecd0bea4d8d052894067428b062c
Valeriy Lopatko <vlopatko1999@gmail.com>
2023-07-11 00:59:07 +0300

    fixing by comments

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,54 +1,54 @@
 
 export interface Student {
-  name: 'string',
-  surname: 'string',
+  name: string,
+  surname: string,
   age: number,
   married: boolean,
   grades: number[],
 }
 
 export enum SortType {
-  Name,
-  Surname,
-  Age,
-  Married,
-  AverageGrade,
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'average',
 }
+type SortOrder = 'asc' | 'desc';
 
-// create SortOrder type
-export type SortOrder = 'asc' | 'desc';
+const getAverage = (arr: number[]): number => {
+  return arr.reduce((a, b) => a + b) / arr.length;
+};
 
 export function sortStudents(
   students: Student[],
   sortBy: SortType,
   order: SortOrder,
-): object[] {
+): Student[] {
   const copyOfStudents = [...students];
-  const getAverage = (arr: number[]): number => {
-    return arr.reduce((a, b) => a + b) / arr.length;
-  };
 
-  switch (sortBy) {
-    case SortType.Name:
-      return copyOfStudents.sort((a, b) => a.name.localeCompare(b.name));
-    case SortType.Surname:
-      return copyOfStudents.sort((a, b) => a.surname.localeCompare(b.surname));
-    case SortType.Age:
-      return copyOfStudents.sort((a, b) => (order === 'asc'
-        ? a.age - b.age
-        : b.age - a.age
-      ));
-    case SortType.Married:
-      return copyOfStudents.sort((a, b) => (order === 'asc'
-        ? Number(a.married) - Number(b.married)
-        : Number(b.married) - Number(a.married)
-      ));
-    case SortType.AverageGrade:
-      return copyOfStudents.sort((a, b) => (
-        order === 'asc'
+  copyOfStudents.sort((a, b) => {
+    switch (sortBy) {
+      case SortType.Name:
+      case SortType.Surname:
+        return order === 'asc'
+          ? a[sortBy].localeCompare(b[sortBy])
+          : b[sortBy].localeCompare(a[sortBy]);
+      case SortType.Age:
+        return order === 'asc'
+          ? a.age - b.age
+          : b.age - a.age;
+      case SortType.Married:
+        return order === 'asc'
+          ? Number(a.married) - Number(b.married)
+          : Number(b.married) - Number(a.married);
+      case SortType.AverageGrade:
+        return order === 'asc'
           ? getAverage(a.grades) - getAverage(b.grades)
-          : getAverage(b.grades) - getAverage(a.grades)
-      ));
-    default: return copyOfStudents;
-  }
+          : getAverage(b.grades) - getAverage(a.grades);
+      default: return 0;
+    }
+  });
+
+  return copyOfStudents;
 }
