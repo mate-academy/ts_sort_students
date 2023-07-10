0c3b6de53e3e3b7934b3fb594695632992983f6e
Zoriana Melnychuk <zoryashka3002@gmail.com>
2023-07-10 12:15:44 +0200

    add some logic

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -8,11 +8,11 @@ export interface Student {
 }
 
 export enum SortType {
-  Name,
-  Surname,
-  Age,
-  Married,
-  AverageGrade,
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
 export type SortOrder = 'asc' | 'desc';
@@ -30,47 +30,16 @@ export function sortStudents(
 
   switch (sortBy) {
     case SortType.Name:
-      sortedStudents.sort(
-        (a, b) => (order === 'asc'
-          ? a.name.localeCompare(b.name)
-          : b.name.localeCompare(a.name)
-        ),
-      );
-      break;
-
     case SortType.Surname:
-      sortedStudents.sort(
-        (a, b) => (order === 'asc'
-          ? a.surname.localeCompare(b.surname)
-          : b.surname.localeCompare(a.surname)
-        ),
-      );
-      break;
+      return order === 'asc'
+        ? sortedStudents.sort((a, b) => a[sortBy].localeCompare(b[sortBy]))
+        : sortedStudents.sort((a, b) => b[sortBy].localeCompare(a[sortBy]));
 
     case SortType.Age:
-      sortedStudents.sort(
-        (a, b) => (order === 'asc'
-          ? a.age - b.age
-          : b.age - a.age
-        ),
-      );
-      break;
-
     case SortType.Married:
-      sortedStudents.sort(
-        (a, b) => {
-          if (a.married === b.married) {
-            return 0;
-          }
-
-          if ((a.married && order === 'asc') || b.married) {
-            return 1;
-          }
-
-          return -1;
-        },
-      );
-      break;
+      return order === 'asc'
+        ? sortedStudents.sort((a, b) => +a[sortBy] - +b[sortBy])
+        : sortedStudents.sort((a, b) => +b[sortBy] - +a[sortBy]);
 
     case SortType.AverageGrade:
       sortedStudents.sort(
