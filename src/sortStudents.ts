785caca6e6f447461cdb0f3055deb8407dece0ea
david.shamiiev <david.shamiiev@rad4m.com>
2023-07-11 12:39:03 +0300

    solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,66 @@
-
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[],
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name,
+  Surname,
+  Age,
+  Married,
+  AverageGrade,
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  function averageMark({ grades }: Student): number {
+    return grades.reduce((acc, cur) => acc + cur, 0) / grades.length;
+  }
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  switch (sortBy) {
+    case SortType.Name:
+    case SortType.Surname:
+      return (order === 'asc')
+        ? students.sort(
+          (a: Student, b: Student) => a[sortBy].localeCompare(b[sortBy]),
+        )
+
+        : students.sort(
+          (a: Student, b: Student) => b[sortBy].localeCompare(a[sortBy]),
+        );
+
+    case SortType.Age:
+      return (order === 'asc')
+        ? students.sort((a: Student, b: Student) => a.age - b.age)
+        : students.sort((a: Student, b: Student) => b.age - a.age);
+
+    case SortType.Married:
+      return (order === 'asc')
+        ? students.sort(
+          (a: Student, b: Student) => Number(a.married) - Number(b.married),
+        )
+        : students.sort(
+          (a: Student, b: Student) => Number(b.married) - Number(a.married),
+        );
+
+    case SortType.AverageGrade:
+      return (order === 'asc')
+        ? students.sort(
+          (a: Student, b: Student) => averageMark(a) - averageMark(b),
+        )
+        : students.sort(
+          (a: Student, b: Student) => averageMark(b) - averageMark(a),
+        );
+
+    default:
+      throw new Error('Something went wrong!');
+  }
 }
