1b2988eeab96108856f6e7d1fd3d2b18ed036802
Anton Ustavshchykov <unton84@gmail.com>
2023-07-14 15:10:16 +0300

    fixes

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -17,45 +17,41 @@ export enum SortType {
 
 export type SortOrder = 'asc' | 'desc';
 
-export function sortStudents(students: Student[],
-  sortBy: SortType, order: SortOrder): Student[] {
-  const avgGradeMethod = (e1: Student, e2: Student): number => {
-    return e1.grades.reduce((sum, x): number => sum + x, 0)
-           / e1.grades.length - e2.grades.reduce((sum, x): number => sum + x, 0)
-           / e2.grades.length;
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const avgGradeMethod = (grades: number[]): number => {
+    return grades.reduce((sum, x): number => sum + x, 0) / grades.length;
   };
 
   const CompareFn = (a: Student, b: Student): number => {
-    switch (true) {
-      case sortBy === SortType.Name && order === 'asc':
-        return (a.name).localeCompare(b.name);
+    switch (sortBy) {
+      case SortType.Name:
+        return order === 'asc'
+          ? (a.name).localeCompare(b.name)
+          : (b.name).localeCompare(a.name);
 
-      case sortBy === SortType.Name && order === 'desc':
-        return (b.name).localeCompare(a.name);
+      case SortType.Surname:
+        return order === 'asc'
+          ? (a.surname).localeCompare(b.surname)
+          : (b.surname).localeCompare(a.surname);
 
-      case sortBy === SortType.Surname && order === 'asc':
-        return (a.surname).localeCompare(b.surname);
+      case SortType.Age:
+        return order === 'asc'
+          ? a.age - b.age
+          : b.age - a.age;
 
-      case sortBy === SortType.Surname && order === 'desc':
-        return (b.surname).localeCompare(a.surname);
+      case SortType.Married:
+        return order === 'asc'
+          ? Number(a.married) - Number(b.married)
+          : Number(b.married) - Number(a.married);
 
-      case sortBy === SortType.Age && order === 'asc':
-        return a.age - b.age;
-
-      case sortBy === SortType.Age && order === 'desc':
-        return b.age - a.age;
-
-      case sortBy === SortType.Married && order === 'asc':
-        return Number(a.married) - Number(b.married);
-
-      case sortBy === SortType.Married && order === 'desc':
-        return Number(b.married) - Number(a.married);
-
-      case sortBy === SortType.AverageGrade && order === 'asc':
-        return avgGradeMethod(a, b);
-
-      case sortBy === SortType.AverageGrade && order === 'desc':
-        return avgGradeMethod(b, a);
+      case SortType.AverageGrade:
+        return order === 'asc'
+          ? avgGradeMethod(a.grades) - avgGradeMethod(b.grades)
+          : avgGradeMethod(b.grades) - avgGradeMethod(a.grades);
 
       default:
         return 0;
