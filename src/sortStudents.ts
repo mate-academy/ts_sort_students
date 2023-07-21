844ab24b82c695ee0ab6b9eb8c103dfb7e96488b
Jane Shavrukova <eva.shavrukova@gmail.com>
2023-07-21 13:52:53 -0700

    Solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,81 @@
 
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[],
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+function average(values: number[]): number {
+  return values
+    .reduce((total: number, value: number) => total + value) / values.length;
+}
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const studentsSort = [...students];
+
+  switch (sortBy) {
+    case SortType.Name:
+    case SortType.Surname:
+      studentsSort.sort((studentOne: Student, studentTwo: Student) => {
+        if (order === 'asc') {
+          return studentOne[sortBy].localeCompare(studentTwo[sortBy]);
+        }
+
+        if (order === 'desc') {
+          return studentTwo[sortBy].localeCompare(studentOne[sortBy]);
+        }
+
+        return 0;
+      });
+      break;
+
+    case SortType.Age:
+    case SortType.Married:
+      studentsSort.sort((studentOne: Student, studentTwo: Student): number => {
+        if (order === 'asc') {
+          return Number(studentOne[sortBy]) - Number(studentTwo[sortBy]);
+        }
+
+        if (order === 'desc') {
+          return Number(studentTwo[sortBy]) - Number(studentOne[sortBy]);
+        }
+
+        return 0;
+      });
+      break;
+
+    case SortType.AverageGrade:
+      studentsSort.sort((studentOne: Student, studentTwo: Student): number => {
+        if (order === 'asc') {
+          return average(studentOne[sortBy]) - average(studentTwo[sortBy]);
+        }
+
+        if (order === 'desc') {
+          return average(studentTwo[sortBy]) - average(studentOne[sortBy]);
+        }
+
+        return 0;
+      });
+      break;
+
+    default: break;
+  }
+
+  return studentsSort;
 }
