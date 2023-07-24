683e5a62cbd6e02723621631da6bac41679feffd
Oleksandr Shtonda <oleksandrshtonda@gmail.com>
2023-07-24 13:18:38 +0200

    fix mistakes

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -6,6 +6,10 @@ export interface Student {
   grades: number[];
 }
 
+interface CopiedStudent extends Student {
+  avgGrade: number
+}
+
 export enum SortType {
   Name,
   Surname,
@@ -22,8 +26,19 @@ export function sortStudents(
   order: SortOrder,
 ): Student[] {
   const orderNum: number = order === 'asc' ? 1 : -1;
+  const copiedStudents: CopiedStudent[] = [...students].map((student) => {
+    const lengthArr = student.grades.length;
+    const averageGrade
+      = student.grades.reduce((acc, grade) => acc + grade, 0) / lengthArr;
 
-  return students.sort((student1: Student, student2: Student): number => {
+    return Object.assign(
+      student, { avgGrade: Math.trunc(averageGrade * 100) / 100 },
+    );
+  });
+
+  return copiedStudents.sort((
+    student1: CopiedStudent, student2: CopiedStudent,
+  ): number => {
     switch (sortBy) {
       case SortType.Name:
         if (student1.name.toLowerCase() > student2.name.toLowerCase()) {
@@ -48,40 +63,25 @@ export function sortStudents(
         return 0;
 
       case SortType.Age:
-        if (student1.age > student2.age) {
-          return 1 * orderNum;
-        }
-
-        if (student1.age < student2.age) {
-          return -1 * orderNum;
-        }
-
-        return 0;
+        return orderNum * (student1.age - student2.age);
 
       case SortType.Married:
-        if (student1.married > student2.married) {
+        if (student1.married && !student2.married) {
           return 1 * orderNum;
         }
 
-        if (student1.married < student2.married) {
+        if (!student1.married && student2.married) {
           return -1 * orderNum;
         }
 
         return 0;
 
       case SortType.AverageGrade: {
-        const student1AverageGrade = student1.grades.reduce((acc, grade) => {
-          return acc + grade;
-        }, 0) / student1.grades.length;
-        const student2AverageGrade = student2.grades.reduce((acc, grade) => {
-          return acc + grade;
-        }, 0) / student2.grades.length;
-
-        if (student1AverageGrade > student2AverageGrade) {
+        if (student1.avgGrade > student2.avgGrade) {
           return 1 * orderNum;
         }
 
-        if (student1AverageGrade < student2AverageGrade) {
+        if (student1.avgGrade < student2.avgGrade) {
           return -1 * orderNum;
         }
 
