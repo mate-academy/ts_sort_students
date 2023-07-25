218e9af5b26cbf69f9c293afa9a99c09ab8cb880
Lukasz Warda <lukaszwarda1989@gmail.com>
2023-07-25 12:24:02 +0200

    Solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,94 @@
 
 export interface Student {
-  // describe Student interface
+  name: string
+  surname: string
+  age: number
+  married: boolean
+  grades: number[]
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades'
 }
 
 // create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+function averageGrade(x: number[]): number {
+  let gradeSum: number = 0;
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  // for (let i: number = 0; i < x.length; i++) {
+  //   gradeSum += x[i];
+  // }
+  x.forEach((grade) => {
+    gradeSum += grade;
+
+    return grade;
+  });
+
+  return gradeSum / x.length;
+}
+
+export function sortStudents(
+  students: Student[], sortBy: SortType, order: SortOrder,
+): Student[] {
+  const studentsCopy = [...students];
+
+  studentsCopy.sort((a: Student, b: Student) => {
+    if (sortBy === SortType.Name || sortBy === SortType.Surname) {
+      if (order === 'asc') {
+        return a[sortBy].localeCompare(b[sortBy]);
+      }
+
+      return b[sortBy].localeCompare(a[sortBy]);
+    }
+
+    if (sortBy === SortType.Age) {
+      if (order === 'asc') {
+        return a[sortBy] - b[sortBy];
+      }
+
+      return b[sortBy] - a[sortBy];
+    }
+
+    if (sortBy === SortType.Married) {
+      if (order === 'asc') {
+        if (b[sortBy] === a[sortBy]) {
+          return 0;
+        }
+
+        if (b[sortBy] && b[sortBy] !== a[sortBy]) {
+          return -1;
+        }
+
+        return 1;
+      }
+
+      if (a[sortBy] === b[sortBy]) {
+        return 0;
+      }
+
+      if (a[sortBy] && a[sortBy] !== b[sortBy]) {
+        return -1;
+      }
+
+      return 1;
+    }
+
+    if (sortBy === SortType.AverageGrade) {
+      if (order === 'asc') {
+        return averageGrade(a[sortBy]) - averageGrade(b[sortBy]);
+      }
+
+      return averageGrade(b[sortBy]) - averageGrade(a[sortBy]);
+    }
+
+    return 21;
+  });
+
+  return studentsCopy;
 }
