f5af3740c374b8b1e83abe851425994bd0518d3a
Max <maxpusto533gmail.com>
2023-07-27 18:53:57 +0300

    result

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,59 @@
 
 export interface Student {
   // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[],
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
 // create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
+// eslint-disable-next-line
+export function sortStudents(students: Student[], sortBy: SortType, order: SortOrder): Student[] {
+  const copy = [...students];
 
+  const getAverageGrade = (grades: number[]): number => {
+  // eslint-disable-next-line
+    return grades.reduce((grade1, grade2) => grade1 + grade2, 0) / grades.length;
+  };
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  switch (sortBy) {
+    case SortType.Age:
+      copy.sort((a, b) => (order === 'asc' ? a.age - b.age : b.age - a.age));
+      break;
+    case SortType.Name:
+      copy.sort((a, b) => (order === 'asc'
+        ? a.name.localeCompare(b.name)
+        : b.name.localeCompare(a.name)));
+      break;
+    case SortType.Surname:
+      copy.sort((a, b) => (order === 'asc'
+        ? a.surname.localeCompare(b.surname)
+        : b.surname.localeCompare(a.surname)));
+      break;
+    case SortType.Married:
+      copy.sort((a, b) => (order === 'asc'
+        ? Number(a.married) - Number(b.married)
+        : Number(b.married) - Number(a.married)));
+      break;
+    case SortType.AverageGrade:
+      copy.sort((a, b) => (order === 'asc'
+        ? getAverageGrade(a.grades) - getAverageGrade(b.grades)
+        : getAverageGrade(b.grades) - getAverageGrade(a.grades)));
+      break;
+    default:
+      return copy;
+  }
+
+  return copy;
 }
