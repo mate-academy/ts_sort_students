4193ffb5f502bbc655e2572468d1117afa6ba6d4
Volodymyr Alieksieiev <125214087+vovaAlieksieiev@users.noreply.github.com>
2023-07-31 12:03:47 +0200

    add task solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,64 @@
 
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[],
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+function average(values: number[]): number {
+  return values
+    .reduce((total: number, value: number) => total + value) / values.length;
+}
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const studentsSort = [...students];
+
+  // eslint-disable-next-line default-case
+  switch (sortBy) {
+    case SortType.Name:
+    case SortType.Surname:
+      studentsSort
+        .sort((studentOne: Student, studentTwo: Student) => (
+          order === 'asc'
+            ? studentOne[sortBy].localeCompare(studentTwo[sortBy])
+            : studentTwo[sortBy].localeCompare(studentOne[sortBy])));
+      break;
+
+    case SortType.Age:
+    case SortType.Married:
+      studentsSort
+        .sort((studentOne: Student, studentTwo: Student): number => (
+          order === 'asc'
+            ? Number(studentOne[sortBy]) - Number(studentTwo[sortBy])
+            : Number(studentTwo[sortBy]) - Number(studentOne[sortBy])));
+      break;
+
+    case SortType.AverageGrade:
+      studentsSort
+        .sort((studentOne: Student, studentTwo: Student): number => (
+          order === 'asc'
+            ? average(studentOne[sortBy]) - average(studentTwo[sortBy])
+            : average(studentTwo[sortBy]) - average(studentOne[sortBy])));
+      break;
+
+    default: break;
+  }
+
+  return studentsSort;
 }
