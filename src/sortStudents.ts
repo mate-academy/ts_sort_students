70c21ac2e453527d62dbf2ea80b98fd441135db5
Lukasz Warda <lukaszwarda1989@gmail.com>
2023-08-01 09:53:34 +0200

    Solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,4 +1,3 @@
-/* eslint-disable no-nested-ternary */
 
 export interface Student {
   name: string
@@ -20,15 +19,9 @@ export enum SortType {
 export type SortOrder = 'asc' | 'desc';
 
 function averageGrade(x: number[]): number {
-  let gradeSum: number = 0;
-
-  x.forEach((grade) => {
-    gradeSum += grade;
-
-    return grade;
-  });
-
-  return gradeSum / x.length;
+  return x.reduce((prev, cur) => {
+    return prev + cur;
+  }, 0) / x.length;
 }
 
 export function sortStudents(
@@ -36,33 +29,23 @@ export function sortStudents(
 ): Student[] {
   const studentsCopy = [...students];
 
+  const sortOrder = order === 'desc' ? -1 : 1;
+
   studentsCopy.sort((a: Student, b: Student) => {
     switch (sortBy) {
       case SortType.Name:
       case SortType.Surname:
-        return order === 'asc'
-          ? a[sortBy].localeCompare(b[sortBy])
-          : b[sortBy].localeCompare(a[sortBy]);
+        return sortOrder * a[sortBy].localeCompare(b[sortBy]);
       case SortType.Age:
-        return order === 'asc'
-          ? a[sortBy] - b[sortBy]
-          : b[sortBy] - a[sortBy];
+        return sortOrder * (a[sortBy] - b[sortBy]);
       case SortType.Married:
-        return order === 'asc'
-          ? b[sortBy] === a[sortBy]
-            ? 0
-            : b[sortBy] && b[sortBy] !== a[sortBy]
-              ? -1
-              : 1
-          : a[sortBy] === b[sortBy]
-            ? 0
-            : a[sortBy] && a[sortBy] !== b[sortBy]
-              ? -1
-              : 1;
+        if (a.married === b.married) {
+          return 0;
+        }
+
+        return sortOrder * (a.married ? 1 : -1);
       case SortType.AverageGrade:
-        return order === 'asc'
-          ? averageGrade(a[sortBy]) - averageGrade(b[sortBy])
-          : averageGrade(b[sortBy]) - averageGrade(a[sortBy]);
+        return sortOrder * (averageGrade(a[sortBy]) - averageGrade(b[sortBy]));
       default:
         return 1;
     }
