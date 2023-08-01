599b20d3d18546fb99c979b308b11554e13c3289
Katia Ilchenko <k17ilch@gmail.com>
2023-08-01 21:11:23 +0300

    Solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,105 @@
 
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[],
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
 // create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(
+  students: Student[],
+  sortBy: string,
+  order: string,
+): Student[] {
+  const sortedStudents: Student[] = [...students];
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  switch (sortBy) {
+    case SortType.Name:
+      return sortedStudents.sort(
+        (
+          student1: Student,
+          student2: Student,
+        ) => (
+          order === 'asc'
+            ? student1.name.localeCompare(student2.name)
+            : student2.name.localeCompare(student1.name)
+        ),
+      );
+
+    case SortType.Surname:
+      return sortedStudents.sort(
+        (
+          student1: Student,
+          student2: Student,
+        ) => (
+          order === 'asc'
+            ? student1.surname.localeCompare(student2.surname)
+            : student2.surname.localeCompare(student1.surname)
+        ),
+      );
+
+    case SortType.Age:
+      return sortedStudents.sort(
+        (
+          student1: Student,
+          student2: Student,
+        ) => (
+          order === 'asc'
+            ? student1.age - student2.age
+            : student2.age - student1.age
+        ),
+      );
+
+    case SortType.Married:
+      return sortedStudents.sort(
+        (
+          student1: Student,
+          student2: Student,
+        ) => (
+          order === 'asc'
+            ? (+student1.married - (+student2.married))
+            : (+student2.married - (+student1.married))
+        ),
+      );
+
+    case SortType.AverageGrade:
+      return sortedStudents.sort(
+        (student1: Student, student2: Student) => {
+          const average1 = (student1.grades.reduce(
+            (sum: number, element: number) => (
+              sum + element
+            ), 0,
+          )) / student1.grades.length;
+
+          const average2 = (student2.grades.reduce(
+            (sum: number, element: number) => (
+              sum + element
+            ), 0,
+          )) / student2.grades.length;
+
+          if (order === 'asc') {
+            return average1 - average2;
+          }
+
+          return average2 - average1;
+        },
+      );
+
+    default:
+      break;
+  }
+
+  return sortedStudents;
 }
