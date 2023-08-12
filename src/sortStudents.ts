739c12bce6ceeaa06f834ac5f1cce8718b2e0b61
Konrad <konrad.domalewski@gmail.com>
2023-08-12 14:54:18 +0200

    init commit

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,71 @@
-
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: Array<number>;
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name,
+  Surname,
+  Age,
+  Married,
+  AverageGrade,
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+function getSortableValue(
+  student: Student,
+  sortBy: SortType,
+): string | number | boolean {
+  const gradesLength = student.grades.length;
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  if (gradesLength === 0 || gradesLength === null) {
+    throw new Error('Dividing by zero');
+  }
+
+  const averageGrade
+    = student.grades.reduce((total, grade) => total + grade, 0) / gradesLength;
+
+  switch (sortBy) {
+    case SortType.Name:
+      return student.name;
+    case SortType.Surname:
+      return student.surname;
+    case SortType.Age:
+      return student.age;
+    case SortType.Married:
+      return student.married;
+    case SortType.AverageGrade:
+      return averageGrade;
+    default:
+      return '';
+  }
+}
+
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const sortedStudents = [...students];
+
+  sortedStudents.sort((a, b) => {
+    const aValue = getSortableValue(a, sortBy);
+    const bValue = getSortableValue(b, sortBy);
+
+    if (aValue < bValue) {
+      return order === 'asc' ? -1 : 1;
+    }
+
+    if (aValue > bValue) {
+      return order === 'asc' ? 1 : -1;
+    }
+
+    return 0;
+  });
+
+  return sortedStudents;
 }
