5ec81fd3e09494f72731fd17a672eb9170cb3bf9
Horizon-git <apbreloaded947@gmail.com>
2023-08-13 22:17:34 +0300

    completed task

--- src/sortStudent.test.ts
+++ src/sortStudent.test.ts
@@ -1,6 +1,6 @@
 /* eslint-disable max-len */
 
-import { sortStudents, SortType } from './sortStudents';
+import sortStudents, { SortType } from './sortStudents';
 
 const students = [
   {
--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,56 @@
-
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[],
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
 // create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+function getAvarageGrade(grades: number[]): number {
+  return grades.reduce((acc, grade) => acc + grade) / grades.length;
+}
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+export default function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  return [...students].sort((student1: Student, student2: Student): number => {
+    let firstToCompare: Student = student1;
+    let secondToCompare: Student = student2;
+
+    if (order === 'desc') {
+      [firstToCompare, secondToCompare] = [secondToCompare, firstToCompare];
+    }
+
+    switch (sortBy) {
+      case SortType.Name:
+      case SortType.Surname:
+        return firstToCompare[sortBy].localeCompare(secondToCompare[sortBy]);
+
+      case SortType.Age:
+      case SortType.Married:
+        return +firstToCompare[sortBy] - +secondToCompare[sortBy];
+
+      case SortType.AverageGrade:
+        return (
+          getAvarageGrade(firstToCompare.grades)
+          - getAvarageGrade(secondToCompare.grades)
+        );
+
+      default:
+        return 0;
+    }
+  });
 }
