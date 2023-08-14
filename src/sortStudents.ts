52b4c832ee5e5ca3b130fb4b1a46a25d4ba49669
Marzena Sówka <marzena.sowka.pl@gmail.com>
2023-08-14 16:21:10 +0200

    add sorting methods

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,79 @@
 
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[],
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name,
+  Surname,
+  Age,
+  Married,
+  AverageGrade,
 }
 
 // create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+function getAvarageGrade(grades: number[]): number {
+  const sum = grades.reduce((a, b) => a + b, 0);
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  return sum / grades.length;
+}
+
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const studentsArr = [...students];
+  const isAsc: boolean = order === 'asc';
+
+  switch (sortBy) {
+    case SortType.Name:
+      studentsArr.sort((a, b) => {
+        return isAsc
+          ? a.name.localeCompare(b.name)
+          : b.name.localeCompare(a.name);
+      });
+      break;
+
+    case SortType.Surname:
+      studentsArr.sort((a, b) => {
+        return isAsc
+          ? a.surname.localeCompare(b.surname)
+          : b.surname.localeCompare(a.surname);
+      });
+      break;
+
+    case SortType.Age:
+      studentsArr.sort((a, b) => {
+        return isAsc ? a.age - b.age : b.age - a.age;
+      });
+      break;
+
+    case SortType.Married:
+      studentsArr.sort((a, b) => {
+        return isAsc
+          ? Number(a.married) - Number(b.married)
+          : Number(b.married) - Number(a.married);
+      });
+      break;
+
+    case SortType.AverageGrade:
+      studentsArr.sort((a, b) => {
+        return isAsc
+          ? getAvarageGrade(a.grades) - getAvarageGrade(b.grades)
+          : getAvarageGrade(b.grades) - getAvarageGrade(a.grades);
+      });
+      break;
+
+    default:
+      break;
+  }
+
+  return studentsArr;
 }
