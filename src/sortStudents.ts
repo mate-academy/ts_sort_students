d129f0c6941683d9d775ca0f7262941d4f9836b4
Hubert Klamann <hubert.klamann1@gmail.com>
2023-08-14 13:23:07 +0200

    task solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,57 @@
 
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: Array<number>,
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'Name',
+  Surname = 'Surname',
+  Age = 'Age',
+  Married = 'Married',
+  AverageGrade = 'AverageGrade',
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(students: Student[],
+  sortBy: SortType, order: SortOrder): Student[] {
+  const asc = order === 'asc';
+  const copyOfStudents = [...students];
+  const averageOfGrades = (grades: Array<number>): number => {
+    return grades.reduce(
+      (acc: number, grade: number) => acc + grade, 0,
+    ) / grades.length;
+  };
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  switch (sortBy) {
+    case SortType.Name:
+      return asc
+        ? copyOfStudents.sort((a, b) => a.name.localeCompare(b.name))
+        : copyOfStudents.sort((a, b) => b.name.localeCompare(a.name));
+    case SortType.Surname:
+      return asc
+        ? copyOfStudents.sort((a, b) => a.surname.localeCompare(b.surname))
+        : copyOfStudents.sort((a, b) => b.surname.localeCompare(a.surname));
+    case SortType.Age:
+      return asc
+        ? copyOfStudents.sort((a, b) => a.age - b.age)
+        : copyOfStudents.sort((a, b) => b.age - a.age);
+    case SortType.Married:
+      return asc
+        ? copyOfStudents.sort((a, b) => Number(a.married) - Number(b.married))
+        : copyOfStudents.sort((a, b) => Number(b.married) - Number(a.married));
+    case SortType.AverageGrade:
+      return asc
+        ? copyOfStudents.sort(
+          (a, b) => averageOfGrades(a.grades) - averageOfGrades(b.grades),
+        )
+        : copyOfStudents.sort(
+          (a, b) => averageOfGrades(b.grades) - averageOfGrades(a.grades),
+        );
+    default: return copyOfStudents;
+  }
 }
