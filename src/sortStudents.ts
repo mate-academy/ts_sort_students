d8e1f60e98fd07807decaa06c24b8ff4710daacb
Szymon Pachucki <szymonpachucki010@gmail.com>
2023-08-14 10:59:26 +0200

    i get this weird error but code looks good to me, message in the error file  @mate-academy/students-ts-config not found

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,55 @@
-
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
+
+const averageGrades = (arrOfGrades: number[]): number => {
+  if (arrOfGrades.length === 0) {
+    return 0;
+  }
+
+  return arrOfGrades.reduce((sum: number, grade: number) => sum + grade, 0) / arrOfGrades.length;
+};
 
 export enum SortType {
-  // describe SortType enum
+  Name,
+  Surname,
+  Age,
+  Married,
+  AverageGrade,
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(students: Student[], sortBy: SortType, order: SortOrder): Student[] {
+  const copyOfStudent = [...students];
+  const orderType = order === 'asc';
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  switch (sortBy) {
+    case SortType.Name:
+      return copyOfStudent.sort((a, b) => orderType
+      ? a.name.localeCompare(b.name)
+      : b.name.localeCompare(a.name));
+    case SortType.Surname:
+      return copyOfStudent.sort((a, b) => orderType
+      ? a.surname.localeCompare(b.surname)
+      : b.surname.localeCompare(a.surname));
+    case SortType.Age:
+      return copyOfStudent.sort((a, b) => orderType
+      ? a.age - b.age
+      : b.age - a.age);
+    case SortType.AverageGrade:
+      return copyOfStudent.sort((a, b) => orderType
+      ? averageGrades(a.grades) - averageGrades(b.grades)
+      : averageGrades(b.grades) - averageGrades(a.grades));
+    case SortType.Married:
+      return copyOfStudent.sort((a, b) => orderType
+      ? Number(a.married) - Number(b.married)
+      : Number(b.married) - Number(a.married));
+    default:
+      return students;
+  }
 }
