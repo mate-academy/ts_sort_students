f33ff7843a45bf22194ee369758351a47e2f2f77
Sviatoslav Vasyletskyi <ditchqiwi@gmail.com>
2023-08-16 08:19:54 +0200

    solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,48 @@
-
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name,
+  Surname,
+  Age,
+  Married,
+  AverageGrade,
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(students: Student[], sortBy: SortType,
+  order: SortOrder): Student[] {
+  const sortedStudents = students.slice();
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  sortedStudents.sort((a, b) => {
+    let comparison = 0;
+
+    if (sortBy === SortType.Name) {
+      comparison = a.name.localeCompare(b.name);
+    } else if (sortBy === SortType.Surname) {
+      comparison = a.surname.localeCompare(b.surname);
+    } else if (sortBy === SortType.Age) {
+      comparison = a.age - b.age;
+    } else if (sortBy === SortType.Married) {
+      // eslint-disable-next-line no-nested-ternary
+      comparison = a.married === b.married ? 0 : a.married ? 1 : -1;
+    } else if (sortBy === SortType.AverageGrade) {
+      const avgA = a.grades.reduce((sum, grade) => sum + grade, 0)
+      / a.grades.length;
+      const avgB = b.grades.reduce((sum, grade) => sum + grade, 0)
+      / b.grades.length;
+
+      comparison = avgA - avgB;
+    }
+
+    return order === 'asc' ? comparison : -comparison;
+  });
+
+  return sortedStudents;
 }
