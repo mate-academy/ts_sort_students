f9c6c04600c96ab42b6025861325981c685dcd12
arturwisnia <wisnia3008@interia.pl>
2023-08-16 18:52:27 +0200

    add task solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,59 @@
-
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name,
+  Surname,
+  Age,
+  Married,
+  AverageGrade,
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(students: Student[],
+  sortBy: SortType, order: SortOrder): Student[] {
+  return students.slice().sort((a, b) => {
+    let aValue,
+      bValue;
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+    if (sortBy === SortType.Name) {
+      aValue = a.name;
+      bValue = b.name;
+    } else if (sortBy === SortType.Surname) {
+      aValue = a.surname;
+      bValue = b.surname;
+    } else if (sortBy === SortType.Age) {
+      aValue = a.age;
+      bValue = b.age;
+    } else if (sortBy === SortType.Married) {
+      aValue = a.married ? 1 : 0;
+      bValue = b.married ? 1 : 0;
+    } else if (sortBy === SortType.AverageGrade) {
+      aValue = a.grades.reduce((sum, grade) => sum + grade, 0)
+       / a.grades.length;
+
+      bValue = b.grades.reduce((sum, grade) => sum + grade, 0)
+       / b.grades.length;
+    }
+
+    if (order === 'asc') {
+      if (aValue === bValue || aValue === undefined || bValue === undefined) {
+        return 0;
+      }
+
+      return aValue < bValue ? -1 : 1;
+    }
+
+    if (aValue === bValue || aValue === undefined || bValue === undefined) {
+      return 0;
+    }
+
+    return aValue < bValue ? 1 : -1;
+  });
 }
