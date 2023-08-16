d86fc925b88c62490666bea1d09cd432f6c6d5f4
MateuszSlezal <M4teks93@gmail.com>
2023-08-16 16:39:56 +0200

    add task solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,68 @@
 
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name,
+  Surname,
+  Age,
+  Married,
+  AverageGrade,
+
 }
 
 // create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+function calcGrade(grades: number[]): number {
+  if (grades.length === 0) {
+    return 0;
+  }
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  const sum = grades.reduce((total, grade) => total + grade, 0);
+
+  return sum / grades.length;
+}
+
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const studCopy = [...students];
+
+  switch (sortBy) {
+    case SortType.Name:
+      return order === 'asc'
+        ? studCopy.sort((a, b) => a.name.localeCompare(b.name))
+        : studCopy.sort((a, b) => b.name.localeCompare(a.name));
+
+    case SortType.Surname:
+      return order === 'asc'
+        ? studCopy.sort((a, b) => a.surname.localeCompare(b.surname))
+        : studCopy.sort((a, b) => b.surname.localeCompare(a.surname));
+
+    case SortType.Age:
+      return order === 'asc'
+        ? studCopy.sort((a, b) => a.age - b.age)
+        : studCopy.sort((a, b) => b.age - a.age);
+
+    case SortType.Married:
+      return order === 'asc'
+        ? studCopy.sort((a, b) => (a.married ? -1 : 1) - (b.married ? -1 : 1))
+        : studCopy.sort((a, b) => (a.married ? -1 : 1) - (b.married ? -1 : 1));
+
+    case SortType.AverageGrade:
+      return order === 'asc'
+        ? studCopy.sort((a, b) => calcGrade(a.grades) - calcGrade(b.grades))
+        : studCopy.sort((a, b) => calcGrade(b.grades) - calcGrade(a.grades));
+
+    default:
+      throw new Error('Nieprawidłowy typ sortowania');
+  }
 }
