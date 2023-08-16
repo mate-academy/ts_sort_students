c86d9a6a4e060fe09c435a0f16bc6593e89f11d7
Emil Owczarek <owczarekemil@gmail.com>
2023-08-16 14:56:42 +0200

    Solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,69 @@
 
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name,
+  Surname,
+  Age,
+  Married,
+  AverageGrade,
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+function getAverageGrade(grades: number[]): number {
+  if (grades.length === 0) {
+    return 0;
+  }
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  const sum = grades.reduce((acc, grade) => acc + grade, 0);
+
+  return sum / grades.length;
+}
+
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const studentsCopy = [...students];
+
+  const compareStudents = (a: Student, b: Student): number => {
+    switch (sortBy) {
+      case SortType.Name:
+        return a.name.localeCompare(b.name);
+      case SortType.Surname:
+        return a.surname.localeCompare(b.surname);
+      case SortType.Age:
+        return a.age - b.age;
+
+      case SortType.Married:
+        if (a.married === b.married) {
+          return 0;
+        }
+
+        return a.married ? 1 : -1;
+
+      case SortType.AverageGrade: {
+        const averageGradeA = getAverageGrade(a.grades);
+        const averageGradeB = getAverageGrade(b.grades);
+
+        return averageGradeA - averageGradeB;
+      }
+      default:
+        return 0;
+    }
+  };
+
+  const direction = order === 'desc' ? -1 : 1;
+
+  studentsCopy.sort((a, b) => direction * compareStudents(a, b));
+
+  return studentsCopy;
 }
