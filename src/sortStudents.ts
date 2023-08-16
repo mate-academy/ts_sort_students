7bf14cd44e7691b5c70de4e8cac65669ecbd112f
Hubert Wiktor <hubertwiktor6@gmail.com>
2023-08-16 12:41:48 +0200

    fix

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -15,60 +15,50 @@ export enum SortType {
   AverageGrade = 'averageGrade'
 }
 
-// create SortOrder type
 export type SortOrder = 'asc' | 'desc';
 
 function avgGrade(student: Student): number {
   return student.grades.reduce(
-    (prev: number, item: number) => prev + item, 0,
+    (prev, item) => prev + item, 0,
   ) / student.grades.length;
+}
+
+function getValue(student: Student, sortBy: SortType): string | number {
+  switch (sortBy) {
+    case 'name':
+    case 'surname':
+      return student[sortBy];
+    case 'age':
+    case 'married':
+      return Number(student[sortBy]);
+    case 'averageGrade':
+      return avgGrade(student);
+    default:
+      throw new Error('Provide sort type.');
+  }
 }
 
 export function sortStudents(
   students: Student[],
   sortBy: SortType,
   order: SortOrder,
-): Student[] | undefined {
+): Student[] {
   const studentsCopy = [...students];
   const orderDirection = order === 'asc';
 
-  switch (sortBy) {
-    case 'name':
-    case 'surname':
+  return studentsCopy.sort((a, b) => {
+    const studentValueA = getValue(a, sortBy);
+    const studentValueB = getValue(b, sortBy);
+
+    if (typeof studentValueA === 'string'
+    && typeof studentValueB === 'string') {
       return orderDirection
-        ? studentsCopy.sort(
-          (a, b) => a[sortBy].localeCompare(b[sortBy]),
-        )
-        : studentsCopy.sort(
-          (a, b) => b[sortBy].localeCompare(a[sortBy]),
-        );
+        ? studentValueA.localeCompare(studentValueB)
+        : studentValueB.localeCompare(studentValueA);
+    }
 
-    case 'age':
-    case 'married':
-      return orderDirection
-        ? studentsCopy.sort(
-          (a, b) => Number(a[sortBy]) - Number(b[sortBy]),
-        )
-        : studentsCopy.sort(
-          (a, b) => Number(b[sortBy]) - Number(a[sortBy]),
-        );
-
-    case 'averageGrade':
-      return orderDirection
-        ? studentsCopy.sort((a, b) => {
-          const aAvg = avgGrade(a);
-          const bAvg = avgGrade(b);
-
-          return aAvg - bAvg;
-        })
-        : studentsCopy.sort((a, b) => {
-          const aAvg = avgGrade(a);
-          const bAvg = avgGrade(b);
-
-          return bAvg - aAvg;
-        });
-
-    default:
-      return undefined;
-  }
+    return orderDirection
+      ? ((studentValueA as number) - (studentValueB as number))
+      : ((studentValueB as number) - (studentValueA as number));
+  });
 }
