cea0e7e75cc6a3345efcc1f8b362ee77382672f4
Andrzej Pec <pimatstudio@gmail.com>
2023-08-16 13:02:17 +0200

    1nitial coomet :3

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,69 @@
+// Sort students
+// Create a `sortStudents` function that takes 3
+// parameters and return a new array
+// with students sorted according to the given
+// params. The original array must not
+// change.
+
+// The parameters are:
+// - `students` an initial array of students
+// - `sortBy` a type of sort (see `SortType` enum)
+// - `order` a sort order that can be `asc` or `desc`
+
+// You also need to add correct types:
+// - Create `Student` interface matching the next data:
+//     ```
+//     {
+//       name: 'Jessica',
+//       surname: 'Buxton',
+//       age: 26,
+//       married: true,
+//       grades: [5, 5, 4, 5, 4, 4, 4, 4, 5, 4, 5, 4],
+//     }
+//     ```
+// - Create a `SortType` enum, having the next values:
+//     ```
+//     Name
+//     Surname
+//     Age
+//     Married
+//     AverageGrade
+//     ```
+//     Hint: `AverageGrade` is sorting by an average value in `grades` array.
+// - Create `SortOrder` type with only 2 values `'asc'` and `'desc'`
+//     Hint: If values are the same the students should go in the original order
+//     (no matter we use `asc` or `desc` order)
+
+// Examples:
+// ```js
+// // from the youngest to the oldest
+// const studentsByAge = sortStudents(students, SortType.Age, 'asc');
+// // Surnames from Z to A
+// const studentsBySurnameDesc
+// = sortStudents(students, SortType.Surname, 'desc');
+// // from the lowest Average grade to the highest
+// const studentsByGrade = sortStudents(students, SortType.AverageGrade, 'asc');
+// ```
 
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[],
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name,
+  Surname,
+  Age,
+  Married,
+  AverageGrade,
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
-
-export function sortStudents(students, sortBy, order) {
-  // write your function
+export function sortStudents(students, sortBy, order): Student[] {
+  return students + sortBy + order;
+  // this is just to make coomet :3
 }
