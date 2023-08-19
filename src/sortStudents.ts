3c1ca348b945aea13f2d83cc92159ceb800925ed
Wave <www.dandelion.ru@gmail.com>
2023-08-19 18:18:27 +0300

    Add solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,59 @@
 
 export interface Student {
-  // describe Student interface
+  name:string,
+  surname: string,
+  age:number,
+  married:boolean,
+  grades:number[],
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name,
+  Surname,
+  Age,
+  Married,
+  AverageGrade,
 }
 
 // create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const sortedStudents: Student[] = [...students];
+  function calculateAverage(student: Student): number {
+    return student.grades.reduce((a, b) => a + b)
+      / student.grades.length;
+  }
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
-}
+  switch (sortBy) {
+    case SortType.Name:
+    case SortType.Surname:
+      return sortedStudents.sort((a, b) => {
+        return order === 'asc'
+          ? a[sortBy].localeCompare(b[sortBy])
+          : b[sortBy].localeCompare(a[sortBy]);
+      });
+
+    case SortType.Age:
+    case SortType.Married:
+      return sortedStudents.sort((a, b) => {
+        return order === 'asc'
+          ? +a[sortBy] - (+b[sortBy])
+          : +b[sortBy] - (+a[sortBy]);
+      });
+
+    case SortType.AverageGrade:
+      return sortedStudents.sort((a, b) => {
+        return order === 'asc'
+          ? calculateAverage(a) - calculateAverage(b)
+          : calculateAverage(b) - calculateAverage(a);
+      });
+
+    default:
+      return sortedStudents;
+  }
+};
