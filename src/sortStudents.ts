d8b6b88a8bd9495d0a3c32c1594a43a4f9f8b2b8
Юрий Мисюра <apriory13@meta.ua>
2023-09-22 17:31:00 +0300

    Solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,47 @@
-
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[],
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(students: Student[], sortBy: SortType, order: SortOrder): Student[] {
+  const copyStudents = [...students];
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  const compareStudents = (a: Student, b: Student): number => {
+    switch (sortBy) {
+      case SortType.Name:
+      case SortType.Surname:
+        return a[sortBy].localeCompare(b[sortBy]);
+
+      case SortType.Age:
+        return a[sortBy] - b[sortBy];
+
+      case SortType.Married:
+        return a[sortBy] && !b[sortBy] ? -1 : 1;
+
+      case SortType.AverageGrade:
+        return getAverageGrades(a[sortBy]) - getAverageGrades(b[sortBy]);
+
+      default:
+        return 0;
+    }
+  }
+
+  return order === 'asc' ? copyStudents.sort((a, b) => compareStudents(a, b)) : copyStudents.sort((a, b) => compareStudents(b, a));
+}
+
+function getAverageGrades(grades: number[]): number {
+  return grades.reduce((acc, grade) => acc + grade, 0) / grades.length;
 }
