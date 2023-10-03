863e1cd9c1cc586b3dd732470f50b58204c60d54
Dmytro Lazarenko <lazarenkodmytro@gmail.com>
2023-10-03 22:50:33 +0200

    add task solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -16,32 +16,52 @@ export enum SortType {
 
 export type SortOrder = 'asc' | 'desc';
 
-export function sortStudents(students: Student[],
-  sortBy: SortType, order: SortOrder): Student[] {
+function calculateAverageGrade(grades: number[]): number {
+  if (grades.length === 0) {
+    return 0;
+  }
+
+  const sum = grades.reduce((acc, grade) => acc + grade, 0);
+
+  return sum / grades.length;
+}
+
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
   const sortedStudents = [...students];
 
   sortedStudents.sort((a, b) => {
+    let avgGradeA: number;
+    let avgGradeB: number;
+
     switch (sortBy) {
       case SortType.Name:
-        return a.name.localeCompare(b.name);
       case SortType.Surname:
-        return a.surname.localeCompare(b.surname);
-      case SortType.Age:
-        return a.age - b.age;
-      case SortType.Married:
-        if (a.married === b.married) {
-          return 0;
-        }
+        return order === 'asc'
+          ? a[sortBy].localeCompare(b[sortBy])
+          : b[sortBy].localeCompare(a[sortBy]);
 
-        return a.married ? 1 : -1;
+      case SortType.Age:
+      case SortType.Married:
+        return order === 'asc'
+          ? Number(a[sortBy]) - Number(b[sortBy])
+          : Number(b[sortBy]) - Number(a[sortBy]);
+
+      case SortType.AverageGrade:
+        avgGradeA = calculateAverageGrade(a.grades);
+        avgGradeB = calculateAverageGrade(b.grades);
+
+        return order === 'asc'
+          ? avgGradeA - avgGradeB
+          : avgGradeB - avgGradeA;
+
       default:
         return 0;
     }
   });
-
-  if (order === 'desc') {
-    sortedStudents.reverse();
-  }
 
   return sortedStudents;
 }
