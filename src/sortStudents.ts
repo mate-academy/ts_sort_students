0368fd0bc1e4fbe96eaf8a6c7da6bcd579e5126a
Andriy Monko <Andriy17monko@gmail.com>
2023-10-16 10:54:30 +0300

    sort students

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,70 @@
-
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
-
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grade',
 }
+export type SortOrder = 'asc' | 'desc';
 
-// create SortOrder type
-export type SortOrder;
+export function sortStudents(
+  students: Student,
+  sortBy: SortType,
+  order: SortOrder,
+):object {
+  return [...students].sort((a:Student, b:Student) => {
+    switch (sortBy) {
+      case SortType.Name:
+        if (order === 'asc') {
+          return a[sortBy].localeCompare(b[sortBy]);
+        }
 
+        return b[sortBy].localeCompare(a[sortBy]);
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+      case SortType.Surname:
+        if (order === 'asc') {
+          return a[sortBy].localeCompare(b[sortBy]);
+        }
+
+        return b[sortBy].localeCompare(a[sortBy]);
+
+      case SortType.Age:
+        if (order === 'asc') {
+          return a[sortBy] - b[sortBy];
+        }
+
+        return b[sortBy] - a[sortBy];
+
+      case SortType.Married:
+        if (order === 'asc') {
+          return +a[sortBy] - +b[sortBy];
+        }
+
+        return +b[sortBy] - +a[sortBy];
+
+      case SortType.AverageGrade:
+        if (order === 'asc') {
+          return a.grades.reduce(
+            (prev, item) => prev + item,
+          ) / a.grades.length
+           - b.grades.reduce((prev, item) => prev + item) / b.grades.length;
+        }
+
+        return b.grades.reduce(
+          (prev, item) => prev + item,
+        ) / b.grades.length - a.grades.reduce(
+          (prev, item) => prev + item,
+        ) / a.grades.length;
+
+      default:
+        return students;
+    }
+  });
 }
