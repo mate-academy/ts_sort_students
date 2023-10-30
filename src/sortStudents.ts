26e9d1389129d0949e4ec330844a9bde1d9ef8bf
Natalia Netzel <140259327+netzelnatalia@users.noreply.github.com>
2023-10-30 14:48:09 +0100

    Update sortStudents.ts

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,41 +1,61 @@
+
+export interface Student {
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: Array<number>,
+}
+
+export enum SortType {
+  Name,
+  Surname,
+  Age,
+  Married,
+  AverageGrade,
+}
+
+export type SortOrder = 'asc' | 'desc';
+
+export function calcAverageGrade(grades: Array<number>): number {
+  const sum = grades.reduce((total, grade) => total + grade, 0);
+
+  return sum / grades.length;
+}
+
 export function sortStudents(
   students: Array<Student>,
   sortBy: SortType,
   order: SortOrder,
-  reverseArray: boolean = false,
 ): Array<Student> {
   const studentsCopy = [...students];
 
-  const compareFunction = (a: Student, b: Student): number => {
-    const direction = reverseArray ? -1 : 1;
+  return studentsCopy.sort((a: Student, b: Student): number => {
+    const direction = order === 'desc' ? -1 : 1;
 
     switch (sortBy) {
       case SortType.Name:
-        return a.name.localeCompare(b.name) * direction;
+        return a.name.localeCompare(b.name);
       case SortType.Surname:
-        return a.surname.localeCompare(b.surname) * direction;
+        return a.surname.localeCompare(b.surname);
       case SortType.Age:
-        return (a.age - b.age) * direction;
+        return direction * (a.age - b.age);
       case SortType.Married:
         if (a.married === b.married) {
           return 0;
         }
-        return a.married ? 1 * direction : -1 * direction;
+
+        return a.married ? direction * 1 : direction * -1;
+
       case SortType.AverageGrade: {
         const averageGradeA = calcAverageGrade(a.grades);
         const averageGradeB = calcAverageGrade(b.grades);
-        return (averageGradeA - averageGradeB) * direction;
+
+        return direction * (averageGradeA - averageGradeB);
       }
+
       default:
         return 0;
     }
-  };
-
-  const sortedStudents = studentsCopy.sort(compareFunction);
-
-  if (reverseArray) {
-    sortedStudents.reverse();
-  }
-
-  return sortedStudents;
+  });
 }
