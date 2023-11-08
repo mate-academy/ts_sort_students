973ca217e47ae7f781885c2897727f0f0f46be6e
Vladislav Balaban <vlad.balaban2020@gmail.com>
2023-11-08 11:32:52 +0200

    Added sort students

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,69 @@
-
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'grades',
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const sortedStudents: Student[] = [...students];
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  function getAverage(student: Student): number {
+    return (
+      student.grades.reduce(
+        (sum, grade) => sum + grade, 0,
+      ) / student.grades.length
+    );
+  }
+
+  sortedStudents.sort((a: Student, b: Student): number => {
+    const sortOrder: boolean = order === 'asc';
+
+    switch (sortBy) {
+      case SortType.Name:
+        return sortOrder
+          ? a.name.localeCompare(b.name)
+          : b.name.localeCompare(a.name);
+
+      case SortType.Surname:
+        return sortOrder
+          ? a.surname.localeCompare(b.surname)
+          : b.surname.localeCompare(a.surname);
+
+      case SortType.Age:
+        return sortOrder
+          ? a.age - b.age
+          : b.age - a.age;
+
+      case SortType.Married:
+        return sortOrder
+          ? (a.married ? -1 : 1) - (b.married ? -1 : 1)
+          : (a.married ? -1 : 1) - (b.married ? -1 : 1);
+
+      case SortType.AverageGrade:
+        return sortOrder
+          ? getAverage(a) - getAverage(b)
+          : getAverage(b) - getAverage(a);
+
+      default:
+        return 0;
+    }
+  });
+
+  return sortedStudents;
 }
