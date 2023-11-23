5df246bae9027e8d18e07fc90a8922923135ad00
Radoslaw Bielski <bielskiradek@gmail.com>
2023-11-23 22:27:01 +0100

    Solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,74 @@
 
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name,
+  Surname,
+  Age,
+  Married,
+  AverageGrade,
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+function calculateAverage(grades: number[]): number {
+  if (grades.length === 0) {
+    return 0;
+  }
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  const sum = grades.reduce((acc, grade) => acc + grade, 0);
+
+  return sum / grades.length;
+}
+
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const sortedStudents = [...students];
+  let avgGradeA: number,
+    avgGradeB: number;
+
+  sortedStudents.sort((a, b) => {
+    let comparison = 0;
+
+    switch (sortBy) {
+      case SortType.Name:
+        comparison = a.name.localeCompare(b.name);
+        break;
+      case SortType.Surname:
+        comparison = a.surname.localeCompare(b.surname);
+        break;
+      case SortType.Age:
+        comparison = a.age - b.age;
+        break;
+      case SortType.Married:
+        if (a.married === b.married) {
+          comparison = 0;
+        } else if (a.married) {
+          comparison = 1;
+        } else {
+          comparison = -1;
+        }
+        break;
+      case SortType.AverageGrade:
+        avgGradeA = calculateAverage(a.grades);
+        avgGradeB = calculateAverage(b.grades);
+        comparison = avgGradeA - avgGradeB;
+        break;
+      default:
+        break;
+    }
+
+    return order === 'asc' ? comparison : -comparison;
+  });
+
+  return sortedStudents;
 }
