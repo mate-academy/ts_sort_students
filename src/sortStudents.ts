823f9bbcac767358363a0d9bb7af497b17b843cc
Kamil <anu88kl@gmail.com>
2023-11-23 16:37:32 +0100

    Solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,67 @@
 
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name,
+  Surname,
+  Age,
+  Married,
+  AverageGrade,
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(students: Student[], sortBy: SortType,
+  order: SortOrder): Student[] {
+  const sorted = [...students];
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  const compare = (a: Student, b: Student): number => {
+    let aVar: string | number | boolean;
+    let bVar: string | number | boolean;
+
+    switch (sortBy) {
+      case SortType.Name:
+        aVar = a.name;
+        bVar = b.name;
+        break;
+      case SortType.Surname:
+        aVar = a.surname;
+        bVar = b.surname;
+        break;
+      case SortType.Age:
+        aVar = a.age;
+        bVar = b.age;
+        break;
+      case SortType.Married:
+        aVar = a.married;
+        bVar = b.married;
+        break;
+      case SortType.AverageGrade:
+        aVar = a.grades
+          .reduce((acc, grade) => acc + grade, 0) / a.grades.length;
+
+        bVar = b.grades
+          .reduce((acc, grade) => acc + grade, 0) / b.grades.length;
+        break;
+      default: throw new Error('Wrong sort type');
+    }
+
+    if (aVar < bVar) {
+      return order === 'asc' ? -1 : 1;
+    }
+
+    if (aVar > bVar) {
+      return order === 'asc' ? 1 : -1;
+    }
+
+    return 0;
+  };
+
+  return sorted.sort(compare);
 }
