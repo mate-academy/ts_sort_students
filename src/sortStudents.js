84ae186d0f17ce50e387f7bada0bc311c02e6d61
jacekkosiba <jac.kosiba@gmail.com>
2023-11-23 22:14:08 +0100

    solution

---
+++ src/sortStudents.js
@@ -0,0 +1,66 @@
+"use strict";
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.sortStudents = exports.SortType = void 0;
+var SortType;
+(function (SortType) {
+    SortType[SortType["Name"] = 0] = "Name";
+    SortType[SortType["Surname"] = 1] = "Surname";
+    SortType[SortType["Age"] = 2] = "Age";
+    SortType[SortType["Married"] = 3] = "Married";
+    SortType[SortType["AverageGrade"] = 4] = "AverageGrade";
+})(SortType || (exports.SortType = SortType = {}));
+function sortStudents(students, sortBy, order) {
+    var compareFunction = function (a, b) {
+        var result = 0;
+        var avgGradeA = 0;
+        var avgGradeB = 0;
+        switch (sortBy) {
+            case SortType.Name:
+                result = a.name.localeCompare(b.name);
+                break;
+            case SortType.Surname:
+                result = a.surname.localeCompare(b.surname);
+                break;
+            case SortType.Age:
+                result = a.age - b.age;
+                break;
+            case SortType.Married:
+                if (a.married === b.married) {
+                    result = 0;
+                }
+                else if (a.married) {
+                    result = 1;
+                }
+                else {
+                    result = -1;
+                }
+                break;
+            case SortType.AverageGrade:
+                avgGradeA = a.grades.reduce(function (acc, curr) {
+                    return acc + curr;
+                }) / a.grades.length;
+                avgGradeB = b.grades.reduce(function (acc, curr) {
+                    return acc + curr;
+                }) / b.grades.length;
+                result = avgGradeA - avgGradeB;
+                break;
+            default:
+                break;
+        }
+        if (order === 'desc') {
+            result *= -1;
+        }
+        return result === 0 ? students.indexOf(a) - students.indexOf(b) : result;
+    };
+    return __spreadArray([], students, true).sort(compareFunction);
+}
+exports.sortStudents = sortStudents;
--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,73 @@
 
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name,
+  Surname,
+  Age,
+  Married,
+  AverageGrade,
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const compareFunction = (a: Student, b: Student): number => {
+    let result = 0;
+    let avgGradeA = 0;
+    let avgGradeB = 0;
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+    switch (sortBy) {
+      case SortType.Name:
+        result = a.name.localeCompare(b.name);
+        break;
+      case SortType.Surname:
+        result = a.surname.localeCompare(b.surname);
+        break;
+      case SortType.Age:
+        result = a.age - b.age;
+        break;
+      case SortType.Married:
+        if (a.married === b.married) {
+          result = 0;
+        } else if (a.married) {
+          result = 1;
+        } else {
+          result = -1;
+        }
+
+        break;
+      case SortType.AverageGrade:
+        avgGradeA = a.grades.reduce((acc, curr) => {
+          return acc + curr;
+        }) / a.grades.length;
+
+        avgGradeB = b.grades.reduce((acc, curr) => {
+          return acc + curr;
+        }) / b.grades.length;
+
+        result = avgGradeA - avgGradeB;
+        break;
+      default:
+        break;
+    }
+
+    if (order === 'desc') {
+      result *= -1;
+    }
+
+    return result === 0 ? students.indexOf(a) - students.indexOf(b) : result;
+  };
+
+  return [...students].sort(compareFunction);
 }
