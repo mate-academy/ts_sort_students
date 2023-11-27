1e73c205fb43588b60ea32e172ffd9dce1b4c6f6
aurelia <aurelia.gorzna.dev@gmail.com>
2023-11-27 08:41:03 +0100

    solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,67 @@
-
 export interface Student {
-  // describe Student interface
+  name: string
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[],
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name,
+  Surname,
+  Age,
+  Married,
+  AverageGrade
 }
 
 // create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+function calculateAverage(grades: number[]): number {
+  const sum = grades.reduce((a, b) => a + b, 0);
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  return sum / grades.length;
+}
+
+export function sortStudents(
+  students: Student[], sortBy: SortType, order: SortOrder,
+): Student[] {
+  const sortedStudents = [...students];
+
+  sortedStudents.sort((a, b) => {
+    switch (sortBy) {
+      case SortType.Name:
+        return order === 'asc'
+          ? a.name.localeCompare(b.name)
+          : b.name.localeCompare(a.name);
+      case SortType.Surname:
+        return order === 'asc'
+          ? a.surname.localeCompare(b.surname)
+          : b.surname.localeCompare(a.surname);
+      case SortType.Age:
+        return order === 'asc' ? a.age - b.age : b.age - a.age;
+
+      case SortType.Married:
+        if (order === 'asc' && a.married !== b.married) {
+          return a.married ? 1 : -1;
+        }
+
+        if (order === 'desc' && a.married !== b.married) {
+          return a.married ? -1 : 1;
+        }
+
+        return 0;
+      case SortType.AverageGrade:
+        // eslint-disable-next-line no-case-declarations
+        const averageA = calculateAverage(a.grades);
+        // eslint-disable-next-line no-case-declarations
+        const averageB = calculateAverage(b.grades);
+
+        return order === 'asc' ? averageA - averageB : averageB - averageA;
+      default:
+        return 0;
+    }
+  });
+
+  return sortedStudents;
 }
