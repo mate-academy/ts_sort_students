a710db3c0a887e0082a7ffc5cf8dd643572a706d
Богдан <BoGoBooM@MacBook-Pro---Bohdan.local>
2024-03-06 00:49:27 +0200

    task solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,66 @@
+/* eslint-disable max-len */
 
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name,
+  Surname,
+  Age,
+  Married,
+  AverageGrade,
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(
+  students: Student[], sortBy: SortType, order: SortOrder,
+): Student[] {
+  const sortedStudents = [...students];
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  sortedStudents.sort((a, b) => {
+    let comparison: number = 0;
+
+    switch (sortBy) {
+      case SortType.Name:
+        comparison = a.name.localeCompare(b.name);
+        break;
+      case SortType.Surname:
+        comparison = a.surname.localeCompare(b.surname);
+        break;
+      case SortType.Age:
+        comparison = a.age - b.age;
+        break;
+      case SortType.Married:
+        if (a.married === b.married) {
+          comparison = 0;
+        } else {
+          comparison = a.married ? 1 : -1;
+        }
+        break;
+      case SortType.AverageGrade:
+        // eslint-disable-next-line no-case-declarations
+        const avgGradeA = a.grades.reduce((total, grade) => total + grade, 0) / a.grades.length;
+        // eslint-disable-next-line no-case-declarations
+        const avgGradeB = b.grades.reduce((total, grade) => total + grade, 0) / b.grades.length;
+
+        comparison = avgGradeA - avgGradeB;
+        break;
+      default:
+        break;
+    }
+
+    if (comparison === 0) {
+      return 0;
+    }
+
+    return (order === 'asc') ? comparison : -comparison;
+  });
+
+  return sortedStudents;
 }
