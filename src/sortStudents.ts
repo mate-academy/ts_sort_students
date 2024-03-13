4c08c0ce4392c7a2c142ac77098649ae2e14fb2b
Bartlomiej Karbownik <bartlomiej.karbownik56@gmail.com>
2024-03-13 11:21:56 +0100

    task completed

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,80 @@
 
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: Array<number>,
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'Name',
+  Surname = 'Surname',
+  Age = 'Age',
+  Married = 'Married',
+  AverageGrade = 'AverageGrade',
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(students: Student[],
+  sortBy: SortType, order: SortOrder): Student[] {
+  const sortedStudents = [...students];
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  sortedStudents.sort((a, b) => {
+    let aValue,
+      bValue;
+
+    switch (sortBy) {
+      case SortType.Name:
+        aValue = a.name;
+        bValue = b.name;
+        break;
+      case SortType.Surname:
+        aValue = a.surname;
+        bValue = b.surname;
+        break;
+      case SortType.Age:
+        aValue = a.age;
+        bValue = b.age;
+        break;
+      case SortType.Married:
+        aValue = a.married;
+        bValue = b.married;
+        break;
+      case SortType.AverageGrade:
+
+        aValue = a.grades.reduce((acc, curr) => acc
+        + curr, 0) / a.grades.length;
+
+        bValue = b.grades.reduce((acc, curr) => acc
+        + curr, 0) / b.grades.length;
+        break;
+      default:
+
+        aValue = a.name;
+        bValue = b.name;
+        break;
+    }
+
+    if (aValue < bValue) {
+      if (order === 'asc') {
+        return -1;
+      }
+
+      return 1;
+    }
+
+    if (aValue > bValue) {
+      if (order === 'asc') {
+        return 1;
+      }
+
+      return -1;
+    }
+
+    return 0;
+  });
+
+  return sortedStudents;
 }
