ac712f0da12ee5f70f6873fdb3a631554087568a
kubsen <jzagrobelny24@gmail.com>
2024-03-13 14:47:24 +0100

    Solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,85 @@
 
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[]
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name,
+  Surname,
+  Age,
+  Married,
+  AverageGrade
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+function calculateGradeAverage(grades: number[]): number {
+  return (grades.reduce((acc, curr) => acc + curr, 0)) / grades.length;
+}
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+export function sortStudents(students: Student[], sortBy: SortType,
+  order: SortOrder): Student[] {
+  const sortedStudents = [...students];
+
+  sortedStudents.sort((a, b) => {
+    let valueA;
+    let valueB;
+
+    switch (sortBy) {
+      case SortType.Name:
+        valueA = a.name.toLowerCase();
+        valueB = b.name.toLowerCase();
+        break;
+      case SortType.Surname:
+        valueA = a.surname.toLowerCase();
+        valueB = b.surname.toLowerCase();
+        break;
+      case SortType.Age:
+        valueA = a.age;
+        valueB = b.age;
+        break;
+      case SortType.Married:
+        valueA = a.married ? 1 : 0;
+        valueB = b.married ? 1 : 0;
+        break;
+      case SortType.AverageGrade:
+        valueA = calculateGradeAverage(a.grades);
+        valueB = calculateGradeAverage(b.grades);
+        break;
+      default:
+        throw new Error('Invalid sort type!');
+    }
+
+    if (order === 'asc') {
+      if (valueA < valueB) {
+        return -1;
+      }
+
+      if (valueA > valueB) {
+        return 1;
+      }
+
+      return 0;
+    }
+
+    if (order === 'desc') {
+      if (valueA > valueB) {
+        return -1;
+      }
+
+      if (valueA < valueB) {
+        return 1;
+      }
+
+      return 0;
+    }
+
+    return 0;
+  });
+
+  return sortedStudents;
 }
