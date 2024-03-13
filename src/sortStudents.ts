fc1bfe4432c0f976d8a1e14b46da28b16ee2bd7a
Konrad Wasilewski <konradwasilewski444@gmail.com>
2024-03-13 19:56:24 +0100

    first version

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,84 @@
-
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name,
+  Surname,
+  Age,
+  Married,
+  AverageGrade,
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] {
+  const sortedStudents = [...students];
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  sortedStudents.sort((a, b) => {
+    if (sortBy === SortType.Name) {
+      if (order === 'asc') {
+        return a.name.localeCompare(b.name);
+      }
+
+      return b.name.localeCompare(a.name);
+    }
+
+    if (sortBy === SortType.Surname) {
+      if (order === 'asc') {
+        return a.surname.localeCompare(b.surname);
+      }
+
+      return b.surname.localeCompare(a.surname);
+    }
+
+    if (sortBy === SortType.Age) {
+      if (order === 'asc') {
+        return a.age - b.age;
+      }
+
+      return b.age - a.age;
+    }
+
+    if (sortBy === SortType.Married) {
+      if (a.married && !b.married) {
+        return order === 'asc' ? 1 : -1;
+      }
+
+      if (!a.married && b.married) {
+        return order === 'asc' ? -1 : 1;
+      }
+
+      return 0;
+    }
+
+    if (sortBy === SortType.AverageGrade) {
+      const avgA = a.grades.reduce((sum, grade) => sum + grade, 0)
+      / a.grades.length;
+      const avgB = b.grades.reduce((sum, grade) => sum + grade, 0)
+      / b.grades.length;
+
+      if (order === 'asc') {
+        return avgA - avgB;
+      }
+
+      return avgB - avgA;
+    }
+
+    if (order === 'asc') {
+      return a.name.localeCompare(b.name);
+    }
+
+    return b.name.localeCompare(a.name);
+  });
+
+  return sortedStudents;
 }
