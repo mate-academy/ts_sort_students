422845ed43d1362587968f15c171389c15a11802
Kacper Grosicki <kacpergrosicki2002@gmail.com>
2024-03-15 15:54:26 +0100

    task solution sort

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,72 @@
-
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'Name',
+  Surname = 'Surname',
+  Age = 'Age',
+  Married = 'Married',
+  AverageGrade = 'AverageGrade',
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(
+  students: Student[], sortBy: SortType, order: SortOrder,
+): Student[] {
+  const sortedStudents = [...students];
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  sortedStudents.sort((a, b) => {
+    let aValue;
+    let bValue;
+
+    switch (sortBy) {
+      case SortType.Name:
+        aValue = a.name.toLowerCase();
+        bValue = b.name.toLowerCase();
+        break;
+      case SortType.Surname:
+        aValue = a.surname.toLowerCase();
+        bValue = b.surname.toLowerCase();
+        break;
+      case SortType.Age:
+        aValue = a.age;
+        bValue = b.age;
+        break;
+      case SortType.Married:
+        aValue = a.married ? 1 : 0;
+        bValue = b.married ? 1 : 0;
+        break;
+
+      case SortType.AverageGrade: {
+        const avgGradeA = a.grades
+          .reduce((acc, curr) => acc + curr, 0) / a.grades.length;
+        const avgGradeB = b.grades
+          .reduce((acc, curr) => acc + curr, 0) / b.grades.length;
+
+        aValue = avgGradeA;
+        bValue = avgGradeB;
+        break;
+      }
+      default:
+        throw new Error('Invalid SortType');
+    }
+
+    if (aValue === bValue) {
+      return students.indexOf(a) - students.indexOf(b);
+    }
+
+    if (order === 'asc') {
+      return aValue < bValue ? -1 : 1;
+    }
+
+    return aValue > bValue ? -1 : 1;
+  });
+
+  return sortedStudents;
 }
